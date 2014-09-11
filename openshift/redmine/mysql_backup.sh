68e77b362f9fdb111f59ea665fe87fd0b9c69979
OpenShift System User <54117a3a4382ec2a31000e92@ex-std-node248.prod.rhcloud.com>
2014-09-11 07:13:51 -0400

    dummy

--- openshift/redmine/mysql_backup.sh
+++ openshift/redmine/mysql_backup.sh
@@ -5,5 +5,5 @@ mysqldump \
 --port=$OPENSHIFT_MYSQL_DB_PORT \
 --user=$OPENSHIFT_MYSQL_DB_USERNAME \
 --password=$OPENSHIFT_MYSQL_DB_PASSWORD \
--x --all-databases | xz > $OPENSHIFT_DATA_DIR/mysql_dump_`date +%a`.xz
+-x --all-databases --events | xz > $OPENSHIFT_DATA_DIR/mysql_dump_`date +%a`.xz
 
