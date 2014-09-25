c8c1b6594cf176a45aa2801c27fa809a99dec528
OpenShift System User <54240b965004461f4100012d@ex-std-node447.prod.rhcloud.com>
2014-09-25 09:43:48 -0400

    dummy

---
+++ openshift/app01/install_step4.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ctl_all stop
+
+export GEM_HOME=${OPENSHIFT_DATA_DIR}.gem
+export RBENV_ROOT=${OPENSHIFT_DATA_DIR}/.rbenv
+export PATH="${OPENSHIFT_DATA_DIR}/.rbenv/bin:$PATH" 
+export PATH="${OPENSHIFT_DATA_DIR}/.gem/bin:$PATH" 
+eval "$(rbenv init -)" 
+export APXS2=${OPENSHIFT_DATA_DIR}/apache/bin/apxs
+export PATH=${OPENSHIFT_DATA_DIR}/apache/bin:$PATH
+export HTTPD=${OPENSHIFT_DATA_DIR}/apache/bin/httpd
+export BINDIR=${OPENSHIFT_DATA_DIR}/apache
+
+time CFLAGS="-O3 -march=native -pipe" CXXFLAGS="-O3 -march=native -pipe" \
+${OPENSHIFT_DATA_DIR}/.gem/bin/passenger-install-apache2-module --auto
+
+ctl_all restart
+
