91ef3d385bd9eeb07be4a6a34fb6eb04ececb6b7
tshr20140816 <tshr20140816@yahoo.co.jp>
2014-10-09 09:23:09 +0900

    Update install_step10.sh

--- openshift/app01/install_step10.sh
+++ openshift/app01/install_step10.sh
@@ -17,11 +17,12 @@ echo `oo-cgroup-read memory.failcnt | awk '{print "Memory Fail Count : " $1}'` >
 
 # ***** webalizer *****
 
-cd ${OPENSHIFT_TMP_DIR}
+pushd ${OPENSHIFT_TMP_DIR} > /dev/nul
 cp ${OPENSHIFT_DATA_DIR}/download_files/webalizer-${webalizer_version}-src.tgz ./
 
 echo `date +%Y/%m/%d" "%H:%M:%S` webalizer tar >> ${OPENSHIFT_LOG_DIR}/install.log
 tar xfz webalizer-${webalizer_version}-src.tgz
+popd > /dev/nul
 cd webalizer-${webalizer_version}
 mv lang/webalizer_lang.japanese lang/webalizer_lang.japanese_euc
 iconv -f euc-jp -t utf-8 lang/webalizer_lang.japanese_euc > lang/webalizer_lang.japanese
