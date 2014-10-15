d519a2da10b44a753cdd7978b2975301ddf11779
tshr20140816 <tshr20140816@yahoo.co.jp>
2014-10-16 01:25:37 +0900

    Update  install_step12x.sh

--- openshift/app01/ install_step12x.sh
+++ openshift/app01/ install_step12x.sh
@@ -1,1 +1,10 @@
 #!/usr/bash
+
+cd /tmp/
+wget http://prdownloads.sourceforge.net/tcl/tcl8.6.2-src.tar.gz
+tar xfz tcl8.6.2-src.tar.gz
+cd tcl8.6.2
+cd unix
+./configure --prefix=${OPENSHIFT_DATA_DIR}/tcl
+make
+make install
