114efc0c62cb39866213aeeb4213b3c8b9ed28ce
tshr20140816 <tshr20140816@yahoo.co.jp>
2014-10-17 11:47:52 +0900

    Create install_step_from_5_to_14.sh

---
+++ openshift/app01/install_step_from_5_to_14.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -x
+
+bash install_step5.sh
+bash install_step6.sh
+bash install_step7.sh
+bash install_step8.sh
+bash install_step9.sh
+bash install_step10.sh
+bash install_step11.sh
+bash install_step12.sh
+bash install_step13.sh
+bash install_step14.sh
