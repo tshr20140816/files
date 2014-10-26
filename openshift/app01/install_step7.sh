af4954c6ab4dccf12b31343b830bb2105c83d2e4
tshr20140816 <tshr20140816@yahoo.co.jp>
2014-10-27 08:49:09 +0900

    Update and rename install_step6.sh to install_step7.sh

--- openshift/app01/install_step7.sh
+++ openshift/app01/install_step7.sh
@@ -10,7 +10,7 @@ do
 done < ${OPENSHIFT_DATA_DIR}/version_list
 
 export TZ=JST-9
-echo `date +%Y/%m/%d" "%H:%M:%S` Install STEP 6 Start >> ${OPENSHIFT_LOG_DIR}/install.log
+echo `date +%Y/%m/%d" "%H:%M:%S` Install STEP 7 Start >> ${OPENSHIFT_LOG_DIR}/install.log
 echo `quota -s | grep -v a | awk '{print "Disk Usage : " $1,$4 " files"}'` >> ${OPENSHIFT_LOG_DIR}/install.log
 echo `oo-cgroup-read memory.usage_in_bytes | awk '{printf "Memory Usage : %\047d\n", $1}'` >> ${OPENSHIFT_LOG_DIR}/install.log
 echo `oo-cgroup-read memory.failcnt | awk '{printf "Memory Fail Count : %\047d\n", $1}'` >> ${OPENSHIFT_LOG_DIR}/install.log
@@ -165,4 +165,4 @@ rm memcached-${memcached_php_ext_version}.tgz
 rm -rf memcached-${memcached_php_ext_version}
 popd > /dev/null
 
-echo `date +%Y/%m/%d" "%H:%M:%S` Install STEP 6 Finish >> ${OPENSHIFT_LOG_DIR}/install.log
+echo `date +%Y/%m/%d" "%H:%M:%S` Install STEP 7 Finish >> ${OPENSHIFT_LOG_DIR}/install.log
