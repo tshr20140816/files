0cf8e5b8196aa90eca028790da687a6ca7316903
tshr20140816 <tshr20140816@yahoo.co.jp>
2014-10-27 08:43:16 +0900

    Update install_step14.sh

--- openshift/app01/install_step14.sh
+++ openshift/app01/install_step14.sh
@@ -10,7 +10,7 @@ do
 done < ${OPENSHIFT_DATA_DIR}/version_list
 
 export TZ=JST-9
-echo `date +%Y/%m/%d" "%H:%M:%S` Install STEP 14 Start >> ${OPENSHIFT_LOG_DIR}/install.log
+echo `date +%Y/%m/%d" "%H:%M:%S` Install STEP 15 Start >> ${OPENSHIFT_LOG_DIR}/install.log
 echo `quota -s | grep -v a | awk '{print "Disk Usage : " $1,$4 " files"}'` >> ${OPENSHIFT_LOG_DIR}/install.log
 echo `oo-cgroup-read memory.usage_in_bytes | awk '{printf "Memory Usage : %\047d\n", $1}'` >> ${OPENSHIFT_LOG_DIR}/install.log
 echo `oo-cgroup-read memory.failcnt | awk '{printf "Memory Fail Count : %\047d\n", $1}'` >> ${OPENSHIFT_LOG_DIR}/install.log
@@ -416,80 +416,4 @@ echo `quota -s | grep -v a | awk '{print "Disk Usage : " $1,$4 " files"}'` >> ${
 echo `oo-cgroup-read memory.usage_in_bytes | awk '{printf "Memory Usage : %\047d\n", $1}'` >> ${OPENSHIFT_LOG_DIR}/install.log
 echo `oo-cgroup-read memory.failcnt | awk '{printf "Memory Fail Count : %\047d\n", $1}'` >> ${OPENSHIFT_LOG_DIR}/install.log
 
-echo `date +%Y/%m/%d" "%H:%M:%S` Install STEP 14 Finish >> ${OPENSHIFT_LOG_DIR}/install.log
-
-# ***** start *****
-
-# kill `netstat -anpt 2>/dev/null | grep ${OPENSHIFT_DIY_IP} | grep LISTEN | awk '{print $7}' | awk -F/ '{print $1}'`
-kill `ps auwx  2>/dev/null | grep testrubyserver.rb | grep -v grep | awk '{print $2}'`
-export TZ=JST-9
-${OPENSHIFT_DATA_DIR}/apache/bin/apachectl -k graceful
-pushd ${OPENSHIFT_DATA_DIR}/delegate
-./delegated -r +=P30080
-popd > /dev/null
-${OPENSHIFT_DATA_DIR}/memcached/bin/memcached -l ${OPENSHIFT_DIY_IP} -p 31211 -d
-
-pushd ${OPENSHIFT_REPO_DIR}/.openshift/cron/minutely > /dev/null
-
-# * for_restart *
-
-cat << '__HEREDOC__' > for_restart.sh
-#!/bin/bash
-
-testrubyserver_count=`ps aux | grep testrubyserver.rb | grep -v grep | wc -l`
-
-if [ ${testrubyserver_count} -gt 0 ]; then
-
-    # *** kill testrubyserver.rb ***
-    kill `ps auwx 2>/dev/null | grep testrubyserver.rb | grep -v grep | awk '{print $2}'`
-
-    # *** apache ***
-    export TZ=JST-9
-    ${OPENSHIFT_DATA_DIR}/apache/bin/apachectl -k graceful
-
-    # *** delegate ***
-    pushd ${OPENSHIFT_DATA_DIR}/delegate
-    ./delegated -r +=P30080
-    popd > /dev/null
-
-    # *** memcached ***
-    ${OPENSHIFT_DATA_DIR}/memcached/bin/memcached -l ${OPENSHIFT_DIY_IP} -p 31211 -d
-fi
-__HEREDOC__
-
-chmod +x for_restart.sh
-echo for_restart.sh >> jobs.allow
-
-# *** webalizer first process ***
-
-wget --spider https://${OPENSHIFT_APP_DNS}/
-wget --spider https://${OPENSHIFT_APP_DNS}/redmine/
-sleep 5s
-
-${OPENSHIFT_REPO_DIR}/.openshift/cron/hourly/webalizer.sh
-
-find ${OPENSHIFT_DATA_DIR}/.gem/gems/ -name passenger-status -type f | xargs --replace={} ruby {} --verbose
-
-# *** memcached information ***
-
-pushd ${OPENSHIFT_TMP_DIR} > /dev/null
-cp ${OPENSHIFT_DATA_DIR}/download_files/memcached-tool ./
-chmod +x memcached-tool
-./memcached-tool ${OPENSHIFT_DIY_IP}:31211 dump
-./memcached-tool ${OPENSHIFT_DIY_IP}:31211 stats
-./memcached-tool ${OPENSHIFT_DIY_IP}:31211 display
-popd > /dev/null
-
-set +x
-
-echo https://${OPENSHIFT_APP_DNS}/wordpress/wp-admin/install.php
-echo https://${OPENSHIFT_APP_DNS}/ttrss/ admin/password
-echo https://${OPENSHIFT_APP_DNS}/mail/
-echo https://${OPENSHIFT_APP_DNS}/webalizer/
-echo https://${OPENSHIFT_APP_DNS}/mrtg/
-echo https://${OPENSHIFT_APP_DNS}/redmine/ admin/admin
-echo https://${OPENSHIFT_APP_DNS}/cacti/ admin/admin
-echo https://${OPENSHIFT_APP_DNS}/info/
-echo https://${OPENSHIFT_APP_DNS}/logs/
-
-echo Do not git push
+echo `date +%Y/%m/%d" "%H:%M:%S` Install STEP 15 Finish >> ${OPENSHIFT_LOG_DIR}/install.log
