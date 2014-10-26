9c64935d85575f730de03bd204809ed5dc1d3dfa
tshr20140816 <tshr20140816@yahoo.co.jp>
2014-10-27 06:28:15 +0900

    Update install_step14.sh

--- openshift/app01/install_step14.sh
+++ openshift/app01/install_step14.sh
@@ -205,8 +205,10 @@ cat << '__HEREDOC__' > delegate.sh
 #!/bin/bash
 
 export TZ=JST-9
-wget --spider http://${OPENSHIFT_DIY_IP}:30080/mail/+
+wget --http-user=__DELEGATE_EMAL_ADDRESS__ --http-passwd=__DELEGATE_EMAIL_PASSWORD__ http://${OPENSHIFT_DIY_IP}:30080/mail/+__DELEGATE_MAIL_ALIAS__/
 __HEREDOC__
+delegate_mail_alias=`echo ${OPENSHIFT_DATA_DIR}/delegate_mail_alias`
+perl -pi -e 's/__DELEGATE_MAIL_ALIAS__/{delegate_mail_alias}/g' delegate.sh
 chmod +x delegate.sh
 # TODO
 # echo delegate.sh >> jobs.allow
