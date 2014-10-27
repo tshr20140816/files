227c41b3ec298eecb4af4aa86c1182be04753904
tshr20140816 <tshr20140816@yahoo.co.jp>
2014-10-27 23:05:01 +0900

    Update install_step1.sh

--- openshift/app01/install_step1.sh
+++ openshift/app01/install_step1.sh
@@ -398,34 +398,6 @@ mkdir -p ${OPENSHIFT_DATA_DIR}/var/www/cgi-bin
 mkdir ${OPENSHIFT_DATA_DIR}/bin
 mkdir ${OPENSHIFT_DATA_DIR}/scripts
 
-# ***** lynx *****
-
-pushd ${OPENSHIFT_TMP_DIR} > /dev/null
-cp ${OPENSHIFT_DATA_DIR}/download_files/lynx2.8.7.tar.gz ./
-
-echo `date +%Y/%m/%d" "%H:%M:%S` lynx tar >> ${OPENSHIFT_LOG_DIR}/install.log
-tar xfz lynx2.8.7.tar.gz
-popd > /dev/null
-
-pushd ${OPENSHIFT_TMP_DIR}lynx2-8-7 > /dev/null
-
-echo `date +%Y/%m/%d" "%H:%M:%S` lynx configure >> ${OPENSHIFT_LOG_DIR}/install.log
-CFLAGS="-O3 -march=native -pipe" CXXFLAGS="-O3 -march=native -pipe" \
-./configure \
---prefix=${OPENSHIFT_DATA_DIR}/lynx 2>&1 | tee ${OPENSHIFT_LOG_DIR}/lynx.configure.log
-
-echo `date +%Y/%m/%d" "%H:%M:%S` lynx make >> ${OPENSHIFT_LOG_DIR}/install.log
-time make -j4
-
-echo `date +%Y/%m/%d" "%H:%M:%S` lynx make install >> ${OPENSHIFT_LOG_DIR}/install.log
-make install
-popd > /dev/null
-
-pushd ${OPENSHIFT_TMP_DIR} > /dev/null
-rm lynx2.8.7.tar.gz
-rm -rf lynx2-8-7
-popd > /dev/null
-
 cd ${OPENSHIFT_DATA_DIR}/github/openshift/app01
 export TMOUT=0
 
