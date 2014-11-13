d1f3d224032c9dff090263f45bdbc3df388e5f0b
tshr20140816 <tshr20140816@yahoo.co.jp>
2014-11-13 10:20:33 +0900

    Create install_step2.sh

---
+++ openshift/app02/install_step2.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -x
+
+while read LINE
+do
+    product=`echo ${LINE} | awk '{print $1}'`
+    version=`echo ${LINE} | awk '{print $2}'`
+    eval "${product}"=${version}
+done < ${OPENSHIFT_DATA_DIR}/version_list
+
+export TZ=JST-9
+echo `date +%Y/%m/%d" "%H:%M:%S` Install STEP 2 Start >> ${OPENSHIFT_LOG_DIR}/install.log
+echo `quota -s | grep -v a | awk '{print "Disk Usage : " $1,$4 " files"}'` >> ${OPENSHIFT_LOG_DIR}/install.log
+echo `oo-cgroup-read memory.usage_in_bytes | awk '{printf "Memory Usage : %\047d\n", $1}'` >> ${OPENSHIFT_LOG_DIR}/install.log
+echo `oo-cgroup-read memory.failcnt | awk '{printf "Memory Fail Count : %\047d\n", $1}'` >> ${OPENSHIFT_LOG_DIR}/install.log
+
+# ***** lynx *****
+
+mkdir -p ${OPENSHIFT_TMP_DIR}/lynx
+
+pushd ${OPENSHIFT_TMP_DIR}lynx > /dev/null
+cp ${OPENSHIFT_DATA_DIR}/download_files/lynx${lynx_version}.tar.gz ./
+
+echo `date +%Y/%m/%d" "%H:%M:%S` lynx tar >> ${OPENSHIFT_LOG_DIR}/install.log
+tar xfz lynx${lynx_version}.tar.gz --strip-components=1
+popd > /dev/null
+
+pushd ${OPENSHIFT_TMP_DIR}lynx > /dev/null
+
+echo `date +%Y/%m/%d" "%H:%M:%S` lynx configure >> ${OPENSHIFT_LOG_DIR}/install.log
+CFLAGS="-O3 -march=native -pipe" CXXFLAGS="-O3 -march=native -pipe" \
+./configure \
+--mandir=/tmp/man \
+--prefix=${OPENSHIFT_DATA_DIR}/lynx 2>&1 | tee ${OPENSHIFT_LOG_DIR}/lynx.configure.log
+
+echo `date +%Y/%m/%d" "%H:%M:%S` lynx make >> ${OPENSHIFT_LOG_DIR}/install.log
+time make -j4
+
+echo `date +%Y/%m/%d" "%H:%M:%S` lynx make install >> ${OPENSHIFT_LOG_DIR}/install.log
+make install
+popd > /dev/null
+
+pushd ${OPENSHIFT_TMP_DIR} > /dev/null
+rm -rf lynx
+popd > /dev/null
+
+echo `date +%Y/%m/%d" "%H:%M:%S` Install STEP 2 Finish >> ${OPENSHIFT_LOG_DIR}/install.log
