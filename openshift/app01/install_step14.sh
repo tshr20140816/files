28a8dc1fad667eacb0e6dfa58f60009309110fe1
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-01-28 13:27:35 +0900

    Update install_step14.sh

--- openshift/app01/install_step14.sh
+++ openshift/app01/install_step14.sh
@@ -12,6 +12,7 @@ done < ${OPENSHIFT_DATA_DIR}/version_list
 set -x
 
 export TZ=JST-9
+echo `date +%Y/%m/%d" "%H:%M:%S` Install STEP 14 Start
 echo `date +%Y/%m/%d" "%H:%M:%S` Install STEP 14 Start >> ${OPENSHIFT_LOG_DIR}/install.log
 echo `quota -s | grep -v a | awk '{print "Disk Usage : " $1,$4 " files"}'` >> ${OPENSHIFT_LOG_DIR}/install.log
 echo `oo-cgroup-read memory.usage_in_bytes | awk '{printf "Memory Usage : %\047d\n", $1}'` >> ${OPENSHIFT_LOG_DIR}/install.log
