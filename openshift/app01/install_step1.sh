03b3e81d1d877e2745a2b865a7d5a2bae37a522d
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-01-30 09:41:21 +0900

    Update install_step1.sh

--- openshift/app01/install_step1.sh
+++ openshift/app01/install_step1.sh
@@ -65,7 +65,7 @@ done < ${OPENSHIFT_DATA_DIR}/version_list
 
 # ***** args *****
 
-if [ $# -ne 9 ]; then
+if [ $# -ne 10 ]; then
     set +x
     echo "arg1 : redmine email address"
     echo "arg2 : redmine email password"
