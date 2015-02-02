51028b58dca2172477ba5c84d16673d0b04a6ce4
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-02-02 10:23:40 +0900

    Update install_step99.sh

--- openshift/app01/install_step99.sh
+++ openshift/app01/install_step99.sh
@@ -43,6 +43,34 @@ pushd ${OPENSHIFT_TMP_DIR} > /dev/null
 rm -rf fping-${fping_version}
 popd > /dev/null
 
+# ***** pcre *****
+
+pushd ${OPENSHIFT_TMP_DIR} > /dev/null
+cp ${OPENSHIFT_DATA_DIR}/download_files/pcre-${pcre_version}.tar.gz ./
+
+echo `date +%Y/%m/%d" "%H:%M:%S` pcre tar >> ${OPENSHIFT_LOG_DIR}/install.log
+tar xfz pcre-${pcre_version}.tar.gz
+popd > /dev/null
+
+pushd ${OPENSHIFT_TMP_DIR}/pcre-${pcre_version} > /dev/null
+
+echo `date +%Y/%m/%d" "%H:%M:%S` pcre configure >> ${OPENSHIFT_LOG_DIR}/install.log
+CFLAGS="-O3 -march=native -pipe" CXXFLAGS="-O3 -march=native -pipe" \
+./configure \
+--mandir=/tmp/man \
+--prefix=${OPENSHIFT_DATA_DIR}/pcre 2>&1 | tee ${OPENSHIFT_LOG_DIR}/pcre.configure.log
+
+echo `date +%Y/%m/%d" "%H:%M:%S` pcre make >> ${OPENSHIFT_LOG_DIR}/install.log
+time make 2>&1 | tee ${OPENSHIFT_LOG_DIR}/pcre.make.log
+
+echo `date +%Y/%m/%d" "%H:%M:%S` pcre make install >> ${OPENSHIFT_LOG_DIR}/install.log
+make install 2>&1 | tee ${OPENSHIFT_LOG_DIR}/pcre.make.install.log
+popd > /dev/null
+
+pushd ${OPENSHIFT_TMP_DIR} > /dev/null
+rm pcre-${pcre_version}.tar.gz
+popd > /dev/null
+
 # ***** xymon *****
 
 pushd ${OPENSHIFT_TMP_DIR} > /dev/null
