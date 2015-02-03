ec33672cedc6bf615ae305f6712736793f76855a
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-02-03 17:10:19 +0900

    Update install_step2.sh

--- openshift/app01/install_step2.sh
+++ openshift/app01/install_step2.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-wget --spider `cat ${OPENSHIFT_DATA_DIR}/web_beacon_server`dummy?server=${OPENSHIFT_GEAR_DNS}&part1=`basename $0 .sh` >/dev/null 2>&1
+wget --spider `cat ${OPENSHIFT_DATA_DIR}/web_beacon_server`dummy?server=${OPENSHIFT_GEAR_DNS}&part=`basename $0 .sh` >/dev/null 2>&1
 
 while read LINE
 do
