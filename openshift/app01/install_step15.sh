d46e5a560010f444a322c1fc4df37a0aa68f9040
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-02-04 23:12:25 +0900

    Update install_step15.sh

--- openshift/app01/install_step15.sh
+++ openshift/app01/install_step15.sh
@@ -470,9 +470,9 @@ cd ${OPENSHIFT_DATA_DIR}/apache/htdocs/info/
 echo `date +%Y/%m/%d" "%H:%M:%S` > process_status.txt
 ps auwx >> process_status.txt
 echo `date +%Y/%m/%d" "%H:%M:%S` > lsof.txt
-lsof > lsof.txt
+lsof >> lsof.txt
 echo `date +%Y/%m/%d" "%H:%M:%S` > lsof_i.txt
-lsof -i > lsof_i.txt
+lsof -i >> lsof_i.txt
 __HEREDOC__
 chmod +x process_status.sh
 echo process_status.sh >> jobs.allow
