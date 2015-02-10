6d4268c9a63d5e026695c6d5f5d8ba8023e690fc
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-02-10 19:26:29 +0900

    Update install_step11.sh

--- openshift/app01/install_step11.sh
+++ openshift/app01/install_step11.sh
@@ -12,7 +12,15 @@ done < ${OPENSHIFT_DATA_DIR}/version_list
 set -x
 
 export TZ=JST-9
-echo `date +%Y/%m/%d" "%H:%M:%S` Install STEP 11 Start | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+
+pushd ${OPENSHIFT_DATA_DIR}/install_check_point > /dev/null
+if -f [ `basename $0`.ok ]; then
+    echo `date +%Y/%m/%d" "%H:%M:%S` Install Skip `basename $0` | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+    exit
+fi
+popd > /dev/null
+
+echo `date +%Y/%m/%d" "%H:%M:%S` Install Start `basename $0` | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 echo `quota -s | grep -v a | awk '{print "Disk Usage : " $1,$4 " files"}'` | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 echo `oo-cgroup-read memory.usage_in_bytes | awk '{printf "Memory Usage : %\047d\n", $1}'` | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 echo `oo-cgroup-read memory.failcnt | awk '{printf "Memory Fail Count : %\047d\n", $1}'` | tee -a ${OPENSHIFT_LOG_DIR}/install.log
@@ -68,4 +76,6 @@ popd > /dev/null
 
 ln -s ${OPENSHIFT_DATA_DIR}/webalizer/www ${OPENSHIFT_DATA_DIR}/apache/htdocs/webalizer
 
-echo `date +%Y/%m/%d" "%H:%M:%S` Install STEP 11 Finish | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+touch ${OPENSHIFT_DATA_DIR}/install_check_point/`basename $0`.ok
+
+echo `date +%Y/%m/%d" "%H:%M:%S` Install Finish `basename $0` | tee -a ${OPENSHIFT_LOG_DIR}/install.log
