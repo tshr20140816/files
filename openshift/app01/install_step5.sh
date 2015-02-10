1e8e9acd755799c227049a3d4cd9ecc31f76e314
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-02-10 17:11:32 +0900

    Update install_step5.sh

--- openshift/app01/install_step5.sh
+++ openshift/app01/install_step5.sh
@@ -5,7 +5,15 @@ wget --spider `cat ${OPENSHIFT_DATA_DIR}/web_beacon_server`dummy?server=${OPENSH
 set -x
 
 export TZ=JST-9
-echo `date +%Y/%m/%d" "%H:%M:%S` Install STEP 5 Start | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+
+pushd ${OPENSHIFT_DATA_DIR}/install_check_point > /dev/null
+if -f [ `basename $0`.ok ]; then
+    echo `date +%Y/%m/%d" "%H:%M:%S` Install Skip `basename $0` | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+    exit
+fi
+popd > /dev/null
+
+echo `date +%Y/%m/%d" "%H:%M:%S` Install Start `basename $0` | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 echo `quota -s | grep -v a | awk '{print "Disk Usage : " $1,$4 " files"}'` | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 echo `oo-cgroup-read memory.usage_in_bytes | awk '{printf "Memory Usage : %\047d\n", $1}'` | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 echo `oo-cgroup-read memory.failcnt | awk '{printf "Memory Fail Count : %\047d\n", $1}'` | tee -a ${OPENSHIFT_LOG_DIR}/install.log
@@ -26,6 +34,8 @@ export BINDIR=${OPENSHIFT_DATA_DIR}/apache
 time CFLAGS="-O3 -march=native -pipe" CXXFLAGS="-O3 -march=native -pipe" \
 ${OPENSHIFT_DATA_DIR}/.gem/bin/passenger-install-apache2-module --auto
 
+touch ${OPENSHIFT_DATA_DIR}/install_check_point/`basename $0`.ok
+
 /usr/bin/gear start
 
-echo `date +%Y/%m/%d" "%H:%M:%S` Install STEP 5 Finish | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+echo `date +%Y/%m/%d" "%H:%M:%S` Install Finish `basename $0` | tee -a ${OPENSHIFT_LOG_DIR}/install.log
