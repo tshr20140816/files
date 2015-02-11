e8b3e18b61689b9a58271f490e8c0b55407faaed
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-02-11 20:35:23 +0900

    Update install_step7.sh

--- openshift/app01/install_step7.sh
+++ openshift/app01/install_step7.sh
@@ -108,7 +108,7 @@ CFLAGS="-O3 -march=native" CXXFLAGS="-O3 -march=native" \
 
 echo `date +%Y/%m/%d" "%H:%M:%S` php make | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 echo $'\n'`date +%Y/%m/%d" "%H:%M:%S` '***** make *****' $'\n'$'\n'>> ${OPENSHIFT_LOG_DIR}/install_php.log
-time make 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_php.log
+time make -j2 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_php.log
 echo `date +%Y/%m/%d" "%H:%M:%S` php make install | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 echo $'\n'`date +%Y/%m/%d" "%H:%M:%S` '***** make install *****' $'\n'$'\n'>> ${OPENSHIFT_LOG_DIR}/install_php.log
 make install 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_php.log
@@ -158,7 +158,7 @@ CFLAGS="-O3 -march=native" CXXFLAGS="-O3 -march=native" \
 
 echo `date +%Y/%m/%d" "%H:%M:%S` libmemcached make | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 echo $'\n'`date +%Y/%m/%d" "%H:%M:%S` '***** make *****' $'\n'$'\n'>> ${OPENSHIFT_LOG_DIR}/install_libmemcached.log
-time make -j3 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_libmemcached.log
+time make -j2 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_libmemcached.log
 
 echo `date +%Y/%m/%d" "%H:%M:%S` libmemcached make install | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 echo $'\n'`date +%Y/%m/%d" "%H:%M:%S` '***** make install *****' $'\n'$'\n'>> ${OPENSHIFT_LOG_DIR}/install_libmemcached.log
