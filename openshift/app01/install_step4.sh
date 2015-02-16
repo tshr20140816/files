2ac6fafbe5addfec9420a211553c35e37be42f8d
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-02-16 13:05:59 +0900

    Update install_step4.sh

--- openshift/app01/install_step4.sh
+++ openshift/app01/install_step4.sh
@@ -54,8 +54,8 @@ eval "$(rbenv init -)"
 echo `oo-cgroup-read memory.failcnt | awk '{printf "Memory Fail Count : %\047d\n", $1}'` | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 echo `date +%Y/%m/%d" "%H:%M:%S` ruby install | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 
-export CFLAGS="-O3 -march=native" 
-export CXXFLAGS="-O3 -march=native" 
+export CFLAGS="-O2 -march=native" 
+export CXXFLAGS="-O2 -march=native" 
 time CONFIGURE_OPTS="--disable-install-doc" MAKE_OPTS="-j 4" \
 rbenv install -v ${ruby_version} >${OPENSHIFT_LOG_DIR}/ruby.rbenv.log 2>&1
 
