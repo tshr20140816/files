973e8290dd2351b5c8ce9fc5d457a6fce8c3f0c5
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-02-16 09:00:30 +0900

    Update install_step5.sh

--- openshift/app01/install_step5.sh
+++ openshift/app01/install_step5.sh
@@ -26,13 +26,16 @@ export RBENV_ROOT=${OPENSHIFT_DATA_DIR}/.rbenv
 export PATH="${OPENSHIFT_DATA_DIR}/.rbenv/bin:$PATH" 
 export PATH="${OPENSHIFT_DATA_DIR}/.gem/bin:$PATH" 
 eval "$(rbenv init -)" 
-export APXS2=${OPENSHIFT_DATA_DIR}/apache/bin/apxs
+# export APXS2=${OPENSHIFT_DATA_DIR}/apache/bin/apxs
 export PATH=${OPENSHIFT_DATA_DIR}/apache/bin:$PATH
 export HTTPD=${OPENSHIFT_DATA_DIR}/apache/bin/httpd
 export BINDIR=${OPENSHIFT_DATA_DIR}/apache
 
 time CFLAGS="-O3 -march=native -pipe" CXXFLAGS="-O3 -march=native -pipe" \
-${OPENSHIFT_DATA_DIR}/.gem/bin/passenger-install-apache2-module --auto --languages ruby
+${OPENSHIFT_DATA_DIR}/.gem/bin/passenger-install-apache2-module \
+--auto \
+--languages ruby \
+--apxs2-path ${OPENSHIFT_DATA_DIR}/apache/bin/apxs
 
 touch ${OPENSHIFT_DATA_DIR}/install_check_point/`basename $0`.ok
 
