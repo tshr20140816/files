aeecbb8e73d0ed30567979c4772ce8f745499b62
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-02-16 21:47:08 +0900

    Update install_step15.sh

--- openshift/app01/install_step15.sh
+++ openshift/app01/install_step15.sh
@@ -46,6 +46,7 @@ do
     usage_in_bytes_format=`echo ${usage_in_bytes} | awk '{printf "%\047d\n", $0}'`
     failcnt=`oo-cgroup-read memory.failcnt | awk '{printf "%\047d\n", $0}'`
     echo ${dt} ${usage_in_bytes_format} ${failcnt} >> ${OPENSHIFT_LOG_DIR}/memory_usage.log
+    [ -f ${OPENSHIFT_TMP_DIR}/stop ] || exit
     sleep 1s
 done
 __HEREDOC__
