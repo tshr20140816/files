731f94be8b41abc1e6dd62698f8666e442686922
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-02-17 13:59:35 +0900

    Update functions.sh

--- openshift/app01/functions.sh
+++ openshift/app01/functions.sh
@@ -5,7 +5,10 @@ function010 {
 
     set -x
 
-    wget --spider $(cat ${OPENSHIFT_DATA_DIR}/params/web_beacon_server)dummy?server=${OPENSHIFT_GEAR_DNS}\&part=$(basename $0 .sh) \
+    mfc=$(oo-cgroup-read memory.memsw.failcnt | awk '{printf "%\047d\n", $1}')
+    query_string="server=${OPENSHIFT_GEAR_DNS}\&part=$(basename $0 .sh)&mfc=${mfc}"
+
+    wget --spider $(cat ${OPENSHIFT_DATA_DIR}/params/web_beacon_server)dummy?${query_string} \
     > /dev/null 2>&1
 
     pushd ${OPENSHIFT_DATA_DIR}/install_check_point > /dev/null
