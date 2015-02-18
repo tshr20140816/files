f40ec4f1789cd1170c249f589dd975a025101596
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-02-18 16:47:46 +0900

    Update monitor_resourse.sh

--- openshift/app01/monitor_resourse.sh
+++ openshift/app01/monitor_resourse.sh
@@ -8,7 +8,7 @@ do
   usage_in_bytes=`oo-cgroup-read memory.usage_in_bytes`
   usage_in_bytes_format=`echo ${usage_in_bytes} | awk '{printf "%\047d\n", $0}'`
   failcnt=`oo-cgroup-read memory.failcnt | awk '{printf "%\047d\n", $0}'`
-  # echo $dt $usage_in_bytes $failcnt;
+  # echo $dt $usage_in_bytes $failcnt
   if [ ${usage_in_bytes} -gt 400000000 ]; then
     echo -e $dt "\e[33m${usage_in_bytes_format}\e[m" $failcnt
   else
