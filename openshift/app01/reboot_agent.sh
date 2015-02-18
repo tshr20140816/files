0d1a906647321e259af99fc923ba28203cf23787
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-02-18 09:36:53 +0900

    Update reboot_agent.sh

--- openshift/app01/reboot_agent.sh
+++ openshift/app01/reboot_agent.sh
@@ -7,14 +7,31 @@ do
     if [ -f ${OPENSHIFT_DATA_DIR}/install_check_point/install_all.ok ]; then
         exit
     fi
-    if [ ! -f ${OPENSHIFT_DATA_DIR}/install_check_point/restart.txt ]; then
+    if [ ! -f ${OPENSHIFT_DATA_DIR}/install_check_point/gear_action.txt ]; then
         sleep 10s
-    else
-        query_string="server=${OPENSHIFT_GEAR_DNS}&action=reboot"
-        wget --spider $(cat ${OPENSHIFT_DATA_DIR}/params/web_beacon_server)dummy?${query_string} > /dev/null 2>&1
-    
-        /usr/bin/gear stop --trace
-        /usr/bin/gear start --trace
-        rm -f ${OPENSHIFT_DATA_DIR}/install_check_point/restart.txt
+        continue
     fi
+
+    case "$(cat ${OPENSHIFT_DATA_DIR}/install_check_point/gear_action.txt)" in
+        "restart" ) 
+            query_string="server=${OPENSHIFT_GEAR_DNS}&action=reboot"
+            wget --spider $(cat ${OPENSHIFT_DATA_DIR}/params/web_beacon_server)dummy?${query_string} > /dev/null 2>&1
+    
+            /usr/bin/gear stop --trace
+            /usr/bin/gear start --trace
+            ;;
+        "stop" )
+            query_string="server=${OPENSHIFT_GEAR_DNS}&action=stop"
+            wget --spider $(cat ${OPENSHIFT_DATA_DIR}/params/web_beacon_server)dummy?${query_string} > /dev/null 2>&1
+    
+            /usr/bin/gear stop --trace
+            ;;
+        "start" )
+            query_string="server=${OPENSHIFT_GEAR_DNS}&action=start"
+            wget --spider $(cat ${OPENSHIFT_DATA_DIR}/params/web_beacon_server)dummy?${query_string} > /dev/null 2>&1
+    
+            /usr/bin/gear start --trace
+            ;;
+    esac
+    rm -f ${OPENSHIFT_DATA_DIR}/install_check_point/gear_action.txt
 done;
