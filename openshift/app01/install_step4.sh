43b8736e5f0219ff4cb56e1d8478be7f131db011
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-02-18 14:49:43 +0900

    Update install_step4.sh

--- openshift/app01/install_step4.sh
+++ openshift/app01/install_step4.sh
@@ -14,7 +14,7 @@ export GEM_HOME=${OPENSHIFT_DATA_DIR}.gem
 
 # *** rbenv ***
 
-echo `date +%Y/%m/%d" "%H:%M:%S` rbenv install >> ${OPENSHIFT_LOG_DIR}/install.log
+echo $(date +%Y/%m/%d" "%H:%M:%S) rbenv install >> ${OPENSHIFT_LOG_DIR}/install.log
 
 # OPENSHIFT用インストーラ
 pushd ${OPENSHIFT_TMP_DIR} > /dev/null
@@ -30,29 +30,29 @@ eval "$(rbenv init -)"
 
 # *** ruby ***
 
-echo `oo-cgroup-read memory.failcnt | awk '{printf "Memory Fail Count : %\047d\n", $1}'` | tee -a ${OPENSHIFT_LOG_DIR}/install.log
-echo `date +%Y/%m/%d" "%H:%M:%S` ruby install | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+echo $(oo-cgroup-read memory.failcnt | awk '{printf "Memory Fail Count : %\047d\n", $1}') | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+echo $(date +%Y/%m/%d" "%H:%M:%S) ruby install | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 
 export CFLAGS="-O2 -march=native" 
 export CXXFLAGS="-O2 -march=native" 
-time CONFIGURE_OPTS="--disable-install-doc" MAKE_OPTS="-j 4" \
+time CONFIGURE_OPTS="--disable-install-doc" MAKE_OPTS="-j $(cat /proc/cpuinfo | grep processor | wc -l)" \
 rbenv install -v ${ruby_version} >${OPENSHIFT_LOG_DIR}/ruby.rbenv.log 2>&1
 
-echo `oo-cgroup-read memory.failcnt | awk '{printf "Memory Fail Count : %\047d\n", $1}'` | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+echo $(oo-cgroup-read memory.failcnt | awk '{printf "Memory Fail Count : %\047d\n", $1}') | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 
 rbenv global ${ruby_version}
 rbenv rehash
 
 # *** bundler ***
 
-echo `date +%Y/%m/%d" "%H:%M:%S` bundler install | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+echo $(date +%Y/%m/%d" "%H:%M:%S) bundler install | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 
 # patch resolv.rb
 # OPENSHIFT では  0.0.0.0 は使えないため OPENSHIFT_DIY_IP に置換
 find ${OPENSHIFT_DATA_DIR}/.rbenv/versions/ -name resolv.rb -type f \
 | xargs perl -pi -e "s/0\.0\.0\.0/${OPENSHIFT_DIY_IP}/g"
 
-echo `date +%Y/%m/%d" "%H:%M:%S` resolv.rb patch check | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+echo $(date +%Y/%m/%d" "%H:%M:%S) resolv.rb patch check | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 find ${OPENSHIFT_DATA_DIR}/.rbenv/versions/ -name resolv.rb -type f \
 | grep ${OPENSHIFT_DIY_IP} >> ${OPENSHIFT_LOG_DIR}/install.log
 
@@ -61,11 +61,11 @@ rbenv rehash
 
 # *** passenger ***
 
-echo `date +%Y/%m/%d" "%H:%M:%S` bundler passenger | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+echo $(date +%Y/%m/%d" "%H:%M:%S) bundler passenger | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 
 time rbenv exec gem install passenger --no-ri --no-rdoc --debug -V >${OPENSHIFT_LOG_DIR}/passenger.gem.rbenv.log 2>&1
 rbenv rehash
 
-touch ${OPENSHIFT_DATA_DIR}/install_check_point/`basename $0`.ok
+touch ${OPENSHIFT_DATA_DIR}/install_check_point/$(basename $0).ok
 
-echo `date +%Y/%m/%d" "%H:%M:%S` Install Finish `basename $0` | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+echo $(date +%Y/%m/%d" "%H:%M:%S) Install Finish $(basename $0) | tee -a ${OPENSHIFT_LOG_DIR}/install.log
