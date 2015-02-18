3514aa83f79f3758e5f119e12182f553d8eaea16
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-02-18 09:58:41 +0900

    Update install_step16.sh

--- openshift/app01/install_step16.sh
+++ openshift/app01/install_step16.sh
@@ -1,29 +1,8 @@
 #!/bin/bash
 
-wget --spider `cat ${OPENSHIFT_DATA_DIR}/params/web_beacon_server`dummy?server=${OPENSHIFT_GEAR_DNS}\&part=`basename $0 .sh` >/dev/null 2>&1
-
-while read LINE
-do
-    product=`echo ${LINE} | awk '{print $1}'`
-    version=`echo ${LINE} | awk '{print $2}'`
-    eval "${product}"=${version}
-done < ${OPENSHIFT_DATA_DIR}/version_list
-
-set -x
-
-export TZ=JST-9
-
-pushd ${OPENSHIFT_DATA_DIR}/install_check_point > /dev/null
-if [ -f `basename $0`.ok ]; then
-    echo `date +%Y/%m/%d" "%H:%M:%S` Install Skip `basename $0` | tee -a ${OPENSHIFT_LOG_DIR}/install.log
-    exit
-fi
-popd > /dev/null
-
-echo `date +%Y/%m/%d" "%H:%M:%S` Install Start `basename $0` | tee -a ${OPENSHIFT_LOG_DIR}/install.log
-echo `quota -s | grep -v a | awk '{print "Disk Usage : " $1,$4 " files"}'` | tee -a ${OPENSHIFT_LOG_DIR}/install.log
-echo `oo-cgroup-read memory.usage_in_bytes | awk '{printf "Memory Usage : %\047d\n", $1}'` | tee -a ${OPENSHIFT_LOG_DIR}/install.log
-echo `oo-cgroup-read memory.failcnt | awk '{printf "Memory Fail Count : %\047d\n", $1}'` | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+source functions.sh
+function010
+$? && exit
 
 # ***** start *****
 
