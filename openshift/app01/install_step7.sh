27ef0fb71c577ebc923c6e745d502d9b5602b708
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-02-18 11:35:14 +0900

    Update install_step7.sh

--- openshift/app01/install_step7.sh
+++ openshift/app01/install_step7.sh
@@ -195,6 +195,4 @@ popd > /dev/null
 
 touch ${OPENSHIFT_DATA_DIR}/install_check_point/`basename $0`.ok
 
-# /usr/bin/gear start
-
 echo `date +%Y/%m/%d" "%H:%M:%S` Install Finish `basename $0` | tee -a ${OPENSHIFT_LOG_DIR}/install.log
