5ca034acf5a5a4a9c2a6e56571b4ed4093bce889
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-02-18 14:02:54 +0900

    Update install_step2.sh

--- openshift/app01/install_step2.sh
+++ openshift/app01/install_step2.sh
@@ -18,31 +18,31 @@ rm -rf ${OPENSHIFT_TMP_DIR}/dbench
 rm -rf ${OPENSHIFT_DATA_DIR}/dbench
 
 pushd ${OPENSHIFT_TMP_DIR} > /dev/null
-echo `date +%Y/%m/%d" "%H:%M:%S` dbench git pull | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+echo $(date +%Y/%m/%d" "%H:%M:%S) dbench git pull | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 git clone git://git.samba.org/sahlberg/dbench.git dbench
 git pull
 popd > /dev/null
 
 pushd ${OPENSHIFT_TMP_DIR}/dbench > /dev/null
 
-echo `date +%Y/%m/%d" "%H:%M:%S` dbench autogen | tee -a ${OPENSHIFT_LOG_DIR}/install.log
-echo `date +%Y/%m/%d" "%H:%M:%S` '***** autogen *****' $'\n'$'\n'> ${OPENSHIFT_LOG_DIR}/install_dbench.log
+echo $(date +%Y/%m/%d" "%H:%M:%S) dbench autogen | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+echo $(date +%Y/%m/%d" "%H:%M:%S) '***** autogen *****' $'\n'$'\n'> ${OPENSHIFT_LOG_DIR}/install_dbench.log
 ./autogen.sh 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_dbench.log
 
-# echo `date +%Y/%m/%d" "%H:%M:%S` dbench configure | tee -a ${OPENSHIFT_LOG_DIR}/install.log
-# echo `date +%Y/%m/%d" "%H:%M:%S` '***** configure *****' $'\n'$'\n'> ${OPENSHIFT_LOG_DIR}/install_dbench.log
+# echo $(date +%Y/%m/%d" "%H:%M:%S) dbench configure | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+# echo $(date +%Y/%m/%d" "%H:%M:%S) '***** configure *****' $'\n'$'\n'> ${OPENSHIFT_LOG_DIR}/install_dbench.log
 # CFLAGS="-O2 -march=native -pipe" CXXFLAGS="-O2 -march=native -pipe" \
 # ./configure \
 # --mandir=/tmp/man \
 # --docdir=/tmp/doc \
 # --prefix=${OPENSHIFT_DATA_DIR}/dbench 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_dbench.log
 
-# echo `date +%Y/%m/%d" "%H:%M:%S` dbench make | tee -a ${OPENSHIFT_LOG_DIR}/install.log
-# echo $'\n'`date +%Y/%m/%d" "%H:%M:%S` '***** make *****' $'\n'$'\n'>> ${OPENSHIFT_LOG_DIR}/install_dbench.log
+# echo $(date +%Y/%m/%d" "%H:%M:%S) dbench make | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+# echo $'\n'$(date +%Y/%m/%d" "%H:%M:%S) '***** make *****' $'\n'$'\n'>> ${OPENSHIFT_LOG_DIR}/install_dbench.log
 # time make -j4 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_dbench.log
 
-# echo `date +%Y/%m/%d" "%H:%M:%S` dbench make install | tee -a ${OPENSHIFT_LOG_DIR}/install.log
-# echo $'\n'`date +%Y/%m/%d" "%H:%M:%S` '***** make install *****' $'\n'$'\n'>> ${OPENSHIFT_LOG_DIR}/install_dbench.log
+# echo $(date +%Y/%m/%d" "%H:%M:%S) dbench make install | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+# echo $'\n'$(date +%Y/%m/%d" "%H:%M:%S) '***** make install *****' $'\n'$'\n'>> ${OPENSHIFT_LOG_DIR}/install_dbench.log
 # make install 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_dbench.log
 popd > /dev/null
 
@@ -71,30 +71,30 @@ mkdir -p ${OPENSHIFT_TMP_DIR}/lynx
 pushd ${OPENSHIFT_TMP_DIR}lynx > /dev/null
 cp ${OPENSHIFT_DATA_DIR}/download_files/lynx${lynx_version}.tar.gz ./
 
-echo `date +%Y/%m/%d" "%H:%M:%S` lynx tar | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+echo $(date +%Y/%m/%d" "%H:%M:%S) lynx tar | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 tar xfz lynx${lynx_version}.tar.gz --strip-components=1
 popd > /dev/null
 
 pushd ${OPENSHIFT_TMP_DIR}lynx > /dev/null
 
-echo `date +%Y/%m/%d" "%H:%M:%S` lynx configure | tee -a ${OPENSHIFT_LOG_DIR}/install.log
-echo `date +%Y/%m/%d" "%H:%M:%S` '***** configure *****' $'\n'$'\n'> ${OPENSHIFT_LOG_DIR}/install_lynx.log
+echo $(date +%Y/%m/%d" "%H:%M:%S) lynx configure | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+echo $(date +%Y/%m/%d" "%H:%M:%S) '***** configure *****' $'\n'$'\n'> ${OPENSHIFT_LOG_DIR}/install_lynx.log
 CFLAGS="-O2 -march=native -pipe" CXXFLAGS="-O2 -march=native -pipe" \
 ./configure \
 --mandir=/tmp/man \
 --prefix=${OPENSHIFT_DATA_DIR}/lynx 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_lynx.log
 
-echo `date +%Y/%m/%d" "%H:%M:%S` lynx make | tee -a ${OPENSHIFT_LOG_DIR}/install.log
-echo $'\n'`date +%Y/%m/%d" "%H:%M:%S` '***** make *****' $'\n'$'\n'>> ${OPENSHIFT_LOG_DIR}/install_lynx.log
-time make -j4 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_lynx.log
+echo $(date +%Y/%m/%d" "%H:%M:%S) lynx make | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+echo $'\n'$(date +%Y/%m/%d" "%H:%M:%S) '***** make *****' $'\n'$'\n'>> ${OPENSHIFT_LOG_DIR}/install_lynx.log
+time make -j$(cat /proc/cpuinfo | grep processor | wc -l) 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_lynx.log
 
-echo `date +%Y/%m/%d" "%H:%M:%S` lynx make install | tee -a ${OPENSHIFT_LOG_DIR}/install.log
-echo $'\n'`date +%Y/%m/%d" "%H:%M:%S` '***** make install *****' $'\n'$'\n'>> ${OPENSHIFT_LOG_DIR}/install_lynx.log
+echo $(date +%Y/%m/%d" "%H:%M:%S) lynx make install | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+echo $'\n'$(date +%Y/%m/%d" "%H:%M:%S) '***** make install *****' $'\n'$'\n'>> ${OPENSHIFT_LOG_DIR}/install_lynx.log
 make install 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_lynx.log
 popd > /dev/null
 
 rm -rf ${OPENSHIFT_TMP_DIR}/lynx
 
-touch ${OPENSHIFT_DATA_DIR}/install_check_point/`basename $0`.ok
+touch ${OPENSHIFT_DATA_DIR}/install_check_point/$(basename $0).ok
 
-echo `date +%Y/%m/%d" "%H:%M:%S` Install Finish `basename $0` | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+echo $(date +%Y/%m/%d" "%H:%M:%S) Install Finish $(basename $0) | tee -a ${OPENSHIFT_LOG_DIR}/install.log
