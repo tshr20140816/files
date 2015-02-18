b28d1dc091049d3621c280cbed45aa204d66aa54
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-02-18 09:42:51 +0900

    Update reboot_agent.sh

--- openshift/app01/reboot_agent.sh
+++ openshift/app01/reboot_agent.sh
@@ -13,20 +13,23 @@ do
     fi
 
     case "$(cat ${OPENSHIFT_DATA_DIR}/install_check_point/gear_action.txt)" in
-        "restart" ) 
-            query_string="server=${OPENSHIFT_GEAR_DNS}&action=reboot"
+        "restart" )
+            echo $(date +%Y/%m/%d" "%H:%M:%S) gear restart
+            query_string="server=${OPENSHIFT_GEAR_DNS}&action=restart"
             wget --spider $(cat ${OPENSHIFT_DATA_DIR}/params/web_beacon_server)dummy?${query_string} > /dev/null 2>&1
     
             /usr/bin/gear stop --trace
             /usr/bin/gear start --trace
             ;;
         "stop" )
+            echo $(date +%Y/%m/%d" "%H:%M:%S) gear stop
             query_string="server=${OPENSHIFT_GEAR_DNS}&action=stop"
             wget --spider $(cat ${OPENSHIFT_DATA_DIR}/params/web_beacon_server)dummy?${query_string} > /dev/null 2>&1
     
             /usr/bin/gear stop --trace
             ;;
         "start" )
+            echo $(date +%Y/%m/%d" "%H:%M:%S) gear start
             query_string="server=${OPENSHIFT_GEAR_DNS}&action=start"
             wget --spider $(cat ${OPENSHIFT_DATA_DIR}/params/web_beacon_server)dummy?${query_string} > /dev/null 2>&1
     
