bc394694bb031ce007d2724531c7e968c71539f6
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-02-23 10:43:31 +0900

    Update install_step15.sh

--- openshift/app01/install_step15.sh
+++ openshift/app01/install_step15.sh
@@ -6,7 +6,17 @@ $? && exit
 
 # ***** baikal *****
 
+rm -rf ${OPENSHIFT_DATA_DIR}/apache/htdocs/baikal-regular
+rm -rf ${OPENSHIFT_DATA_DIR}/apache/htdocs/baikal
 
+pushd ${OPENSHIFT_DATA_DIR}/apache/htdocs/ > /dev/null
+cp -f ${OPENSHIFT_DATA_DIR}/download_files/baikal-regular-${baikal_version}.tgz ./
+echo $(date +%Y/%m/%d" "%H:%M:%S) baikal tar | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+tar xfz baikal-regular-${baikal_version}.tgz
+mv baikal-regular baikal
+popd > /dev/null
+
+rm ${OPENSHIFT_DATA_DIR}/apache/htdocs/baikal-regular-${baikal_version}.tgz
 
 touch ${OPENSHIFT_DATA_DIR}/install_check_point/$(basename $0).ok
 
