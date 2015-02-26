9a04822ae894c863316bd1ce15975bc303fc7961
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-02-26 22:11:40 +0900

    Delete mysql_backup.sh

--- openshift/redmine/mysql_backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mysqldump \
---host=$OPENSHIFT_MYSQL_DB_HOST \
---port=$OPENSHIFT_MYSQL_DB_PORT \
---user=$OPENSHIFT_MYSQL_DB_USERNAME \
---password=$OPENSHIFT_MYSQL_DB_PASSWORD \
--x --all-databases --events | xz > $OPENSHIFT_DATA_DIR/mysql_dump_`date +%a`.xz
-
