b3614fd1c60529a3cce0bcf04515beb0226eb197
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-02-26 22:11:57 +0900

    Delete app_restart.sh

--- openshift/redmine/app_restart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-http_status=`curl -LI http://${OPENSHIFT_GEAR_DNS}/ -o /dev/null -w '%{http_code}\n' -s`
-echo http_status $http_status
-if test $http_status -eq 503 ; then
-    echo ctl_all restart
-    ctl_all restart
-fi
-
