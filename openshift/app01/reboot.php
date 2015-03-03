f140a90b5c192b66a235b35e06cc8f543ef8b8a8
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-03-03 16:05:57 +0900

    Update reboot.php

--- openshift/app01/reboot.php
+++ openshift/app01/reboot.php
@@ -1,32 +1,24 @@
 <?php
-$action = $_POST['action']
-$url = $_POST['url']
-$rc = 'OK';
-
-if(preg_match('^\w+?-\w+?\.rhcloud\.com$', $url) === 0)
-{
-    $filename = getenve('OPENSHIFT_DATA_DIR') + 'ignore_server_list/' + $url
-    switch ($action)
-    {
+$action = $_POST['action'];
+$url    = $_POST['url'];
+$rc     = 'OK';
+if (preg_match('^\w+?-\w+?\.rhcloud\.com$', $url) === 0) {
+    $filename = getenve('OPENSHIFT_DATA_DIR') + 'ignore_server_list/' + $url;
+    switch ($action) {
         case "ignore":
             touch($filename);
             break;
         case "reboot_check":
-            if(file_exists($filename))
-            {
-                if(mktime() - filemtime($filename) < 60 * 60 * 3)
-                {
+            if (file_exists($filename)) {
+                if (mktime() - filemtime($filename) < 60 * 60 * 3) {
                     $rc = 'NG';
-                }
-                else
-                {
+                } else {
                     unlink($filename);
                 }
             };
             break;
         case "remove":
-            if(file_exists($filename))
-            {
+            if (file_exists($filename)) {
                 unlink($filename);
             };
             break;
@@ -35,5 +27,5 @@ if(preg_match('^\w+?-\w+?\.rhcloud\.com$', $url) === 0)
     }
 }
 header('Content-type: text/plain');
-echo $rc
+echo $rc;
 ?>
