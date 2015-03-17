a29a7dec5d73e04264b8f302c7644081a65d5c2b
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-03-17 09:14:31 +0900

    Update install_step9.sh

--- openshift/app01/install_step9.sh
+++ openshift/app01/install_step9.sh
@@ -16,17 +16,18 @@ tar xfz ${ttrss_version}.tar.gz --strip-components=1
 popd > /dev/null
 
 # create database
-ttrssuser_password=$(uuidgen | base64 | head -c 25)
 pushd ${OPENSHIFT_TMP_DIR} > /dev/null
 cat << '__HEREDOC__' > create_database_ttrss.txt
 DROP DATABASE IF EXISTS ttrss;
 CREATE DATABASE ttrss CHARACTER SET utf8 COLLATE utf8_general_ci;
-GRANT ALL PRIVILEGES ON ttrss.* TO ttrssuser@__OPENSHIFT_MYSQL_DB_HOST__ IDENTIFIED BY '__PASSWORD__';
-FLUSH PRIVILEGES;
 EXIT
 __HEREDOC__
+# GRANT ALL PRIVILEGES ON ttrss.* TO __OPENSHIFT_MYSQL_DB_USERNAME__@__OPENSHIFT_MYSQL_DB_HOST__ IDENTIFIED BY '__OPENSHIFT_MYSQL_DB_PASSWORD__';
+# FLUSH PRIVILEGES;
+
+perl -pi -e 's/__OPENSHIFT_MYSQL_DB_USERNAME__/$ENV{OPENSHIFT_MYSQL_DB_USERNAME}/g' create_database_ttrss.txt
 perl -pi -e 's/__OPENSHIFT_MYSQL_DB_HOST__/$ENV{OPENSHIFT_MYSQL_DB_HOST}/g' create_database_ttrss.txt
-perl -pi -e "s/__PASSWORD__/${ttrssuser_password}/g" create_database_ttrss.txt
+perl -pi -e 's/__OPENSHIFT_MYSQL_DB_PASSWORD__/$ENV{OPENSHIFT_MYSQL_DB_PASSWORD}/g' create_database_ttrss.txt
 
 mysql -u "${OPENSHIFT_MYSQL_DB_USERNAME}" \
 --password="${OPENSHIFT_MYSQL_DB_PASSWORD}" \
@@ -38,7 +39,6 @@ mysql -u "${OPENSHIFT_MYSQL_DB_USERNAME}" \
 -h "${OPENSHIFT_MYSQL_DB_HOST}" \
 -P "${OPENSHIFT_MYSQL_DB_PORT}" ttrss < ${OPENSHIFT_DATA_DIR}/apache/htdocs/ttrss/schema/ttrss_schema_mysql.sql
 
-echo $(date +%Y/%m/%d" "%H:%M:%S) Tiny Tiny RSS mysql ttrssuser/${ttrssuser_password} | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 popd > /dev/null
 
 pushd ${OPENSHIFT_DATA_DIR}/apache/htdocs/ttrss > /dev/null
@@ -46,9 +46,9 @@ pushd ${OPENSHIFT_DATA_DIR}/apache/htdocs/ttrss > /dev/null
 cp config.php-dist config.php
 perl -pi -e "s/define\(\'DB_TYPE\', \"pgsql\"/define('DB_TYPE', 'mysql'/g" config.php
 perl -pi -e "s/define\(\'DB_HOST\', \"localhost\"/define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST')/g" config.php
-perl -pi -e "s/define\(\'DB_USER\', \"fox\"/define('DB_USER', 'ttrssuser'/g" config.php
+perl -pi -e "s/define\(\'DB_USER\', \"fox\"/define('DB_USER', 'getenv('OPENSHIFT_MYSQL_DB_USERNAME')'/g" config.php
 perl -pi -e "s/define\(\'DB_NAME\', \"fox\"/define('DB_NAME', 'ttrss'/g" config.php
-perl -pi -e "s/define\(\'DB_PASS\', \"XXXXXX\"/define('DB_PASS', '${ttrssuser_password}'/g" config.php
+perl -pi -e "s/define\(\'DB_PASS\', \"XXXXXX\"/define('DB_PASS', 'getenv('OPENSHIFT_MYSQL_DB_PASSWORD')'/g" config.php
 perl -pi -e "s/define\(\'DB_PORT\', \'\'/define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT')/g" config.php
 perl -pi -e "s/define\(\'MYSQL_CHARSET\', \'UTF8\'\);/define('_ENABLE_PDO', true);\r\n        define('MYSQL_CHARSET', 'UTF8');/g" config.php
 perl -pi -e "s/define\(\'SELF_URL_PATH\', \'http:\/\/example.org\/tt-rss\/\'/define('SELF_URL_PATH', 'http:\/\/${OPENSHIFT_APP_DNS}\/ttrss\/'/g" config.php
