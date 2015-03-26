0876150dbce45eb2e0b52711c68bd3a50ffafff1
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-03-26 09:51:09 +0900

    Update install_step7.sh

--- openshift/app01/install_step7.sh
+++ openshift/app01/install_step7.sh
@@ -26,7 +26,7 @@ CFLAGS="-O2 -march=native -pipe" CXXFLAGS="-O2 -march=native -pipe" \
 
 echo $(date +%Y/%m/%d" "%H:%M:%S) memcached make | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 echo $'\n'$(date +%Y/%m/%d" "%H:%M:%S) '***** make *****' $'\n'$'\n'>> ${OPENSHIFT_LOG_DIR}/install_memcached.log
-time make -j$(cat /proc/cpuinfo | grep processor | wc -l) 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_memcached.log
+time make -j$(cat /proc/cpuinfo | grep -c processor) 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_memcached.log
 
 echo $(date +%Y/%m/%d" "%H:%M:%S) memcached make install | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 echo $'\n'$(date +%Y/%m/%d" "%H:%M:%S) '***** make install *****' $'\n'$'\n'>> ${OPENSHIFT_LOG_DIR}/install_memcached.log
@@ -148,7 +148,7 @@ CFLAGS="-O2 -march=native -pipe" CXXFLAGS="-O2 -march=native -pipe" \
 
 echo $(date +%Y/%m/%d" "%H:%M:%S) libmemcached make | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 echo $'\n'$(date +%Y/%m/%d" "%H:%M:%S) '***** make *****' $'\n'$'\n'>> ${OPENSHIFT_LOG_DIR}/install_libmemcached.log
-time make -j$(cat /proc/cpuinfo | grep processor | wc -l) 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_libmemcached.log
+time make -j$(cat /proc/cpuinfo | grep -c processor) 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_libmemcached.log
 
 echo $(date +%Y/%m/%d" "%H:%M:%S) libmemcached make install | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 echo $'\n'$(date +%Y/%m/%d" "%H:%M:%S) '***** make install *****' $'\n'$'\n'>> ${OPENSHIFT_LOG_DIR}/install_libmemcached.log
@@ -186,7 +186,7 @@ CFLAGS="-O2 -march=native -pipe" CXXFLAGS="-O2 -march=native -pipe" \
 
 echo $(date +%Y/%m/%d" "%H:%M:%S) memcached_php_ext make | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 echo $'\n'$(date +%Y/%m/%d" "%H:%M:%S) '***** make *****' $'\n'$'\n'>> ${OPENSHIFT_LOG_DIR}/install_memcached_php_extention.log
-time make -j$(cat /proc/cpuinfo | grep processor | wc -l) 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_memcached_php_extention.log
+time make -j$(cat /proc/cpuinfo | grep -c processor) 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_memcached_php_extention.log
 
 echo $(date +%Y/%m/%d" "%H:%M:%S) memcached_php_ext make install | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 echo $'\n'$(date +%Y/%m/%d" "%H:%M:%S) '***** make install *****' $'\n'$'\n'>> ${OPENSHIFT_LOG_DIR}/install_memcached_php_extention.log
