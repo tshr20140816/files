d4fe07fc572e6b471aa9d6e68b1913348c945c12
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-03-26 09:43:44 +0900

    Update create_app.sh

--- openshift/app01/create_app.sh
+++ openshift/app01/create_app.sh
@@ -11,6 +11,6 @@ do
     yes | rhc app delete -a ${server}
     yes | rhc app create ${server} diy-0.1 mysql-5.5 cron-1.4 phpmyadmin-4 --server openshift.redhat.com
     server_link=$(rhc apps | grep ssh | grep ${server} | awk '{print $3}' | awk -F/ '{print $3}')
-    processor_count=$(ssh ${server_link} cat /proc/cpuinfo | grep processor | wc -l)
+    processor_count=$(ssh ${server_link} cat /proc/cpuinfo | grep -c processor)
     [ ${processor_count} -eq 4 ] && exit
 done
