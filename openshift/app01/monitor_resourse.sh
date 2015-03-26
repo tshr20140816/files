45e00cbd235898bef1c2756c5b420e16fd422639
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-03-26 10:31:39 +0900

    Update monitor_resourse.sh

--- openshift/app01/monitor_resourse.sh
+++ openshift/app01/monitor_resourse.sh
@@ -3,10 +3,10 @@
 export TZ=JST-9
 while :
 do
-  dt=`date +%Y/%m/%d" "%H:%M:%S`
+  dt=$(date +%Y/%m/%d" "%H:%M:%S)
   # usage_in_bytes=`oo-cgroup-read memory.usage_in_bytes | awk '{printf "%\047d\n", $0}'`
   usage_in_bytes=`oo-cgroup-read memory.usage_in_bytes`
-  usage_in_bytes_format=`echo ${usage_in_bytes} | awk '{printf "%\047d\n", $0}'`
+  usage_in_bytes_format=$(echo ${usage_in_bytes} | awk '{printf "%\047d\n", $0}')
   failcnt=`oo-cgroup-read memory.failcnt | awk '{printf "%\047d\n", $0}'`
   # echo $dt $usage_in_bytes $failcnt
   if [ ${usage_in_bytes} -gt 400000000 ]; then
