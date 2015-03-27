1e8a39e62311d3cb153c704ae9289fc11325c209
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-03-27 15:10:29 +0900

    Update install_step9.sh

--- openshift/app01/install_step9.sh
+++ openshift/app01/install_step9.sh
@@ -68,4 +68,4 @@ popd > /dev/null
 
 touch ${OPENSHIFT_DATA_DIR}/install_check_point/$(basename $0).ok
 
-echo $(date +%Y/%m/%d" "%H:%M:%S) Install Finish $(basename $0) | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+echo "$(date +%Y/%m/%d" "%H:%M:%S) Install Finish $(basename "${0}")" | tee -a ${OPENSHIFT_LOG_DIR}/install.log
