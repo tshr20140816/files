17e15828cf5ee0d0c7dd36fd3d9530c1dad7f263
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-03-28 07:25:17 +0900

    Update install_step2.sh

--- openshift/app01/install_step2.sh
+++ openshift/app01/install_step2.sh
@@ -128,6 +128,6 @@ popd > /dev/null
 
 rm -rf ${OPENSHIFT_TMP_DIR}/lynx
 
-touch ${OPENSHIFT_DATA_DIR}/install_check_point/$(basename "${0}).ok
+touch ${OPENSHIFT_DATA_DIR}/install_check_point/$(basename "${0}").ok
 
 echo "$(date +%Y/%m/%d" "%H:%M:%S) Install Finish $(basename "${0}")" | tee -a ${OPENSHIFT_LOG_DIR}/install.log
