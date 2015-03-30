db5ccc9de9966c86e0e1ec02cccaef40f9d4bc7e
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-03-30 15:28:09 +0900

    Update log_garbage.sh

--- openshift/app01/log_garbage.sh
+++ openshift/app01/log_garbage.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 [ $# -ne 1 ] && exit
+
+log_name="${1}"
+
+for index in 6 5 4 3 2 1
+do
+    mv -q -f ${log_name}.${index} ${log_name}.$((index + 1))
+done
+
+mv -f ${log_name} ${log_name}.1
