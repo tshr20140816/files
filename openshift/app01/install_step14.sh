d96d8031baa4823a78467f9f6d772540ebe00a65
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-04-07 14:34:31 +0900

    Update install_step14.sh

--- openshift/app01/install_step14.sh
+++ openshift/app01/install_step14.sh
@@ -91,6 +91,7 @@ echo "$(date +%Y/%m/%d" "%H:%M:%S) rhc install" | tee -a ${OPENSHIFT_LOG_DIR}/in
 gem list | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 
 time rbenv exec gem install rhc --no-rdoc --no-ri --verbose > ${OPENSHIFT_LOG_DIR}/rhc.gem.log 2>&1
+rbenv rehash
 mv ${OPENSHIFT_LOG_DIR}/rhc.gem.log ${OPENSHIFT_LOG_DIR}/install/
 
 rhc --version
