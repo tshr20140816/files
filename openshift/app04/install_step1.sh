fd4688e7ac25c906c4a348b06caa06ab4ce8caf8
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-04-09 08:48:58 +0900

    Update install_step1.sh

--- openshift/app04/install_step1.sh
+++ openshift/app04/install_step1.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-cd ${OPENSHIFT_TMP_DIR}
-wget https://distcc.googlecode.com/files/distcc-3.1.tar.bz2
-tar jxf distcc-3.1.tar.bz2
-cd distcc-3.1
-./configure --prefix=${OPENSHIFT_DATA_DIR}/distcc
-time make -j4
-make install
+distcc_version=3.1
 
-cd ${OPENSHIFT_DATA_DIR}/distcc
+pushd ${OPENSHIFT_TMP_DIR} > /dev/null
+wget https://distcc.googlecode.com/files/distcc-${distcc_version}.tar.bz2
+tar jxf distcc-${distcc_version}.tar.bz2
+popd > /dev/null
+pushd ${OPENSHIFT_TMP_DIR}/distcc-${distcc_version} > /dev/null
+./configure --prefix=${OPENSHIFT_DATA_DIR}/distcc
+time make -j$(grep -c -e processor /proc/cpuinfo)
+make install
+popd > /dev/null
+
+pushd ${OPENSHIFT_DATA_DIR}/distcc > /dev/null
+touch ${OPENSHIFT_LOG_DIR}/distccd.log
 ./bin/distccd --daemon --listen ${OPENSHIFT_DIY_IP} --jobs 2 --port 33632 \
- --allow 0.0.0.0/0 --log-file=${OPENSHIFT_TMP_DIR}/distccd.log --verbose --log-stderr 
+ --allow 0.0.0.0/0 --log-file=${OPENSHIFT_LOG_DIR}/distccd.log --verbose --log-stderr 
+popd > /dev/null
