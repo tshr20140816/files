aad9d097fa4aa6049a6d59473f3f76eaa63b228a
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-04-09 22:32:15 +0900

    Update install_step17.sh

--- openshift/app01/install_step17.sh
+++ openshift/app01/install_step17.sh
@@ -5,8 +5,10 @@ function010
 [ $? -eq 0 ] || exit
 
 pushd ${OPENSHIFT_DATA_DIR} > /dev/null
-find ${OPENSHIFT_DATA_DIR} -name *.sh -type f -print0 | xargs -0i bash -n {} 2>&1 >> ${OPENSHIFT_LOG_DIR}/shell_syntax_error.log
-find ${OPENSHIFT_CRON_DIR} -name *.sh -type f -print0 | xargs -0i bash -n {} 2>&1 >> ${OPENSHIFT_LOG_DIR}/shell_syntax_error.log
+find ${OPENSHIFT_DATA_DIR} -name *.sh -type f -print0 | xargs -0i bash -n {} 2>&1 \
+ >> ${OPENSHIFT_LOG_DIR}/shell_syntax_error.log
+find ${OPENSHIFT_REPO_DIR}/.openshift/cron/ -name *.sh -type f -print0 | xargs -0i bash -n {} 2>&1 \
+ >> ${OPENSHIFT_LOG_DIR}/shell_syntax_error.log
 popd > /dev/null
 
 touch ${OPENSHIFT_DATA_DIR}/install_check_point/$(basename $0).ok
