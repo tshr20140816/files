3c7eefdb5e50337d9f158c50309ce62ec36230b6
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-04-13 15:06:41 +0900

    Update functions.sh

--- openshift/app01/functions.sh
+++ openshift/app01/functions.sh
@@ -2,7 +2,7 @@
 function010() {
 
     export TZ=JST-9
-    export MAKEFLAGS="-j $(grep -c -e processor /proc/cpuinfo)"
+    # export MAKEFLAGS="-j $(grep -c -e processor /proc/cpuinfo)"
 
     set -x
 
