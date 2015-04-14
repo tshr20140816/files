5a3e8625bd35144a8b223b9ddc5fcda2d8375720
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-04-14 19:06:31 +0900

    Update install_ccache.sh

--- openshift/app04/install_ccache.sh
+++ openshift/app04/install_ccache.sh
@@ -32,7 +32,13 @@ cd /tmp
 wget http://ftp.riken.jp/net/apache//httpd/httpd-2.2.29.tar.bz2
 tar jxf httpd-2.2.29.tar.bz2
 cd httpd-2.2.29
-CC="ccache gcc" ./configure --prefix=${OPENSHIFT_DATA_DIR}/apache
+CC="ccache gcc" CFLAGS="-O2 -march=native -pipe" CXXFLAGS="-O2 -march=native -pipe" \
+ ./configure \
+ --prefix=${OPENSHIFT_DATA_DIR}/apache \
+ --mandir=/tmp/man \
+ --docdir=/tmp/doc \
+ --enable-mods-shared='all proxy'
+ 
 time make -j4
 
 cd /tmp
