bc746ef5cee66c8e61f57e7d868b42480a198977
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-04-14 10:45:56 +0900

    Update install_file_server.sh

--- openshift/app01/install_file_server.sh
+++ openshift/app01/install_file_server.sh
@@ -11,37 +11,37 @@ tar xfz nginx-${nginx_version}.tar.gz
 pushd cd nginx-${nginx_version} > /dev/null
 
 CFLAGS="-O2 -march=native -pipe" CXXFLAGS="-O2 -march=native -pipe" \
-./configure --prefix=${OPENSHIFT_DATA_DIR}/nginx \
---without-select_module \
---without-poll_module \
---without-http_charset_module \
---without-http_gzip_module \
---without-http_ssi_module \
---without-http_userid_module \
---without-http_access_module \
---without-http_auth_basic_module \
---without-http_geo_module \
---without-http_map_module \
---without-http_split_clients_module \
---without-http_referer_module \
---without-http_rewrite_module \
---without-http_proxy_module \
---without-http_fastcgi_module \
---without-http_uwsgi_module \
---without-http_scgi_module \
---without-http_memcached_module \
---without-http_limit_conn_module \
---without-http_limit_req_module \
---without-http_empty_gif_module \
---without-http_browser_module \
---without-http_upstream_ip_hash_module \
---without-http_upstream_least_conn_module \
---without-http_upstream_keepalive_module \
---without-http-cache \
---without-mail_pop3_module \
---without-mail_imap_module \
---without-mail_smtp_module \
---without-pcre
+ ./configure --prefix=${OPENSHIFT_DATA_DIR}/nginx \
+ --without-select_module \
+ --without-poll_module \
+ --without-http_charset_module \
+ --without-http_gzip_module \
+ --without-http_ssi_module \
+ --without-http_userid_module \
+ --without-http_access_module \
+ --without-http_auth_basic_module \
+ --without-http_geo_module \
+ --without-http_map_module \
+ --without-http_split_clients_module \
+ --without-http_referer_module \
+ --without-http_rewrite_module \
+ --without-http_proxy_module \
+ --without-http_fastcgi_module \
+ --without-http_uwsgi_module \
+ --without-http_scgi_module \
+ --without-http_memcached_module \
+ --without-http_limit_conn_module \
+ --without-http_limit_req_module \
+ --without-http_empty_gif_module \
+ --without-http_browser_module \
+ --without-http_upstream_ip_hash_module \
+ --without-http_upstream_least_conn_module \
+ --without-http_upstream_keepalive_module \
+ --without-http-cache \
+ --without-mail_pop3_module \
+ --without-mail_imap_module \
+ --without-mail_smtp_module \
+ --without-pcre
 
 time make -j$(grep -c -e processor /proc/cpuinfo)
 make install
