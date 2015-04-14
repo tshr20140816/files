59cb664b20c7cd16ab30db42094663355d4d61ea
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-04-14 10:54:07 +0900

    Update install_file_server.sh

--- openshift/app01/install_file_server.sh
+++ openshift/app01/install_file_server.sh
@@ -66,5 +66,41 @@ mkdir ${OPENSHIFT_DATA_DIR}/files
 ln -s ${OPENSHIFT_DATA_DIR}/files files
 popd > /dev/null
 
+pushd ${OPENSHIFT_REPO_DIR}/.openshift/cron/daily > /dev/null
+rm -f ./*
+touch jobs.deny
+
+# *** gem ***
+
+cat << '__HEREDOC__' > gem.sh
+#!/bin/bash
+
+pushd ${OPENSHIFT_DATA_DIR}/files/ > /dev/null
+
+for gem in bundler rack passenger
+do
+    rm -f ${gem}.html
+    wget https://rubygems.org/gems/${gem} -O ${gem}.html
+    version=$(grep -e canonical ${gem}.html | sed -r -e 's|^.*versions/(.+)".*$|\1|g')
+    if [ ! -f ${gem}-${version}.gem ]; then
+        wget https://rubygems.org/downloads/${gem}-${version}.gem -O ${gem}-${version}.gem
+        perl -pi -e 's/(\r|\n)//g' ${gem}.html
+        perl -pi -e 's/.*gem__sha"> +//g' ${gem}.html
+        perl -pi -e 's/ +<.*//g' ${gem}.html
+        gem_sha256=$(cat ${gem}.html)
+        file_sha256=$(sha256sum ${gem}-${version}.gem | cut -d ' ' -f 1)
+        if [ "${gem_sha256}" != "${file_sha256}" ]; then
+            rm ${gem}-${version}.gem
+        fi
+    fi
+    rm -f ${gem}.html
+done
+popd > /dev/null
+__HEREDOC__
+chmod +x gem.sh
+echo gem.sh >> jobs.allow
+
+popd > /dev/null
+
 /usr/bin/gear stop
 /usr/bin/gear start
