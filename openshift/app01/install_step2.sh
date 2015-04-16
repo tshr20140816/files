849c97ec3c739d4e406d1086752500de569a6757
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-04-16 09:36:23 +0900

    Update install_step2.sh

--- openshift/app01/install_step2.sh
+++ openshift/app01/install_step2.sh
@@ -105,7 +105,7 @@ do
     ./bin/fio -rw=${rwtype} -bs=4k -size=10m -numjobs=10 -runtime=60 \
     -direct=1 -invalidate=1 \
     -iodepth=32 -iodepth_batch=32 -group_reporting -name=${rwtype} -directory=${OPENSHIFT_DATA_DIR}/work \
-    | tee ${OPENSHIFT_LOG_DIR}/fio_${rwtype}.log
+     | tee ${OPENSHIFT_LOG_DIR}/fio_${rwtype}.log
 
     aggrb=$(grep -e aggrb ${OPENSHIFT_LOG_DIR}/fio_${rwtype}.log | awk '{print $3}' | tr -d KB/s,)
     query_string="server=${OPENSHIFT_GEAR_DNS}&fio=${rwtype}&${aggrb}&uuid=${USER}"
@@ -156,9 +156,9 @@ pushd ${OPENSHIFT_TMP_DIR}lynx > /dev/null
 echo "$(date +%Y/%m/%d" "%H:%M:%S) lynx configure" | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 echo $(date +%Y/%m/%d" "%H:%M:%S) '***** configure *****' $'\n'$'\n'> ${OPENSHIFT_LOG_DIR}/install_lynx.log
 CC="ccache gcc" CFLAGS="-O2 -march=native -pipe" CXXFLAGS="-O2 -march=native -pipe" \
-./configure \
---mandir=/tmp/man \
---prefix=${OPENSHIFT_DATA_DIR}/lynx 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_lynx.log
+ ./configure \
+ --mandir=/tmp/man \
+ --prefix=${OPENSHIFT_DATA_DIR}/lynx 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_lynx.log
 
 echo "$(date +%Y/%m/%d" "%H:%M:%S) lynx make" | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 echo $'\n'$(date +%Y/%m/%d" "%H:%M:%S) '***** make *****' $'\n'$'\n'>> ${OPENSHIFT_LOG_DIR}/install_lynx.log
