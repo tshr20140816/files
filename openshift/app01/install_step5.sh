7863e293a1574e7339fe07024d746fb6d8506a07
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-04-16 11:48:11 +0900

    Update install_step5.sh

--- openshift/app01/install_step5.sh
+++ openshift/app01/install_step5.sh
@@ -45,7 +45,7 @@ export PATH=${OPENSHIFT_DATA_DIR}/apache/bin:$PATH
 export HTTPD=${OPENSHIFT_DATA_DIR}/apache/bin/httpd
 export BINDIR=${OPENSHIFT_DATA_DIR}/apache
 
-time CFLAGS="-O2 -march=native" CXXFLAGS="-O2 -march=native" \
+time CFLAGS="-O2 -march=native -pipe -fomit-frame-pointer" CXXFLAGS="-O2 -march=native -pipe" \
 CC="ccache gcc" CXX="ccache g++" \
 ${OPENSHIFT_DATA_DIR}/.gem/bin/passenger-install-apache2-module \
 --auto \
