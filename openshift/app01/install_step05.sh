4d8fcd48bb71275783df9a638d6738699d1bc67e
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-04-16 18:43:18 +0900

    Update install_step05.sh

--- openshift/app01/install_step05.sh
+++ openshift/app01/install_step05.sh
@@ -1,46 +1,16 @@
 #!/bin/bash
 
-export TZ=JST-9
+source functions.sh
+function010 stop
+[ $? -eq 0 ] || exit
 
-set -x
-
-processor_count=$(grep -c -e processor /proc/cpuinfo)
-mfc=$(oo-cgroup-read memory.failcnt | awk '{printf "%\047d\n", $1}')
-query_string="server=${OPENSHIFT_GEAR_DNS}&part=$(basename $0 .sh)&mfc=${mfc}"
-
-wget --spider $(cat ${OPENSHIFT_DATA_DIR}/params/web_beacon_server)dummy?${query_string} > /dev/null 2>&1
-
-pushd ${OPENSHIFT_DATA_DIR}/install_check_point > /dev/null
-if [ -f $(basename "${0}").ok ]; then
-    echo $(date +%Y/%m/%d" "%H:%M:%S) Install Skip $(basename $0) | tee -a ${OPENSHIFT_LOG_DIR}/install.log
-    exit
-fi
-popd > /dev/null
-
-echo $(date +%Y/%m/%d" "%H:%M:%S) Install Start $(basename $0) \
-| tee -a ${OPENSHIFT_LOG_DIR}/install.log
-quota -s | grep -v a | awk '{print "Disk Usage : " $1,$4 " files"}' \
-| tee -a ${OPENSHIFT_LOG_DIR}/install.log
-oo-cgroup-read memory.usage_in_bytes | awk '{printf "Memory Usage : %\047d\n", $1}' \
-| tee -a ${OPENSHIFT_LOG_DIR}/install.log
-oo-cgroup-read memory.failcnt | awk '{printf "Memory Fail Count : %\047d\n", $1}' \
-| tee -a ${OPENSHIFT_LOG_DIR}/install.log
-oo-cgroup-read memory.memsw.failcnt | awk '{printf "Swap Memory Fail Count : %\047d\n", $1}' \
-| tee -a ${OPENSHIFT_LOG_DIR}/install.log
-
-# メモリが厳しいのでアプリケーションを止めて行う
-echo "stop" > ${OPENSHIFT_DATA_DIR}/install_check_point/gear_action.txt
-while :
-do
-    [ -f ${OPENSHIFT_DATA_DIR}/install_check_point/gear_action.txt ] && sleep 10s || break
-done
+# ***** passenger-install-apache2-module *****
 
 export GEM_HOME=${OPENSHIFT_DATA_DIR}.gem
 export RBENV_ROOT=${OPENSHIFT_DATA_DIR}/.rbenv
 export PATH="${OPENSHIFT_DATA_DIR}/.rbenv/bin:$PATH"
 export PATH="${OPENSHIFT_DATA_DIR}/.gem/bin:$PATH"
 eval "$(rbenv init -)" 
-# export APXS2=${OPENSHIFT_DATA_DIR}/apache/bin/apxs
 export PATH=${OPENSHIFT_DATA_DIR}/apache/bin:$PATH
 export HTTPD=${OPENSHIFT_DATA_DIR}/apache/bin/httpd
 export BINDIR=${OPENSHIFT_DATA_DIR}/apache
