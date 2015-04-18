7be98fcf6bc29e69a9622bb914cc8a72fb79e2c2
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-04-18 23:10:34 +0900

    Update and rename install_step_from_02_to_19.sh to install_step_from_02_to_20.sh

--- openshift/app01/install_step_from_02_to_20.sh
+++ openshift/app01/install_step_from_02_to_20.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-set -x
 cd ${OPENSHIFT_DATA_DIR}/github/openshift/app01
+# timeout -s HUP 1h bash install_stepNN.sh
 bash install_step02.sh
 bash install_step03.sh
 bash install_step04.sh
 bash install_step05.sh
 bash install_step06.sh
-# timeout -s HUP 1h bash install_step07.sh
 bash install_step07.sh
 bash install_step08.sh
 bash install_step09.sh
@@ -14,10 +13,10 @@ bash install_step10.sh
 bash install_step11.sh
 bash install_step12.sh
 bash install_step13.sh
-# timeout -s HUP 1h bash install_step14.sh
 bash install_step14.sh
 bash install_step15.sh
 bash install_step16.sh
 bash install_step17.sh
 bash install_step18.sh
 bash install_step19.sh
+bash install_step20.sh
