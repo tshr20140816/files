4588171c67673988acf75cdf4bcc9df547609725
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-04-18 21:54:28 +0900

    Update install_apache.sh

--- openshift/app04/install_apache.sh
+++ openshift/app04/install_apache.sh
@@ -9,3 +9,10 @@ tar jxf httpd-2.2.29.tar.bz2
 cd httpd-2.2.29
 
 ./configure --config-cache 
+
+# CC="ccache gcc" CFLAGS="-O2 -march=native -pipe -fomit-frame-pointer -s" CXXFLAGS="-O2 -march=native -pipe" \
+#  ./configure \
+#  --prefix=${OPENSHIFT_DATA_DIR}/apache \
+#  --mandir=/tmp/man \
+#  --docdir=/tmp/doc \
+#  --enable-mods-shared='all proxy'
