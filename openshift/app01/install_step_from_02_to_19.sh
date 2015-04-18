5c3e7e66a79489d2532dc38e885d809105af667a
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-04-18 11:40:36 +0900

    Update install_step_from_02_to_19.sh

--- openshift/app01/install_step_from_02_to_19.sh
+++ openshift/app01/install_step_from_02_to_19.sh
@@ -6,7 +6,6 @@ bash install_step03.sh
 bash install_step04.sh
 bash install_step05.sh
 bash install_step06.sh
-bash install_step07.sh
 # timeout -s HUP 1h bash install_step07.sh
 bash install_step07.sh
 bash install_step08.sh
@@ -15,7 +14,6 @@ bash install_step10.sh
 bash install_step11.sh
 bash install_step12.sh
 bash install_step13.sh
-bash install_step14.sh
 # timeout -s HUP 1h bash install_step14.sh
 bash install_step14.sh
 bash install_step15.sh
