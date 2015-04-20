005aed7baddf805b85df35c36d4be44dd5cb56aa
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-04-20 15:05:59 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -4,7 +4,10 @@ echo "$(date)" >> ${OPENSHIFT_LOG_DIR}/test.log
 
 cd /tmp
 
+rm -f pbzip2-1.1.12.tar.gz
 wget https://launchpad.net/pbzip2/1.1/1.1.12/+download/pbzip2-1.1.12.tar.gz >> ${OPENSHIFT_LOG_DIR}/test.log
+
+tar xvz pbzip2-1.1.12.tar.gz
 
 cd pbzip2-1.1.12
 
