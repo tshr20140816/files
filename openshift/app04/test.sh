c731b86551159316fa72d5e3b5c91d0c60a5f686
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-04-20 16:30:57 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -21,11 +21,17 @@ rm -f ccache.*.bz2
 # time tar cf ccache.tar.bz2 --use-compress-program=/tmp/pbzip2-1.1.12/pbzip2 ccache 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/test.log
 # time tar -c ccache | /tmp/pbzip2-1.1.12/pbzip2 --best -m200 -p4 -z > ccache9_200.tar.bz2 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/test.log
 
-wget http://downloads.sourceforge.net/project/pixz/pixz-1.0.2.tgz
-tar xfz pixz-1.0.2.tgz
-cd pixz-1.0.2
-time make -j4 >> ${OPENSHIFT_LOG_DIR}/test.log 2>&1
+wget http://tukaani.org/xz/xz-5.2.1.tar.gz
+tar xfz xz-5.2.1.tar.gz
+cd xz-5.2.1
 ls -lang >> ${OPENSHIFT_LOG_DIR}/test.log 2>&1
+
+rm -rf pixz-1.0.2
+# wget http://downloads.sourceforge.net/project/pixz/pixz-1.0.2.tgz
+# tar xfz pixz-1.0.2.tgz
+# cd pixz-1.0.2
+# time make -j4 >> ${OPENSHIFT_LOG_DIR}/test.log 2>&1
+# ls -lang >> ${OPENSHIFT_LOG_DIR}/test.log 2>&1
 
 cd /tmp
 ls -lang >> ${OPENSHIFT_LOG_DIR}/test.log 2>&1
