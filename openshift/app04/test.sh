382cd7dbd9ca4246ccdbeedc4a059bd914de06fb
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-04-20 15:43:14 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -12,9 +12,12 @@ cd /tmp
 # time make -j4 >> ${OPENSHIFT_LOG_DIR}/test.log 2>&1
 # ls -lang >> ${OPENSHIFT_LOG_DIR}/test.log 2>&1
 
-rm -f ccache.tar.xz
-wget https://files3-20150207.rhcloud.com/files/ccache.tar.xz >> ${OPENSHIFT_LOG_DIR}/test.log 2>&1
+# rm -f ccache.tar.xz
+# wget https://files3-20150207.rhcloud.com/files/ccache.tar.xz >> ${OPENSHIFT_LOG_DIR}/test.log 2>&1
 
-tar Jxf ccache.tar.xz
+# tar Jxf ccache.tar.xz
 
-ls -lang >> ${OPENSHIFT_LOG_DIR}/test.log 2>&1
+# ls -lang >> ${OPENSHIFT_LOG_DIR}/test.log 2>&1
+
+tar cf ccache.tar.bz2 --use-compress-program=/tmp/pbzip2 ccache
+
