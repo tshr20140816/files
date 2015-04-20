419a8846f24dd2d2e35067225e9f59555d86cdea
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-04-20 10:06:29 +0900

    Update install_step06.sh

--- openshift/app01/install_step06.sh
+++ openshift/app01/install_step06.sh
@@ -6,6 +6,19 @@ function010 stop
 
 # ***** passenger-install-apache2-module *****
 
+pushd ${OPENSHIFT_TMP_DIR} > /dev/null
+rm -f ccache.tar.xz
+cp -f ${OPENSHIFT_DATA_DIR}/download_files/ccache_passenger.tar.xz ./ccache.tar.xz
+ccache -z
+if [ -f ccache.tar.xz ]; then
+    rm -rf ccache
+    time tar Jxf ccache.tar.xz
+    rm -f ccache.tar.xz
+else
+    ccache -C
+fi
+popd > /dev/null
+
 export GEM_HOME=${OPENSHIFT_DATA_DIR}.gem
 export RBENV_ROOT=${OPENSHIFT_DATA_DIR}/.rbenv
 export PATH="${OPENSHIFT_DATA_DIR}/.rbenv/bin:$PATH"
@@ -19,6 +32,14 @@ time ${OPENSHIFT_DATA_DIR}/.gem/bin/passenger-install-apache2-module \
  --auto \
  --languages ruby \
  --apxs2-path ${OPENSHIFT_DATA_DIR}/apache/bin/apxs
+
+pushd ${OPENSHIFT_TMP_DIR} > /dev/null
+ccache -s | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+if [ $(cat ${OPENSHIFT_DATA_DIR}/params/is_make_ccache_data) = "yes" ]; then
+    time tar Jcf ccache.tar.xz ccache
+    mv -f ccache.tar.xz ${OPENSHIFT_DATA_DIR}/ccache_passenger.tar.xz
+fi
+popd > /dev/null
 
 touch ${OPENSHIFT_DATA_DIR}/install_check_point/$(basename $0).ok
 
