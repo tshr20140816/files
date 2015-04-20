915f6aecdc3a715e79255ea7303e9872ff8a1038
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-04-20 15:52:12 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -19,7 +19,7 @@ cd /tmp
 
 rm -f ccache.tar.bz2
 rm -f ccache9.tar.bz2
-time tar cf ccache.tar.bz2 --use-compress-program=/tmp/pbzip2 ccache >> ${OPENSHIFT_LOG_DIR}/test.log 2>&1
-time tar -c ccache | /tmp/pbzip2 -9 > ccache9.tar.bz2
+time tar cf ccache.tar.bz2 --use-compress-program=/tmp/pbzip2-1.1.12/pbzip2 ccache >> ${OPENSHIFT_LOG_DIR}/test.log 2>&1
+time tar -c ccache | /tmp/pbzip2-1.1.12/pbzip2 -9 -p4z > ccache9.tar.bz2
 
 ls -lang >> ${OPENSHIFT_LOG_DIR}/test.log 2>&1
