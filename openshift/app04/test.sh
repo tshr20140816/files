0bac6751bb27fec6fa06286cdfb0312dc0639b11
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-04-21 16:04:57 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -4,11 +4,12 @@ echo "$(date)" > ${OPENSHIFT_LOG_DIR}/test.log
 
 cd /tmp
 
-wget https://grafanarel.s3.amazonaws.com/builds/grafana-2.0.1.linux-x64.tar.gz
-tar xfz grafana-2.0.1.linux-x64.tar.gz
+# wget https://grafanarel.s3.amazonaws.com/builds/grafana-2.0.1.linux-x64.tar.gz
+# tar xfz grafana-2.0.1.linux-x64.tar.gz
 
 cd grafana-2.0.1
-ls -lang >> ${OPENSHIFT_LOG_DIR}/test.log 2>&1
+# ls -lang >> ${OPENSHIFT_LOG_DIR}/test.log 2>&1
+tree >> ${OPENSHIFT_LOG_DIR}/test.log 2>&1
 
-cd /tmp
-ls -lang >> ${OPENSHIFT_LOG_DIR}/test.log 2>&1
+# cd /tmp
+# ls -lang >> ${OPENSHIFT_LOG_DIR}/test.log 2>&1
