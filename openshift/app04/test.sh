2adb3ed8e94bc31c340a95480b2ed8ed530c6f0e
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-04-23 08:41:51 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -29,51 +29,9 @@ export CCACHE_BASEDIR=${OPENSHIFT_HOME_DIR}
 export CFLAGS="-O2 -march=native -pipe -fomit-frame-pointer -s"
 export CXXFLAGS="${CFLAGS}"
 
-ps auwx | grep ccache
-ccache -s
+git init
+git remote add origin https://github.com/tshr20140816/files.git
+git pull origin master
 
-# exit
+ls -lang
 
-# ccache -C
-ccache -z
-
-cd /tmp
-if [ ! -f php-5.6.8.tar.xz ]; then
-    wget https://files3-20150207.rhcloud.com/files/php-5.6.8.tar.xz
-fi
-rm -rf php-5.6.8
-tar Jxf php-5.6.8.tar.xz
-
-echo "$(date)" >> ${OPENSHIFT_LOG_DIR}/test.log
-cd php-5.6.8
-./configure \
---prefix=${OPENSHIFT_DATA_DIR}/php \
---mandir=${OPENSHIFT_TMP_DIR}/man \
---docdir=${OPENSHIFT_TMP_DIR}/doc \
---with-mysql \
---with-pdo-mysql \
---without-sqlite3 \
---without-pdo-sqlite \
---with-curl \
---with-libdir=lib64 \
---with-bz2 \
---with-iconv \
---with-openssl \
---with-zlib \
---with-gd \
---enable-exif \
---enable-ftp \
---enable-xml \
---enable-mbstring \
---enable-mbregex \
---enable-sockets \
---disable-ipv6 \
---with-gettext=${OPENSHIFT_DATA_DIR}/php \
-CONFIG_SITE=${OPENSHIFT_LOG_DIR}/php_config_cache
-# --cache-file=${OPENSHIFT_LOG_DIR}/php_config_cache
-echo "$(date)" >> ${OPENSHIFT_LOG_DIR}/test.log
-
-# nohup time make -j2 >> test2.log 2>&1 &
-# echo "$(date)" > ${OPENSHIFT_LOG_DIR}/test.log
-
-# ccache -s >> test.log
