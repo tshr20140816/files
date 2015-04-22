ce3cd0721a954c5cdb578b5ee7d2d4f8a5b913e5
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-04-22 09:00:56 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -4,15 +4,61 @@ echo "$(date)" > ${OPENSHIFT_LOG_DIR}/test.log
 
 cd /tmp
 
-# wget https://grafanarel.s3.amazonaws.com/builds/grafana-2.0.1.linux-x64.tar.gz
-# tar xfz grafana-2.0.1.linux-x64.tar.gz
 
-cd grafana-2.0.1
-# ls -lang >> ${OPENSHIFT_LOG_DIR}/test.log 2>&1
-# tree >> ${OPENSHIFT_LOG_DIR}/test.log 2>&1
-cat conf/defaults.ini >> ${OPENSHIFT_LOG_DIR}/test.log
-echo "-----" >> ${OPENSHIFT_LOG_DIR}/test.log
-cat conf/sample.ini >> ${OPENSHIFT_LOG_DIR}/test.log
+cd /tmp
+if [ ! -e ${OPENSHIFT_DATA_DIR}/ccache ]; then
+    if [ ! -f ccache-3.2.1.tar.xz ]; then
+        wget https://files3-20150207.rhcloud.com/files/ccache-3.2.1.tar.xz
+    fi
+    tar xfz ccache-3.2.1.tar.xz
+    cd ccache-3.2.1
+    CFLAGS="-O2 -march=native -pipe -fomit-frame-pointer -s" CXXFLAGS="-O2 -march=native -pipe" \
+     ./configure --prefix=${OPENSHIFT_DATA_DIR}/ccache --mandir=/tmp/man --docdir=/tmp/doc
+    make
+    make install
+fi
 
-# cd /tmp
-# ls -lang >> ${OPENSHIFT_LOG_DIR}/test.log 2>&1
+export PATH="${OPENSHIFT_DATA_DIR}/ccache/bin:$PATH"
+export CC="ccache gcc"
+export CXX="ccache g++"
+export CCACHE_DIR=${OPENSHIFT_TMP_DIR}/ccache
+export CCACHE_TEMPDIR=${OPENSHIFT_TMP_DIR}/tmp_ccache
+export CCACHE_LOGFILE=${OPENSHIFT_LOG_DIR}/ccache.log
+export CCACHE_MAXSIZE=300M
+export CCACHE_BASEDIR=${OPENSHIFT_HOME_DIR}
+
+cd /tmp
+if [ ! -f php-5.6.8.tar.xz ]; then
+    wget https://files3-20150207.rhcloud.com/files/php-5.6.8.tar.xz
+fi
+rm -rf php-5.6.8
+tar xfz php-5.6.8.tar.xz
+
+cd php-5.6.8
+./configure \
+--prefix=${OPENSHIFT_DATA_DIR}/php \
+--mandir=${OPENSHIFT_TMP_DIR}/man \
+--docdir=${OPENSHIFT_TMP_DIR}/doc \
+--with-mysql \
+--with-pdo-mysql \
+--without-sqlite3 \
+--without-pdo-sqlite \
+--with-curl \
+--with-libdir=lib64 \
+--with-bz2 \
+--with-iconv \
+--with-openssl \
+--with-zlib \
+--with-gd \
+--enable-exif \
+--enable-ftp \
+--enable-xml \
+--enable-mbstring \
+--enable-mbregex \
+--enable-sockets \
+--disable-ipv6 \
+--with-gettext=${OPENSHIFT_DATA_DIR}/php
+
+(time make) >> test.log 2>&1
+
+ccache -s >> test.log
