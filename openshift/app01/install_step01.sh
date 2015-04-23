6d7d40edd85a2cca047a9668c9f6f02b5c7a5e9e
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-04-23 10:43:08 +0900

    Update install_step01.sh

--- openshift/app01/install_step01.sh
+++ openshift/app01/install_step01.sh
@@ -192,13 +192,13 @@ echo "keyserver hkp://keyserver.ubuntu.com:80" >> ${GNUPGHOME}/gpg.conf
 
 if [ "${mirror_server}" != "none" ]; then
     # cache apache
-    wget -t1 ${mirror_server}/ccache_apache.tar.xz
-    wget -t1 ${mirror_server}/ccache_php.tar.xz
-    wget -t1 ${mirror_server}/ccache_libmemcached.tar.xz
-    wget -t1 ${mirror_server}/ccache_delegate.tar.xz
-    wget -t1 ${mirror_server}/ccache_ruby.tar.xz
-    wget -t1 ${mirror_server}/ccache_tcl.tar.xz
-    wget -t1 ${mirror_server}/ccache_passenger.tar.xz
+    wget -t1 ${mirror_server}/ccache_apache.tar.xz &
+    wget -t1 ${mirror_server}/ccache_php.tar.xz &
+    wget -t1 ${mirror_server}/ccache_libmemcached.tar.xz &
+    wget -t1 ${mirror_server}/ccache_delegate.tar.xz &
+    wget -t1 ${mirror_server}/ccache_ruby.tar.xz &
+    wget -t1 ${mirror_server}/ccache_tcl.tar.xz &
+    wget -t1 ${mirror_server}/ccache_passenger.tar.xz &
 
     # apache
     wget -t1 ${mirror_server}/httpd-${apache_version}.tar.bz2
