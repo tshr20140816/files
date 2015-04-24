9511d2901d8abc0a4696753eb297e40b83e540b6
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-04-24 15:11:03 +0900

    Update build_pack.sh

--- openshift/app04/build_pack.sh
+++ openshift/app04/build_pack.sh
@@ -10,6 +10,28 @@ set -x
 export target_data_dir='xxx' # ${OPENSHIFT_DATA_DIR}
 export target_tmp_dir='xxx' # ${OPENSHIFT_TMP_DIR}
 
+cd /tmp
+if [ ! -e ${OPENSHIFT_DATA_DIR}/ccache ]; then
+    if [ ! -f ccache-3.2.1.tar.xz ]; then
+        wget https://files3-20150207.rhcloud.com/files/ccache-3.2.1.tar.xz
+    fi
+    tar Jxf ccache-3.2.1.tar.xz
+    cd ccache-3.2.1
+    CFLAGS="-O2 -march=native -pipe -fomit-frame-pointer -s" CXXFLAGS="-O2 -march=native -pipe" \
+     ./configure --prefix=${OPENSHIFT_DATA_DIR}/ccache --mandir=/tmp/man --docdir=/tmp/doc
+    make -j$(grep -c -e processor /proc/cpuinfo)
+    make install
+fi
+
+export PATH="${OPENSHIFT_DATA_DIR}/ccache/bin:$PATH"
+export CC="ccache gcc"
+export CXX="ccache g++"
+export CCACHE_DIR=${OPENSHIFT_TMP_DIR}/ccache
+export CCACHE_TEMPDIR=${OPENSHIFT_TMP_DIR}/tmp_ccache
+export CCACHE_LOGFILE=${OPENSHIFT_LOG_DIR}/ccache.log
+export CCACHE_MAXSIZE=300M
+export CCACHE_BASEDIR=${OPENSHIFT_HOME_DIR}
+
 export CFLAGS="-O2 -march=native -pipe -fomit-frame-pointer -s"
 export CXXFLAGS="${CFLAGS}"
 
