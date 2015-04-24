b3cf91ce99f8ab9ea276e62a04962cd9311ad117
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-04-24 14:27:12 +0900

    Update and rename install_apache.sh to build_pack.sh

--- openshift/app04/build_pack.sh
+++ openshift/app04/build_pack.sh
@@ -3,9 +3,12 @@
 set -x
 
 export target_data_dir='xxx' # ${OPENSHIFT_DATA_DIR}
+export target_tmp_dir='xxx' # ${OPENSHIFT_TMP_DIR}
 
 export CFLAGS="-O2 -march=native -pipe -fomit-frame-pointer -s"
 export CXXFLAGS="${CFLAGS}"
+
+# apache
 
 cd /tmp
 
@@ -15,8 +18,8 @@ cd httpd-2.2.29
 
 ./configure \
  --prefix=${target_data_dir}/apache \
- --mandir=${OPENSHIFT_TMP_DIR}/man \
- --docdir=${OPENSHIFT_TMP_DIR}/doc \
+ --mandir=${target_tmp_dir}/man \
+ --docdir=${target_tmp_dir}/doc \
  --enable-mods-shared='all proxy'
 
 time make -j$(grep -c -e processor /proc/cpuinfo)
@@ -25,17 +28,35 @@ cd ..
 tar Jcf httpd.tar.xz httpd-2.2.29
 rm -rf httpd-2.2.29
 
+# libmemcached
+
+cd /tmp
+
 wget https://launchpad.net/libmemcached/1.0/1.0.18/+download/libmemcached-1.0.18.tar.gz
 tar zxf libmemcached-1.0.18.tar.gz
 cd libmemcached-1.0.18
 
 ./configure \
  --prefix=${target_data_dir}/libmemcached \
- --mandir=${OPENSHIFT_TMP_DIR}/man \
- --docdir=${OPENSHIFT_TMP_DIR}/doc
+ --mandir=${target_tmp_dir}/man \
+ --docdir=${target_tmp_dir}/doc
 
 time make -j2 -l3
 
 cd ..
 tar Jcf libmemcached.tar.xz libmemcached-1.0.18
 rm -rf libmemcached-1.0.18
+
+# delegate
+
+cd /tmp
+
+wget http://www.delegate.org/anonftp/DeleGate/delegate9.9.13.tar.gz
+tar zxf delegate9.9.13.tar.gz
+
+cd delegate9.9.13.tar.gz
+time make -j$(grep -c -e processor /proc/cpuinfo)
+
+cd ..
+tar Jcf delegate.tar.xz delegate9.9.13
+rm -rf delegate9.9.13
