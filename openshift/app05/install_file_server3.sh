ba33adfe6709f89c8a6cd1129163b8499ba726e6
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-04-25 10:54:44 +0900

    Update install_file_server3.sh

--- openshift/app05/install_file_server3.sh
+++ openshift/app05/install_file_server3.sh
@@ -77,47 +77,7 @@ sed -i -e "s|__BUILD_PASSWORD__|${build_password}|g" build_action.php
 popd > /dev/null
 
 pushd  ${OPENSHIFT_DATA_DIR} > /dev/null
-cat << '__HEREDOC__' > build_action.sh
-#!/bin/bash
-
-if [ $# -ne 2 ]; then
-    exit
-fi
-
-export data_dir=${1}
-export tmp_dir=${2}
-
-if [ ! -f ${OPENSHIFT_DATA_DIR}/version_list ]; then
-    exit
-fi
-
-while read LINE
-do
-    product=$(echo "${LINE}" | awk '{print $1}')
-    version=$(echo "${LINE}" | awk '{print $2}')
-    eval "${product}"="${version}"
-done < ${OPENSHIFT_DATA_DIR}/version_list
-
-export PATH="${OPENSHIFT_DATA_DIR}/ccache/bin:$PATH"
-export CC="ccache gcc"
-export CXX="ccache g++"
-export CCACHE_DIR=${OPENSHIFT_TMP_DIR}/ccache
-export CCACHE_TEMPDIR=${OPENSHIFT_TMP_DIR}/tmp_ccache
-export CCACHE_LOGFILE=${OPENSHIFT_LOG_DIR}/ccache.log
-export CCACHE_MAXSIZE=300M
-
-export CFLAGS="-O2 -march=native -pipe -fomit-frame-pointer -s"
-export CXXFLAGS="${CFLAGS}"
-
-pushd ${OPENSHIFT_TMP_DIR} > /dev/null
-
-rm -rf httpd-${apache_version}
-rm -f httpd-${apache_version}.tar.bz2
-
-tar jxf httpd-${apache_version}.tar.bz2
-
-popd > /dev/null
-__HEREDOC__
+wget https://raw.githubusercontent.com/tshr20140816/files/master/openshift/app05/build_action.sh
 popd > /dev/null
 
 # ***** cron minutely *****
