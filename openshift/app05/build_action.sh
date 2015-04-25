3e18f87d6a6356b11476b9d65d4ed884ef76bb60
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-04-25 10:29:16 +0900

    Update build_action.sh

--- openshift/app05/build_action.sh
+++ openshift/app05/build_action.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
+if [ $# -ne 3 ]; then
     exit
 fi
 
-export data_dir=${1}
-export tmp_dir=${2}
+export host_name=${1}
+export data_dir=${2}
+export tmp_dir=${3}
 
 if [ ! -f ${OPENSHIFT_DATA_DIR}/version_list ]; then
     exit
 fi
+
+rm -f ${OPENSHIFT_DATA_DIR}/files/${host_name}_maked_*
 
 while read LINE
 do
@@ -29,11 +32,30 @@ export CCACHE_MAXSIZE=300M
 export CFLAGS="-O2 -march=native -pipe -fomit-frame-pointer -s"
 export CXXFLAGS="${CFLAGS}"
 
+# ***** apache *****
+
 pushd ${OPENSHIFT_TMP_DIR} > /dev/null
 
 rm -rf httpd-${apache_version}
 rm -f httpd-${apache_version}.tar.bz2
 
+cp ${OPENSHIFT_DATA_DIR}/files/httpd-${apache_version}.tar.bz2 ./
+if [ ! -f httpd-${apache_version}.tar.bz2 ]; then
+    wget http://ftp.riken.jp/net/apache//httpd/httpd-${apache_version}.tar.bz2
+fi
 tar jxf httpd-${apache_version}.tar.bz2
+pushd httpd-${apache_version} > /dev/null
+./configure \
+ --prefix=${data_dir}/apache \
+ --mandir=${tmp_dir}/man \
+ --docdir=${tmp_dir}/doc \
+ --enable-mods-shared='all proxy'
+
+time make -j$(grep -c -e processor /proc/cpuinfo)
+popd > /dev/null
+rm -rf maked_httpd-${apache_version}.tar.xz
+time tar Jcf ${host_name}_maked_httpd-${apache_version}.tar.xz httpd-${apache_version}
+mv -f ${host_name}_maked_httpd-${apache_version}.tar.xz ${OPENSHIFT_DATA_DIR}/files/
+rm -rf httpd-${apache_version}
 
 popd > /dev/null
