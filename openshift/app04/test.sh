2d18ab8303ebc18efab37baf00441edb79a95e67
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-04-28 13:24:31 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -31,37 +31,15 @@ export CCACHE_BASEDIR=${OPENSHIFT_HOME_DIR}
 export CFLAGS="-O2 -march=native -pipe -fomit-frame-pointer -s"
 export CXXFLAGS="${CFLAGS}"
 
-ls -lang /tmp
-
-cd /tmp
-
 ccache -z
 
 ls -lang /tmp
 
-export target_data_dir=/var/lib/openshift/553a0abbfcf9339532000021/app-root/data/
-export target_tmp_dir=/tmp/
+cd /tmp
 
-rm -rf httpd-2.2.29
-rm -f httpd-2.2.29.tar.bz2
+wget http://s3.amazonaws.com/phusion-passenger/releases/passenger-5.0.6.tar.gz
+mkdir opt/passenger
+cd opt/passenger
+tar xfz /tmp/passenger-5.0.6.tar.gz
 
-wget http://ftp.riken.jp/net/apache//httpd/httpd-2.2.29.tar.bz2
-tar jxf httpd-2.2.29.tar.bz2
-cd httpd-2.2.29
-
-./configure \
- --prefix=${target_data_dir}/apache \
- --mandir=${target_tmp_dir}/man \
- --docdir=${target_tmp_dir}/doc \
- --enable-mods-shared='all proxy'
-
-time make -j$(grep -c -e processor /proc/cpuinfo)
-
-cd ..
-rm -rf maked_httpd-2.2.29.tar.xz
-time tar Jcf maked_httpd-2.2.29.tar.xz httpd-2.2.29
-rm -rf httpd-2.2.29
-
-ccache -s
-
-ls -lang /tmp
+ls -lang /opt/passenger
