4d6aae87b86b3a1b93a63c36a52c0338e52cc0b2
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-05-02 02:51:57 +0900

    Update install_step07.sh

--- openshift/app01/install_step07.sh
+++ openshift/app01/install_step07.sh
@@ -106,7 +106,7 @@ echo $'\n'$(date +%Y/%m/%d" "%H:%M:%S) '***** make *****' $'\n'$'\n'>> ${OPENSHI
 # j2 is limit (-l3 --load-average=3)
 # time make -j2 -l3 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_php.log
 (
-trap 'touch /tmp/trap_php_make.txt' 0 
+trap 'touch /tmp/trap_php_make.txt' 0 1 2 3 15 
 time make 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_php.log
 )
 # echo "$(date +%Y/%m/%d" "%H:%M:%S) php make test" | tee -a ${OPENSHIFT_LOG_DIR}/install.log
@@ -115,7 +115,7 @@ time make 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_php.log
 echo "$(date +%Y/%m/%d" "%H:%M:%S) php make install" | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 echo $'\n'$(date +%Y/%m/%d" "%H:%M:%S) '***** make install *****' $'\n'$'\n'>> ${OPENSHIFT_LOG_DIR}/install_php.log
 (
-trap 'touch /tmp/trap_php_make_install.txt' 0 
+trap 'touch /tmp/trap_php_make_install.txt' 0 1 2 3 15 
 make install 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_php.log
 )
 echo "$(date +%Y/%m/%d" "%H:%M:%S) php make conf" | tee -a ${OPENSHIFT_LOG_DIR}/install.log
