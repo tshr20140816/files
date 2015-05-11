b8a2e3e46a943d3ede8d41fe59ede8fef2c0a171
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-05-11 10:42:42 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -81,10 +81,13 @@ wget http://ftp.jaist.ac.jp/pub/OpenBSD/OpenSSH/portable/openssh-${openssh_versi
 tar xfz openssh-${openssh_version}.tar.gz
 popd > /dev/null
 export CC=distcc
+env_home_backup=${HOME}
+export HOME=${OPENSHIFT_DATA_DIR}
 pushd ${OPENSHIFT_TMP_DIR}/openssh-${openssh_version} > /dev/null
 ./configure --prefix=${OPENSHIFT_DATA_DIR}/openssh > /dev/null 2>&1
 time make -j$(grep -c -e processor /proc/cpuinfo) 2>&1
 # make install
 popd > /dev/null
+export HOME=${env_home_backup}
 
 ls ${OPENSHIFT_DATA_DIR}/openssh
