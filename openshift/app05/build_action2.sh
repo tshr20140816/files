5a657e941906f8b95d567e71b5d7a734de4bb18c
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-05-14 10:02:52 +0900

    Update build_action2.sh

--- openshift/app05/build_action2.sh
+++ openshift/app05/build_action2.sh
@@ -47,6 +47,40 @@ export GEM_HOME=${OPENSHIFT_DATA_DIR}/.gem
 
 ccache -z
 
+# ***** tcl *****
+
+echo "$(date +%Y/%m/%d" "%H:%M:%S) tcl"
+
+pushd ${OPENSHIFT_TMP_DIR} > /dev/null
+
+rm -rf tcl${tcl_version}
+rm -f tcl${tcl_version}-src.tar.gz
+
+cp ${OPENSHIFT_DATA_DIR}/files/tcl${tcl_version}-src.tar.gz ./
+if [ ! -f tcl${tcl_version}-src.tar.gz ]; then
+    wget http://prdownloads.sourceforge.net/tcl/tcl${tcl_version}-src.tar.gz
+fi
+tar xfz tcl${tcl_version}-src.tar.gz
+
+pushd ${OPENSHIFT_TMP_DIR}/tcl${tcl_version}/unix > /dev/null
+./configure \
+ --mandir=${tmp_dir}/man \
+ --disable-symbols \
+ --prefix=${data_dir}/tcl
+
+time make -j2 -l3
+popd > /dev/null
+ccache -s
+rm -f ${app_uuid}_maked_tcl${tcl_version}-src.tar.xz
+time tar Jcf ${app_uuid}_maked_tcl${tcl_version}-src.tar.xz tcl${tcl_version}
+mv -f ${app_uuid}_maked_tcl${tcl_version}-src.tar.xz ${OPENSHIFT_DATA_DIR}/files/
+rm -rf tcl${tcl_version}
+rm -f tcl${tcl_version}-src.tar.gz
+popd > /dev/null
+
+memory_fail_count=$(oo-cgroup-read memory.failcnt | awk '{printf "Memory Fail Count : %\047d\n", $1}')
+echo "$(date +%Y/%m/%d" "%H:%M:%S) Memory Fail Count : ${memory_fail_count}"
+
 # ***** apache *****
 
 echo "$(date +%Y/%m/%d" "%H:%M:%S) apache"
@@ -157,40 +191,6 @@ mv -f ${app_uuid}_maked_ruby_${ruby_version}_rbenv.tar.xz ${OPENSHIFT_DATA_DIR}/
 popd > /dev/null
 
 rm -rf ${OPENSHIFT_DATA_DIR}.rbenv
-
-# ***** tcl *****
-
-echo "$(date +%Y/%m/%d" "%H:%M:%S) tcl"
-
-pushd ${OPENSHIFT_TMP_DIR} > /dev/null
-
-rm -rf tcl${tcl_version}
-rm -f tcl${tcl_version}-src.tar.gz
-
-cp ${OPENSHIFT_DATA_DIR}/files/tcl${tcl_version}-src.tar.gz ./
-if [ ! -f tcl${tcl_version}-src.tar.gz ]; then
-    wget http://prdownloads.sourceforge.net/tcl/tcl${tcl_version}-src.tar.gz
-fi
-tar xfz tcl${tcl_version}-src.tar.gz
-
-pushd ${OPENSHIFT_TMP_DIR}/tcl${tcl_version}/unix > /dev/null
-./configure \
- --mandir=${tmp_dir}/man \
- --disable-symbols \
- --prefix=${data_dir}/tcl
-
-time make -j2 -l3
-popd > /dev/null
-ccache -s
-rm -f ${app_uuid}_maked_tcl${tcl_version}-src.tar.xz
-time tar Jcf ${app_uuid}_maked_tcl${tcl_version}-src.tar.xz tcl${tcl_version}
-mv -f ${app_uuid}_maked_tcl${tcl_version}-src.tar.xz ${OPENSHIFT_DATA_DIR}/files/
-rm -rf tcl${tcl_version}
-rm -f tcl${tcl_version}-src.tar.gz
-popd > /dev/null
-
-memory_fail_count=$(oo-cgroup-read memory.failcnt | awk '{printf "Memory Fail Count : %\047d\n", $1}')
-echo "$(date +%Y/%m/%d" "%H:%M:%S) Memory Fail Count : ${memory_fail_count}"
 
 # ***** delegate *****
 
