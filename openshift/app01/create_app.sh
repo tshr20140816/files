c876a8250ef352266142d682cd4b3d772b88575f
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-05-15 14:45:14 +0900

    Update create_app.sh

--- openshift/app01/create_app.sh
+++ openshift/app01/create_app.sh
@@ -13,6 +13,8 @@ do
     case "${type}" in
         "diy" ) yes | rhc app create ${server} diy-0.1 mysql-5.5 cron-1.4 phpmyadmin-4 --server openshift.redhat.com
         ;;
+        "diy2" ) yes | rhc app create ${server} diy-0.1 cron-1.4 --server openshift.redhat.com
+        ;;
         "php" ) yes | rhc app create ${server} php-5.4 cron-1.4 --server openshift.redhat.com
         ;;
         "ruby" ) yes | rhc app create ${server} ruby-2.0 cron-1.4 --server openshift.redhat.com
