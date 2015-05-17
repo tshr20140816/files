6098c621113b000966fb3d45b6d79c4776922eca
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-05-18 08:58:54 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -35,6 +35,6 @@ cd /tmp
 
 printenv
 gem help install
-gem install rhc --no-rdoc --no-ri --with-cflags=\"-O2 -pipe -march=native -fomit-frame-pointer -s\"
+gem install commander -v 4.2.1 --no-rdoc --no-ri --with-cflags=\"-O2 -pipe -march=native -fomit-frame-pointer -s\"
 
 # dummy
