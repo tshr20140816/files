d92039e58156772b43a21fb04590225be8aeeb92
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-05-17 23:28:27 +0900

    Update install_step04.sh

--- openshift/app01/install_step04.sh
+++ openshift/app01/install_step04.sh
@@ -135,14 +135,6 @@ LogLevel QUIET
 __HEREDOC__
 popd > /dev/null
 
-# ***** rhc *****
-
-export GEM_HOME=${OPENSHIFT_DATA_DIR}/.gem
-export PATH="${OPENSHIFT_DATA_DIR}/.gem/bin:$PATH"
-gem environment
-gem install commander -v 4.2.1 --no-rdoc --no-ri --with-cflags=\"-O2 -pipe -march=native -fomit-frame-pointer -s\"
-gem install rhc --no-rdoc --no-ri --with-cflags=\"-O2 -pipe -march=native -fomit-frame-pointer -s\"
-
 touch ${OPENSHIFT_DATA_DIR}/install_check_point/$(basename "${0}").ok
 
 echo "$(date +%Y/%m/%d" "%H:%M:%S) Install Finish $(basename "${0}")" | tee -a ${OPENSHIFT_LOG_DIR}/install.log
