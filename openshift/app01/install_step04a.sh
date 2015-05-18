ba28f2bcd0301464753007c7d81ca49c01b37f33
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-05-18 18:40:58 +0900

    Update install_step04a.sh

--- openshift/app01/install_step04a.sh
+++ openshift/app01/install_step04a.sh
@@ -174,7 +174,7 @@ while read LINE
 do
     user_fqdn=$(echo "${LINE}")
     ssh -V
-    ssh -fMNv ${user_fqdn} 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+    ssh -fMNvvv ${user_fqdn} 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install.log
     user_string=$(echo "${LINE}" | awk -F@ '{print $1}')
     distcc_hosts_string="${distcc_hosts_string} ${user_fqdn}/2:/var/lib/openshift/${user_string}/app-root/data/distcc/bin/distccd_start"
     # distcc_hosts_string="${distcc_hosts_string} ${user_fqdn}/2:/var/lib/openshift/${user_string}/app-root/data/distcc/bin/distccd_start,lzo"
