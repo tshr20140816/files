a43828ac301030f8aa5aabcd30d91f761a19cdd9
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-05-21 20:32:30 +0900

    Update install_step04a.sh

--- openshift/app01/install_step04a.sh
+++ openshift/app01/install_step04a.sh
@@ -4,6 +4,7 @@ source functions.sh
 function010
 [ $? -eq 0 ] || exit
 
+if [ 1 -eq 0 ]; then
 # ***** Tcl *****
 
 pushd ${OPENSHIFT_TMP_DIR} > /dev/null
@@ -110,6 +111,7 @@ popd > /dev/null
 
 query_string="server=${OPENSHIFT_APP_DNS}&installed=expect"
 wget --spider "$(cat ${OPENSHIFT_DATA_DIR}/params/web_beacon_server)dummy?${query_string}" > /dev/null 2>&1
+fi
 
 # ***** rhc *****
 # ruby はデフォルトインストールのものに頼る
@@ -131,6 +133,7 @@ echo "$(date +%Y/%m/%d" "%H:%M:%S) rhc setup" | tee -a ${OPENSHIFT_LOG_DIR}/inst
 distcc_server_account=$(cat ${OPENSHIFT_DATA_DIR}/params/distcc_server_account)
 distcc_server_password=$(cat ${OPENSHIFT_DATA_DIR}/params/distcc_server_password)
 
+if [ 1 - eq 0 ]; then
 echo set timeout 60 > ${OPENSHIFT_TMP_DIR}/rhc_setup.txt
 echo spawn ${OPENSHIFT_DATA_DIR}.gem/bin/rhc setup --server openshift.redhat.com \
 --create-token -l ${distcc_server_account} -p ${distcc_server_password} >> ${OPENSHIFT_TMP_DIR}/rhc_setup.txt
@@ -180,6 +183,60 @@ rm -f user_fqdn.txt
 popd > /dev/null
 distcc_hosts_string="${distcc_hosts_string:1}"
 echo "${distcc_hosts_string}" > ${OPENSHIFT_DATA_DIR}/params/distcc_hosts.txt
+export HOME=${env_home_backup}
+fi
+
+# ***** openssh *****
+
+mkdir ${OPENSHIFT_DATA_DIR}/.ssh
+pushd ${OPENSHIFT_DATA_DIR}/.ssh > /dev/null
+ssh -V
+ssh-keygen -t rsa -f id_rsa -P ''
+cat << __HEREDOC__ > config
+Host *
+  IdentityFile __OPENSHIFT_DATA_DIR__.ssh/id_rsa
+  StrictHostKeyChecking no
+  UserKnownHostsFile /dev/null
+  LogLevel QUIET
+#  LogLevel DEBUG3
+  Protocol 2
+  ConnectionAttempts 5
+__HEREDOC__
+sed -i -e "s|__OPENSHIFT_DATA_DIR__|${OPENSHIFT_DATA_DIR}|g" config
+popd > /dev/null
+
+# ***** rhc *****
+# ruby はデフォルトインストールのものに頼る
+
+distcc_server_account=$(cat ${OPENSHIFT_DATA_DIR}/params/distcc_server_account)
+distcc_server_password=$(cat ${OPENSHIFT_DATA_DIR}/params/distcc_server_password)
+
+env_home_backup=${HOME}
+export GEM_HOME=${OPENSHIFT_DATA_DIR}/.gem
+export PATH="${OPENSHIFT_DATA_DIR}/.gem/bin:$PATH"
+export HOME=${OPENSHIFT_DATA_DIR}
+# gem --version
+# gem environment
+# gem help install
+# gem install commander -v 4.2.1 --verbose --no-rdoc --no-ri -- --with-cflags=\"-O2 -pipe -march=native -fomit-frame-pointer -s\"
+gem install rhc --verbose --no-rdoc --no-ri -- --with-cflags=\"-O2 -pipe -march=native -fomit-frame-pointer -s\"
+
+yes | rhc setup --server openshift.redhat.com --create-token -l ${distcc_server_account} -p ${distcc_server_password}
+pushd  ${OPENSHIFT_TMP_DIR} > /dev/null
+rhc apps | grep -e SSH | grep -v -e ${OPENSHIFT_APP_UUID} | awk '{print $2}' | tee user_fqdn.txt
+cat user_fqdn.txt | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+while read LINE
+do
+    user_fqdn=$(echo "${LINE}")
+    ssh -24n -F ${OPENSHIFT_DATA_DIR}/.ssh/config ${user_fqdn} pwd 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+    user_string=$(echo "${user_fqdn}" | awk -F@ '{print $1}')
+    distcc_hosts_string="${user_fqdn}/2:/var/lib/openshift/${user_string}/app-root/data/distcc/bin/distccd_start "
+    # distcc_hosts_string="${user_fqdn}/2:/var/lib/openshift/${user_string}/app-root/data/distcc/bin/distccd_start,lzo"
+    echo -n "${distcc_hosts_string}" >> ${OPENSHIFT_DATA_DIR}/params/distcc_hosts.txt
+done < user_fqdn.txt
+rm -f user_fqdn.txt
+popd > /dev/null
+cat ${OPENSHIFT_DATA_DIR}/params/distcc_hosts.txt | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 export HOME=${env_home_backup}
 
 touch ${OPENSHIFT_DATA_DIR}/install_check_point/$(basename "${0}").ok
