51227b70697154517a7bef34b798fe342c1679d6
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-05-21 23:21:22 +0900

    Update install_step04a.sh

--- openshift/app01/install_step04a.sh
+++ openshift/app01/install_step04a.sh
@@ -208,7 +208,8 @@ mkdir ${OPENSHIFT_DATA_DIR}/bin
 pushd ${OPENSHIFT_DATA_DIR}/bin > /dev/null
 cat << __HEREDOC__ > distcc_ssh.sh
 #!/bin/bash
-
+echo "START" >> ${OPENSHIFT_LOG_DIR}/distcc_ssh.log
+echo ${DISTCC_HOSTS} >> ${OPENSHIFT_LOG_DIR}/distcc_ssh.log
 echo "$@" >> ${OPENSHIFT_LOG_DIR}/distcc_ssh.log
 /usr/bin/ssh -F ${OPENSHIFT_DATA_DIR}/.ssh/config $@
 __HEREDOC__
