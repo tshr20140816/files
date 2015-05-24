29f0914f3c80e99cd92de9fe862ad03c1ca5e317
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-05-25 01:12:28 +0900

    Create install_step18a.sh

---
+++ openshift/app01/install_step18a.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+source functions.sh
+function010
+[ $? -eq 0 ] || exit
+
+# ***** cadaver *****
+
+rm -f ${OPENSHIFT_TMP_DIR}/cadaver-${cadaver_version}.tar.gz
+rm -rf ${OPENSHIFT_TMP_DIR}/cadaver-${cadaver_version}
+rm -rf ${OPENSHIFT_DATA_DIR}/cadaver
+
+pushd ${OPENSHIFT_TMP_DIR} > /dev/null
+cp ${OPENSHIFT_DATA_DIR}/download_files/cadaver-${cadaver_version}.tar.gz ./
+echo "$(date +%Y/%m/%d" "%H:%M:%S) cadaver tar" >> ${OPENSHIFT_LOG_DIR}/install.log
+tar zxf cadaver-${cadaver_version}.tar.gz
+popd > /dev/null
+
+pushd ${OPENSHIFT_TMP_DIR}/cadaver-${cadaver_version} > /dev/null
+echo "$(date +%Y/%m/%d" "%H:%M:%S) cadaver configure" | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+echo $(date +%Y/%m/%d" "%H:%M:%S) '***** configure *****' $'\n'$'\n'> ${OPENSHIFT_LOG_DIR}/install_cadaver.log
+
+./configure \
+ --mandir=${OPENSHIFT_TMP_DIR}/man \
+ --docdir=${OPENSHIFT_TMP_DIR}/doc \
+ --prefix=${OPENSHIFT_DATA_DIR}/cadaver 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_cadaver.log
+
+echo "$(date +%Y/%m/%d" "%H:%M:%S) cadaver make" | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+echo $'\n'$(date +%Y/%m/%d" "%H:%M:%S) '***** make *****' $'\n'$'\n'>> ${OPENSHIFT_LOG_DIR}/install_cadaver.log
+
+time make -j$(grep -c -e processor /proc/cpuinfo) 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_cadaver.log
+
+echo "$(date +%Y/%m/%d" "%H:%M:%S) cadaver make install" | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+echo $'\n'$(date +%Y/%m/%d" "%H:%M:%S) '***** make install *****' $'\n'$'\n'>> ${OPENSHIFT_LOG_DIR}/install_cadaver.log
+
+make install 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_cadaver.log
+mv ${OPENSHIFT_LOG_DIR}/install_cadaver.log ${OPENSHIFT_LOG_DIR}/install/
+popd > /dev/null
+
+rm -f ${OPENSHIFT_TMP_DIR}/cadaver-${cadaver_version}.tar.gz
+rm -rf ${OPENSHIFT_TMP_DIR}/cadaver-${cadaver_version}
+
+touch ${OPENSHIFT_DATA_DIR}/install_check_point/$(basename $0).ok
+
+echo "$(date +%Y/%m/%d" "%H:%M:%S) Install Finish $(basename "${0}")" | tee -a ${OPENSHIFT_LOG_DIR}/install.log
