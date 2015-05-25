1349739ae088d654c60a71b9afd161fc379f0f33
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-05-25 13:51:45 +0900

    Update install_step19.sh

--- openshift/app01/install_step19.sh
+++ openshift/app01/install_step19.sh
@@ -62,6 +62,9 @@ do
         log_file_name=${OPENSHIFT_LOG_DIR}/cadaver.log
         remote_dir=/users/$(cat ${OPENSHIFT_DATA_DIR}/params/hidrive_account)
         ${OPENSHIFT_DATA_DIR}/scripts/./cadaver_put.sh ${OPENSHIFT_LOG_DIR}/backup/ ${remote_dir} ${file_name} | tee ${log_file_name}
+        if [ $(grep -c -e succeeded ${log_file_name}) -eq 1 ]; then
+            rm ${file_name}
+        fi
     fi
 done
 __HEREDOC__
