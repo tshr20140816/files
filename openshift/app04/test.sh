e5184bb16a967cdedc80f1b30f48c32379cf0565
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-05-26 08:25:48 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -2,54 +2,13 @@
 
 # echo "$(date)" > ${OPENSHIFT_LOG_DIR}/test.log
 
-cd /tmp
-if [ ! -e ${OPENSHIFT_DATA_DIR}/ccache ]; then
-    if [ ! -f ccache-3.2.1.tar.xz ]; then
-        wget https://files3-20150207.rhcloud.com/files/ccache-3.2.1.tar.xz
-    fi
-    tar Jxf ccache-3.2.1.tar.xz
-    cd ccache-3.2.1
-    CFLAGS="-O2 -march=native -pipe -fomit-frame-pointer -s" CXXFLAGS="-O2 -march=native -pipe" \
-     ./configure --prefix=${OPENSHIFT_DATA_DIR}/ccache --mandir=/tmp/man --docdir=/tmp/doc
-    make
-    make install
-fi
-
-# export PATH="${OPENSHIFT_DATA_DIR}/ccache/bin:$PATH"
-# export CC="ccache gcc"
-# export CXX="ccache g++"
-export CCACHE_DIR=${OPENSHIFT_TMP_DIR}/ccache
-export CCACHE_TEMPDIR=${OPENSHIFT_TMP_DIR}/tmp_ccache
-export CCACHE_LOGFILE=${OPENSHIFT_LOG_DIR}/ccache.log
-export CCACHE_MAXSIZE=300M
-export CCACHE_BASEDIR=${OPENSHIFT_HOME_DIR}
-
-export CFLAGS="-O2 -march=native -pipe -fomit-frame-pointer -s"
-export CXXFLAGS="${CFLAGS}"
-
-# ccache -z
-
 set -x
 
 cd /tmp
 
-cat << '__HEREDOC__' > cadaver_put.sh
-#!/bin/bash
-
-if [ $# -ne 3 ]; then
-    echo "NG"
-    exit 1
-fi
-
-pushd ${1}
+export GEM_HOME=${OPENSHIFT_DATA_DIR}/.gem
+export PATH="${OPENSHIFT_DATA_DIR}/.gem/bin:$PATH"
 export HOME=${OPENSHIFT_DATA_DIR}
-exec ${OPENSHIFT_DATA_DIR}/cadaver/bin/cadaver https://webdav.hidrive.strato.com/ << '__HEREDOC_2__'
-cd ${2}
-put ${3}
-quit
-__HEREDOC_2__
-popd
-exit 0
-__HEREDOC__
+gem --version
 
-cat cadaver_put.sh
+rhc apps
