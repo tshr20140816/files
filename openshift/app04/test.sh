4859ea12d78c07ce4bb128116d23aae77e519a4d
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-05-26 10:24:28 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -13,7 +13,26 @@ export HOME=${OPENSHIFT_DATA_DIR}
 # rhc apps
 
 cd $OPENSHIFT_DATA_DIR
+cd .ssh
 
-ls -la .ssh
+cat << '__HEREDOC__' > config
+Host *
+  IdentityFile __OPENSHIFT_DATA_DIR__.ssh/id_rsa
+  StrictHostKeyChecking no
+  UserKnownHostsFile /dev/null
+  LogLevel QUIET
+#  LogLevel DEBUG3
+  Protocol 2
+  PasswordAuthentication no
+  ConnectionAttempts 5
+  ControlMaster auto
+  ControlPath __OPENSHIFT_DATA_DIR__.ssh/master-%r@%h:%p
+# ssh -O exit REMOTE
+#  ControlPersist yes
+  ControlPersist 60s
+__HEREDOC__
+sed -i -e "s|__OPENSHIFT_DATA_DIR__|${OPENSHIFT_DATA_DIR}|g" config
 
-cat .ssh/known_hosts
+ssh -24n -F config 55630afc5973caf283000214@v1-20150216.rhcloud.com pwd
+
+ps auwx | grep ssh
