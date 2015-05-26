408aa779fe3a1975de5ef60cd1d7181084b675dd
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-05-26 16:31:44 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -20,7 +20,7 @@ Host *
   ConnectionAttempts 5
   ControlMaster auto
   ControlPath /tmp/.ssh/master-%r@%h:%p
-  ControlPersist 2m
+  ControlPersist 10s
 __HEREDOC__
 sed -i -e "s|__OPENSHIFT_DATA_DIR__|${OPENSHIFT_DATA_DIR}|g" config
 
@@ -31,3 +31,5 @@ ssh -24n -F config 555894314382ec8df40000e1@b1-20150430.rhcloud.com pwd
 ssh -24n -F config 555895235973ca539500007e@b2-20150430.rhcloud.com pwd
 ssh -24n -F config 555895dbfcf9337761000009@b3-20150430.rhcloud.com pwd
 ps auwx | grep ssh
+
+ls $OPENSHIFT_DATA_DIR
