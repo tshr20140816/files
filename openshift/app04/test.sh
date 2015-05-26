42047e1cad5b37b43926e6ea0c4f9e202ee28e1a
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-05-26 18:13:46 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -47,6 +47,7 @@ export PATH="${OPENSHIFT_DATA_DIR}/distcc/bin:$PATH"
 export DISTCC_DIR=${OPENSHIFT_DATA_DIR}.distcc
 # export DISTCC_LOG=/dev/null
 export DISTCC_LOG=/tmp/distcc.log
+rm -f /tmp/distcc.log
 export CC="distcc gcc"
 export CXX="distcc g++"
 export CFLAGS="-O2 -march=x86-64 -fomit-frame-pointer -s"
