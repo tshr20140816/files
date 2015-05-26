4b9250f78b4497e74b18d9039d918f7b45ae0ef1
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-05-26 12:41:21 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -6,45 +6,4 @@ set -x
 
 cd /tmp
 
-# export GEM_HOME=${OPENSHIFT_DATA_DIR}/.gem
-# export PATH="${OPENSHIFT_DATA_DIR}/.gem/bin:$PATH"
-export HOME=${OPENSHIFT_DATA_DIR}
-
-# rhc apps
-
-cd $OPENSHIFT_DATA_DIR
-cd .ssh
-
-cat << '__HEREDOC__' > config
-Host *
-  IdentityFile __OPENSHIFT_DATA_DIR__.ssh/id_rsa
-  StrictHostKeyChecking no
-  UserKnownHostsFile /dev/null
-#  LogLevel QUIET
-  LogLevel DEBUG3
-  Protocol 2
-  PasswordAuthentication no
-  ConnectionAttempts 5
-  ControlMaster auto
-  # ControlPath too long
-#  ControlPath __OPENSHIFT_DATA_DIR__.ssh/master-%r@%h:%p
-  ControlPath /tmp/.ssh/master-%r@%h:%p
-# ssh -O exit REMOTE
-#  ControlPersist yes
-  ControlPersist 2m
-__HEREDOC__
-sed -i -e "s|__OPENSHIFT_DATA_DIR__|${OPENSHIFT_DATA_DIR}|g" config
-
-mkdir -p /tmp/.ssh 2>/dev/null
-
-ssh -24n -F config 55630afc5973caf283000214@v1-20150216.rhcloud.com pwd
-
-ls -la /tmp/.ssh
-
-ps auwx | grep ssh
-
-ssh -24n -F config 55630afc5973caf283000214@v1-20150216.rhcloud.com pwd
-
-ls -la /tmp/.ssh
-
-ps auwx | grep ssh
+make --help
