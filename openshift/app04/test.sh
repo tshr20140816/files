6d017635889b93696464fe1acfaf5e8b0a43a82d
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-05-26 14:04:23 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -27,5 +27,7 @@ sed -i -e "s|__OPENSHIFT_DATA_DIR__|${OPENSHIFT_DATA_DIR}|g" config
 ssh -24n -F config 55630afc5973caf283000214@v1-20150216.rhcloud.com pwd
 ssh -24n -F config 55630b63e0b8cd7ed000007f@v2-20150216.rhcloud.com pwd
 ssh -24n -F config 55630c675973caf283000251@v3-20150216.rhcloud.com pwd
-
+ssh -24n -F config 555894314382ec8df40000e1@b1-20150430.rhcloud.com pwd
+ssh -24n -F config 555895235973ca539500007e@b2-20150430.rhcloud.com pwd
+ssh -24n -F config 555895dbfcf9337761000009@b3-20150430.rhcloud.com pwd
 ps auwx | grep ssh
