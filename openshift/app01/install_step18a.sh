5d7ebba00b0467978847303bd29c1228dee978d0
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-05-29 00:05:54 +0900

    Update install_step18a.sh

--- openshift/app01/install_step18a.sh
+++ openshift/app01/install_step18a.sh
@@ -7,29 +7,57 @@ function010
 # ***** cadaver *****
 
 rm -f ${OPENSHIFT_TMP_DIR}/cadaver-${cadaver_version}.tar.gz
+rm -f ${OPENSHIFT_TMP_DIR}/${OPENSHIFT_APP_UUID}_maked_cadaver-${cadaver_version}.tar.xz
 rm -rf ${OPENSHIFT_TMP_DIR}/cadaver-${cadaver_version}
 rm -rf ${OPENSHIFT_DATA_DIR}/cadaver
 
 pushd ${OPENSHIFT_TMP_DIR} > /dev/null
-cp ${OPENSHIFT_DATA_DIR}/download_files/cadaver-${cadaver_version}.tar.gz ./
-echo "$(date +%Y/%m/%d" "%H:%M:%S) cadaver tar" | tee -a ${OPENSHIFT_LOG_DIR}/install.log
-tar zxf cadaver-${cadaver_version}.tar.gz
+if [ $(cat ${OPENSHIFT_DATA_DIR}/params/build_server_password) != "none" ]; then
+    file_name=${OPENSHIFT_APP_UUID}_maked_cadaver-${cadaver_version}.tar.xz
+    url=$(cat ${OPENSHIFT_DATA_DIR}/params/mirror_server)/${file_name}
+    while :
+    do
+        if [ $(wget -nv --spider --timeout 60 -t 1 ${url} 2>&1 | grep -c '200 OK') -eq 1 ]; then
+            echo "$(date +%Y/%m/%d" "%H:%M:%S) cadaver maked wget" | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+            break
+        else
+            echo "$(date +%Y/%m/%d" "%H:%M:%S) cadaver maked waiting" | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+            sleep 10s
+        fi
+    done
+    wget $(cat ${OPENSHIFT_DATA_DIR}/params/mirror_server)/${file_name}
+    echo "$(date +%Y/%m/%d" "%H:%M:%S) cadaver maked tar" | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+    tar Jxf ${file_name}
+    rm -f ${file_name}
+else
+    cp ${OPENSHIFT_DATA_DIR}/download_files/cadaver-${cadaver_version}.tar.gz ./
+    echo "$(date +%Y/%m/%d" "%H:%M:%S) cadaver tar" | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+    tar zxf cadaver-${cadaver_version}.tar.gz
+fi
 popd > /dev/null
 
+# *** configure make install ***
+
 pushd ${OPENSHIFT_TMP_DIR}/cadaver-${cadaver_version} > /dev/null
-echo "$(date +%Y/%m/%d" "%H:%M:%S) cadaver configure" | tee -a ${OPENSHIFT_LOG_DIR}/install.log
-echo $(date +%Y/%m/%d" "%H:%M:%S) '***** configure *****' $'\n'$'\n'> ${OPENSHIFT_LOG_DIR}/install_cadaver.log
 
-./configure \
- --mandir=${OPENSHIFT_TMP_DIR}/man \
- --docdir=${OPENSHIFT_TMP_DIR}/doc \
- --with-ssl=openssl \
- --prefix=${OPENSHIFT_DATA_DIR}/cadaver 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_cadaver.log
+if [ $(cat ${OPENSHIFT_DATA_DIR}/params/build_server_password) != "none" ]; then
+    export CC="ccache gcc"
+    export CXX="ccache g++"
+else
+    echo "$(date +%Y/%m/%d" "%H:%M:%S) cadaver configure" | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+    echo $(date +%Y/%m/%d" "%H:%M:%S) '***** configure *****' $'\n'$'\n'> ${OPENSHIFT_LOG_DIR}/install_cadaver.log
 
-echo "$(date +%Y/%m/%d" "%H:%M:%S) cadaver make" | tee -a ${OPENSHIFT_LOG_DIR}/install.log
-echo $'\n'$(date +%Y/%m/%d" "%H:%M:%S) '***** make *****' $'\n'$'\n'>> ${OPENSHIFT_LOG_DIR}/install_cadaver.log
+    ./configure \
+     --mandir=${OPENSHIFT_TMP_DIR}/man \
+     --docdir=${OPENSHIFT_TMP_DIR}/doc \
+     --with-ssl=openssl \
+     --prefix=${OPENSHIFT_DATA_DIR}/cadaver 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_cadaver.log
 
-time make -j$(grep -c -e processor /proc/cpuinfo) 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_cadaver.log
+    echo "$(date +%Y/%m/%d" "%H:%M:%S) cadaver make" | tee -a ${OPENSHIFT_LOG_DIR}/install.log
+    echo $'\n'$(date +%Y/%m/%d" "%H:%M:%S) '***** make *****' $'\n'$'\n'>> ${OPENSHIFT_LOG_DIR}/install_cadaver.log
+
+    time make -j$(grep -c -e processor /proc/cpuinfo) 2>&1 | tee -a ${OPENSHIFT_LOG_DIR}/install_cadaver.log
+fi
 
 echo "$(date +%Y/%m/%d" "%H:%M:%S) cadaver make install" | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 echo $'\n'$(date +%Y/%m/%d" "%H:%M:%S) '***** make install *****' $'\n'$'\n'>> ${OPENSHIFT_LOG_DIR}/install_cadaver.log
