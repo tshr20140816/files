782a2868098ecafc6dce463698b5b01035748470
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-05-29 21:37:31 +0900

    Update install_step05.sh

--- openshift/app01/install_step05.sh
+++ openshift/app01/install_step05.sh
@@ -110,6 +110,8 @@ do
     # distcc_hosts_string="${user_fqdn}/2:/var/lib/openshift/${user_string}/app-root/data/distcc/bin/distccd_start,cpp "
     echo -n "${distcc_hosts_string}" >> ${OPENSHIFT_DATA_DIR}/params/distcc_hosts.txt
 done < ${OPENSHIFT_DATA_DIR}/params/user_fqdn_2.txt
+cat ${OPENSHIFT_DATA_DIR}/params/user_fqdn_2.txt >> ${OPENSHIFT_DATA_DIR}/params/user_fqdn.txt
+rm -f ${OPENSHIFT_DATA_DIR}/params/user_fqdn_2.txt
 popd > /dev/null
 cat ${OPENSHIFT_DATA_DIR}/params/distcc_hosts.txt | tee -a ${OPENSHIFT_LOG_DIR}/install.log
 echo " " | tee -a ${OPENSHIFT_LOG_DIR}/install.log
