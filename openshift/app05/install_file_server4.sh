8b053524dcdcb247dbd65706168eb1fe5f111390
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-05-30 08:59:44 +0900

    Update install_file_server4.sh

--- openshift/app05/install_file_server4.sh
+++ openshift/app05/install_file_server4.sh
@@ -102,9 +102,12 @@ pushd ${OPENSHIFT_TMP_DIR} > /dev/null
 wget http://ftp.jaist.ac.jp/pub/OpenBSD/OpenSSH/portable/openssh-${openssh_version}.tar.gz
 wget http://www.psc.edu/index.php/hpn-ssh-patches/hpn-14-kitchen-sink-patches/finish/24-hpn-14-kitchen-sink-patches/102-openssh-6-3p1-hpnssh14v2-kitchen-sink-patch \
  -O openssh-6.3p1-hpnssh14v2.diff.gz
-tar xfz openssh-${openssh_version}.tar.gz
+tar zxf openssh-${openssh_version}.tar.gz
+tar zxf openssh-6.3p1-hpnssh14v2.diff.gz
 popd > /dev/null
 pushd ${OPENSHIFT_TMP_DIR}/openssh-${openssh_version} > /dev/null
+patch < ../openssh-6.3p1-hpnssh14v2.diff
+./configure --help
 ./configure \
  --prefix=${OPENSHIFT_DATA_DIR}/openssh \
  --infodir=${OPENSHIFT_TMP_DIR}/info \
