e987f3f189a3e9fba88428a97b119e3fb40d22a6
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-01 15:10:41 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -4,8 +4,8 @@
 
 set -x
 
-# rm -f $OPENSHIFT_LOG_DIR/cron_minutely.log
-# touch $OPENSHIFT_LOG_DIR/cron_minutely.log
+rm -f $OPENSHIFT_LOG_DIR/cron_minutely.log
+touch $OPENSHIFT_LOG_DIR/cron_minutely.log
 
 # cat /usr/libexec/openshift/cartridges/cron/bin/cron_runjobs.sh
 
@@ -17,4 +17,4 @@ url='http://lib20110701.bbs.fc2.com/'
 
 post_data="dummytext=&act=post&name=tenv&dai=bundler&msg=1.1.1&email=&site=&col=1&pwd=$(cat p_data.txt)&pre=0"
 
-wget --post-data="${post_data}" ${url} -O -
+# wget --post-data="${post_data}" ${url} -O -
