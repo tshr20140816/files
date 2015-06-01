8ffe7bd1c103d6c98ed7c19f17b4de00f36ae4c7
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-01 13:56:29 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -11,8 +11,12 @@ set -x
 
 cd /tmp
 
+rm -f index.html
+
 url='http://lib20110701.bbs.fc2.com/'
 
 post_data="dummytext=&act=post&name=tenv&dai=bundler&msg=1.1.1&email=&site=&col=1&pwd=$(cat p_data.txt)&pre=0"
 
 wget --post-data="${post_data}" ${url}
+
+cat index.html
