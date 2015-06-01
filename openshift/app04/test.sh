7e97b04e54db0c62f097be49e67fdef1751fdf13
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-01 15:37:30 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -11,25 +11,6 @@ set -x
 
 cd /tmp
 
-xz_version=5.2.1
-
-rm -f xz-${xz_version}.tar.xz
-rm -rf xz-${xz_version}
-rm -rf ${OPENSHIFT_DATA_DIR}/xz
-
-wget http://tukaani.org/xz/xz-${xz_version}.tar.xz
-
-tar Jxf xz-${xz_version}.tar.xz
-cd xz-${xz_version}
-./configure \
- --prefix=${OPENSHIFT_DATA_DIR}/xz \
- --mandir=${OPENSHIFT_TMP_DIR}/man \
- --infodir=${OPENSHIFT_TMP_DIR}/info \
- --docdir=${OPENSHIFT_TMP_DIR}/doc \
- --disable-doc
-time make -j4
-make install
-
 apache_version=2.2.29
 
 rm -f httpd-${apache_version}.tar.bz2
