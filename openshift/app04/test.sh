86b82ea456b7f057183f5e89bfb2ad8c5cde3ba0
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-01 11:33:42 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -11,23 +11,7 @@ set -x
 
 cd /tmp
 
-# * gpg *
-
 rm -rf ${OPENSHIFT_DATA_DIR}/.gnupg
-mkdir ${OPENSHIFT_DATA_DIR}/.gnupg
-export GNUPGHOME=${OPENSHIFT_DATA_DIR}/.gnupg
-chmod 700 ${GNUPGHOME}
-gpg --list-keys
-echo "keyserver hkp://keyserver.ubuntu.com:80" >> ${GNUPGHOME}/gpg.conf
-
-cat ${GNUPGHOME}/gpg.conf
-
 rm -f xz-5.2.1.tar.xz
 rm -f xz-5.2.1.tar.xz.sig
 rm -rf xz-5.2.1
-wget http://tukaani.org/xz/xz-5.2.1.tar.xz
-
-wget http://tukaani.org/xz/xz-5.2.1.tar.xz.sig
-
-gpg --recv-keys $(gpg --verify xz-5.2.1.tar.xz.sig 2>&1 | grep "RSA key ID" | awk '{print $NF}')
-gpg --verify xz-5.2.1.tar.xz.sig 2>&1
