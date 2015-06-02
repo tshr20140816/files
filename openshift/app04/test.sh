af2acd957fbd55f8afe8abe7bf10e5e3e820c2d2
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-02 17:23:13 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -11,15 +11,4 @@ set -x
 
 cd /tmp
 
-rm -rf binutils-2.25
-rm -rf bison-3.0
-rm -f cadaver_put.sh
-rm -rf distcc-3.1
-rm -f monitor_resourse.sh
-rm -f test.txt
-rm -rf xz-5.2.1
-
-gcc -march=native -Q --help=target
-
-ls -lang
-ls -lang ${OPENSHIFT_DATA_DIR}
+head --help
