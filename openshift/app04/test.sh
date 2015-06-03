ae3d451f1afa9e062ef7d1369d9e99198522c0fb
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-03 14:28:56 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -9,6 +9,20 @@ set -x
 
 # cat /usr/libexec/openshift/cartridges/cron/bin/cron_runjobs.sh
 
+export PATH="${OPENSHIFT_DATA_DIR}/ccache/bin:$PATH"
+export CC="ccache gcc"
+export CXX="ccache g++"
+export CCACHE_DIR=${OPENSHIFT_TMP_DIR}/ccache
+export CCACHE_TEMPDIR=${OPENSHIFT_TMP_DIR}/tmp_ccache
+export CCACHE_LOGFILE=${OPENSHIFT_LOG_DIR}/ccache.log
+# export CCACHE_LOGFILE=/dev/null
+export CCACHE_MAXSIZE=300M
+
+mkdir ${OPENSHIFT_TMP_DIR}/ccache
+mkdir ${OPENSHIFT_TMP_DIR}/tmp_ccache
+
+tree ${OPENSHIFT_DATA_DIR}/ccache/bin
+
 cd /tmp
 
 # rm -f gcc-5.1.1-1.fc22.x86_64.rpm
@@ -24,6 +38,7 @@ rm -f glibc-2.14.1.tar.xz
 cd glibc-2.14.1
 mkdir build
 cd build
+make clean
 ../configure --prefix=${OPENSHIFT_DATA_DIR}/lib
 time make -j4
 
@@ -31,10 +46,6 @@ find /tmp -name libc.so* -print
 
 # wget http://ftp.gnu.org/gnu/glibc/glibc-2.14.1.tar.xz
 # wget ftp://195.220.108.108/linux/fedora/linux/releases/22/Everything/x86_64/os/Packages/g/gcc-5.1.1-1.fc22.x86_64.rpm
-
-# mkdir gcc
-# cd gcc
-# mv ../gcc-5.1.1-1.fc22.x86_64.rpm ./
 
 # rpm2cpio gcc-5.1.1-1.fc22.x86_64.rpm | cpio -idmv
 
