c5d9898eff5096cf9731c62cbb993043536ed96b
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-03 13:14:22 +0900

    Update functions.sh

--- openshift/app01/functions.sh
+++ openshift/app01/functions.sh
@@ -48,7 +48,10 @@ function010() {
     # NG : distcc & -march=native
     # export CFLAGS="-O2 -march=native -pipe -fomit-frame-pointer -s"
     # -march=ivybridge E5-2670 v2
-    export CFLAGS="-O2 -march=core2 -fomit-frame-pointer -s"
+    # export CFLAGS="-O2 -march=core2 -fomit-frame-pointer -s"
+    export CFLAGS="-O2 -march=core2 -maes -mavx -mcx16 -mpclmul -mpopcnt -msahf"
+    export CFLAGS="${CFLAGS} -msse -msse2 -msse3 -msse4 -msse4.1 -msse4.2 -mssse3 -mtune=generic"
+    export CFLAGS="${CFLAGS} -pipe -fomit-frame-pointer -s"
     export CXXFLAGS="${CFLAGS}"
 
     # shellcheck disable=SC2034
