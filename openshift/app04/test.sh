129ea02c176e6835c9d530948a54c0a49d07ef4a
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-03 13:15:21 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -13,8 +13,8 @@ cd /tmp
 
 rm -f gcc-5.1.1-1.fc22.x86_64.rpm
 
-/tmp/gcc/usr/lib/gcc --version
-/tmp/gcc/usr/lib/gcc --help
+/tmp/gcc/usr/bin/gcc --version
+/tmp/gcc/usr/bin/gcc --help
 
 # wget ftp://195.220.108.108/linux/fedora/linux/releases/22/Everything/x86_64/os/Packages/g/gcc-5.1.1-1.fc22.x86_64.rpm
 
