7c0c2f0279f4e369045dd61beaeff9a6b5bb2e48
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-03 12:58:31 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -11,9 +11,6 @@ set -x
 
 cd /tmp
 
-rm -f libmemcached-1.0.18.tar.gz
-rm -rf libmemcached-1.0.18
-
 # wget ftp://195.220.108.108/linux/fedora/linux/releases/22/Everything/x86_64/os/Packages/g/gcc-5.1.1-1.fc22.x86_64.rpm
 
 # mkdir gcc
