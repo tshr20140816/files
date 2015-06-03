72b1e1dadfcf46b3ed3c013eab5d0940a1c827d0
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-03 17:24:23 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -8,13 +8,9 @@ rm -f $OPENSHIFT_LOG_DIR/cron_minutely.log.*
 
 cd /tmp
 
-rm -rf glibc-2.14.1
-rm -rf gcc
-rm -f cc*
-rm -f test.tar.xz
-
-wget http://ftp.gnu.org/gnu/binutils/binutils-2.25.tar.bz2
-tar jxf binutils-2.25.tar.bz2
+# wget http://ftp.gnu.org/gnu/binutils/binutils-2.25.tar.bz2
+# tar jxf binutils-2.25.tar.bz2
 cd binutils-2.25
-./configure --help
-./configure
+# ./configure --help
+# ./configure
+make -j4
