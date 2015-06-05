b03c06edc3dc57ebed5d30cea20fcd1a6c3cbac8
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-05 11:04:31 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -35,7 +35,7 @@ rm -rf binutils-2.25
 tar jxf binutils-2.25.tar.bz2
 cd binutils-2.25
 ./configure --help
-./configure --enable-gold
+./configure --enable-gold --disable-libquadmath --disable-libstdcxx
 time make -j4
 
 ccache -s
