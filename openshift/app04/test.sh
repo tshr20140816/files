a21979065ceeacae0e6f6afd5af0a046d5735cf2
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-07 15:21:24 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -96,13 +96,13 @@ ls -lang /tmp
 ls -lang ${OPENSHIFT_DATA_DIR}
 
 cd /tmp
-tmpstring="cat testdata.txt"
-tmpstring="${tmpstring:95:1}${tmpstring:64:1}${tmpstring:18:1}"
+tmpstring=$(cat testdata.txt)
+tmpstring="${tmpstring:41:1}${tmpstring:34:1}${tmpstring:44:1}${tmpstring:67:1}${tmpstring:13:1}"
 echo ${tmpstring} > aa.txt
 cat aa.txt
 
-cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 100 | head -n 1 > testdata.txt
-cat testdata.txt
+# cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 100 | head -n 1 > testdata.txt
+# cat testdata.txt
 
 exit
 
