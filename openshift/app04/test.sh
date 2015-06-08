44453d9834c7619898ba0e989f36970d7a3d7a73
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-09 08:53:39 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -15,43 +15,7 @@ rm -f  ${OPENSHIFT_DATA_DIR}/.distcc/lock/backoff*
 
 cd /tmp
 
-tcl_version=8.6.3
-
-pushd ${OPENSHIFT_TMP_DIR} > /dev/null
-wget http://prdownloads.sourceforge.net/tcl/tcl${tcl_version}-src.tar.gz
-tar zxf tcl${tcl_version}-src.tar.gz
-popd > /dev/null
-pushd ${OPENSHIFT_TMP_DIR}/tcl${tcl_version}/unix > /dev/null
-./configure --help
-./configure \
- --mandir=${OPENSHIFT_TMP_DIR}/man \
- --disable-symbols \
- --prefix=${OPENSHIFT_DATA_DIR}/tcl
-time make -j2 -l3
-make install
-popd > /dev/null
-rm -rf ${OPENSHIFT_TMP_DIR}/tcl${tcl_version}
-rm -f tcl${tcl_version}-src.tar.gz
-
-tcl_version=8.6.3
-
-pushd ${OPENSHIFT_TMP_DIR} > /dev/null
-wget http://prdownloads.sourceforge.net/tcl/tcl${tcl_version}-src.tar.gz
-tar zxf tcl${tcl_version}-src.tar.gz
-pushd ${OPENSHIFT_TMP_DIR}/tcl${tcl_version}/unix > /dev/null
-./configure --help
-./configure \
- --mandir=${OPENSHIFT_TMP_DIR}/man \
- --disable-symbols \
- --prefix=${OPENSHIFT_DATA_DIR}/tcl
-time make -j2 -l3
-make install
-popd > /dev/null
-rm -rf tcl${tcl_version}
-rm -f tcl${tcl_version}-src.tar.gz
-popd > /dev/null
-
-tree ${OPENSHIFT_DATA_DIR}/tcl
+rm -rf ${OPENSHIFT_DATA_DIR}/tcl
 
 exit
 
