eadd0779b89ecff2365d9badbf6ded6622dc6d2c
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-08 12:55:26 +0900

    Update build_action2.sh

--- openshift/app05/build_action2.sh
+++ openshift/app05/build_action2.sh
@@ -281,7 +281,7 @@ wc -l ${OPENSHIFT_TMP_DIR}/strip_starget.txt
 # done
 # wait
 # time cat ${OPENSHIFT_TMP_DIR}/strip_starget.txt | xargs -0rti -L1 -n1 -P4 strip --strip-all {}
-time cat ${OPENSHIFT_TMP_DIR}/strip_starget.txt | xargs -0ti strip --strip-all {}
+time cat ${OPENSHIFT_TMP_DIR}/strip_starget.txt | xargs -0ti -L1 strip --strip-all {}
 
 ccache --show-stats
 
