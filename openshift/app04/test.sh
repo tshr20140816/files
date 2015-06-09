cfbb3a913f74cf3858a28c5f4a2d6a3c6e773cea
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-09 13:40:25 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -59,6 +59,9 @@ rm -rf expect${expect_version}
 popd > /dev/null
 popd > /dev/null
 
+${OPENSHIFT_DATA_DIR}/expect/bin/expect --version
+${OPENSHIFT_DATA_DIR}/expect/bin/expect --help
+
 exit
 
 mkdir ${OPENSHIFT_DATA_DIR}/.ssh 2>/dev/null
