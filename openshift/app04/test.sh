08863d33d94e41a75245d639ffd77481f587646f
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-10 12:31:59 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -12,6 +12,8 @@ rm -f ${OPENSHIFT_LOG_DIR}/ccache.log
 rm -f ${OPENSHIFT_LOG_DIR}/distcc.log
 rm -f ${OPENSHIFT_LOG_DIR}/distcc_ssh.log
 rm -f ${OPENSHIFT_TMP_DIR}/cc*.s
+rm -rf ${OPENSHIFT_TMP_DIR}/delegate-
+rm -f ${OPENSHIFT_TMP_DIR}/distcc_server_stderr_*
 
 rm -f  ${OPENSHIFT_DATA_DIR}/.distcc/lock/backoff*
 
