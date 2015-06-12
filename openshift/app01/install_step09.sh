269f8c6900fc616cfd25730ab1cc3aa74328ce85
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-12 16:39:50 +0900

    Update install_step09.sh

--- openshift/app01/install_step09.sh
+++ openshift/app01/install_step09.sh
@@ -93,6 +93,7 @@ echo $(date +%Y/%m/%d" "%H:%M:%S) '***** configure *****' $'\n'$'\n'> ${OPENSHIF
 --with-pdo-mysql \
 --without-sqlite3 \
 --without-pdo-sqlite \
+--without-cdb \
 --without-pear \
 --with-curl \
 --with-libdir=lib64 \
