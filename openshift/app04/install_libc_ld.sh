548d0b5f40bbba2f4896cf69760b19213e8fcc61
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-13 14:44:38 +0900

    Update install_libc_ld.sh

--- openshift/app04/install_libc_ld.sh
+++ openshift/app04/install_libc_ld.sh
@@ -25,7 +25,54 @@ pushd glibc-${glibc_version}/work
  --mandir=${OPENSHIFT_TMP_DIR}/man \
  --docdir=${OPENSHIFT_TMP_DIR}/doc \
  --infodir=${OPENSHIFT_TMP_DIR}/info
-time make -j4
+# over 30min
+time make -j$(grep -c -e processor /proc/cpuinfo)
 popd > /dev/null
 time tar Jcf maked_glibc-${glibc_version}.tar.xz glibc-${glibc_version}
+cp maked_glibc-${glibc_version}.tar.xz ${OPENSHIFT_DATA_DIR}/
+rm -rf glibc-${glibc_version}
+rm -f glibc-${glibc_version}.tar.xz
+popd > /dev/null
+
+# ***** ld.gold *****
+
+bison_version=3.0.4
+binutils_version=2.25
+
+pushd ${OPENSHIFT_TMP_DIR} > /dev/null
+
+# *** bison ***
+
+wget http://ftp.gnu.org/gnu/bison/bison-${bison_version}.tar.xz
+tar Jxf bison-${bison_version}.tar.xz
+pushd bison-${bison_version}
+./configure --prefix=${OPENSHIFT_TMP_DIR}/local
+time make -j$(grep -c -e processor /proc/cpuinfo)
+make install
+popd > /dev/null
+rm -rf ./bison-${bison_version}
+rm -f bison-${bison_version}.tar.xz
+tree ./local
+
+strip --strip-all ./local/bin/bison ./local/lib/liby.a
+
+export PATH=${OPENSHIFT_TMP_DIR}/local/bin:$PATH
+
+# ***  ld.gold ***
+
+wget http://ftp.gnu.org/gnu/binutils/binutils-${binutils_version}.tar.gz
+tar zxf binutils-${binutils_version}.tar.gz
+pushd binutils-${binutils_version} > /dev/null
+./configure
+time make -j$(grep -c -e processor /proc/cpuinfo)
+pushd gold > /dev/null
+./configure --prefix=${OPENSHIFT_TMP_DIR}/local
+time make -j$(grep -c -e processor /proc/cpuinfo)
+make install
+popd > /dev/null
+popd > /dev/null
+rm -rf binutils-${binutils_version}
+rm -f binutils-${binutils_version}.tar.gz
+strip --strip-all ./local/bin/ld.gold
+cp ./local/bin/ld.gold ${OPENSHIFT_DATA_DIR}/
 popd > /dev/null
