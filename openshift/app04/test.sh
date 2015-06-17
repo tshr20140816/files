cb04034956cf6b41865e032ed5f95624551a35a6
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-18 08:31:31 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -125,7 +125,7 @@ export HOME=${OPENSHIFT_DATA_DIR}
 
 # export CFLAGS="-I/tmp/local/include -I/tmp/libxml2/include/libxml2"
 # export CFLAGS="${CFLAGS} -O2 -march=native -pipe -fomit-frame-pointer -s"
-export CFLAGS="-O2 -march=native -pipe -fomit-frame-pointer -s"
+export CFLAGS="-O3 -march=native -pipe -fomit-frame-pointer -s"
 export CXXFLAGS="${CFLAGS}"
 export LIBS="-L/tmp/local/lib"
 
