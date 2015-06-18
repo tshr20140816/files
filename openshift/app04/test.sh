3410465f3f288904078fd1d5bd1aa38e5e904967
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-18 17:27:24 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -9,6 +9,24 @@ echo "$(date)"
 
 set -x
 
+CC="gcc"
+OPT="-march=native"
+NATIVE=$(echo | ${CC} -E -v ${OPT} - 2>&1 | grep cc1)
+NOARCH=$(echo | ${CC} -E -v - 2>&1 | grep cc1)
+
+for native in ${NATIVE} ; do
+        FOUND=0
+        for noarch in ${NOARCH} ; do
+                if [ "${native}" = "${noarch}" -a "${native}" != "${OPT}" ] ; then
+                        FOUND=1
+                        break
+                fi
+        done
+        if [ ${FOUND} -eq 0 ] ; then
+                echo -n "${native} "
+        fi
+done
+
 # find / -name "libxml*" -print 2>/dev/null
 # strip --help
 
