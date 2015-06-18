6dbee000488c3a905653d212ad1af3fee635ec9d
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-18 18:18:19 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -121,8 +121,11 @@ export DISTCC_SSH="${OPENSHIFT_DATA_DIR}/bin/distcc-ssh"
 
 export HOME=${OPENSHIFT_DATA_DIR}
 
-export CFLAGS="-O2 -march=core2 -maes -mavx -mcx16 -mpclmul -mpopcnt -msahf"
-export CFLAGS="${CFLAGS} -msse -msse2 -msse3 -msse4 -msse4.1 -msse4.2 -mssse3 -mtune=generic"
+# export CFLAGS="-O2 -march=core2 -maes -mavx -mcx16 -mpclmul -mpopcnt -msahf"
+# export CFLAGS="${CFLAGS} -msse -msse2 -msse3 -msse4 -msse4.1 -msse4.2 -mssse3 -mtune=generic"
+# export CFLAGS="${CFLAGS} -pipe -fomit-frame-pointer -s"
+export CFLAGS="-O2 -march=core2 -mcx16 -msahf -maes -mpclmul -mpopcnt -mavx"
+export CFLAGS="${CFLAGS} --param l1-cache-size=32 --param l1-cache-line-size=64 --param l2-cache-size=25600 -mtune=generic"
 export CFLAGS="${CFLAGS} -pipe -fomit-frame-pointer -s"
 
 # export CFLAGS="-I/tmp/local/include -I/tmp/libxml2/include/libxml2"
