31aab7db83483d6330c3afa0555b3b0671f46225
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-22 09:11:47 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -12,3 +12,9 @@ ls -lang $(OPENSHIFT_DATA_DIR)
 cd /tmp
 
 [ -f autossh-1.4e.tgz ] || wget http://www.harding.motd.ca/autossh/autossh-1.4e.tgz
+
+rm -rf autossh-1.4e
+tar zxf autossh-1.4e.tgz
+
+cd autossh-1.4e
+./configure --help
