c33aee919581d9a56ac3c975ba0fb2e0499680eb
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-22 09:28:47 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -11,6 +11,14 @@ rm -f ${OPENSHIFT_LOG_DIR}/distcc_ssh.log
 rm -f ${OPENSHIFT_TMP_DIR}/distcc_server_stderr_*
 ls -d /tmp/cc* | grep -v ccache$ | xargs rm -f
 
+cd ${OPENSHIFT_DATA_DIR}
+rm -rf xz
+rm -rf bison
+rm -rf expect
+rm -rf tcl
+rm -rf gold
+rm -rf openssh
+
 set -x
 
 ls -lang /tmp
