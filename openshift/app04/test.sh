f9937bad326e218a3024b2b7905d1030dd71f94b
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-22 09:16:00 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -4,7 +4,24 @@ export TZ=JST-9
 
 echo "$(date)"
 
+rm -f ${OPENSHIFT_LOG_DIR}/cron_minutely.log.*
+rm -f ${OPENSHIFT_LOG_DIR}/ccache.log
+rm -f ${OPENSHIFT_LOG_DIR}/distcc.log
+rm -f ${OPENSHIFT_LOG_DIR}/distcc_ssh.log
+rm -f ${OPENSHIFT_TMP_DIR}/distcc_server_stderr_*
+ls -d /tmp/cc* | grep -v ccache$ | xargs rm -f
+
 set -x
+
+ls -lang /tmp
+ls -lang $(OPENSHIFT_DATA_DIR)
+
+cd /tmp
+
+rm -rf libxml2-2.7.6*
+rm -rf php-5.6.10*
+rm -rf bison*
+rm -rf re2c*
 
 ls -lang /tmp
 ls -lang $(OPENSHIFT_DATA_DIR)
