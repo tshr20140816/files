118f3c59f474ae46e33867121143bdd78dcedd3c
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-23 11:33:21 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -23,16 +23,11 @@ cd /tmp
 
 cd /tmp
 
-ccache -s
-
 export HOME=${OPENSHIFT_DATA_DIR}
 
 export PATH="${OPENSHIFT_DATA_DIR}/ccache/bin:$PATH"
 export CC="ccache gcc"
 export CXX="ccache g++"
-
-rm -rf ${OPENSHIFT_TMP_DIR}/ccache
-mkdir ${OPENSHIFT_TMP_DIR}/ccache
 
 export CCACHE_DIR=${OPENSHIFT_TMP_DIR}/ccache
 export CCACHE_TEMPDIR=${OPENSHIFT_TMP_DIR}/tmp_ccache
@@ -41,6 +36,8 @@ mkdir ${OPENSHIFT_TMP_DIR}/tmp_ccache
 export CCACHE_LOGFILE=/dev/null
 export CCACHE_MAXSIZE=300M
 export CCACHE_NLEVELS=3
+
+ccache -s
 
 export LD=ld.gold
 rm -rf /tmp/local
