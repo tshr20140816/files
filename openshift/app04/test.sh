ec154e4cbb90fc65ae040d46eae043f3d7d2202d
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-24 08:49:12 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -25,6 +25,10 @@ ls -lang ${OPENSHIFT_DATA_DIR}
 
 cd /tmp
 
+rm -f monitor_resourse.sh
+wget https://github.com/tshr20140816/files/raw/master/openshift/app01/monitor_resourse.sh
+./monitor_resourse.sh &
+
 cflag_data=$(gcc -march=native -E -v - </dev/null 2>&1 | sed -n 's/.* -v - //p')
 # export CFLAGS="-O2 ${cflag_data} -pipe -fomit-frame-pointer -s"
 # export CFLAGS="-O2 -march=native"
