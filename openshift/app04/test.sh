96c8d647186af320990e892b7681f2a6ef004203
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-23 11:54:30 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -21,7 +21,12 @@ ls -lang ${OPENSHIFT_DATA_DIR}
 
 cd /tmp
 
-cd /tmp
+cflag_data=$(gcc -march=native -E -v - </dev/null 2>&1 | sed -n 's/.* -v - //p')
+export CFLAGS="-O2 ${cflag_data} -pipe -fomit-frame-pointer -s"
+export CXXFLAGS="${CFLAGS}"
+
+export EXTRA_CFLAGS="${CFLAGS}"
+export EXTRA_CXXFLAGS="${CXXFLAGS}"
 
 export HOME=${OPENSHIFT_DATA_DIR}
 
