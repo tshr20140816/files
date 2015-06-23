03ca561100e2b533b986f5130589afe1b552bdb0
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-24 08:50:38 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -84,7 +84,7 @@ export PATH=${OPENSHIFT_DATA_DIR}/apache/bin:$PATH
 
 tmp_string=$(echo ${DISTCC_HOSTS} | sed -e "s|/4:|/1:|g")
 export DISTCC_HOSTS="${tmp_string}"
-# export MAKEOPTS="-j2"
+export MAKEOPTS="-j2"
 # 32MB
 export RUBY_GC_MALLOC_LIMIT=33554432
 
