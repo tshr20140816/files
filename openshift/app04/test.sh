1cc6a8a49e1d16eeac28bca62de946850fab4d32
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-24 10:01:47 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -30,6 +30,11 @@ wget https://github.com/tshr20140816/files/raw/master/openshift/app01/monitor_re
 chmod +x monitor_resourse.sh
 ./monitor_resourse.sh &
 
+touch ${OPENSHIFT_LOG_DIR}/distcc.log
+touch ${OPENSHIFT_LOG_DIR}/distcc_ssh.log
+tail -f ${OPENSHIFT_LOG_DIR}/distcc.log &
+tail -f ${OPENSHIFT_LOG_DIR}/distcc_ssh.log &
+
 cflag_data=$(gcc -march=native -E -v - </dev/null 2>&1 | sed -n 's/.* -v - //p')
 # export CFLAGS="-O2 ${cflag_data} -pipe -fomit-frame-pointer -s"
 # export CFLAGS="-O2 -march=native"
