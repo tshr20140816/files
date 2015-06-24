1cb0d4331b921ab4f225e0ff0b72ecc8b95fbf39
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-25 08:26:45 +0900

    Update functions.sh

--- openshift/app01/functions.sh
+++ openshift/app01/functions.sh
@@ -25,6 +25,8 @@ function010() {
             export PATH="${OPENSHIFT_DATA_DIR}/ccache/bin:$PATH"
             export CCACHE_DIR=${OPENSHIFT_TMP_DIR}/ccache
             export CCACHE_TEMPDIR=${OPENSHIFT_TMP_DIR}/tmp_ccache
+            rm -rf ${CCACHE_TEMPDIR}
+            mkdir -p ${CCACHE_TEMPDIR}
             # export CCACHE_LOGFILE=${OPENSHIFT_LOG_DIR}/ccache.log
             export CCACHE_LOGFILE=/dev/null
             export CCACHE_NLEVELS=3
