e45edaeb2fb6d41ae1b72e12cd78e77777a801ad
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-25 10:15:41 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -48,11 +48,10 @@ pid_3=$!
 tail -f ${OPENSHIFT_LOG_DIR}/distcc_ssh.log &
 pid_4=$!
 
-cd ${OPENSHIFT_DATA_DIR}/.gem/passenger-*
+pushd ${OPENSHIFT_DATA_DIR}/.gem/gems/passenger-*
 sed -i -e 's|make -j2|make -j6|g' common_library.rb
 sed -i -e 's|cflags = "#{EXTRA_CFLAGS} -w"|cflags = "-O2 -w"|g' common_library.rb
-
-cd /tmp
+popd
 
 cflag_data=$(gcc -march=native -E -v - </dev/null 2>&1 | sed -n 's/.* -v - //p')
 # export CFLAGS="-O2 ${cflag_data} -pipe -fomit-frame-pointer -s"
