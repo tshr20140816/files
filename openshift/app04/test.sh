d40af5b0d8caa30b85820ce331aa51fc1f8dfba0
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-26 08:49:03 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -11,3 +11,19 @@ set -x
 
 ls -lang /tmp
 ls -lang ${OPENSHIFT_DATA_DIR}
+
+cd /tmp
+rm -f ccGfPr09.s
+rm -f distcc_1af7ff60.ii
+rm -f distcc_b4f80226.ii
+rm -f distcc_server_stderr_b144bc50.txt
+rm -rf man
+cd $OPENSHIFT_DATA_DIR
+rm -rf apache
+rm -rf .gem
+rm -rf .rbenv
+tree .pki
+tree .subversion
+
+ls -lang /tmp
+ls -lang ${OPENSHIFT_DATA_DIR}
