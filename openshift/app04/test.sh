f4d3fc45f4ee69ef5177648762c55b0961f01535
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-25 09:22:59 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -39,7 +39,7 @@ pid_1=$!
 touch ${OPENSHIFT_LOG_DIR}/ccache.log
 touch ${OPENSHIFT_LOG_DIR}/distcc.log
 touch ${OPENSHIFT_LOG_DIR}/distcc_ssh.log
-tail -f ${OPENSHIFT_LOG_DIR}/ccache.log | grep -v Config &
+tail -f ${OPENSHIFT_LOG_DIR}/ccache.log &
 pid_2=$!
 tail -f ${OPENSHIFT_LOG_DIR}/distcc.log &
 pid_3=$!
