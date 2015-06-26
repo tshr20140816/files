c733b23eea394766d9d1307c2c07ed57a6fe68e0
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-26 09:13:38 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -16,10 +16,7 @@ cflag_data=$(gcc -march=native -E -v - </dev/null 2>&1 | sed -n 's/.* -v - //p')
 export CFLAGS="-O2 ${cflag_data} -pipe -fomit-frame-pointer -s"
 export CXXFLAGS="${CFLAGS}"
 
-cd $OPENSHIFT_DATA_DIR
-rm -rf .pki
-rm -rf .subversion
-
+if [ 1 -eq 0 ]; then
 cd /tmp
 wget http://ftp.riken.jp/net/apache//httpd/httpd-2.2.29.tar.bz2
 
@@ -32,5 +29,6 @@ cd httpd-2.2.29
  --enable-mods-shared='all proxy'
 
 time make -j4
-
 make install
+fi
+tree ${OPENSHIFT_DATA_DIR}/apache
