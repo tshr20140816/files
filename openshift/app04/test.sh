3aba385b44c0074cc021ad9fdc9c3919f4156b93
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-06-30 10:14:15 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -12,8 +12,6 @@ set -x
 ls -lang /tmp
 ls -lang ${OPENSHIFT_DATA_DIR}
 
-exit
-
 cflag_data=$(gcc -march=native -E -v - </dev/null 2>&1 | sed -n 's/.* -v - //p')
 export CFLAGS="-O2 ${cflag_data} -pipe -fomit-frame-pointer -s"
 export CXXFLAGS="${CFLAGS}"
@@ -36,43 +34,6 @@ cd $OPENSHIFT_DATA_DIR
 rm -rf apache/manual
 fi
 
-if [ 1 -eq 0 ]; then
-cd /tmp
-rm rbenv-installer
-wget https://raw.github.com/Seppone/openshift-rbenv-installer/master/bin/rbenv-installer
-bash rbenv-installer
-rm rbenv-installer
-
-export GEM_HOME=${OPENSHIFT_DATA_DIR}.gem
-export RBENV_ROOT=${OPENSHIFT_DATA_DIR}/.rbenv
-export PATH="${OPENSHIFT_DATA_DIR}/.rbenv/bin:$PATH"
-export PATH="${OPENSHIFT_DATA_DIR}/.gem/bin:$PATH"
-eval "$(rbenv init -)"
-
-time \
- CONFIGURE_OPTS="--disable-install-doc --mandir=${OPENSHIFT_TMP_DIR}/man --docdir=${OPENSHIFT_TMP_DIR}/doc" \
- RUBY_CONFIGURE_OPTS="--with-out-ext=tk,tk/*" \
- MAKE_OPTS="-j $(grep -c -e processor /proc/cpuinfo)" \
- rbenv install -v 2.1.6
-
-tree ${OPENSHIFT_DATA_DIR}/.rbenv
-tree ${OPENSHIFT_DATA_DIR}/.gem
-
-rbenv global 2.1.6
-rbenv rehash
-
-find ${OPENSHIFT_DATA_DIR}/.rbenv/versions/ -name resolv.rb -type f -print0 \
- | xargs -0 perl -pi -e "s/0\.0\.0\.0/${OPENSHIFT_PHP_IP}/g"
-
-for gem in bundler rack passenger
-do
-    time rbenv exec gem install ${gem} --no-rdoc --no-ri --debug \
-     -V -- --with-cflags=\"${CFLAGS}\"
-    rbenv rehash
-done
-fi
-
-if [ 1 -eq 0 ]; then
 cd /tmp
 
 export HOME=${OPENSHIFT_DATA_DIR}
@@ -82,6 +43,8 @@ export CC="ccache gcc"
 export CXX="ccache g++"
 export CCACHE_COMPILERCHECK=none
 export CCACHE_DIR=${OPENSHIFT_TMP_DIR}/ccache
+rm -rf ${CCACHE_DIR}
+mkdir ${CCACHE_DIR}
 export CCACHE_TEMPDIR=${OPENSHIFT_TMP_DIR}/tmp_ccache
 rm -rf ${OPENSHIFT_TMP_DIR}/tmp_ccache
 mkdir ${OPENSHIFT_TMP_DIR}/tmp_ccache
@@ -97,28 +60,4 @@ mkdir -p /tmp/local/bin
 cp -f /tmp/ld.gold /tmp/local/bin/
 export PATH="/tmp/local/bin:$PATH"
 
-export GEM_HOME=${OPENSHIFT_DATA_DIR}.gem
-export RBENV_ROOT=${OPENSHIFT_DATA_DIR}/.rbenv
-export PATH="${OPENSHIFT_DATA_DIR}/.rbenv/bin:$PATH"
-export PATH="${OPENSHIFT_DATA_DIR}/.gem/bin:$PATH"
-eval "$(rbenv init -)" 
-export PATH=${OPENSHIFT_DATA_DIR}/apache/bin:$PATH
-
-rbenv global 2.1.6
-rbenv rehash
-
-time ${OPENSHIFT_DATA_DIR}/.gem/bin/passenger-install-apache2-module \
- --auto \
- --languages ruby \
- --apxs2-path ${OPENSHIFT_DATA_DIR}/apache/bin/apxs
-
 ccache -s
-
-tar Jcf ccache_passenger-install-apache2-module.tar.xz ccache
-fi
-
-tree ${OPENSHIFT_DATA_DIR}/.gem
-
-find ${OPENSHIFT_DATA_DIR}/.gem -name '*.md' -type f -print0 | xargs -0i -t -P 4 -n 3 rm -f {}
-
-tree ${OPENSHIFT_DATA_DIR}/.gem
