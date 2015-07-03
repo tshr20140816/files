7da05dfd5a9d893d3e568ff84ca480bbc325cacd
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-07-03 15:32:09 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -9,8 +9,16 @@ echo ${OPENSHIFT_PHP_IP}
 
 set -x
 
+rm -rf libmemcached-1.0.18
+rm -rf openssh-6.6p1
+rn -f ruby-build.20150626092624.123468.log
+rm -rf httpd-2.2.29
+rm -f httpd-2.2.29.tar.bz2
+
 ls -lang /tmp
 ls -lang ${OPENSHIFT_DATA_DIR}
+
+exit
 
 cflag_data=$(gcc -march=native -E -v - </dev/null 2>&1 | sed -n 's/.* -v - //p')
 export CFLAGS="-O2 ${cflag_data} -pipe -fomit-frame-pointer -s"
