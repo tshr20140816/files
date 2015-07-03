685ac38c5b19c4e2ef5d38d1fd9157f27a531a12
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-07-03 15:29:17 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -9,8 +9,6 @@ echo ${OPENSHIFT_PHP_IP}
 
 set -x
 
-find / -name apxs -print 2>/dev/null
-
 ls -lang /tmp
 ls -lang ${OPENSHIFT_DATA_DIR}
 
@@ -19,30 +17,23 @@ export CFLAGS="-O2 ${cflag_data} -pipe -fomit-frame-pointer -s"
 export CXXFLAGS="${CFLAGS}"
 
 cd /tmp
-rm -f libmemcached-${libmemcached_version}.tar.gz
-rm -rf libmemcached-${libmemcached_version}
-openssh_version=6.6p1
 
-pushd ${OPENSHIFT_TMP_DIR} > /dev/null
-wget http://ftp.jaist.ac.jp/pub/OpenBSD/OpenSSH/portable/openssh-${openssh_version}.tar.gz
-wget http://downloads.sourceforge.net/project/hpnssh/HPN-SSH%2014.5%206.6p1/openssh-6.6p1-hpnssh14v5.diff.gz
-tar zxf openssh-${openssh_version}.tar.gz
-gzip -d openssh-6.6p1-hpnssh14v5.diff.gz
-popd > /dev/null
-pushd ${OPENSHIFT_TMP_DIR}/openssh-${openssh_version} > /dev/null
-patch -p1 < ../openssh-6.6p1-hpnssh14v5.diff
-./configure --help
+wget http://ftp.riken.jp/net/apache//httpd/httpd-2.2.29.tar.bz2
+
+tar jxf httpd-2.2.29.tar.bz2
+cd httpd-2.2.29
 ./configure \
- --prefix=${OPENSHIFT_DATA_DIR}/openssh \
- --infodir=${OPENSHIFT_TMP_DIR}/info \
+ --prefix=${OPENSHIFT_DATA_DIR}/apache2 \
  --mandir=${OPENSHIFT_TMP_DIR}/man \
  --docdir=${OPENSHIFT_TMP_DIR}/doc \
- --disable-etc-default-login \
- --disable-utmp \
- --disable-utmpx \
- --disable-wtmp \
- --disable-wtmpx \
- --with-lastlog=${OPENSHIFT_LOG_DIR}/ssh_lastlog.log
-time make -j$(grep -c -e processor /proc/cpuinfo)
+ --enable-mods-shared='all proxy'
 
-popd > /dev/null
+time make -j4
+make install
+cd $OPENSHIFT_DATA_DIR
+rm -rf ./apache2/manual
+
+cd apache2
+./bin/apachectl -v
+
+tree ./
