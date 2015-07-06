ff237894a668c51c1b128d086b5174d09676c3b7
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-07-06 10:04:58 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# 1508
-
 export TZ=JST-9
 
 echo "$(date)"
@@ -11,13 +9,6 @@ set -x
 
 cd /tmp
 
-wget https://files4-20150524.rhcloud.com/files/5599764d4382ec427c0001d9_maked_ruby_2.1.6_rbenv.tar.xz
-wget https://files4-20150524.rhcloud.com/files/5599764d4382ec427c0001d9_maked_php-5.6.10.tar.xz
-
 ls -lang
-
-rm -rf php-5.6.10
-
-tar Jxf 5599764d4382ec427c0001d9_maked_php-5.6.10.tar.xz
 
 tree php-5.6.10
