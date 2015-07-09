6970e9479ac908a5bf651bfd3b45bf4edb1d13c8
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-07-09 10:51:32 +0900

    Update functions.sh

--- openshift/app01/functions.sh
+++ openshift/app01/functions.sh
@@ -139,7 +139,7 @@ function010() {
                 done
             fi
             # 最後はこの関数を使用せずに restart する
-            /usr/bin/gear stop --cart phpmyadmin
+            /usr/bin/gear stop --cart phpmyadmin &
         fi
     fi
 
