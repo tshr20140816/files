9ac86c3b883e4f49ead98815b419ee590e867dfa
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-07-23 09:10:30 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -4,15 +4,4 @@ set -x
 
 cd /tmp
 
-rm gcc-4.8.4.tar.bz2
-rm -rf gcc-4.8.4
-
-exit
-
-wget http://ftp.tsukuba.wide.ad.jp/software/gcc/releases/gcc-4.8.4/gcc-4.8.4.tar.bz2
-tar jxf gcc-4.8.4.tar.bz2
-cd gcc-4.8.4
-mkdir work
-cd work
-../configure --help
-../configure --prefix=${OPENSHIFT_DATA_DIR}/gnu
+quota -s
