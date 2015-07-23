d993072e7e51518eadf88eef75253356df15ab8a
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-07-23 17:20:35 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -11,6 +11,15 @@ ls -lang
 /tmp/gcc/usr/bin/gcc --version
 /tmp/gcc/usr/bin/gcc --help
 
+cd gcc
+wget http://ftp-srv2.kddilabs.jp/Linux/packages/fedora/releases/21/Server/x86_64/os/Packages/g/glibc-2.20-5.fc21.x86_64.rpm
+rpm2cpio glibc-2.20-5.fc21.x86_64.rpm | cpio -idmv
+tree ./
+
+cd /tmp
+/tmp/gcc/usr/bin/gcc --version
+/tmp/gcc/usr/bin/gcc --help
+
 exit
 
 # wget http://ftp-srv2.kddilabs.jp/Linux/packages/fedora/releases/21/Server/x86_64/os/Packages/g/gcc-4.9.2-1.fc21.x86_64.rpm
