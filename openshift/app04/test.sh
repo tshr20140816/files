2c1d168147343a97f9a17a05c4567a0599f84be2
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-07-23 09:46:02 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -6,4 +6,5 @@ cd /tmp
 
 quota -s
 
-wget http://download.fedora.redhat.com/pub/fedora/linux/updates/21/x86_64/g/gcc-4.9.2-6.fc21.x86_64.rpm
+wget http://ftp-srv2.kddilabs.jp/Linux/packages/fedora/releases/21/Server/x86_64/os/Packages/g/gcc-4.9.2-1.fc21.x86_64.rpm
+
