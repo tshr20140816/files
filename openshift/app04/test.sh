8442a363bdfedbf47e6f1ecd000f1ce8a1d1d18a
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-07-23 17:16:20 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -8,6 +8,11 @@ cd /tmp
 
 ls -lang
 
+/tmp/gcc/usr/bin/gcc --version
+/tmp/gcc/usr/bin/gcc --help
+
+exit
+
 # wget http://ftp-srv2.kddilabs.jp/Linux/packages/fedora/releases/21/Server/x86_64/os/Packages/g/gcc-4.9.2-1.fc21.x86_64.rpm
 
 mkdir gcc
