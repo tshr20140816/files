3730669aaf3931429ea42284edcbbf684aefbd60
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-07-23 16:27:26 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -18,7 +18,7 @@ cd /tmp
 
 # wget http://rpm.org/releases/rpm-4.8.x/rpm-4.8.0.tar.bz2
 
-export CPPFLAGS="-I/tmp/nss/usr/include/nss3 -I/usr/include/nspr4"
+export CPPFLAGS="-I/tmp/nss/usr/include/nss3 -I/usr/include/nspr4 -L/tmp/nss/usr/lib64"
 
 # tar jxf rpm-4.8.0.tar.bz2
 cd rpm-4.8.0
