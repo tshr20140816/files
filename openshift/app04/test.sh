f133377b4e0b1dfddb8f5b6e4c6d4c8119c967d4
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-07-24 13:28:46 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -6,10 +6,6 @@ set -x
 
 cd /tmp
 
-# wget http://ftp.tsukuba.wide.ad.jp/software/gcc/releases/gcc-4.9.3/gcc-4.9.3.tar.bz2
-tar jxf gcc-4.9.3.tar.bz2
-
-cd gcc-4.9.3
-rm -rf libjava
+rm -rf gcc*
 
 quota -s
