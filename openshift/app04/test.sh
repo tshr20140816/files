88d388cf9579923df215ae5bc2b49f9c542b30de
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-07-30 09:54:31 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -9,12 +9,19 @@ export CXXFLAGS="${CFLAGS}"
 
 cd /tmp
 
+rm -rf gmp*
+
+cd /tmp
+
 wget http://ftp.jaist.ac.jp/pub/GNU/gmp/gmp-4.3.2.tar.bz2
 tar jxf gmp-4.3.2.tar.bz2
 cd gmp-4.3.2
 ./configure --help
-./configure --prefix=/tmp/gcc --disable-shared --enable-static
+./configure --prefix=/tmp/gcc --disable-shared --enable-static --infodir=/dev/null --mandir=/dev/null --docdir=/dev/null
 time make -j4
 make install
 
+cd /tmp
+
 quota -s
+ls -lang
