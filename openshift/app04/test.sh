6f71eba2fad43e3139964331f34b3668f6c5dfa9
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-07-30 09:46:29 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -4,8 +4,17 @@
 
 set -x
 
+export CFLAGS="-O2 -march=native -pipe -fomit-frame-pointer -s"
+export CXXFLAGS="${CFLAGS}"
+
 cd /tmp
 
-rm -rf gcc*
+wget http://ftp.jaist.ac.jp/pub/GNU/gmp/gmp-4.3.2.tar.bz2
+tar jxf gmp-4.3.2.tar.bz2
+cd gmp-4.3.2
+./configure --help
+./configure --prefix=/tmp/gcc --disable-shared --enable-static
+time make -j4
+make install
 
 quota -s
