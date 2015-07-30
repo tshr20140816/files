f742df481201b4e33cb89ae9ab99f9dc0d1f73fd
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-07-30 10:03:40 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -9,7 +9,11 @@ export CXXFLAGS="${CFLAGS}"
 
 cd /tmp
 
+rm -rf /tmp/gomi
+# rm -rf gcc
 rm -rf gmp*
+rm -rf mpfr*
+rm -rf mpc*
 
 if [ 1 -eq 1 ]; then
 cd /tmp
@@ -32,6 +36,19 @@ tar Jxf mpfr-2.4.2.tar.xz
 cd mpfr-2.4.2
 ./configure --help
 ./configure --prefix=/tmp/gcc --disable-shared --enable-static --with-gmp=/tmp/gcc \
+ --infodir=/tmp/gomi --mandir=/tmp/gomi --docdir=/tmp/gomi
+time make -j4
+make install
+fi
+
+cd /tmp
+
+if [ 1 -eq 1 ]; then
+wget http://ftp.jaist.ac.jp/pub/GNU/mpc/mpc-1.0.3.tar.gz
+tar zxf mpc-1.0.3.tar.gz
+cd mpc-1.0.3
+./configure --help
+./configure --prefix=/tmp/gcc --disable-shared --enable-static --with-gmp=/tmp/gcc --with-mpfr=/tmp/gcc \
  --infodir=/tmp/gomi --mandir=/tmp/gomi --docdir=/tmp/gomi
 time make -j4
 make install
