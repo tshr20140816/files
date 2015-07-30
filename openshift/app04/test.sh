2600ae34178399009416f79ffbd35cd33d3c0ea4
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-07-30 10:00:18 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -11,15 +11,31 @@ cd /tmp
 
 rm -rf gmp*
 
+if [ 1 -eq 1 ]; then
 cd /tmp
 
 wget http://ftp.jaist.ac.jp/pub/GNU/gmp/gmp-4.3.2.tar.bz2
 tar jxf gmp-4.3.2.tar.bz2
 cd gmp-4.3.2
 ./configure --help
-./configure --prefix=/tmp/gcc --disable-shared --enable-static --infodir=/dev/null --mandir=/dev/null --docdir=/dev/null
+./configure --prefix=/tmp/gcc --disable-shared --enable-static \
+ --infodir=/tmp/gomi --mandir=/tmp/gomi --docdir=/tmp/gomi
 time make -j4
 make install
+fi
+
+cd /tmp
+
+if [ 1 -eq 1 ]; then
+wget http://ftp.jaist.ac.jp/pub/GNU/mpfr/mpfr-2.4.2.tar.xz
+tar Jxf mpfr-2.4.2.tar.xz
+cd mpfr-2.4.2
+./configure --help
+./configure --prefix=/tmp/gcc --disable-shared --enable-static --with-gmp=/tmp/gcc \
+ --infodir=/tmp/gomi --mandir=/tmp/gomi --docdir=/tmp/gomi
+time make -j4
+make install
+fi
 
 cd /tmp
 
