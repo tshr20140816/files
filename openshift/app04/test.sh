66a3f6942b00ee00480e45cb0e9f9089f54c8639
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-08-18 10:10:22 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -9,10 +9,11 @@ cd /tmp
 rm -rf ${OPENSHIFT_DATA_DIR}/.gnupg
 mkdir ${OPENSHIFT_DATA_DIR}/.gnupg
 export GNUPGHOME=${OPENSHIFT_DATA_DIR}/.gnupg
-chmod 700 ${GNUPGHOME}
+# chmod 700 ${GNUPGHOME}
 ls -la ${GNUPGHOME}
 gpg --list-keys
 echo "keyserver hkp://keyserver.ubuntu.com:80" >> ${GNUPGHOME}/gpg.conf
+chmod 600 ${GNUPGHOME}/gpg.conf
 
 # rm -f cadaver*
 # wget http://www.webdav.org/cadaver/cadaver-0.23.3.tar.gz
