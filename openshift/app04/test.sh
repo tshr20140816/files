e11e16999e18315a6d4379539f7f07d20aad00e6
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-08-19 14:17:54 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -8,6 +8,39 @@ quota -s
 
 cd /tmp
 
+wget http://ftp.jaist.ac.jp/pub/GNU/gmp/gmp-4.3.2.tar.bz2
+tar jxf gmp-4.3.2.tar.bz2
+cd gmp-4.3.2
+./configure --help
+./configure --prefix=/tmp/gcc --disable-shared --enable-static \
+ --infodir=/tmp/gomi --mandir=/tmp/gomi --docdir=/tmp/gomi
+time make -j4
+make install
+
+cd /tmp
+
+wget http://ftp.jaist.ac.jp/pub/GNU/mpfr/mpfr-2.4.2.tar.xz
+tar Jxf mpfr-2.4.2.tar.xz
+cd mpfr-2.4.2
+./configure --help
+./configure --prefix=/tmp/gcc --disable-shared --enable-static --with-gmp=/tmp/gcc \
+ --infodir=/tmp/gomi --mandir=/tmp/gomi --docdir=/tmp/gomi
+time make -j4
+make install
+
+cd /tmp
+
+wget http://ftp.jaist.ac.jp/pub/GNU/mpc/mpc-1.0.3.tar.gz
+tar zxf mpc-1.0.3.tar.gz
+cd mpc-1.0.3
+./configure --help
+./configure --prefix=/tmp/gcc --disable-shared --enable-static --with-gmp=/tmp/gcc --with-mpfr=/tmp/gcc \
+ --infodir=/tmp/gomi --mandir=/tmp/gomi --docdir=/tmp/gomi
+time make -j4with-mpfr
+make install
+
+cd /tmp
+
 # wget http://ftp.tsukuba.wide.ad.jp/software/gcc/releases/gcc-4.4.7/gcc-core-4.4.7.tar.bz2
 # wget http://ftp.tsukuba.wide.ad.jp/software/gcc/releases/gcc-4.4.7/gcc-g++-4.4.7.tar.bz2
 
@@ -18,7 +51,7 @@ ls -lang
 
 cd gcc-4.4.7
 ./configure --help
-./configure
+./configure --with-gmp=/tmp/gcc --with-mpfr=/tmp/gcc
 
 quota -s
 exit
