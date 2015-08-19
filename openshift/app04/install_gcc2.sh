caa359bde27be608feed9600f712b9d23825f13b
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-08-20 08:48:37 +0900

    Update install_gcc2.sh

--- openshift/app04/install_gcc2.sh
+++ openshift/app04/install_gcc2.sh
@@ -2,12 +2,10 @@
 
 set -x
 
-quota -s
-
 cd /tmp
 
-export CFLAGS="-O2 -march=native -pipe -fomit-frame-pointer -s"
-export CXXFLAGS="${CFLAGS}"
+# export CFLAGS="-O2 -march=native -pipe -fomit-frame-pointer -s"
+# export CXXFLAGS="${CFLAGS}"
 
 wget http://ftp.jaist.ac.jp/pub/GNU/gmp/gmp-4.3.2.tar.bz2
 tar jxf gmp-4.3.2.tar.bz2
@@ -15,7 +13,7 @@ cd gmp-4.3.2
 ./configure --help
 ./configure --prefix=/tmp/gcc --disable-shared --enable-static \
  --infodir=/tmp/gomi --mandir=/tmp/gomi --docdir=/tmp/gomi
-time make -j4
+time make -j2
 make install
 rm -rf /tmp/gmp-4.3.2
 
@@ -27,7 +25,7 @@ cd mpfr-2.4.2
 ./configure --help
 ./configure --prefix=/tmp/gcc --disable-shared --enable-static --with-gmp=/tmp/gcc \
  --infodir=/tmp/gomi --mandir=/tmp/gomi --docdir=/tmp/gomi
-time make -j4
+time make -j2
 make install
 rm -rf /tmp/mpfr-2.4.2
 
@@ -39,24 +37,27 @@ cd mpc-1.0.3
 ./configure --help
 ./configure --prefix=/tmp/gcc --disable-shared --enable-static --with-gmp=/tmp/gcc --with-mpfr=/tmp/gcc \
  --infodir=/tmp/gomi --mandir=/tmp/gomi --docdir=/tmp/gomi
-time make -j4
+time make -j2
 make install
 rm -rf /tmp/mpc-1.0.3
 
 cd /tmp
 
-wget http://ftp.tsukuba.wide.ad.jp/software/gcc/releases/gcc-4.4.7/gcc-core-4.4.7.tar.bz2
-wget http://ftp.tsukuba.wide.ad.jp/software/gcc/releases/gcc-4.4.7/gcc-g++-4.4.7.tar.bz2
+wget http://ftp.tsukuba.wide.ad.jp/software/gcc/releases/gcc-4.9.3/gcc-4.9.3.tar.bz2
 
-tar jxf gcc-core-4.4.7.tar.bz2
-tar jxf gcc-g++-4.4.7.tar.bz2
+tar jxf gcc-4.9.3.tar.bz2
 
-cd gcc-4.4.7
-./configure --help
-./configure --with-gmp=/tmp/gcc --with-mpfr=/tmp/gcc --prefix=/tmp/gcc \
- --infodir=/tmp/gomi --mandir=/tmp/gomi --disable-multilib
-time make
+cd gcc-4.9.3
+mkdir work
+cd work
+../configure --help
+../configure --with-gmp=/tmp/gcc --with-mpfr=/tmp/gcc --prefix=/tmp/gcc \
+ --infodir=/tmp/gomi --mandir=/tmp/gomi --enable-multilib --enable-stage1-languages=c,c++ \
+ --enable-stage1-checking=c,c++ target=x86_64-redhat-linux-gnu \
+ --disable-libjava --disable-libgo --disable-libgfortran --enable-languages=c,c++
+time make -j2
 make install
-rm -rf /tmp/gcc-4.4.7
+rm -rf /tmp/gcc-4.9.3
 
+cd /tmp
 tree /tmp/gcc
