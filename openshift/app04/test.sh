26dbfedd3ee21bb3148d0de00c32b71402361518
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-08-21 08:52:14 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-exit
+set -x
+
+quota -s
+
+cd /tmp
+
+export PATH="${OPENSHIFT_TMP_DIR}/gcc/bin:$PATH"
+export LD_LIBRARY_PATH="${OPENSHIFT_TMP_DIR}/gcc/lib64:$LD_LIBRARY_PATH"
+export CC=gcc-493
+export CXX=gcc-493
+
+gcc-493 --version
