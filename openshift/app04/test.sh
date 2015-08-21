015710795f22ea17990f7facde66cd9ae1fb64ae
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-08-21 09:00:41 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -10,6 +10,8 @@ export PATH="${OPENSHIFT_TMP_DIR}/gcc/bin:$PATH"
 export LD_LIBRARY_PATH="${OPENSHIFT_TMP_DIR}/gcc/lib64:$LD_LIBRARY_PATH"
 export CC=gcc-493
 export CXX=gcc-493
+export CFLAGS="-O2 -march=native -fomit-frame-pointer -s -pipe"
+export CXXFLAGS="${CFLAGS}"
 
 gcc-493 --version
 
