0199f1ef7cfecf8d1e600480d050132a24d5c940
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-08-21 10:28:19 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -27,7 +27,6 @@ pushd ${OPENSHIFT_TMP_DIR}/distcc-${distcc_version} > /dev/null
  --prefix=${OPENSHIFT_DATA_DIR}/distcc \
  --infodir=${OPENSHIFT_TMP_DIR}/info \
  --mandir=${OPENSHIFT_TMP_DIR}/man \
- --docdir=${OPENSHIFT_TMP_DIR}/doc \
  --without-avahi \
  --disable-Werror
 time make -j$(grep -c -e processor /proc/cpuinfo)
