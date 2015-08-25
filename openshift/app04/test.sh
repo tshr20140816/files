c6f73b2e68f36b3f148da96dd1a93b81fd22dc49
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-08-26 08:40:03 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -5,6 +5,6 @@ set -x
 cd ${OPENSHIFT_REPO_DIR}
 
 rm -f test.php
-wget https://github.com/tshr20140816/files/blob/master/openshift/app04/test.php
+wget https://raw.githubusercontent.com/tshr20140816/files/master/openshift/app04/test.php
 
 cat test.php
