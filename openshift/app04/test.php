ea68764f14a37ec846d2236e957fcfe11aceb8fd
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-08-26 17:05:05 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -1,3 +1,11 @@
 <?php
-
+header('Content-type: text/plain; charset=utf-8');
+$fp = fopen("https://packages.debian.org/jessie-backports/admin/", "r");
+while( ! feof($fp)){
+  $buffer = fgets($fp);
+  if(preg_match("/^<dt>/", $buffer)){
+    echo $buffer;
+  }
+}
+fclose($fp);
 ?>
