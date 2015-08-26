c41ea0ca13f99902e32694be94d5f095ed09ceb8
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-08-26 11:38:43 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -1,4 +1,5 @@
 <?php
+header('Content-type: text/plain; charset=utf-8');
 $fp = gzopen("https://packages.debian.org/jessie-backports/allpackages?format=txt.gz", "r");
 while( ! feof($fp)){
   $buffer = fgets($fp) . "<br>";
