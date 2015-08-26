9ad9644efa6bb1fac5d79b89d76c78ef8f2d246a
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-08-26 11:06:19 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -3,6 +3,8 @@ $fp = gzopen("https://packages.debian.org/jessie-backports/allpackages?format=tx
 while( ! feof($fp)){
   $buffer = fgets($fp) . "<br>";
   if( ! preg_match("^Generated.+", $buffer)){
+  } elseif( ! preg_match("^Copyright", $buffer)) {
+  } else {
     echo $buffer;
   }
 }
