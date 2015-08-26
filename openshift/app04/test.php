bed4ce23710fd1dded637aca24a470e2638c7316
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-08-27 08:57:12 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -1,5 +1,21 @@
 <?php
 // https://packages.debian.org/sid/
+$xml = <<< __HEREDOC__
+<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0">
+<channel>
+  <title>packages.debian.org</title>
+  <link>https://packages.debian.org/</link>
+  <description>packages.debian.org</description>
+  <language>ja</language>
+  {0}
+</channel>
+</rss>
+__HEREDOC__;
+
+$item_template = <<< __HEREDOC__
+<item><title>{0}</title><link>https://packages.debian.org{0}</link><description /><pubDate /></item>
+__HEREDOC__;
 
 header('Content-type: text/plain; charset=utf-8');
 $fp = fopen("https://packages.debian.org/sid/", "r");
@@ -13,9 +29,15 @@ while( ! feof($fp)){
     break;
   }
   if($start_flag && preg_match('/ href="(.+?)"/', $buffer, $matchs)){
-    # echo $buffer;
+    // echo $buffer;
     echo $matchs[1] . "\n";
+    $items[] = str_replace("{0}", $matchs[1], $item_template);
   }
 }
+fclose($fp);
+
+$fp = fopen("./packages.xml", "w");
+$buffer = str_replace("{0}", implode($items), $xml);
+fwrite($fp, $buffer);
 fclose($fp);
 ?>
