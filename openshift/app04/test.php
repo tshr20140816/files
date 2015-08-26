1dbba7123c8679c5c789e4144c2f4478b17f732b
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-08-26 11:02:30 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -1,7 +1,10 @@
 <?php
 $fp = gzopen("https://packages.debian.org/jessie-backports/allpackages?format=txt.gz", "r");
-while( ! feof($fp) ){
-  echo fgets($fp) . "<br>";
+while( ! feof($fp)){
+  $buffer = fgets($fp) . "<br>";
+  if( ! preg_match("^Generated:.+", $buffer)){
+    echo $buffer;
+  }
 }
 fclose($fp);
 ?>
