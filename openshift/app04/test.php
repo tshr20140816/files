73e11e2b02e1678f780f885310bedd3f4b2fe5d7
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-08-27 08:35:59 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -5,6 +5,13 @@ header('Content-type: text/plain; charset=utf-8');
 $fp = fopen("https://packages.debian.org/sid/", "r");
 while( ! feof($fp)){
   $buffer = fgets($fp);
+  if($buffer === '<div id="content">'){
+    $start_flag = true;
+    continue;
+  }
+  if($start_flag && $buffer === '<h1>List of sections in "sid"</h1>'){
+    break;
+  }
   if(preg_match("/ href=/", $buffer)){
     echo $buffer;
   }
