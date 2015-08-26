5ab12cafeea5fd6575c7ea96845a32a577867de3
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-08-27 08:39:21 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -12,8 +12,9 @@ while( ! feof($fp)){
   if($start_flag && $buffer === '<h1>List of sections in "sid"</h1>'){
     break;
   }
-  if(preg_match("/ href=/", $buffer)){
+  if(preg_match('/ href="(.+?)"/', $buffer, $matchs)){
     echo $buffer;
+    echo $matchs[1];
   }
 }
 fclose($fp);
