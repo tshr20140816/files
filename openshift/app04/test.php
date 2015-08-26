8c5e089236a52cdf98fc673443a151cd3c4b919b
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-08-26 11:21:57 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -3,7 +3,8 @@ $fp = gzopen("https://packages.debian.org/jessie-backports/allpackages?format=tx
 while( ! feof($fp)){
   $buffer = fgets($fp) . "<br>";
   if(preg_match("/\(/", $buffer)){
-    echo $buffer;
+    $buffer_array = explode(" ", $buffer, 3);
+    echo $buffer_array[0] . $buffer_array[1];
   }
 }
 fclose($fp);
