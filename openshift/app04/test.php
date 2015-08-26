610346b7bfb7403984387e662d2ecfbbcc108793
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-08-26 13:40:47 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -20,12 +20,12 @@ __HEREDOC__;
 header('Content-type: text/xml; charset=utf-8');
 $fp = gzopen("https://packages.debian.org/jessie-backports/allpackages?format=txt.gz", "r");
 while( ! feof($fp)){
-  $buffer = fgets($fp) . "<br>";
+  $buffer = fgets($fp);
   if(preg_match("/\(/", $buffer)){
     list($title, $version) = explode(" ", $buffer, 2);
     list($version, $dummy) = explode(")", $version, 2);
     $version .= ")";
-    echo $title . $version . "\n";
+    // echo $title . $version . "\n";
     $items[] = str_replace("{0}", $title . $version, $item_template);
   }
 }
