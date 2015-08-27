33c9f28f44fdd2ea14e10db75d28c4cf79fbd43a
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-08-27 10:57:57 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -1,3 +1,12 @@
 <?php
+// https://packages.debian.org/jessie-updates/utils/
 
+$fp = fopen("https://packages.debian.org/jessie-updates/utils/", "r");
+while( ! feof($fp)){
+  $buffer = fgets($fp);
+  if(preg_match('/^<dt>/', $buffer)){
+    echo $buffer
+  }
+}
+fclose($fp);
 ?>
