65b7fc4d6f6d2012859e4422528f0a69bb26f53f
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-08-27 11:00:18 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -4,7 +4,7 @@
 $fp = fopen("https://packages.debian.org/jessie-updates/utils/", "r");
 while( ! feof($fp)){
   $buffer = fgets($fp);
-  if(preg_match('/^<dt>/', $buffer)){
+  if(preg_match('/^<dt>.+dt>$/', $buffer)){
     $buffer = preg_replace("/<.+?>/", "", $buffer);
     echo $buffer;
   }
