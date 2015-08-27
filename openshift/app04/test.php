051ca61a4ba079d9f063cab4c51b38bcc8f75c9e
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-08-27 10:10:38 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -26,12 +26,14 @@ foreach($sections as &$section){
     if(preg_match('/<h1>List of sections in /', $buffer)){
       $start_flag = true;
       continue;
-    }
-    if($start_flag && trim($buffer) === '<div id="footer">'){
+    } elseif($start_flag === false) {
       break;
     }
-    if($start_flag && preg_match('/ href="(.+?)"/', $buffer, $matchs)){
-      if($match[1] == "allpackages"){
+    if(trim($buffer) === '<div id="footer">'){
+      break;
+    }
+    if(preg_match('/ href="(.+?)"/', $buffer, $matchs)){
+      if(trim($match[1]) == "allpackages"){
         break;
       }
       echo "https://packages.debian.org" . $section . $matchs[1] . "\n";
