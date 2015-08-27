e14ce1003e655e20ff207488a4262a825004f186
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-08-27 09:57:10 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -1,24 +1,41 @@
 <?php
-// https://packages.debian.org/jessie-updates/
 
-header('Content-type: text/plain; charset=utf-8');
-$fp = fopen("https://packages.debian.org/jessie/", "r");
+$fp = fopen("https://packages.debian.org/sid/", "r");
 while( ! feof($fp)){
   $buffer = fgets($fp);
-  if(preg_match('/<h1>List of sections in /', $buffer)){
+  if(trim($buffer) === '<div id="content">'){
     $start_flag = true;
     continue;
   }
-  if($start_flag && trim($buffer) === '<div id="footer">'){
+  if($start_flag && trim($buffer) === '<h1>List of sections in "sid"</h1>'){
     break;
   }
   if($start_flag && preg_match('/ href="(.+?)"/', $buffer, $matchs)){
-    if($match[1] === "allpackages"){
-      break;
-    }
-    echo $matchs[1] . "\n";
+    $sections[] = $matchs[1];
   }
 }
+$sections[] = "/sid/";
 fclose($fp);
+
+foreach($sections as &$section){
+  $fp = fopen("https://packages.debian.org" . $section, "r");
+  while( ! feof($fp)){
+    $buffer = fgets($fp);
+    if(preg_match('/<h1>List of sections in /', $buffer)){
+      $start_flag = true;
+      continue;
+    }
+    if($start_flag && trim($buffer) === '<div id="footer">'){
+      break;
+    }
+    if($start_flag && preg_match('/ href="(.+?)"/', $buffer, $matchs)){
+      if($match[1] === "allpackages"){
+        break;
+      }
+      echo "https://packages.debian.org" . $section . $matchs[1] . "\n";
+    }
+  }
+  fclose($fp);
+}
 
 ?>
