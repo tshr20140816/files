b7b38e8444f9f7a3104ccc258795d611f51badbb
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-08-27 10:36:38 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -35,10 +35,15 @@ foreach($sections as &$section){
       break;
     }
     if(preg_match('/ href="(.+?)\/"/', $buffer, $matchs)){
-      echo "https://packages.debian.org" . $section . $matchs[1] . "/\n";
+      // echo "https://packages.debian.org" . $section . $matchs[1] . "/\n";
+      $items[] = str_replace("{0}", $section . $matchs[1], $item_template);
     }
   }
   fclose($fp);
 }
 
+$fp = fopen("./debian.package.sections.xml", "w");
+$buffer = str_replace("{0}", implode($items), $xml);
+fwrite($fp, $buffer);
+fclose($fp);
 ?>
