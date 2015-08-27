9d707476fa3005ed14aab2137a8eef6f3fdb72ae
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-08-27 09:43:48 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -1,3 +1,21 @@
 <?php
+// https://packages.debian.org/jessie-updates/
+
+header('Content-type: text/plain; charset=utf-8');
+$fp = fopen("https://packages.debian.org/jessie-updates/", "r");
+while( ! feof($fp)){
+  $buffer = fgets($fp);
+  if(preg_match('/<h1>List of sections in /', $buffer)){
+    $start_flag = true;
+    continue;
+  }
+  if($start_flag && trim($buffer) === '<div id="footer">'){
+    break;
+  }
+  if($start_flag && preg_match('/ href="(.+?)"/', $buffer, $matchs)){
+    echo $matchs[1] . "\n";
+  }
+}
+fclose($fp);
 
 ?>
