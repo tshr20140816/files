ea57241f0e37c9a8486fe7713f3eb2f0aed97c04
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-08-31 15:50:12 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,48 +1,5 @@
 #!/bin/bash
 
-# 1426
-
-# ls -lang ${OPENSHIFT_LOG_DIR}
-
 set -x
 
 quota -s
-
-cd ${OPENSHIFT_LOG_DIR}
-rm php.log-*
-ls -lang ${OPENSHIFT_LOG_DIR}
-
-ls -lang /tmp
-
-exit
-
-cd ${OPENSHIFT_REPO_DIR}
-
-ls -lang
-
-nohup php test.php >${OPENSHIFT_LOG_DIR}test.php.log 2>&1 &
-
-ls -lang
-
-exit
-
-cd ${OPENSHIFT_REPO_DIR}
-
-rm -f debian.*.xml
-
-rm -f test.php
-wget https://raw.githubusercontent.com/tshr20140816/files/master/openshift/app04/test.php
-
-php -l test.php
-cat test.php
-
-cd /tmp
-
-wget https://tenv-20150207.rhcloud.com/test.php
-
-cat test.php
-rm test.php
-
-cd ${OPENSHIFT_REPO_DIR}
-
-ls -lang
