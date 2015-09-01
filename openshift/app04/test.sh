dbc97dc58966e30dbbb113ac80298dd6626f8633
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-01 09:05:09 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -13,3 +13,21 @@ tar Jxf squid-3.5.7.tar.xz
 cd squid-3.5.7
 
 ./configure --help
+
+./configure --prefix=${OPENSHIFT_DATA_DIR}/squid \
+ --mandir=/tmp/gomi \
+ --infodir=/tmp/gomi \
+ --docdir=/tmp/gomi \
+ --disable-dependency-tracking \
+ --enable-shared \
+ --enable-static=no \
+ --enable-fast-install \
+ --disable-icap-client \
+ --disable-wccp \
+ --disable-wccpv2 \
+ --disable-snmp \
+ --disable-eui \
+ --disable-htcp \
+ --disable-devpoll \
+ --disable-ipv6 \
+ --disable-auto-locale
