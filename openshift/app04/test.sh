48311518a88af61a00616638d2bdaa6d4c37a765
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-01 10:29:20 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -6,12 +6,12 @@ quota -s
 
 cd /tmp
 
-rm -f squid-3.5.7.tar.xz
-rm -rf squid-3.5.7
+# rm -f squid-3.5.7.tar.xz
+# rm -rf squid-3.5.7
 
-wget http://www.squid-cache.org/Versions/v3/3.5/squid-3.5.7.tar.xz
+# wget http://www.squid-cache.org/Versions/v3/3.5/squid-3.5.7.tar.xz
 
-tar Jxf squid-3.5.7.tar.xz
+# tar Jxf squid-3.5.7.tar.xz
 
 cd squid-3.5.7
 
@@ -20,22 +20,22 @@ export CXXFLAGS="${CFLAGS}"
 
 ./configure --help
 
-time ./configure --prefix=${OPENSHIFT_DATA_DIR}/squid \
- --mandir=/tmp/gomi \
- --infodir=/tmp/gomi \
- --docdir=/tmp/gomi \
- --disable-dependency-tracking \
- --enable-shared \
- --enable-static=no \
- --enable-fast-install \
- --disable-icap-client \
- --disable-wccp \
- --disable-wccpv2 \
- --disable-snmp \
- --disable-eui \
- --disable-htcp \
- --disable-devpoll \
- --disable-ipv6 \
- --disable-auto-locale
+# time ./configure --prefix=${OPENSHIFT_DATA_DIR}/squid \
+#  --mandir=/tmp/gomi \
+#  --infodir=/tmp/gomi \
+#  --docdir=/tmp/gomi \
+#  --disable-dependency-tracking \
+#  --enable-shared \
+#  --enable-static=no \
+#  --enable-fast-install \
+#  --disable-icap-client \
+#  --disable-wccp \
+#  --disable-wccpv2 \
+#  --disable-snmp \
+#  --disable-eui \
+#  --disable-htcp \
+#  --disable-devpoll \
+#  --disable-ipv6 \
+#  --disable-auto-locale
 
-# time make -j4
+time make -j4
