f19daffa19e029a7957a49a5c01f4f80c52d62c9
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-02 14:20:42 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -52,82 +52,12 @@ rm /var/lib/openshift/554b6b37e0b8cda2a300005d/app-root/data//squid/var/logs/cac
 ${OPENSHIFT_DATA_DIR}/squid/sbin/squid -kparse -f/tmp/squid.conf
 ${OPENSHIFT_DATA_DIR}/squid/sbin/squid -f/tmp/squid.conf
 cat /var/lib/openshift/554b6b37e0b8cda2a300005d/app-root/data//squid/var/logs/cache.log
+
+curl --proxy http://${OPENSHIFT_PHP_IP}:33128 http://www.google.com
+
+cat /var/lib/openshift/554b6b37e0b8cda2a300005d/app-root/data//squid/var/logs/cache.log
+
 tree ${OPENSHIFT_DATA_DIR}/squid/var/run/
 ${OPENSHIFT_DATA_DIR}/squid/sbin/squid -kshutdown
 
 cat /var/lib/openshift/554b6b37e0b8cda2a300005d/app-root/data//squid/var/logs/cache.log
-exit
-
-# tree ${OPENSHIFT_DATA_DIR}/squid
-cp ${OPENSHIFT_DATA_DIR}/squid/etc/* ${OPENSHIFT_LOG_DIR}
-
-exit
-
-export PATH="${OPENSHIFT_DATA_DIR}/ccache/bin:$PATH"
-export CCACHE_DIR=${OPENSHIFT_TMP_DIR}/ccache
-export CCACHE_TEMPDIR=${OPENSHIFT_TMP_DIR}/tmp_ccache
-rm -rf ${CCACHE_TEMPDIR}
-mkdir -p ${CCACHE_TEMPDIR}
-export CCACHE_LOGFILE=${OPENSHIFT_LOG_DIR}/ccache.log
-export CCACHE_NLEVELS=3
-export CCACHE_MAXSIZE=300M
-export CCACHE_COMPILERCHECK=none
-export CC="ccache gcc"
-export CXX="ccache g++"
-
-ccache --show-stats
-ccache --zero-stats 
-
-cd /tmp
-
-# rm -f squid-3.5.7.tar.xz
-rm -rf squid-3.5.7
-
-if [ ! -f squid-3.5.7.tar.xz ]; then
-    wget http://www.squid-cache.org/Versions/v3/3.5/squid-3.5.7.tar.xz
-fi
-
-# tar Jxf squid-3.5.7.tar.xz
-if [ -f squid_src.tar.xz ]; then
-  tar Jxf squid_src.tar.xz
-else
-  tar Jxf squid-3.5.7.tar.xz
-fi
-
-cd squid-3.5.7
-
-export CFLAGS="-O2 -march=native -pipe -fomit-frame-pointer -s"
-export CXXFLAGS="${CFLAGS}"
-
-# ./configure --help
-
-if [ ! -f /tmp/squid_src.tar.xz ]; then
- time ./configure --prefix=${OPENSHIFT_DATA_DIR}/squid \
-  --mandir=/tmp/gomi \
-  --infodir=/tmp/gomi \
-  --docdir=/tmp/gomi \
-  --disable-dependency-tracking \
-  --enable-shared \
-  --enable-static=no \
-  --enable-fast-install \
-  --disable-icap-client \
-  --disable-wccp \
-  --disable-wccpv2 \
-  --disable-snmp \
-  --disable-eui \
-  --disable-htcp \
-  --disable-devpoll \
-  --disable-ipv6 \
-  --disable-auto-locale
-fi
-
-cd ..
-if [ ! -f squid_src.tar.xz ]; then
-  tar Jcf squid_src.tar.xz squid-3.5.7
-fi
-
-cd squid-3.5.7
-
-time make -j4
-
-time make install
