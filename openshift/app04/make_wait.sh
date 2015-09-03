1dd7d1a68f0401817268befd08116e5cf4da981a
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-03 11:45:41 +0900

    Create make_wait.sh

---
+++ openshift/app04/make_wait.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+export TZ=JST-9
+
+set -x
+
+if [ $# -ne 4 ]; then
+    exit
+fi
+
+target_uuid=${1}
+target_app=${2}
+target_version=${3}
+make_server=${4}
+
+echo "$(date +%Y/%m/%d" "%H:%M:%S) start $(basename "${0}")"
+
+file_name=${target_uuid}_maked_${target_app}-${target_version}.tar.xz
+url=${make_server}/${file_name}
+while:
+do
+    if [ $(wget -nv --spider --timeout 60 -t 1 ${url} 2>&1 | grep -c '200 OK') -eq 1 ]; then
+        echo "$(date +%Y/%m/%d" "%H:%M:%S) ${target_app} maked wget"
+        break
+    else
+        echo "$(date +%Y/%m/%d" "%H:%M:%S) ${target_app} maked waiting"
+        sleep 10s
+    fi
+done
+pushd ${OPENSHIFT_TMP_DIR} >/dev/null
+rm -f ${file_name}
+wget $(cat ${OPENSHIFT_DATA_DIR}/params/mirror_server)/${file_name}
+mv ${file_name} ${OPENSHIFT_DATA_DIR}/files/
+popd >/dev/null
+
+echo "$(date +%Y/%m/%d" "%H:%M:%S) finish $(basename "${0}")"
