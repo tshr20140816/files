0f2ce5ad25b7c522d1c164ae2fa63525df080a57
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-08 08:08:55 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -3,3 +3,11 @@
 set -x
 
 quota -s
+
+cd /tmp
+
+sphinx_version 2.2.9
+wget http://sphinxsearch.com/files/sphinx-${sphinx_version}-release.tar.gz
+tar zxf sphinx-${sphinx_version}-release.tar.gz
+
+ls -lang
