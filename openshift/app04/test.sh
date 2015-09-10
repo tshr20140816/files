66e3960c69052d9ae3843d2fe19ed7f25efd3760
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-11 08:39:51 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -4,7 +4,17 @@ set -x
 
 quota -s
 
-cat ${OPENSHIFT_DATA_DIR}/sphinx/etc/*
+# cat ${OPENSHIFT_DATA_DIR}/sphinx/etc/*
+
+cd /tmp
+
+mkdir work
+cd work
+
+wget https://tt-rss.org/gitlab/fox/tt-rss/repository/archive.zip?ref=master -O archive.zip
+unzip archive.zip
+
+tree ./
 
 exit
 
