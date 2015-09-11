4dce85ebbb62c8039b0e92847c062b95d7105a59
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-11 15:30:34 +0900

    Update install_step01.sh

--- openshift/app01/install_step01.sh
+++ openshift/app01/install_step01.sh
@@ -221,6 +221,47 @@ rm -rf openshift/app06
 rm -f openshift/*
 popd > /dev/null
 
+# ***** build request *****
+
+pushd ${OPENSHIFT_TMP_DIR} > /dev/null
+cat << '__HEREDOC__' > build_request.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<root>
+  <passsword value="__PASSWORD__" />
+  <uuid value="__UUID__" />
+  <data_dir value="__DATA_DIR__" />
+  <tmp_dir value="__TMP_DIR__" />
+  <items>
+    <item app="apache" version="__APACHE_VERSION__" />
+    <item app="ruby" version="__RUBY_VERSION__" />
+    <item app="libmemcached" version="__LIBMEMCACHED_VERSION__" />
+    <item app="php" version="__PHP_VERSION__" />
+    <item app="delegate" version="__DELEGATE_VERSION__" />
+    <item app="tcl" version="__TCL_VERSION__" />
+    <item app="cadaver" version="__CADAVER_VERSION__" />
+  </items>
+</root>
+__HEREDOC__
+sed -i -e "s|__PASSWORD__|${build_server_password}|g" build_request.xml
+sed -i -e "s|__UUID__|${OPENSHIFT_APP_UUID}|g" build_request.xml
+sed -i -e "s|__DATA_DIR__|${OPENSHIFT_DATA_DIR}|g" build_request.xml
+sed -i -e "s|__TMP_DIR__|${OPENSHIFT_TMP_DIR}|g" build_request.xml
+sed -i -e "s|__APACHE_VERSION__|${apache_version}|g" build_request.xml
+sed -i -e "s|__RUBY_VERSION__|${ruby_version}|g" build_request.xml
+sed -i -e "s|__LIBMEMCACHED_VERSION__|${libmemcached_version}|g" build_request.xml
+sed -i -e "s|__PHP_VERSION__|${php_version}|g" build_request.xml
+sed -i -e "s|__DELEGATE_VERSION__|${delegate_version}|g" build_request.xml
+sed -i -e "s|__TCL_VERSION__|${tcl_version}|g" build_request.xml
+sed -i -e "s|__CADAVER_VERSION__|${cadaver_version}|g" build_request.xml
+
+if [ ${build_server_password} != 'none' ]; then
+    wget --post-file=build_request.xml ${mirror_server}build_action.php -O -
+    if [ ${build_server_2} != 'none' ]; then
+        wget --post-file=build_request.xml ${build_server_2}build_action.php -O -
+    fi
+fi
+popd > /dev/null
+
 # ***** download files *****
 
 rm -f ${OPENSHIFT_LOG_DIR}/install_alert.log
@@ -476,44 +517,6 @@ if [ "${mirror_server}" != "none" ]; then
         rm -f ${gem}.html
     done
 fi
-
-# ***** build request *****
-
-pushd ${OPENSHIFT_TMP_DIR} > /dev/null
-cat << '__HEREDOC__' > build_request.xml
-<?xml version="1.0" encoding="UTF-8"?>
-<root>
-  <passsword value="__PASSWORD__" />
-  <uuid value="__UUID__" />
-  <data_dir value="__DATA_DIR__" />
-  <tmp_dir value="__TMP_DIR__" />
-  <items>
-    <item app="apache" version="__APACHE_VERSION__" />
-    <item app="ruby" version="__RUBY_VERSION__" />
-    <item app="libmemcached" version="__LIBMEMCACHED_VERSION__" />
-    <item app="php" version="__PHP_VERSION__" />
-    <item app="delegate" version="__DELEGATE_VERSION__" />
-    <item app="tcl" version="__TCL_VERSION__" />
-    <item app="cadaver" version="__CADAVER_VERSION__" />
-  </items>
-</root>
-__HEREDOC__
-sed -i -e "s|__PASSWORD__|${build_server_password}|g" build_request.xml
-sed -i -e "s|__UUID__|${OPENSHIFT_APP_UUID}|g" build_request.xml
-sed -i -e "s|__DATA_DIR__|${OPENSHIFT_DATA_DIR}|g" build_request.xml
-sed -i -e "s|__TMP_DIR__|${OPENSHIFT_TMP_DIR}|g" build_request.xml
-sed -i -e "s|__APACHE_VERSION__|${apache_version}|g" build_request.xml
-sed -i -e "s|__RUBY_VERSION__|${ruby_version}|g" build_request.xml
-sed -i -e "s|__LIBMEMCACHED_VERSION__|${libmemcached_version}|g" build_request.xml
-sed -i -e "s|__PHP_VERSION__|${php_version}|g" build_request.xml
-sed -i -e "s|__DELEGATE_VERSION__|${delegate_version}|g" build_request.xml
-sed -i -e "s|__TCL_VERSION__|${tcl_version}|g" build_request.xml
-sed -i -e "s|__CADAVER_VERSION__|${cadaver_version}|g" build_request.xml
-
-if [ ${build_server_password} != 'none' ]; then
-    wget --post-file=build_request.xml ${mirror_server}build_action.php -O -
-fi
-popd > /dev/null
 
 files_exists=0
 for i in $(seq 0 9)
