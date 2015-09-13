a282fa2ee6b11abcd0c5c1b66de693cc6e19e9af
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-14 08:28:37 +0900

    Update install_step21.sh

--- openshift/app01/install_step21.sh
+++ openshift/app01/install_step21.sh
@@ -138,6 +138,8 @@ ${OPENSHIFT_DATA_DIR}/apache/bin/apachectl -k graceful \
 pushd ${OPENSHIFT_DATA_DIR}/delegate
 ./delegated -r +=P30080 \
  >> ${OPENSHIFT_LOG_DIR}/start.log 2>&1
+./delegated -r +=P33128 \
+ >> ${OPENSHIFT_LOG_DIR}/start.log 2>&1
 popd > /dev/null
 
 # *** memcached ***
