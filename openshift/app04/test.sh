20a6f680d2427dd3ea0711c9165cfd510a582f18
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-16 08:40:22 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -5,3 +5,14 @@ set -x
 quota -s
 
 cd /tmp
+
+wget http://www.delegate.org/anonftp/DeleGate/delegate9.9.13.tar.gz
+
+tar zxf delegate9.9.13.tar.gz
+
+cd delegate9.9.13
+
+time make -j4 ADMIN=user@rhcloud.local
+
+mkdir ${OPENSHIFT_DATA_DIR}/delegate/
+cp src/delegated ${OPENSHIFT_DATA_DIR}/delegate/
