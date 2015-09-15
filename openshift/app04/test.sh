9e780571ac346dafcc292194470edfe97450e190
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-15 14:10:47 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -15,11 +15,12 @@ wget http://pkg-config.freedesktop.org/releases/pkgconfig-0.18.tar.gz
 tar zxf pkgconfig-0.18.tar.gz
 cd pkgconfig-0.18
 ./configure --help
-time ./configure --prefix=/tmp/local
-time make
+time ./configure --prefix=/tmp/local --mandir=/tmp/gomi --infodir=/tmp/gomi
+time make -j4
 
 cd /tmp
 rm -rf work
+rm -rf gomi
 exit
 
 cd /tmp
