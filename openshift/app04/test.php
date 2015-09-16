1b290fdc65757d04ac40c86c247c4c50edcdcd04
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-16 15:37:00 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -5,9 +5,24 @@ $contents = file_get_contents('http://www.carp.co.jp/headline15/index.html');
 
 // <div id="contents">
 
+$start_flg = 0;
+$lines[] = array();
+
 foreach(explode("\n", $contents) as $value){
-  echo $value;
-  echo "\n";
+  # echo $value;
+  # echo "\n";
+  if(trim($value) == '<div id="contents">'){
+    $start_flg = 1;
+    continue;
+  }
+  if($start_flg == 1 && trim($value) == '</ul>'){
+    break;
+  }
+  if($start_flg == 1){
+    $lines[] = $value;
+    echo $value;
+    echo "\n";
+  }
 }
 
 echo "\n";
