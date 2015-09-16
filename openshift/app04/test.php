c03cc71ea34451c73b9fbf2aad9774abd62e7d14
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-16 15:29:08 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -1,4 +1,13 @@
 <?php
 header('Content-type: text/plain; charset=utf-8');
-echo "TEST";
+
+$contents = file_get_contents('http://www.carp.co.jp/headline15/index.html');
+
+// <div id="contents">
+
+foreach(explode("\n", $contents) as $value){
+  echo $value;
+}
+
+echo "\n";
 ?>
