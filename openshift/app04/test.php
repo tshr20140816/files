0791f3bd9d06057884ea5ae20718a428aba098c7
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-16 16:45:22 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -3,14 +3,10 @@ header('Content-type: text/plain; charset=utf-8');
 
 $contents = file_get_contents('http://www.carp.co.jp/headline15/index.html');
 
-// <div id="contents">
-
 $start_flg = 0;
 $lines[] = array();
 
 foreach(explode("\n", $contents) as $value) {
-  // echo $value;
-  // echo "\n";
   if(trim($value) == '<div id="contents">') {
     $start_flg = 1;
     continue;
@@ -24,7 +20,6 @@ foreach(explode("\n", $contents) as $value) {
 }
 
 $items[] = array();
-
 foreach($lines as $value) {
   $cnt = preg_match_all('/.+?<a href="(.+?)">(.+?)</', $value, $m);
   for($i = 0; $i < $cnt; $i++) {
@@ -34,10 +29,10 @@ foreach($lines as $value) {
       $url = "http://www.carp.co.jp/headline15/" . $m[1][$i];
     }
     $dt = '20' . str_replace('.', '/', substr($m[2][$i], 0, 8));
-    $title = trim(substr($m[2][$i], 8), '　');
-    echo $url . "\n";
-    echo $dt . "\n";
-    echo $title . "\n";
+    $title = substr($m[2][$i], 8);
+    // echo $url . "\n";
+    // echo $dt . "\n";
+    // echo $title . "\n";
     $buffer = str_replace("{0}", $title, $item_template);
     $buffer = str_replace("{1}", $url, $buffer);
     $items[] = str_replace("{2}", $dt, $buffer);
