ec071c4678df516a21da29d54c4d6bedcae72d86
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-18 09:52:43 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -3,8 +3,8 @@
 header('Content-type: text/plain; charset=utf-8');
 
 for($i = 0; $i < 7; $i++){
-  dt = strtotime("-" . $i . " day");
-  $contents = file_get_contents('http://news.yahoo.co.jp/hl?c=l34&p=1&d=' . date("Ymd", dt));
+  $dt = strtotime("-" . $i . " day");
+  $contents = file_get_contents('http://news.yahoo.co.jp/hl?c=l34&p=1&d=' . date("Ymd", $dt));
   
   foreach(explode("\n", $contents) as $value) {
     $cnt = preg_match('/.*<li><p class="ttl"><a href="(.+?)".*?>(.+?)<.+/', $value, $m);
