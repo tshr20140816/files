b219178255c6fa3df168c2a98496c1b4d0c34681
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-18 10:25:41 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -3,7 +3,6 @@ header('Content-type: text/plain; charset=utf-8');
 for($i = 0; $i < 7; $i++) {
   $dt = strtotime("-" . $i . " day");
   $contents = file_get_contents('http://news.yahoo.co.jp/hl?c=l34&p=1&d=' . date("Ymd", $dt));
-  
   foreach(explode("\n", $contents) as $value) {
     $cnt = preg_match('/.*<li><p class="ttl"><a href="(.+?)".*?>(.+?)<.+/', $value, $m);
     if($cnt > 0) {
@@ -16,6 +15,7 @@ for($i = 0; $i < 7; $i++) {
     }
   }
 }
+
 $urls = array();
 $urls[] = 'http://h50146.www5.hp.com/directplus/personal/';
 $urls[] = 'http://h50146.www5.hp.com/directplus/smb/';
