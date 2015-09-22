f0e4e212813ef211c084ef1ed5f5c7813a04b2d2
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-22 15:39:59 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -1,5 +1,23 @@
 <?php
 header('Content-type: text/plain; charset=utf-8');
+
+$sb = array();
+$sb[] = 'BEGIN:VCALENDAR'
+$sb[] = 'PRODID:tshr_original_20150922'
+$sb[] = 'VERSION:2.0'
+$sb[] = 'BEGIN:VTIMEZONE'
+$sb[] = 'TZID:Asia/Tokyo'
+$sb[] = 'X-LIC-LOCATION:Asia/Tokyo'
+$sb[] = 'BEGIN:STANDARD'
+$sb[] = 'TZOFFSETFROM:+0900'
+$sb[] = 'TZOFFSETTO:+0900'
+$sb[] = 'TZNAME:JST'
+$sb[] = 'DTSTART:20010101T000000'
+$sb[] = 'END:STANDARD'
+$sb[] = 'END:VTIMEZONE'
+
+$created =
+
 # $contents = file_get_contents('http://soccer.phew.homeip.net/download/schedule/data/SJIS_all_hirosima.csv');
 $contents = file_get_contents('/tmp/SJIS_all_hirosima.csv');
 $contents = mb_convert_encoding($contents, "UTF-8", "SJIS");
@@ -12,7 +30,18 @@ foreach(explode("\n", $contents) as $value) {
       echo ltrim($temp_array[0], '"') . "\n";
       echo $temp_array[1] . "\n";
       echo $temp_array[2] . "\n";
+      $sb[] = 'BEGIN:VEVENT'
+      $sb[] = 'CREATED:'
+      $sb[] = 'LAST-MODIFIED:'
+      $sb[] = 'DTSTAMP:'
+      $sb[] = 'UID:'
+      $sb[] = 'DTSTART:'
+      $sb[] = 'DTEND:'
+      $sb[] = 'SUMMARY:'
+      $sb[] = 'TRANSP:TRANSPARENT'
+      $sb[] = 'END:VEVENT'
     }
   }
 }
+sb[] = 'END:VCALENDAR'
 ?>
