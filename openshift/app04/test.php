1646e3c43e1e7dfb4e10acc1ab87744a65f3c4ef
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-22 15:12:52 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -1,20 +1,17 @@
 <?php
 header('Content-type: text/plain; charset=utf-8');
-for($i = 0; $i < 7; $i++) {
-  # $contents = file_get_contents('http://soccer.phew.homeip.net/download/schedule/data/SJIS_all_hirosima.csv');
-  $contents = file_get_contents('/tmp/SJIS_all_hirosima.csv');
-  $contents = mb_convert_encoding($contents, "UTF-8", "SJIS");
-  foreach(explode("\n", $contents) as $value) {
-    $cnt = preg_match('/.*エディオンスタジアム広島.+/', $value, $m);
-    if($cnt > 0) {
-      // echo $value . "\n";
-      $temp_array = explode('","', $value);
-      echo $temp_array[1] . ' ' . date('Y/m/d', strtotime("-2 day")). "\n";
-      if($temp_array[1] >= date('Y/m/d', strtotime("-2 day"))){
-        echo $temp_array[0] . "\n";
-        echo $temp_array[1] . "\n";
-        echo $temp_array[2] . "\n";
-      }
+# $contents = file_get_contents('http://soccer.phew.homeip.net/download/schedule/data/SJIS_all_hirosima.csv');
+$contents = file_get_contents('/tmp/SJIS_all_hirosima.csv');
+$contents = mb_convert_encoding($contents, "UTF-8", "SJIS");
+foreach(explode("\n", $contents) as $value) {
+  $cnt = preg_match('/.*エディオンスタジアム広島.+/', $value, $m);
+  if($cnt > 0) {
+    // echo $value . "\n";
+    $temp_array = explode('","', $value);
+    if($temp_array[1] >= date('Y/m/d', strtotime("-2 day"))){
+      echo $temp_array[0] . "\n";
+      echo $temp_array[1] . "\n";
+      echo $temp_array[2] . "\n";
     }
   }
 }
