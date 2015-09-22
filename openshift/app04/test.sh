4c58632f639ead82f9de166e41bf35cab7c8588d
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-22 14:31:40 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -8,74 +8,12 @@ quota -s
 
 cd /tmp
 
-rm yahoo_news_hiroshima.php
-rm hp_campaign.php
-wget https://raw.githubusercontent.com/tshr20140816/files/master/openshift/app01/rss/yahoo_news_hiroshima.php
-wget https://raw.githubusercontent.com/tshr20140816/files/master/openshift/app01/rss/hp_campaign.php
+rm test.php
+wget https://raw.githubusercontent.com/tshr20140816/files/master/openshift/app04/test.php
 
-php yahoo_news_hiroshima.php > ${OPENSHIFT_REPO_DIR}/yahoo_news_hiroshima.xml
-php hp_campaign.php > ${OPENSHIFT_REPO_DIR}/hp_campaign.xml
+php test.php
 
-rm yahoo_news_hiroshima.php
-rm hp_campaign.php
+rm test.php
 
-ls -lang ${OPENSHIFT_REPO_DIR}
 exit
 
-# cd /tmp
-# rm ${OPENSHIFT_LOG_DIR}/cron_minutely.log-*
-
-cd ${OPENSHIFT_DATA_DIR}/delegate/
-
-cat << '__HEREDOC__' > P33128
--P__OPENSHIFT_DIY_IP__:33128
-SERVER=http
-ADMIN=dummy@dummy.local
-DGROOT=__OPENSHIFT_DATA_DIR__delegate
-LOGDIR="__OPENSHIFT_LOG_DIR__"
-LOGFILE=${LOGDIR}/delegate_${PORT}.log[date+.%w]
-PROTOLOG=${LOGDIR}/delegate_${PORT}.${PROTO}.log[date+.%w]:%X
-ERRORLOG=${LOGDIR}/delegate_errors.log[date+.%w]
-CACHEDIR=__OPENSHIFT_DATA_DIR__delegate/cache
-CACHE=do
-MAXIMA=delegated:10
-REMITTABLE=http,ftp,https
-HTTPCONF=methods:GET,CONNECT
-HTTPCONF="kill-head:Via,HTTP-VIA,DeleGate-Ver"
-HTTPCONF=cache:any
-DGSIGN="x.x.x/x.x.x"
-CRON='0 7 * * * -expire 2'
-# CMAP="cache:FSV:http:www.yahoo.co.jp"
-__HEREDOC__
-perl -pi -e 's/__OPENSHIFT_DIY_IP__/$ENV{OPENSHIFT_PHP_IP}/g' P33128
-perl -pi -e 's/__OPENSHIFT_DATA_DIR__/$ENV{OPENSHIFT_DATA_DIR}/g' P33128
-perl -pi -e 's/__OPENSHIFT_LOG_DIR__/$ENV{OPENSHIFT_LOG_DIR}/g' P33128
-
-cat P33128
-
-./delegated -r +=P33128
-
-cd /tmp
-
-curl --proxy http://${OPENSHIFT_PHP_IP}:33128 http://www.honda.co.jp/ > /dev/null
-curl --proxy http://${OPENSHIFT_PHP_IP}:33128 http://www.mazda.co.jp/ > /dev/null
-curl --proxy http://${OPENSHIFT_PHP_IP}:33128 http://www.toyota.co.jp/ > /dev/null
-curl --proxy http://${OPENSHIFT_PHP_IP}:33128 http://www.nissan.co.jp/ > /dev/null
-curl --proxy http://${OPENSHIFT_PHP_IP}:33128 http://www.subaru.co.jp/ > /dev/null
-curl --proxy http://${OPENSHIFT_PHP_IP}:33128 http://www.yahoo.co.jp/ > /dev/null
-curl --proxy http://${OPENSHIFT_PHP_IP}:33128 http://www.microsoft.com/ > /dev/null
-curl --proxy http://${OPENSHIFT_PHP_IP}:33128 http://www.google.com/ > /dev/null
-curl --proxy http://${OPENSHIFT_PHP_IP}:33128 http://www.google.co.jp/ > /dev/null
-curl --proxy http://${OPENSHIFT_PHP_IP}:33128 http://headlines.yahoo.co.jp/hl?a=20150916-00010000-fullcount-base > /dev/null
-curl --proxy http://${OPENSHIFT_PHP_IP}:33128 http://baseball.yahoo.co.jp/npb/standings/ > /dev/null
-
-ls -lang
-
-tree ${OPENSHIFT_DATA_DIR}/delegate/cache/
-
-find ${OPENSHIFT_DATA_DIR}/delegate/cache -name '*' -type f -print | grep -v www.honda.co.jp | xargs rm
-find ${OPENSHIFT_DATA_DIR}/delegate/cache -name '*' -type d -empty -print | xargs rm -rf
-
-tree ${OPENSHIFT_DATA_DIR}/delegate/cache/
-
-#
