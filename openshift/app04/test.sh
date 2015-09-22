bd4c3129297bf23f885fc45fce2517700185c44c
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-22 15:13:03 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -8,7 +8,7 @@ quota -s
 
 cd /tmp
 
-wget http://soccer.phew.homeip.net/download/schedule/data/SJIS_all_hirosima.csv
+# wget http://soccer.phew.homeip.net/download/schedule/data/SJIS_all_hirosima.csv
 rm test.php
 wget https://raw.githubusercontent.com/tshr20140816/files/master/openshift/app04/test.php
 cat test.php
