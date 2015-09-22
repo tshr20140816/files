dc63a30e666652cd00cca0c9ea2f7bd18d34ecdd
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-22 14:39:11 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -6,7 +6,11 @@ for($i = 0; $i < 7; $i++) {
   foreach(explode("\n", $contents) as $value) {
     $cnt = preg_match('/.*エディオンスタジアム広島.+/', $value, $m);
     if($cnt > 0) {
-      echo $value . "\n";
+      // echo $value . "\n";
+      $temp_array = explode(",", $value);
+      echo $temp_array[0] . "\n";
+      echo $temp_array[1] . "\n";
+      echo $temp_array[2] . "\n";
     }
   }
 }
