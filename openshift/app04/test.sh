eab638dab2e0b6d010f2cb0ea6092ae31c9ca727
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-23 16:18:55 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -8,13 +8,17 @@ quota -s
 
 cd /tmp
 
-rm test.php
-wget https://raw.githubusercontent.com/tshr20140816/files/master/openshift/app04/test.php
-cat test.php
+# rm test.php
+# wget https://raw.githubusercontent.com/tshr20140816/files/master/openshift/app04/test.php
+# cat test.php
+# php test.php
+# rm test.php
 
-php test.php
-
-rm test.php
+rm HPHP-2.1.0.zip
+wget https://github.com/facebook/hhvm/archive/HPHP-2.1.0.zip
+unzip HPHP-2.1.0.zip
+rm HPHP-2.1.0.zip
+ls -lang
 
 exit
 
