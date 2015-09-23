2fa8a74125c64d11f81a1c143dce8a6a4776251b
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-23 17:22:05 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -14,25 +14,21 @@ __HEREDOC__;
 
 header('Content-type: text/plain; charset=utf-8');
 
-$url = 'http://www.npb.or.jp/announcement/2015/roster_cl' . date('Ymd') . '.html'
+$url = 'http://www.npb.or.jp/announcement/2015/roster_cl' . date('Ymd') . '.html';
 $contents = file_get_contents($url);
 $contents = mb_convert_encoding($contents, "UTF-8", "SJIS");
-// echo $contents;
 
 $start_flg = 0;
 $lines = array();
 foreach(explode("\n", $contents) as $value) {
   if(trim($value) == '<!-- 公示日付 -->') {
-    //echo trim($value) . "\n";
     $start_flg = 1;
     continue;
   }
   if($start_flg == 1 && trim($value) == '<!-- 出場選手一覧 -->') {
-    //echo trim($value) . "\n";
     break;
   }
   if($start_flg == 1) {
-    //echo trim($value) . "\n";
     $lines[] = trim($value);
   }
 }
@@ -41,7 +37,6 @@ $items = array();
 foreach($lines as $value) {
   $tmp = preg_replace('/<.+?>/', ' ', $value);
   $tmp = preg_replace('/ +/', ' ', $tmp);
-  // echo trim($tmp) . "\n";
   $items[] = $tmp;
 }
 echo sprintf($xml, date('Y.m.d'), $url, implode("&lt;br /&gt;\n", $items));
