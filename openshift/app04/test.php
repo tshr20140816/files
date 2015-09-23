6806af09cab9785d74d58e876b66b448d442b3cf
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-23 14:29:04 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -20,13 +20,12 @@ $created = date('Ymd\THis\Z');
 $uid = strtoupper(sha1($created));
 $i = 0;
 
-# $contents = file_get_contents('http://soccer.phew.homeip.net/download/schedule/data/SJIS_all_hirosima.csv');
+// $contents = file_get_contents('http://soccer.phew.homeip.net/download/schedule/data/SJIS_all_hirosima.csv');
 $contents = file_get_contents('/tmp/SJIS_all_hirosima.csv');
 $contents = mb_convert_encoding($contents, "UTF-8", "SJIS");
 foreach(explode("\n", $contents) as $value) {
   $cnt = preg_match('/.*エディオンスタジアム広島.+/', $value, $m);
   if($cnt > 0) {
-    // echo $value . "\n";
     $temp_array = explode('","', $value);
     if($temp_array[1] >= date('Y/m/d', strtotime("-2 day"))){
       $start_time = explode('":"', $temp_array[2]);
