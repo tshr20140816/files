9adfc58be0374ab88530eb43ca2fbe977f787df5
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-23 15:20:26 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -1,3 +1,25 @@
 <?php
 
+header('Content-type: text/plain; charset=utf-8');
+
+$start_flg = 0;
+$lines = array();
+foreach(explode("\n", file_get_contents('http://www.npb.or.jp/announcement/2015/roster_cl' . date('Ymd') . '.html')) as $value) {
+  if(trim($value) == '<!-- 公示日付 -->') {
+    $start_flg = 1;
+    continue;
+  }
+  if($start_flg == 1 && trim($value) == '<!-- 出場選手一覧 -->') {
+    break;
+  }
+  if($start_flg == 1) {
+    $lines[] = trim($value);
+  }
+}
+
+$items = array();
+foreach($lines as $value) {
+  $value = preg_replace('/<.+?>/', '', $value);
+  echo trim($value) + "\n";
+}
 ?>
