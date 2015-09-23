892d4210abbcf4c6e0e0280fe99938f1eb19104f
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-23 14:48:50 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -11,10 +11,5 @@ cd /tmp
 rm ${OPENSHIFT_REPO_DIR}test.ics
 rm ${OPENSHIFT_REPO_DIR}test.xml
 
-
-curl --digest -u tshrapp9:$(date +%Y%m%d%H) \
- -F "url=https://woo-20140818.rhcloud.com/" \
- https://tshrapp9.appspot.com/createwebcroninformation
-
 exit
 
