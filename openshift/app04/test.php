8157d6f4b2f0dee25979bef7dd5727545ac1b6e4
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-23 15:25:49 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -2,10 +2,12 @@
 
 header('Content-type: text/plain; charset=utf-8');
 
+$contents = file_get_contents('http://www.npb.or.jp/announcement/2015/roster_cl' . date('Ymd') . '.html');
+$contents = mb_convert_encoding($contents, "UTF-8", "SJIS");
+
 $start_flg = 0;
 $lines = array();
-foreach(explode("\n", file_get_contents('http://www.npb.or.jp/announcement/2015/roster_cl' . date('Ymd') . '.html')) as $value) {
-  echo $value . "\n";
+foreach(explode("\n", $contents) as $value) {
   if(trim($value) == '<!-- 公示日付 -->') {
     $start_flg = 1;
     continue;
