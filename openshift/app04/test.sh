b134d1d99bb1b770cb0bf393d04d9861c9c47ad9
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-24 10:57:44 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -8,10 +8,10 @@ quota -s
 
 cd /tmp
 
-rm test.php
-wget https://raw.githubusercontent.com/tshr20140816/files/master/openshift/app04/test.php
-cat test.php
-php test.php | tee ${OPENSHIFT_REPO_DIR}test.xml
-rm test.php
+ls -lang
+
+ls -lang ${OPENSHIFT_REPO_DIR}
+
+ls -lang ${OPENSHIFT_DATA_DIR}
 
 exit
