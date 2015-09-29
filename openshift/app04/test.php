eb8cb52ab100b79929ebd6356e7a301f6ccad74d
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-29 15:07:37 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -5,6 +5,7 @@ $contents = file_get_contents('http://www.formula1.com/content/fom-website/en/ch
 
 $start_flag = 0;
 foreach(explode("\n", $contents) as $value) {
+  echo trim($contents);
   if(trim($contents) == '<p class="teaser-date">') {
     $buffer = trim($contents);
     $start_flag = 1;
