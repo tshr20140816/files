2a88bca24582f653372bb596c36e926697c03714
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-29 15:08:59 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -17,6 +17,9 @@ rm test.php
 ls -lang
 
 ls -lang ${OPENSHIFT_REPO_DIR}
+rm ${OPENSHIFT_REPO_DIR}hp_campaign.xml
+rm ${OPENSHIFT_REPO_DIR}yahoo_news_hiroshima.xml
+rm ${OPENSHIFT_REPO_DIR}test.xml
 
 ls -lang ${OPENSHIFT_DATA_DIR}
 
