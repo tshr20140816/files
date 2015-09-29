8c130bd606957ca7ef1dec4d1dc9b82ee27dff8c
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-09-29 15:01:58 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -1,3 +1,21 @@
 <?php
+header('Content-type: text/calendar; charset=utf-8');
+
+$contents = file_get_contents('http://www.formula1.com/content/fom-website/en/championship/races/2015.html');
+
+$start_flag = 0;
+foreach(explode("\n", $contents) as $value) {
+  if(trim($contents) == '<p class="teaser-date">') {
+    $buffer = trim($contents);
+    $start_flag = 1;
+  } elseif($start_flag === 1){
+    if(trim($contents) == '</p>'){
+      echo $buffer;
+      $start_flag = 0;
+    } else {
+      $buffer .= trim($contents);
+    }
+  }
+}
 
 ?>
