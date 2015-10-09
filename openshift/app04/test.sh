ca28a98a046265d6e7e979280721d3c77dab3000
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-10-09 09:31:28 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -14,13 +14,4 @@ cat test.php
 php test.php
 rm test.php
 
-ls -lang
-
-ls -lang ${OPENSHIFT_REPO_DIR}
-rm ${OPENSHIFT_REPO_DIR}hp_campaign.xml
-rm ${OPENSHIFT_REPO_DIR}yahoo_news_hiroshima.xml
-rm ${OPENSHIFT_REPO_DIR}test.xml
-
-ls -lang ${OPENSHIFT_DATA_DIR}
-
 exit
