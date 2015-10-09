ef53055634d92deffb2c9d899ccc4deb3daf03aa
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-10-09 09:41:45 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -3,6 +3,7 @@ header('Content-type: text/plain; charset=utf-8');
 
 $headers = get_headers('http://www.cellstar.co.jp/mcd/gps/img/gps_date_top.gif');
 
+print $headers['Last-Modified'];
 print var_dump($headers);
 
 ?>
