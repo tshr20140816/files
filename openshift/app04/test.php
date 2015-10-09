7c7e07b41bd3539976749db7d37dc4b9bd407b1c
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-10-09 09:30:44 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -1,22 +1,8 @@
 <?php
-header('Content-type: text/calendar; charset=utf-8');
+header('Content-type: text/plain; charset=utf-8');
 
-$contents = file_get_contents('http://www.formula1.com/content/fom-website/en/championship/races/2015.html');
+$headers = get_headers('http://www.cellstar.co.jp/mcd/gps/img/gps_date_top.gif');
 
-$start_flag = 0;
-foreach(explode("\r\n", $contents) as $value) {
-  echo trim($value);
-  if(trim($value) == '<p class="teaser-date">') {
-    $buffer = trim($value);
-    $start_flag = 1;
-  } elseif($start_flag === 1){
-    if(trim($value) == '</p>'){
-      echo $buffer;
-      $start_flag = 0;
-    } else {
-      $buffer .= trim($value);
-    }
-  }
-}
+print $headers;
 
 ?>
