faceaa053cc863be1970eb41c35f88389cfc1892
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-10-13 09:57:22 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -8,6 +8,7 @@ $items = array();
 $url = 'http://www.hiroden.co.jp/what/new/topic.htm';
 $contents = mb_convert_encoding($contents, "UTF-8", "SJIS");
 foreach(explode("\n", $contents) as $value) {
+  echo trim($value);
   if(substr(trim($value), 0, 4) == '<h3>'){
     echo trim($value);
   }
