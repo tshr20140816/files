1027033a44d47420834511fd94cc14a361a695d8
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-10-22 11:53:01 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -4,4 +4,10 @@ set -x
 
 ls -al ${OPENSHIFT_REPO_DIR}
 
+cd ${OPENSHIFT_REPO_DIR}
+
+wget https://github.com/tshr20140816/files/raw/master/openshift/app04/test.php
+
+ls -al ${OPENSHIFT_REPO_DIR}
+
 exit
