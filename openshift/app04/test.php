d34f01e2c0b05bde207391f8e31a711899e7708f
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-10-22 11:51:39 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -1,19 +1,5 @@
 <?php
-header('Content-type: text/plain; charset=utf-8');
+header('Content-type: application/force-download')
 
-$item_template = "<item><title>%s</title><link>%s</link><description /><pubDate>%s</pubDate></item>";
-
-$items = array();
-
-$url = 'http://www.hiroden.co.jp/what/new/topic.htm';
-$contents = mb_convert_encoding($contents, "UTF-8", "SJIS");
-echo $contents;
-foreach(explode("\n", $contents) as $value) {
-  //echo trim($value);
-  if(substr(trim($value), 0, 4) == '<h3>'){
-    echo trim($value);
-  }
-}
-
-//print var_dump($items);
+echo date('Ymd');
 ?>
