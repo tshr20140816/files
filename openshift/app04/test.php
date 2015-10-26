6c5ae13fd0d7bc422ea14297c518190e568e1fa9
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-10-26 15:54:14 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -5,6 +5,7 @@ header('Content-type: text/plain');
 // echo date('Ymd');
 
 $url = "http://www.yahoo.co.jp/";
+$url = "https://github.com/tshr20140816/files/raw/master/openshift/app04/test.php";
 $ch = curl_init();
 
 curl_setopt($ch, CURLOPT_URL, $url); 
