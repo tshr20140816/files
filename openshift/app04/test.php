8277b3405a976434496524a2d5fce45f586369db
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-10-26 15:30:11 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -1,5 +1,16 @@
 <?php
-header('Content-type: application/force-download');
+// header('Content-type: application/force-download');
+header('Content-type: text/plain');
 
-echo date('Ymd');
+// echo date('Ymd');
+
+$url = "http://www.yahoo.co.jp/";
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, $url); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$info = curl_getinfo($ch);
+curl_close($ch);
+
+print $info["http_code"];
 ?>
