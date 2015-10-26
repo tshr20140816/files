69a372e2880c372904f9d8d8f7f2d8c814ead558
tshr20140816 <tshr20140816@yahoo.co.jp>
2015-10-26 15:51:21 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -9,7 +9,8 @@ $ch = curl_init();
 
 curl_setopt($ch, CURLOPT_URL, $url); 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//curl_setopt($ch, CURLOPT_NOBODY, true);
+curl_setopt($ch, CURLOPT_NOBODY, true);
+curl_exec($ch);
 $info = curl_getinfo($ch);
 curl_close($ch);
 
