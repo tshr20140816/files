0d39db87ed5ac5b32753dcb7ddeb53310b0ebefb
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-02-25 12:47:13 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -4,16 +4,26 @@ set -x
 
 # 1620
 
-ls -al ${OPENSHIFT_REPO_DIR}
+# ls -al ${OPENSHIFT_REPO_DIR}
 
-cd ${OPENSHIFT_REPO_DIR}
+# cd ${OPENSHIFT_REPO_DIR}
 
-rm test.php*
+# rm test.php*
 
-wget https://github.com/tshr20140816/files/raw/master/openshift/app04/test.php
-cat test.php
-php test.php
+# wget https://github.com/tshr20140816/files/raw/master/openshift/app04/test.php
+# cat test.php
+# php test.php
 
-ls -al ${OPENSHIFT_REPO_DIR}
+# ls -al ${OPENSHIFT_REPO_DIR}
+
+cd /tmp
+
+ls -lang
+
+wget https://github.com/krakjoe/apcu/archive/v4.0.10.zip
+
+unzip v4.0.10.zip
+
+ls -lang
 
 exit
