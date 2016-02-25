1feec7d00ceba853e25eb29f9a68b37c174dee34
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-02-25 14:36:40 +0900

    Create install_step11a.sh

---
+++ openshift/app01/install_step11a.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source functions.sh
+function010 restart
+[ $? -eq 0 ] || exit
+
+
+
+
+touch ${OPENSHIFT_DATA_DIR}/install_check_point/$(basename $0).ok
+
+echo "$(date +%Y/%m/%d" "%H:%M:%S) Install Finish $(basename "${0}")" | tee -a ${OPENSHIFT_LOG_DIR}/install.log
