cebb7450844e7b7f79ff939ebd4e3f349379817f
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-02-25 12:52:09 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -2,28 +2,21 @@
 
 set -x
 
-# 1620
-
-# ls -al ${OPENSHIFT_REPO_DIR}
-
-# cd ${OPENSHIFT_REPO_DIR}
-
-# rm test.php*
-
-# wget https://github.com/tshr20140816/files/raw/master/openshift/app04/test.php
-# cat test.php
-# php test.php
-
-# ls -al ${OPENSHIFT_REPO_DIR}
-
 cd /tmp
 
 ls -lang
 
-wget https://github.com/krakjoe/apcu/archive/v4.0.10.zip
+# wget https://github.com/krakjoe/apcu/archive/v4.0.10.zip
+# unzip v4.0.10.zip
 
-unzip v4.0.10.zip
+cd apcu-4.0.10
 
 ls -lang
+
+phpize
+
+ls -lang
+
+./configure --help
 
 exit
