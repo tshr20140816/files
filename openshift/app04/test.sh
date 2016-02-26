60998bcd80f6f2b57739d40d3fbee79d47e43577
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-02-26 10:01:23 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -2,17 +2,6 @@
 
 set -x
 
-cd /tmp
-
-rm -f v4.0.10.zip
-rm -rf apcu-4.0.10
-
-wget http://www.graphviz.org/pub/graphviz/stable/SOURCES/graphviz-2.38.0.tar.gz
-tar xfz graphviz-2.38.0.tar.gz
-ls -lang
-cd graphviz*
-./configure --help
-./configure
-make
+echo -n "admin"  | openssl sha1
 
 exit
