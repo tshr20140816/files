6ffcf3ba1408a563e83c108e666a80dbe69c6529
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-02-26 17:17:25 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-echo "1708"
+echo "1715"
 
-set -x
+# set -x
+
+cd /tmp
+
+ls -lang
 
 download_list=()
 download_list[${#download_list[@]}]="spdy,mod-spdy-beta_current_x86_64.rpm,https://dl-ssl.google.com/dl/linux/direct/mod-spdy-beta_current_x86_64.rpm"
@@ -13,9 +17,13 @@ do
     title=$(echo $item | awk -F, '{print $1}')
     file_name=$(echo $item | awk -F, '{print $2}')
     url=$(echo $item | awk -F, '{print $3}')
-    echo $title
-    echo $file_name
-    echo $url
+    # echo $title
+    # echo $file_name
+    # echo $url
+    if [ ! -f $file_name ]; then
+        echo "$(date +%Y/%m/%d" "%H:%M:%S) $title wget"
+        wget $url
+    fi
 done
 
 exit
