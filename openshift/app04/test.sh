50a77fb9de0884c87ee487ee98ffdc1738bd22a4
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-02-26 16:32:43 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-echo "1423"
+echo "1632"
 
 set -x
 
-word="admin"
-abc=$(echo -n ${word}  | openssl sha1 | awk '{print $2;}')
-echo $abc
+download_list = ()
+download_list[0] = "spdy mod-spdy-beta_current_x86_64.rpm https://dl-ssl.google.com/dl/linux/direct/mod-spdy-beta_current_x86_64.rpm"
+download_list[0] = "rbenv-installer rbenv-installer https://raw.github.com/Seppone/openshift-rbenv-installer/master/bin/rbenv-installer"
+
+for item in ${download_list[@]}
+do
+    echo "$item"
+done
 
 exit
