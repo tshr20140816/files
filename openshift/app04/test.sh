d4a5b279f5b5789b5095753d3f39cb8b27c2aa2f
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-02-26 16:58:45 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -5,8 +5,8 @@ echo "1649"
 set -x
 
 download_list=()
-download_list[0]="spdy,mod-spdy-beta_current_x86_64.rpm,https://dl-ssl.google.com/dl/linux/direct/mod-spdy-beta_current_x86_64.rpm"
-download_list[1]="rbenv-installer,rbenv-installer,https://raw.github.com/Seppone/openshift-rbenv-installer/master/bin/rbenv-installer"
+download_list[${#download_list[@]}]="spdy,mod-spdy-beta_current_x86_64.rpm,https://dl-ssl.google.com/dl/linux/direct/mod-spdy-beta_current_x86_64.rpm"
+download_list[${#download_list[@]}]="rbenv-installer,rbenv-installer,https://raw.github.com/Seppone/openshift-rbenv-installer/master/bin/rbenv-installer"
 
 for item in ${download_list[@]}
 do
