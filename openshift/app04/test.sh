482502a7446167060d58ba4129d22e23982e76e3
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-02-29 16:40:34 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-echo "1635"
+echo "1640"
 
 # set -x
 
 cd /tmp
+
+echo production.log.$(date --date '2 days ago' +%Y%m%d)
+echo $(date --date '2 days ago' +%w)
 
 date -d 20160227 '+%w'
 date -d 20160228 '+%w'
