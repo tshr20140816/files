cd03846e28e5e84d1c46b1ddb6cdcd894a30ede2
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-02-29 09:10:28 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -6,7 +6,7 @@ set -x
 
 cd /tmp
 
-echo __PROJECT_DB_MYSQL_HOST__ > test2016029.txt
+echo -n __PROJECT_DB_MYSQL_HOST__ > test2016029.txt
 perl -pi -e 's/__PROJECT_DB_MYSQL_HOST__/$ENV{OPENSHIFT_MYSQL_DB_HOST}/g' test2016029.txt
 
 cat test2016029.txt
