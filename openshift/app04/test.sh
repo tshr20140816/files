54d946717a0799d21aeb22a135f18985b5d86d43
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-02-29 10:12:30 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -16,7 +16,7 @@ cat << '__HEREDOC__' > test2016029.txt
 __PROJECT_DB_MYSQL_HOST__
 __HEREDOC__
 
-# perl -pi -e 's/__PROJECT_DB_MYSQL_HOST__/$ENV{OPENSHIFT_MYSQL_DB_HOST}/g' test2016029.txt
+# perl -pi -e 's/__PROJECT_DB_MYSQL_HOST__/$ENV{OPENSHIFT_GEAR_UUID}/g' test2016029.txt
 
 cat test2016029.txt
 
