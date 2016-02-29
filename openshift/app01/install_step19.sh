4aea3936568c2afac534edd6fd4bca6cf2f20894
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-02-29 14:08:19 +0900

    Update install_step19.sh

--- openshift/app01/install_step19.sh
+++ openshift/app01/install_step19.sh
@@ -6,29 +6,6 @@ function010 stop
 
 pushd ${OPENSHIFT_DATA_DIR}/scripts > /dev/null
 
-# # ***** logrotate (zantei) *****
-# # https://bugzilla.redhat.com/show_bug.cgi?id=1181059
-#
-# cat << '__HEREDOC__' > logrotate_zantei.sh
-# #!/bin/bash
-#
-# export TZ=JST-9
-# day='00'
-# while :
-# do
-#     sleep 10m
-#     if [ ${day} != $(date +%d) ]; then
-#         day=$(date +%d)
-#         date +%Y/%m/%d" "%H:%M:%S >> ${OPENSHIFT_LOG_DIR}/logrotate_zantei.log
-#         /usr/sbin/logrotate -v \
-#             -s ${OPENSHIFT_DATA_DIR}/logrotate/logrotate.status \
-#             -f ${OPENSHIFT_DATA_DIR}/logrotate/logrotate.conf \
-#             >> ${OPENSHIFT_LOG_DIR}/logrotate_zantei.log
-#     fi
-# done
-# __HEREDOC__
-# chmod +x logrotate_zantei.sh &
-
 # ***** memory usage logging *****
 
 cat << '__HEREDOC__' > memory_usage_logging.sh
