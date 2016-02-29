28888b5e72ab0958915f21ae8e5dd86d5afc4a5c
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-02-29 09:08:28 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,29 +1,14 @@
 #!/bin/bash
 
-echo "1715"
+echo "0907"
 
-# set -x
+set -x
 
 cd /tmp
 
-ls -lang
+echo __PROJECT_DB_MYSQL_HOST__ > test2016029.txt
+perl -pi -e 's/__PROJECT_DB_MYSQL_HOST__/$ENV{OPENSHIFT_MYSQL_DB_HOST}/g' test2016029.txt
 
-download_list=()
-download_list[${#download_list[@]}]="spdy,mod-spdy-beta_current_x86_64.rpm,https://dl-ssl.google.com/dl/linux/direct/mod-spdy-beta_current_x86_64.rpm"
-download_list[${#download_list[@]}]="rbenv-installer,rbenv-installer,https://raw.github.com/Seppone/openshift-rbenv-installer/master/bin/rbenv-installer"
-
-for item in ${download_list[@]}
-do
-    title=$(echo $item | awk -F, '{print $1}')
-    file_name=$(echo $item | awk -F, '{print $2}')
-    url=$(echo $item | awk -F, '{print $3}')
-    # echo $title
-    # echo $file_name
-    # echo $url
-    if [ ! -f $file_name ]; then
-        echo "$(date +%Y/%m/%d" "%H:%M:%S) $title wget"
-        wget $url
-    fi
-done
+cat test2016029.txt
 
 exit
