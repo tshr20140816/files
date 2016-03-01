7dfc593a51489f4f9a69371038699f587fbed4ec
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-02 07:03:21 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -6,11 +6,7 @@ echo "1640"
 
 cd /tmp
 
-echo production.log.$(date --date '2 days ago' +%Y%m%d)
-echo $(date --date '2 days ago' +%w)
-
-date -d 20160227 '+%w'
-date -d 20160228 '+%w'
-date -d 20160229 '+%w'
+rhc-list-ports
+tree
 
 exit
