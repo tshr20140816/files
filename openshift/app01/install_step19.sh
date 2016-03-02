72fd823d14904dd4dafea3ab453bd076c559bcd1
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-02 09:38:07 +0900

    Update install_step19.sh

--- openshift/app01/install_step19.sh
+++ openshift/app01/install_step19.sh
@@ -335,6 +335,9 @@ echo $(date +%Y/%m/%d" "%H:%M:%S) > ${OPENSHIFT_TMP_DIR}/lsof_i_n_P.txt
 uptime >> ${OPENSHIFT_TMP_DIR}/lsof_i_n_P.txt
 lsof -i -n -P >> ${OPENSHIFT_TMP_DIR}/lsof_i_n_P.txt
 cp -f ${OPENSHIFT_TMP_DIR}/lsof_i_n_P.txt lsof_i_n_P.txt
+echo $(date +%Y/%m/%d" "%H:%M:%S) > ${OPENSHIFT_TMP_DIR}/listen_port.txt
+rhc-list-ports >> > ${OPENSHIFT_TMP_DIR}/listen_port.txt
+cp -f ${OPENSHIFT_TMP_DIR}/listen_port.txt listen_port.txt
 __HEREDOC__
 chmod +x process_status.sh &
 
