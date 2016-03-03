59125578a18fad91a172816341a374c523094274
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-03 09:58:30 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "0947"
+echo "0958"
 
 # set -x
 
@@ -8,11 +8,17 @@ cd /tmp
 
 whereis java
 rm -f compiler-latest.zip
-rm -f README
-#wget http://closure-compiler.googlecode.com/files/compiler-latest.zip
-wget http://dl.google.com/closure-compiler/compiler-latest.zip
-unzip compiler-latest.zip
+# rm -f README
+# wget http://closure-compiler.googlecode.com/files/compiler-latest.zip
+# wget http://dl.google.com/closure-compiler/compiler-latest.zip
+# unzip compiler-latest.zip
+
+wget https://foo-20140818.rhcloud.com/caldavzap/lib/jquery-ui-1.11.4.custom.js
+
+java -jar compiler.jar --js jquery-ui-1.11.4.custom.js --js_output_file jquery-ui-1.11.4.custom2.js
 
 ls -lang
+
+cat jquery-ui-1.11.4.custom2.js
 
 exit
