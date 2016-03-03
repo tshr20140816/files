0dbbb6bf5c533b96ab2e05a4a04648ff8f95d120
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-03 15:03:15 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,40 +1,18 @@
 #!/bin/bash
 
-echo "1121"
+echo "1502"
 
 # set -x
 
 cd /tmp
 
-mkdir compiled
+rm -rf compiled
 
-whereis java
-rm -f compiler-latest.zip
-rm -f *.js
-# rm -f README
-# wget http://closure-compiler.googlecode.com/files/compiler-latest.zip
-# wget http://dl.google.com/closure-compiler/compiler-latest.zip
-# unzip compiler-latest.zip
-
-wget https://foo-20140818.rhcloud.com/caldavzap/lib/jquery-ui-1.11.4.custom.js
-wget https://foo-20140818.rhcloud.com/caldavzap/lib/spectrum.js
-
-java -jar compiler.jar --version
-# java -jar compiler.jar --help
-
-time java -jar compiler.jar --summary_detail_level 3 --js jquery-ui-1.11.4.custom.js --js_output_file compiled/jquery-ui-1.11.4.custom.js 2> result.txt
-if [ "$(cat ./result.txt)" = "0 error(s), 0 warning(s)" ]; then
-    echo "MATCH"
-fi
-time java -jar compiler.jar --summary_detail_level 3 --js spectrum.js --js_output_file compiled/spectrum.js 2> result.txt
-if [ "$(cat ./result.txt)" = "0 error(s), 0 warning(s)" ]; then
-    echo "MATCH"
-fi
-
-head -n1 ./result.txt
-
-find ./ -name "*.js" -type f -print0 | xargs -n 2 -0i basename {}
-
+wget http://www.kokkonen.net/tjko/src/jpegoptim-1.4.3.tar.gz
+tar xvfz jpegoptim-1.4.3.tar.gz
 ls -lang
+cd jpegoptim*
+ls -lang
+./configure --help
 
 exit
