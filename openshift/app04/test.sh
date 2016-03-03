01166dfa65982d700450b877990924a63467a0ac
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-03 10:13:35 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "1001"
+echo "1013"
 
 # set -x
 
@@ -19,8 +19,8 @@ wget https://foo-20140818.rhcloud.com/caldavzap/lib/spectrum.js
 
 java -jar compiler.jar --help
 
-java -jar compiler.jar --js jquery-ui-1.11.4.custom.js --js_output_file jquery-ui-1.11.4.custom2.js
-java -jar compiler.jar --js spectrum.js --js_output_file spectrum2.js
+time java -jar compiler.jar --js jquery-ui-1.11.4.custom.js --js_output_file jquery-ui-1.11.4.custom2.js
+time java -jar compiler.jar --js spectrum.js --js_output_file spectrum2.js
 
 ls -lang
 
