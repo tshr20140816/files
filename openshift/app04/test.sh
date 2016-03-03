b489ddd15cab7bb350142b6cbdcb9e2ea4c693cb
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-03 10:41:10 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -14,14 +14,15 @@ rm -f *.js
 # wget http://dl.google.com/closure-compiler/compiler-latest.zip
 # unzip compiler-latest.zip
 
-wget https://foo-20140818.rhcloud.com/caldavzap/lib/jquery-ui-1.11.4.custom.js
+# wget https://foo-20140818.rhcloud.com/caldavzap/lib/jquery-ui-1.11.4.custom.js
 wget https://foo-20140818.rhcloud.com/caldavzap/lib/spectrum.js
 
 java -jar compiler.jar --version
 java -jar compiler.jar --help
 
-time java -jar compiler.jar --summary_detail_level 3 --js jquery-ui-1.11.4.custom.js --js_output_file jquery-ui-1.11.4.custom2.js
-time java -jar compiler.jar --summary_detail_level 3 --js spectrum.js --js_output_file spectrum2.js
+# time java -jar compiler.jar --summary_detail_level 3 --js jquery-ui-1.11.4.custom.js --js_output_file jquery-ui-1.11.4.custom2.js
+time java -jar compiler.jar --summary_detail_level 3 --js spectrum.js --js_output_file spectrum2.js > result.txt
+wc -l result.txt
 
 find ./ -name "*.js" -type f -print0
 
