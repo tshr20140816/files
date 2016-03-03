a093f9316219a072679a11a93885c3f78c929b02
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-03 09:40:30 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-echo "0940"
+echo "0941"
 
 # set -x
 
 cd /tmp
 
 whereis java
+rm -f compiler-latest.zip
 wget http://closure-compiler.googlecode.com/files/compiler-latest.zip
 unzip compiler-latest.zip
 
