aacd2b4f394c4cc77c71a44a95b95cb84d313cba
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-03 11:21:30 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-echo "1119"
+echo "1121"
 
 # set -x
 
 cd /tmp
+
+mkdir compiled
 
 whereis java
 rm -f compiler-latest.zip
@@ -14,15 +16,17 @@ rm -f *.js
 # wget http://dl.google.com/closure-compiler/compiler-latest.zip
 # unzip compiler-latest.zip
 
-# wget https://foo-20140818.rhcloud.com/caldavzap/lib/jquery-ui-1.11.4.custom.js
+wget https://foo-20140818.rhcloud.com/caldavzap/lib/jquery-ui-1.11.4.custom.js
 wget https://foo-20140818.rhcloud.com/caldavzap/lib/spectrum.js
 
 java -jar compiler.jar --version
 # java -jar compiler.jar --help
 
-# time java -jar compiler.jar --summary_detail_level 3 --js jquery-ui-1.11.4.custom.js --js_output_file jquery-ui-1.11.4.custom2.js
-time java -jar compiler.jar --summary_detail_level 3 --js spectrum.js --js_output_file spectrum2.js 2> result.txt
-wc -l result.txt
+time java -jar compiler.jar --summary_detail_level 3 --js jquery-ui-1.11.4.custom.js --js_output_file compiled/jquery-ui-1.11.4.custom.js 2> result.txt
+if [ "$(cat ./result.txt)" = "0 error(s), 0 warning(s)" ]; then
+    echo "MATCH"
+fi
+time java -jar compiler.jar --summary_detail_level 3 --js spectrum.js --js_output_file compiled/spectrum.js 2> result.txt
 if [ "$(cat ./result.txt)" = "0 error(s), 0 warning(s)" ]; then
     echo "MATCH"
 fi
