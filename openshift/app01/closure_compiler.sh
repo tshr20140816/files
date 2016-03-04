22373fa64e44f7b624258581821dd86c1ba0d088
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-04 13:29:03 +0900

    Update closure_compiler.sh

--- openshift/app01/closure_compiler.sh
+++ openshift/app01/closure_compiler.sh
@@ -2,8 +2,9 @@
 
 pushd ${OPENSHIFT_TMP_DIR} > /dev/null
 
-compiled_file=./$(basename ${1})
-result_file=${compiled_file}.result.txt
+random_value=${RANDOM}
+compiled_file=./$(basename ${1}).${random_value}
+result_file=${compiled_file}.result.txt.${random_value}
 
 time java -jar ./compiler.jar \
  --summary_detail_level 3 \
@@ -14,10 +15,10 @@ time java -jar ./compiler.jar \
 if [ "$(cat ${result_file})" = "0 error(s), 0 warning(s)" ]; then
     echo "CHANGED ${1}"
     cp -f ${1} ${1}.$(date '+%Y%m%d')
-    mv -f ./$(basename ${1}) ${1}
+    mv -f ./${compiled_file} ${1}
 else
     echo "NOT CHANGED $(head -n1 ${result_file}) ${1}"
 fi
-rm -f ./$(basename ${1}).result.txt
+rm -f ./${result_file}
 
 popd > /dev/null
