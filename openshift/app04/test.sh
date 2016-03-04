883c7bc0d28dcd9000cd053554f30d06af6569b5
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-04 10:10:23 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-echo "0957"
+echo "1010"
 
 # set -x
 
 cd /tmp
 
+rm -rf jpegoptim
+rm -rf jpegoptim-1.4.3
+rm -rf optipng
+rm -rf optipng-0.7.5
+rm -f result.txt
+rm -f test2016029.txt
+
 ls -lang >> ${OPENSHIFT_LOG_DIR}/test.log
 
 cd ${OPENSHIFT_DATA_DIR}
 
+rm -rf sphinx
+
 ls -lang >> ${OPENSHIFT_LOG_DIR}/test.log
+
 
 exit
