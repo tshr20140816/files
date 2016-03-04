3d1ab960ced6239116cc91e171cf28a063665d2f
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-04 16:12:25 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -9,6 +9,9 @@ rm -f ${OPENSHIFT_LOG_DIR}/cron_minutely.log-*
 
 cd /tmp
 
+rm -f *.png
+rm -f *.png.bak
+
 ls -lang
 
 # optipng_version="0.7.5"
@@ -19,5 +22,10 @@ ls -lang
 # tar xfz optipng-${optipng_version}.tar.gz
 
 ./optipng/bin/optipng --help
+
+cp -f $(head -n 1 png_compress_target_list.txt) ./$(basename $(head -n 1 png_compress_target_list.txt))
+time ./optipng/bin/optipng ./$(basename $(head -n 1 png_compress_target_list.txt)
+
+ls -lang
 
 exit
