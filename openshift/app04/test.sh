8b7bd0a03bc3fb44fb764c3c9221f112337f490e
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-04 09:58:12 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,27 +1,15 @@
 #!/bin/bash
 
-echo "1549"
+echo "0957"
 
 # set -x
 
 cd /tmp
 
-# wget http://www.kokkonen.net/tjko/src/jpegoptim-1.4.3.tar.gz
-# tar xvfz jpegoptim-1.4.3.tar.gz
-# ls -lang
-# cd jpegoptim*
-# ls -lang
-# ./configure --help
-# time ./configure --prefix=/tmp/jpegoptim
-# time make
-# time make install
-
-# wget http://downloads.sourceforge.net/project/optipng/OptiPNG/optipng-0.7.5/optipng-0.7.5.tar.gz
-# tar xvfz optipng-0.7.5.tar.gz
 ls -lang
-cd optipng*
-./configure --help
-time ./configure --prefix=/tmp/optipng
-time make
-time make install
+
+cd ${OPENSHIFT_DATA_DIR}
+
+ls -lang
+
 exit
