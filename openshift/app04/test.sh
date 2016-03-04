8f07ab1e52a17091da57c24de8fa1dd3401a07fb
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-04 10:41:17 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "1033"
+echo "1041"
 
 set -x
 
@@ -9,6 +9,7 @@ cd /tmp
 # wget https://tt-rss.org/gitlab/fox/tt-rss/repository/archive.zip?ref=master -O ttrss_archive.zip
 # wget http://www.kokkonen.net/tjko/src/jpegoptim-1.4.3.tar.gz
 # wget http://downloads.sourceforge.net/project/optipng/OptiPNG/optipng-0.7.5/optipng-0.7.5.tar.gz
+rm -f compiler-latest.zip
 wget http://dl.google.com/closure-compiler/compiler-latest.zip
 
 # cp ./ttrss_archive.zip ${OPENSHIFT_DATA_DIR}/
