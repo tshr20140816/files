bdf8d5cd40e1f24cf0f9110c28e1cb790a48f7f7
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-04 16:07:20 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -9,34 +9,15 @@ rm -f ${OPENSHIFT_LOG_DIR}/cron_minutely.log-*
 
 cd /tmp
 
-rm -f *.js*
-
 ls -lang
 
-optipng_version="0.7.5"
-rm -f  optipng-${optipng_version}.tar.gz
-rm -rf optipng*
-wget http://downloads.sourceforge.net/project/optipng/OptiPNG/optipng-${optipng_version}/optipng-${optipng_version}.tar.gz
+# optipng_version="0.7.5"
+# rm -f  optipng-${optipng_version}.tar.gz
+# rm -rf optipng*
+# wget http://downloads.sourceforge.net/project/optipng/OptiPNG/optipng-${optipng_version}/optipng-${optipng_version}.tar.gz
 
-tar xfz optipng-${optipng_version}.tar.gz
+# tar xfz optipng-${optipng_version}.tar.gz
 
-cd optipng*
-./configure --prefix=/tmp/optipng
-time make -j4
-make install
-
-cd /tmp
-
-rm -rf optipng-*
-rm -f  optipng-${optipng_version}.tar.gz
-
-find ${OPENSHIFT_DATA_DIR} -name "*.png" -mindepth 2 -type f -print | tee -a ./png_compress_target_list.txt
-
-cat ./png_compress_target_list.txt
-
-ls -lang optipng
-ls -lang optipng/bin
-
-tree optipng
+./optipng/bin/optipng --help
 
 exit
