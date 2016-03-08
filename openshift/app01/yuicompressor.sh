53075f53761b1c79819e129f8cffbf916b80ff4a
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-08 11:44:13 +0900

    Create yuicompressor.sh

---
+++ openshift/app01/yuicompressor.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+export TZ=JST-9
+
+echo "$(date +%Y/%m/%d" "%H:%M:%S) $$ ${1}"
+echo "$(date +%Y/%m/%d" "%H:%M:%S) $$ $(oo-cgroup-read memory.usage_in_bytes | awk '{printf "%\047d\n", $1}')" \
+ >> ${OPENSHIFT_LOG_DIR}/yuicompressor.log
+while :
+do
+    local usage_in_bytes=$(oo-cgroup-read memory.usage_in_bytes)
+    if [ ${usage_in_bytes} -gt 350000000 ]; then
+        echo "$(date +%Y/%m/%d" "%H:%M:%S) $$ $(oo-cgroup-read memory.usage_in_bytes | awk '{printf "%\047d\n", $1}') waiting" \
+         >> ${OPENSHIFT_LOG_DIR}/yuicompressor.log
+        sleep 5s
+    else
+        break
+    fi
+done
+pushd ${OPENSHIFT_TMP_DIR} > /dev/null
+local suffix=$(date '+%Y%m%d')
+local target_file=${1}
+local compressed_file=./$(basename ${target_file}).$$
+rm -f ${compressed_file}.$$
+time java -jar ${OPENSHIFT_TMP_DIR}/yuicompressor.jar \
+ --type css \
+ -o ${compressed_file} \
+ ${target_file} \
+ >> ${OPENSHIFT_LOG_DIR}/yuicompressor.log 2>&1
+if [ ! -f ${compressed_file} ]; then
+    echo "$(date +%Y/%m/%d" "%H:%M:%S) $$ NOT CHANGED (ERROR) ${target_file}" \
+     >> ${OPENSHIFT_LOG_DIR}/yuicompressor.log
+else
+    local size_original=$(wc -c < ${target_file})
+    local size_compiled=$(wc -c < ${compressed_file})
+    if [ ${size_original} -gt ${size_compiled} ]; then
+        echo "$(date +%Y/%m/%d" "%H:%M:%S) $$ CHANGED ${size_original} ${size_compiled} ${target_file}" \
+         >> ${OPENSHIFT_LOG_DIR}/yuicompressor.log
+        mv -f ${target_file} ${target_file}.${suffix}
+        mv -f ${compressed_file} ${target_file}
+    else
+        echo "$(date +%Y/%m/%d" "%H:%M:%S) $$ NOT CHANGED (SIZE NOT DOWNED) ${size_original} ${size_compiled} ${target_file}" \
+         >> ${OPENSHIFT_LOG_DIR}/yuicompressor.log
+        rm -f ${compressed_file}
+    fi
+fi
+popd > /dev/null
