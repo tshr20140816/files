0761d7eff42c0a387c37f7470934f2a51c11482d
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-09 08:40:36 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "0927"
+echo "0840"
 
 # set -x
 
@@ -9,6 +9,15 @@ rm -f ${OPENSHIFT_LOG_DIR}/cron_minutely.log-*
 
 cd /tmp
 
-find ${OPENSHIFT_DATA_DIR} -name *.css -print
+rm -f compiler-latest.zip
+wget http://dl.google.com/closure-compiler/compiler-latest.zip
+unzip compiler-latest.zip
+rm -f compiler-latest.zip
+
+mv -f ./compiler.jar ${OPENSHIFT_DATA_DIR}/compiler.jar
+
+ls -lang
+
+ls -lang ${OPENSHIFT_DATA_DIR}
 
 exit
