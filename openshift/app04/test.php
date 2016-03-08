1b4cc8fb10ceb36aad6139f95bc0efa6743e3d22
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-09 08:58:45 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -2,26 +2,20 @@
 // header('Content-type: application/force-download');
 header('Content-type: text/plain');
 
-// echo date('Ymd');
+$url = "https://woo-20140818.rhcloud.com/ttrss/js/FeedTree.js";
+$s = file_get_contents($url);
+if($s == FALSE){
+    print "ERROR 1";
+    return;
+}
 
-$url = "http://www.yahoo.co.jp/";
-$url = "https://github.com/tshr20140816/files/raw/master/openshift/app04/test.php";
-$ch = curl_init();
-
-curl_setopt($ch, CURLOPT_URL, $url); 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_NOBODY, true);
-curl_exec($ch);
-$info = curl_getinfo($ch);
-curl_close($ch);
-
-//print $info["http_code"];
-print var_dump($info);
-
-$context = stream_context_create(array(
-    'http' => array('ignore_errors' => true, 'follow_location' => 1);
-));
-
-$response = file_get_contents($url, false, $context);
-print $http_response_header[0];
+$cmd = "java -jar " . getenv("OPENSHIFT_DATA_DIR") . "/compiler.jar --summary_detail_level 3 --compilation_level SIMPLE_OPTIMIZATIONS --js " . $url . " --js_output_file=/tmp/compiled.js";
+exec($cmd, $arr, $res);
+if($res === 0){
+    print var_dump($arr);
+} else {
+    print "ERROR 2";
+    return;
+}
+// print var_dump($info);
 ?>
