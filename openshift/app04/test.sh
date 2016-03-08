9f57159cf120621fa9c411c9bdb3048a745949f3
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-08 09:14:33 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "0909"
+echo "0914"
 
 set -x
 
@@ -9,26 +9,10 @@ rm -f ${OPENSHIFT_LOG_DIR}/cron_minutely.log-*
 
 cd /tmp
 
+rm -f 300000000
+
 yuicompressor_version="2.4.8"
-# wget https://github.com/yui/yuicompressor/releases/download/v${yuicompressor_version}/yuicompressor-${yuicompressor_version}.jar
-
-time java -jar ${OPENSHIFT_TMP_DIR}/yuicompressor-${yuicompressor_version}.jar \
- --type css \
- -o ./cdm.css \
- ${OPENSHIFT_DATA_DIR}/tt-rss.git/css/cdm.css
-
-cat cdm.css
-cat ${OPENSHIFT_DATA_DIR}/tt-rss.git/css/cdm.css
-
-ls -lang
-
-# cd ${OPENSHIFT_DATA_DIR}
-
-# rm -rf tt-rss.git
-# unzip ttrss_archive.zip
-
-# ls -lang
-
-# find ./ -name *.css -print
+rm -f yuicompressor-${yuicompressor_version}.jar
+rm -f cdm.css
 
 exit
