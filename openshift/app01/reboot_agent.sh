6c02270bb28278806521315d95eac54df9d86563
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-08 09:39:26 +0900

    Update reboot_agent.sh

--- openshift/app01/reboot_agent.sh
+++ openshift/app01/reboot_agent.sh
@@ -8,56 +8,6 @@ fi
 install_script_file=${1}
 web_beacon_server=${2}
 
-func_closure_compile() {
-    echo "$$ $(oo-cgroup-read memory.usage_in_bytes | awk '{printf "%\047d\n", $1}')" \
-     >> ${OPENSHIFT_LOG_DIR}/closure_compiler.log
-    while :
-    do
-        local usage_in_bytes=$(oo-cgroup-read memory.usage_in_bytes)
-        if [ ${usage_in_bytes} -gt 350000000 ]; then
-            echo "$$ $(oo-cgroup-read memory.usage_in_bytes | awk '{printf "%\047d\n", $1}') waiting" \
-             >> ${OPENSHIFT_LOG_DIR}/closure_compiler.log
-            sleep 5s
-        else
-            break
-        fi
-    done
-    pushd ${OPENSHIFT_TMP_DIR} > /dev/null
-    local suffix=$(date '+%Y%m%d')
-    local rand=${RANDOM}
-    local target_file=${1}
-    local compiled_file=./$(basename ${target_file}).${rand}
-    local result_file=${compiled_file}.result.txt.${rand}
-    rm -f ${compiled_file}.${rand}
-    rm -f ${result_file}.${rand}
-    time java -jar ${OPENSHIFT_DATA_DIR}/compiler.jar \
-     --summary_detail_level 3 \
-     --compilation_level SIMPLE_OPTIMIZATIONS \
-     --js ${target_file} \
-     --js_output_file ${compiled_file} \
-     2> ${result_file}
-    if [ "$(cat ${result_file})" = "0 error(s), 0 warning(s)" ]; then
-        local size_original=$(wc -c < ${target_file})
-        local size_compiled=$(wc -c < ${compiled_file})
-        if [ ${size_original} -gt ${size_compiled} ]; then
-            echo "$(date +%Y/%m/%d" "%H:%M:%S) CHANGED ${size_original} ${size_compiled} ${target_file}" \
-             >> ${OPENSHIFT_LOG_DIR}/closure_compiler.log
-            cp -f ${target_file} ${target_file}.${suffix}
-            mv -f ${compiled_file} ${target_file}
-        else
-            echo "$(date +%Y/%m/%d" "%H:%M:%S) NOT CHANGED (SIZE NOT DOWNED) ${size_original} ${size_compiled} ${target_file}" \
-             >> ${OPENSHIFT_LOG_DIR}/closure_compiler.log
-        fi
-    else
-        echo "$(date +%Y/%m/%d" "%H:%M:%S) NOT CHANGED (ERROR OR WARNING) ${target_file} $(tail -n 1 ./${result_file})" \
-         >> ${OPENSHIFT_LOG_DIR}/closure_compiler.log
-        # cat ${result_file} >> ${OPENSHIFT_LOG_DIR}/closure_compiler.log
-    fi
-    rm -f ${compiled_file}
-    rm -f ${result_file}
-    popd > /dev/null
-}
-
 loop_counter=0
 
 while :
@@ -216,7 +166,9 @@ do
         unzip compiler-latest.zip
         rm -f compiler-latest.zip
         popd > /dev/null
-        cat ${OPENSHIFT_DATA_DIR}/javascript_compress_target_list.txt | xargs -P 3 -n 1 func_closure_compile
+        chmod +x ${OPENSHIFT_DATA_DIR}/download_files/closure_compiler.sh
+        cat ${OPENSHIFT_DATA_DIR}/javascript_compress_target_list.txt \
+         | xargs -I {} -P 3 -n 1 ${OPENSHIFT_DATA_DIR}/download_files/closure_compiler.sh {}
         rm -f ${OPENSHIFT_DATA_DIR}/compiler.jar
         pushd ${OPENSHIFT_LOG_DIR} > /dev/null
             zip -9 ${OPENSHIFT_APP_NAME}-${OPENSHIFT_NAMESPACE}.closure_compiler.log.zip closure_compiler.log
