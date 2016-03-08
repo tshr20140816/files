cfba811b33c4150fcd89b97c69d9484a638748d6
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-08 09:30:01 +0900

    Update closure_compiler.sh

--- openshift/app01/closure_compiler.sh
+++ openshift/app01/closure_compiler.sh
@@ -1,25 +1,50 @@
 #!/bin/bash
 
+export TZ=JST-9
+
+echo "$$ $(oo-cgroup-read memory.usage_in_bytes | awk '{printf "%\047d\n", $1}')" \
+ >> ${OPENSHIFT_LOG_DIR}/closure_compiler.log
+while :
+do
+    local usage_in_bytes=$(oo-cgroup-read memory.usage_in_bytes)
+    if [ ${usage_in_bytes} -gt 350000000 ]; then
+        echo "$$ $(oo-cgroup-read memory.usage_in_bytes | awk '{printf "%\047d\n", $1}') waiting" \
+         >> ${OPENSHIFT_LOG_DIR}/closure_compiler.log
+        sleep 5s
+    else
+        break
+    fi
+done
 pushd ${OPENSHIFT_TMP_DIR} > /dev/null
-
-compiled_file=./$(basename ${1}).$$
-result_file=${compiled_file}.result.txt.$$
-
-time java -jar ./compiler.jar \
+local suffix=$(date '+%Y%m%d')
+local target_file=${1}
+local compiled_file=./$(basename ${target_file}).$$
+local result_file=${compiled_file}.result.txt.$$
+rm -f ${compiled_file}.$$
+rm -f ${result_file}.$$
+time java -jar ${OPENSHIFT_DATA_DIR}/compiler.jar \
  --summary_detail_level 3 \
- --js ${1}
+ --compilation_level SIMPLE_OPTIMIZATIONS \
+ --js ${target_file} \
  --js_output_file ${compiled_file} \
  2> ${result_file}
-
 if [ "$(cat ${result_file})" = "0 error(s), 0 warning(s)" ]; then
-    size_original=$(wc -c < ${1})
-    size_compiled=$(wc -c < ./${compiled_file})
-    echo "CHANGED ${size_original} ${size_compiled} ${1}"
-    cp -f ${1} ${1}.$(date '+%Y%m%d')
-    mv -f ./${compiled_file} ${1}
+    local size_original=$(wc -c < ${target_file})
+    local size_compiled=$(wc -c < ${compiled_file})
+    if [ ${size_original} -gt ${size_compiled} ]; then
+        echo "$(date +%Y/%m/%d" "%H:%M:%S) CHANGED ${size_original} ${size_compiled} ${target_file}" \
+         >> ${OPENSHIFT_LOG_DIR}/closure_compiler.log
+        cp -f ${target_file} ${target_file}.${suffix}
+        mv -f ${compiled_file} ${target_file}
+    else
+        echo "$(date +%Y/%m/%d" "%H:%M:%S) NOT CHANGED (SIZE NOT DOWNED) ${size_original} ${size_compiled} ${target_file}" \
+         >> ${OPENSHIFT_LOG_DIR}/closure_compiler.log
+    fi
 else
-    echo "NOT CHANGED $(head -n1 ${result_file}) ${1}"
+    echo "$(date +%Y/%m/%d" "%H:%M:%S) NOT CHANGED (ERROR OR WARNING) ${target_file} $(tail -n 1 ./${result_file})" \
+     >> ${OPENSHIFT_LOG_DIR}/closure_compiler.log
+    # cat ${result_file} >> ${OPENSHIFT_LOG_DIR}/closure_compiler.log
 fi
-rm -f ./${result_file}
-
+rm -f ${compiled_file}
+rm -f ${result_file}
 popd > /dev/null
