f71e84a7b5d7ac4b879772f2e95be4744dbcd68d
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-08 10:30:26 +0900

    Update closure_compiler.sh

--- openshift/app01/closure_compiler.sh
+++ openshift/app01/closure_compiler.sh
@@ -2,13 +2,14 @@
 
 export TZ=JST-9
 
-echo "$$ $(oo-cgroup-read memory.usage_in_bytes | awk '{printf "%\047d\n", $1}')" \
+echo "$(date +%Y/%m/%d" "%H:%M:%S) $$ ${1}"
+echo "$(date +%Y/%m/%d" "%H:%M:%S) $$ $(oo-cgroup-read memory.usage_in_bytes | awk '{printf "%\047d\n", $1}')" \
  >> ${OPENSHIFT_LOG_DIR}/closure_compiler.log
 while :
 do
     local usage_in_bytes=$(oo-cgroup-read memory.usage_in_bytes)
     if [ ${usage_in_bytes} -gt 350000000 ]; then
-        echo "$$ $(oo-cgroup-read memory.usage_in_bytes | awk '{printf "%\047d\n", $1}') waiting" \
+        echo "$(date +%Y/%m/%d" "%H:%M:%S) $$ $(oo-cgroup-read memory.usage_in_bytes | awk '{printf "%\047d\n", $1}') waiting" \
          >> ${OPENSHIFT_LOG_DIR}/closure_compiler.log
         sleep 5s
     else
@@ -32,16 +33,16 @@ if [ "$(cat ${result_file})" = "0 error(s), 0 warning(s)" ]; then
     local size_original=$(wc -c < ${target_file})
     local size_compiled=$(wc -c < ${compiled_file})
     if [ ${size_original} -gt ${size_compiled} ]; then
-        echo "$(date +%Y/%m/%d" "%H:%M:%S) CHANGED ${size_original} ${size_compiled} ${target_file}" \
+        echo "$(date +%Y/%m/%d" "%H:%M:%S) $$ CHANGED ${size_original} ${size_compiled} ${target_file}" \
          >> ${OPENSHIFT_LOG_DIR}/closure_compiler.log
         cp -f ${target_file} ${target_file}.${suffix}
         mv -f ${compiled_file} ${target_file}
     else
-        echo "$(date +%Y/%m/%d" "%H:%M:%S) NOT CHANGED (SIZE NOT DOWNED) ${size_original} ${size_compiled} ${target_file}" \
+        echo "$(date +%Y/%m/%d" "%H:%M:%S) $$ NOT CHANGED (SIZE NOT DOWNED) ${size_original} ${size_compiled} ${target_file}" \
          >> ${OPENSHIFT_LOG_DIR}/closure_compiler.log
     fi
 else
-    echo "$(date +%Y/%m/%d" "%H:%M:%S) NOT CHANGED (ERROR OR WARNING) ${target_file} $(tail -n 1 ./${result_file})" \
+    echo "$(date +%Y/%m/%d" "%H:%M:%S) $$ NOT CHANGED (ERROR OR WARNING) ${target_file} $(tail -n 1 ./${result_file})" \
      >> ${OPENSHIFT_LOG_DIR}/closure_compiler.log
     # cat ${result_file} >> ${OPENSHIFT_LOG_DIR}/closure_compiler.log
 fi
