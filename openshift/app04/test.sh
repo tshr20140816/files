b3d39b9281c776233a81b6b81a2244cc10497134
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-09 15:27:06 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "1445"
+echo "1527"
 
 # set -x
 
@@ -9,11 +9,22 @@ rm -f ${OPENSHIFT_LOG_DIR}/cron_minutely.log-*
 
 cd /tmp
 
-rm -f test.php
-wget https://github.com/tshr20140816/files/raw/master/openshift/app04/test.php
+# rm -f test.php
+# wget https://github.com/tshr20140816/files/raw/master/openshift/app04/test.php
 
-cp -f ./test.php ${OPENSHIFT_REPO_DIR}/
+# cp -f ./test.php ${OPENSHIFT_REPO_DIR}/
 
 ls -lang
+
+mkdir temp1
+cd temp1
+rm -f FeedTree.js
+wget https://woo-20140818.rhcloud.com/ttrss/js/FeedTree.js
+js_code=$(cat ./FeedTree.js)
+js_code=$(php -r 'echo urlencode("${js_code}");');
+echo ${js_code}
+# wget --post-data="suffix=$$&js_code=2" --content-disposition https://tenv-20150605.rhcloud.com/test.php 
+cd ..
+rm -rf temp1
 
 exit
