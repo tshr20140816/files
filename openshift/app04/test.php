8a1eba65725ab3024df272297cd4165f5f0e6751
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-09 15:08:48 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -1,31 +1,34 @@
 <?php
-// header('Content-type: application/force-download');
-header('Content-type: text/plain');
 
-print uniqid();
-print session_id();
+$suffix = urldecode($_POST["suffix"]);
+$js_code = urldecode($_POST["js_code"]);
 
-$url = "https://woo-20140818.rhcloud.com/ttrss/js/FeedTree.js";
-$s = file_get_contents($url);
-if($s == FALSE){
-    print "ERROR 1";
-    return;
-}
+$original_file = getenv("OPENSHIFT_TMP_DIR") . "/original.$suffix.js";
+$compiled_file = getenv("OPENSHIFT_TMP_DIR") . "/compiled.$suffix.js";
+$result_file = getenv("OPENSHIFT_TMP_DIR") . "/result.$suffix.txt";
+$zip_file = getenv("OPENSHIFT_TMP_DIR") . "/result.$suffix.zip";
+$download_file = "result.$suffix.zip";
 
-print urlencode($s);
-print urldecode(urlencode($s));
-
-$fp = fopen("/tmp/FeedTree.js", "w");
-fwrite($fp, $s);
+$fp = fopen($original_file, "w");
+fwrite($fp, $js_code);
 fclose($fp);
 
-$cmd = "java -jar " . getenv("OPENSHIFT_DATA_DIR") . "/compiler.jar --summary_detail_level 3 --compilation_level SIMPLE_OPTIMIZATIONS --js /tmp/FeedTree.js --js_output_file /tmp/compiled.js 2>&1";
+$cmd = "java -jar " . getenv("OPENSHIFT_DATA_DIR") . "/compiler.jar --summary_detail_level 3 --compilation_level SIMPLE_OPTIMIZATIONS --js $original_file --js_output_file $compiled_file 2>&1";
 exec($cmd, $arr, $res);
-if($res === 0){
-    print var_dump($arr);
-} else {
-    print "ERROR 2";
-    return;
-}
-// print var_dump($info);
+
+$fp = fopen($result_file, "w");
+fwrite($fp, $arr[0]);
+fclose($fp);
+
+$cmd = "zip -9 $zip_file $compiled_file $result_file";
+exec($cmd, $arr, $res);
+
+header("Content-Type: application/octet-stream");
+header("Content-Disposition: attachment; filename=$download_file");
+readfile($zip_file);
+
+unlink($original_file);
+unlink($compiled_file);
+unlink($result_file);
+unlink($zip_file);
 ?>
