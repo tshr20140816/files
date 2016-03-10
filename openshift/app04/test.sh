dac51b72f68b7afb536ee539709c068ce539a086
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-10 13:39:00 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "1810"
+echo "1337"
 
 # set -x
 
@@ -9,26 +9,15 @@ rm -f ${OPENSHIFT_LOG_DIR}/cron_minutely.log-*
 
 cd /tmp
 
-# rm -f test.php
-# wget https://github.com/tshr20140816/files/raw/master/openshift/app04/test.php
+cat << '__HEREDOC__' > test.txt
+111@aaa-bbb.test.local
+222@aaa-bbb.test.local
+333@aaa-bbb.test.local
+abc@aaa-bbb.test.local
+__HEREDOC__
 
-# cp -f ./test.php ${OPENSHIFT_REPO_DIR}/
+sed -e s/^.+?@// test.txt
 
-ls -lang
-
-mkdir temp1
-cd temp1
-rm -f FeedTree.js
-wget https://woo-20140818.rhcloud.com/ttrss/js/FeedTree.js
-# js_code=$(cat ./FeedTree.js)
-ruby -r uri -ne 'puts URI.escape $_.chomp' < ./FeedTree.js | tr "\n" " " | sed 's/ /%0D%0A/g'
-# echo ${js_code}
-# php -r 'echo urlencode("$js_code");'
-# js_code=$(php -r 'echo urlencode("$js_code");')
-# js_code=$(php -r "echo urlencode(\"$js_code\");")
-# echo "${js_code}"
-# wget --post-data="suffix=$$&js_code=2" --content-disposition https://tenv-20150605.rhcloud.com/test.php 
-cd ..
-rm -rf temp1
+cat test.txt
 
 exit
