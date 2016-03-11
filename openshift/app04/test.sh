957e40dafe11806929f9ede351696586b67b32ec
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-11 10:08:14 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "1657"
+echo "1007"
 
 # set -x
 
@@ -9,25 +9,8 @@ rm -f ${OPENSHIFT_LOG_DIR}/cron_minutely.log-*
 
 cd /tmp
 
-cat << '__HEREDOC__' > test.txt
-111@aaa-bbb.test.local
-222@aaa-bbb.test.local
-333@aaa-bbb.test.local
-abc@aaa-bbb.test.local
-__HEREDOC__
-
-sed -e 's/^.\+\?@//g' test.txt
-
-cat test.txt
-
-line=$((10%3+1))
-sed -n -e ${line}p test.txt
-
-curl http://ftp.riken.jp/net/apache//httpd/httpd-2.2.31.tar.bz2 > /dev/null 2>&1 &
-# ps auwx | grep curl
-pgrep -l curl
-pgrep -f curl
-pgrep -fl curl
-wait
+cd ~
+pwd
+tree
 
 exit
