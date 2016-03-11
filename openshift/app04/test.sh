777adcdee96d24a5d0d258a390a7acc99f69e30e
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-11 11:23:09 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-echo "1116"
+echo "1123"
 
-set -x
+# set -x
 
 rm -f ${OPENSHIFT_LOG_DIR}/test.log
 rm -f ${OPENSHIFT_LOG_DIR}/cron_minutely.log-*
 
 cd /tmp
 
-find ${OPENSHIFT_DATA_DIR} -name "*" -mindepth 2 -type f -print0 \
+rm -f httpd-2.2.31.tar.bz2
+rm -rf httpd-2.2.31
+rm -rf apache
+wget http://ftp.riken.jp/net/apache/httpd/httpd-2.2.31.tar.bz2
+tar jxf httpd-2.2.31.tar.bz2
+cd httpd-2.2.31
+./configure --prefix=/tmp/apache
+time make
+time make install
+
+find /tmp/apache -name "*" -mindepth 2 -type f -print0 \
  | xargs -0i file {} \
  | grep -e "not stripped" \
  | grep -v -e "delegated" 
