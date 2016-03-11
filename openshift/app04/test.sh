0a0398ae853de9b53716a287744e613f7da7a5d4
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-11 10:24:50 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -2,7 +2,7 @@
 
 echo "1007"
 
-# set -x
+set -x
 
 rm -f ${OPENSHIFT_LOG_DIR}/test.log
 rm -f ${OPENSHIFT_LOG_DIR}/cron_minutely.log-*
@@ -11,6 +11,14 @@ cd /tmp
 
 cd ~
 pwd
-tree
+# tree
+
+ls -lang php/configuration/etc/conf/
+ls -lang php/configuration/etc/conf.d/
+
+cat php/configuration/etc/conf/httpd.conf
+cat php/configuration/etc/conf/httpd_nolog.conf
+cat php/configuration/etc/conf.d/openshift.conf
+cat php/configuration/etc/conf.d/php.conf
 
 exit
