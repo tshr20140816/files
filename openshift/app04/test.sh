7646490a0b15fa432c3c415e3bbd8dc4cf579e56
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-11 12:30:17 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "1158"
+echo "1230"
 
 # set -x
 
@@ -12,27 +12,27 @@ cd /tmp
 rm -f httpd-2.2.31.tar.bz2
 rm -rf httpd-2.2.31
 
-mkdir 20160311
 cd 20160311
-cp -f /tmp/apache/lib/libapr-1.so.0.5.2 ./
-cp -f /tmp/apache/lib/apr-util-1/apr_dbd_pgsql-1.so ./
-cp -f /tmp/apache/lib/apr-util-1/apr_dbd_sqlite3-1.so ./
-cp -f /tmp/apache/lib/apr-util-1/apr_dbd_odbc-1.so ./
-cp -f /tmp/apache/lib/libaprutil-1.so.0.5.4 ./
-cp -f /tmp/apache/bin/htpasswd ./
-cp -f /tmp/apache/bin/htdigest ./
-cp -f /tmp/apache/bin/rotatelogs ./
-cp -f /tmp/apache/bin/logresolve ./
-cp -f /tmp/apache/bin/ab ./
-cp -f /tmp/apache/bin/htdbm ./
-cp -f /tmp/apache/bin/htcacheclean ./
-cp -f /tmp/apache/bin/httxt2dbm ./
-cp -f /tmp/apache/bin/checkgid ./
-cp -f /tmp/apache/bin/httpd ./
 
 ls -lang
 
+time strip --strip-debug ab
+time strip --strip-debug apr_dbd_odbc-1.so
+time strip --strip-debug apr_dbd_pgsql-1.so
+time strip --strip-debug apr_dbd_sqlite3-1.so
+time strip --strip-debug checkgid
+time strip --strip-debug htcacheclean
+time strip --strip-debug htdbm
+time strip --strip-debug htdigest
+time strip --strip-debug htpasswd
+time strip --strip-debug httpd
+time strip --strip-debug httxt2dbm
+time strip --strip-debug libapr-1.so.0.5.2
+time strip --strip-debug libaprutil-1.so.0.5.4
+time strip --strip-debug logresolve
+time strip --strip-debug rotatelogs
 
+ls -lang
 
 # time strip --strip-debug /tmp/apache/bin/rotatelogs 
 
