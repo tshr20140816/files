d8b9e0fad9aca0059b1c15718125b4043fcb1f17
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-11 11:58:14 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "1137"
+echo "1158"
 
-set -x
+# set -x
 
 rm -f ${OPENSHIFT_LOG_DIR}/test.log
 rm -f ${OPENSHIFT_LOG_DIR}/cron_minutely.log-*
@@ -11,14 +11,23 @@ cd /tmp
 
 rm -f httpd-2.2.31.tar.bz2
 rm -rf httpd-2.2.31
+rm -rf apache
 
-# time find /tmp/apache -name "*" -mindepth 2 -type f -print0 \
-#  | xargs -0i file {} \
-#  | grep -e "not stripped" \
-#  | grep -v -e "delegated" | awk -F':' '{printf $1"\n"}' > /tmp/strip_starget.txt
+wget http://ftp.riken.jp/net/apache//httpd/httpd-2.2.31.tar.bz2
 
-# cat /tmp/strip_starget.txt | xargs -t -P 1 -n 1 time strip --strip-debug
+tar jxf httpd-2.2.31.tar.bz2
+cd httpd-2.2.31
+./configure --prefix=/tmp/apache > /dev/null
+make > /dev/null
+make install > /dev/null
 
-time strip --strip-debug /tmp/apache/bin/rotatelogs 
+time find /tmp/apache -name "*" -mindepth 2 -type f -print0 \
+ | xargs -0i file {} \
+ | grep -e "not stripped" \
+ | grep -v -e "delegated" | awk -F':' '{printf $1"\n"}' > /tmp/strip_starget.txt
+
+cat /tmp/strip_starget.txt
+
+# time strip --strip-debug /tmp/apache/bin/rotatelogs 
 
 exit
