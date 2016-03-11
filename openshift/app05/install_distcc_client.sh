9da526b3c98d1d9c4b1d3e311a1f3f953fb085a0
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-11 09:30:15 +0900

    Update install_distcc_client.sh

--- openshift/app05/install_distcc_client.sh
+++ openshift/app05/install_distcc_client.sh
@@ -63,7 +63,54 @@ popd > /dev/null
 # ***** PHP (Closure Compiler) *****
 
 pushd ${OPENSHIFT_REPO_DIR} > /dev/null
-wget https://raw.githubusercontent.com/tshr20140816/files/master/openshift/app05/closure_compiler.php
+# wget https://raw.githubusercontent.com/tshr20140816/files/master/openshift/app05/closure_compiler.php
+cat << '__HEREDOC__' > closure_compiler.php
+<?php
+// wget http://dl.google.com/closure-compiler/compiler-latest.zip
+// curl https://xxx/xxx.php -F "file=@./jquery-1.7.1.min.js" -F "suffix=uuid"
+$suffix = $_POST["suffix"];
+if (preg_match('/^\w+$/') == 0)
+{
+    header('HTTP', true, 500);
+    exit;
+}
+$file_name = $_FILES['file']['name'];
+$original_file = $file_name . "." . $suffix;
+$compiled_file = "compiled.$suffix.js";
+$result_file = "result.$suffix.txt";
+$zip_file = "result.$suffix.zip";
+$download_file = "result.$suffix.zip";
+$closure_compiler = getenv("OPENSHIFT_DATA_DIR") . "/compiler.jar";
+if (preg_match('/\.js$/') == 0)
+{
+    header('HTTP', true, 500);
+    exit;
+}
+move_uploaded_file($_FILES['file']['tmp_name'], getenv("OPENSHIFT_TMP_DIR") . "/" . $original_file);
+chdir(getenv("OPENSHIFT_TMP_DIR"));
+$cmd = "java -jar $closure_compiler --summary_detail_level 3 --compilation_level SIMPLE_OPTIMIZATIONS --js $original_file --js_output_file $compiled_file 2>&1";
+exec($cmd, $arr, $res);
+$fp = fopen($result_file, "w");
+fwrite($fp, $arr[0]);
+fclose($fp);
+if (file_exists($compiled_file))
+{
+    $cmd = "zip -9 $zip_file $compiled_file $result_file";
+}
+else
+{
+    $cmd = "zip -9 $zip_file $result_file";
+}
+exec($cmd, $arr, $res);
+header("Content-Type: application/octet-stream");
+header("Content-Disposition: attachment; filename=$download_file");
+readfile($zip_file);
+unlink($original_file);
+unlink($compiled_file);
+unlink($result_file);
+unlink($zip_file);
+?>
+__HEREDOC__
 popd > /dev/null
 
 # ***** cron *****
@@ -73,7 +120,6 @@ popd > /dev/null
 pushd ${OPENSHIFT_REPO_DIR}/.openshift/cron/hourly > /dev/null
 rm -f ./*
 touch jobs.deny
-
 
 cat << '__HEREDOC__' > delete_log.sh
 #!/bin/bash
