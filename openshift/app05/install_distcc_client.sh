62aa22559bfe83353593d5e83a4db573a334033f
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-13 00:06:52 +0900

    Update install_distcc_client.sh

--- openshift/app05/install_distcc_client.sh
+++ openshift/app05/install_distcc_client.sh
@@ -65,6 +65,45 @@ wget https://github.com/yui/yuicompressor/releases/download/v${yuicompressor_ver
 cp yuicompressor-${yuicompressor_version}.jar yuicompressor.jar
 popd > /dev/null
 
+# ***** PHP (YUI Compressor) *****
+
+pushd ${OPENSHIFT_REPO_DIR} > /dev/null
+cat << '__HEREDOC__' > yuicompressor.php
+<?php
+$suffix = $_POST["suffix"];
+if (preg_match('/^\w+$/', $suffix) == 0)
+{
+    header('HTTP', true, 500);
+    exit;
+}
+$file_name = $_FILES['file']['name'];
+$original_file = $file_name . "." . $suffix;
+$compressed_file = "compressed.$suffix.css";
+$yuicompressor = getenv("OPENSHIFT_DATA_DIR") . "/yuicompressor.jar";
+if (preg_match('/\.js$/', $file_name) == 0)
+{
+    header('HTTP', true, 500);
+    exit;
+}
+move_uploaded_file($_FILES['file']['tmp_name'], getenv("OPENSHIFT_TMP_DIR") . "/" . $original_file);
+chdir(getenv("OPENSHIFT_TMP_DIR"));
+$cmd = "java -jar $yuicompressor --type css -o $compressed_file $original_file 2>&1";
+exec($cmd, $arr, $res);
+if (file_exists($compressed_file))
+{
+    header("Content-Type: text/css");
+    echo file_get_contents($compressed_file);
+}
+else
+{
+    header('HTTP', true, 500);
+}
+@unlink($original_file);
+@unlink($compressed_file);
+?>
+__HEREDOC__
+popd > /dev/null
+
 # ***** Closure Compiler *****
 
 pushd ${OPENSHIFT_DATA_DIR} > /dev/null
