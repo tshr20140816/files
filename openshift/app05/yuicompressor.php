0fc6465c6125a1d4568e845897f50a255e8e3fde
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-14 10:35:07 +0900

    Update yuicompressor.php

--- openshift/app05/yuicompressor.php
+++ openshift/app05/yuicompressor.php
@@ -1,8 +1,11 @@
+		
+
 <?php
+
 if (!isset($_POST['suffix'], $_POST['path']))
 {
-    header('HTTP', true, 500);
-    exit;
+	header('HTTP', true, 500);
+	exit;
 }
 
 $suffix = $_POST["suffix"];
@@ -10,19 +13,20 @@ $path = $_POST["path"];
 
 if (preg_match('/^\w+$/', $suffix) == 0)
 {
-    header('HTTP', true, 500);
-    exit;
+	header('HTTP', true, 500);
+	exit;
 }
 
 if (preg_match('/.*\.\..*/', $path) == 1)
 {
-    header('HTTP', true, 500);
-    exit;
+	header('HTTP', true, 500);
+	exit;
 }
+
 if (preg_match('/^app-root\/data\/.+$/', $path) == 0)
 {
-    header('HTTP', true, 500);
-    exit;
+	header('HTTP', true, 500);
+	exit;
 }
 
 $compressed_path = getenv("OPENSHIFT_DATA_DIR") . "compressed/";
@@ -31,38 +35,45 @@ $file_name = $_FILES['file']['name'];
 $original_file = getenv("OPENSHIFT_TMP_DIR") . $file_name . "." . $suffix;
 $compressed_file = getenv("OPENSHIFT_TMP_DIR") . "compressed.$suffix.css";
 $yuicompressor = getenv("OPENSHIFT_DATA_DIR") . "/yuicompressor.jar";
+
 if (preg_match('/\.js$/', $file_name) == 0)
 {
-    header('HTTP', true, 500);
-    exit;
+	header('HTTP', true, 500);
+	exit;
 }
+
 move_uploaded_file($_FILES['file']['tmp_name'], $original_file);
 chdir(getenv("OPENSHIFT_TMP_DIR"));
-if(file_exists($compressed_path . ".compressed") && file_exists($compressed_path))
+
+if (file_exists($compressed_path . ".compressed") && file_exists($compressed_path))
 {
-    if(file_get_contents($original_file) == file_get_contents($compressed_path))
-    {
-        copy($compressed_path . ".compressed", $compressed_file);
-    }
-} else {
-    $cmd = "java -jar $yuicompressor --type css -o $compressed_file $original_file 2>&1";
-    exec($cmd, $arr, $res);
-}
-if (file_exists($compressed_file))
-{
-    header("Content-Type: text/css");
-    echo file_get_contents($compressed_file);
-    if (!file_exists($compressed_path . ".compressed"))
-    {
-        @mkdir(pathinfo($compressed_path, PATHINFO_DIRNAME), "0777", TRUE);
-        copy($original_file, $compressed_path);
-        copy($compressed_file, $compressed_path . ".compressed");
-    }
+	if (file_get_contents($original_file) == file_get_contents($compressed_path))
+	{
+		copy($compressed_path . ".compressed", $compressed_file);
+	}
 }
 else
 {
-    header('HTTP', true, 500);
+	$cmd = "java -jar $yuicompressor --type css -o $compressed_file $original_file 2>&1";
+	exec($cmd, $arr, $res);
 }
+
+if (file_exists($compressed_file))
+{
+	header("Content-Type: text/css");
+	echo file_get_contents($compressed_file);
+	if (!file_exists($compressed_path . ".compressed"))
+	{
+		@mkdir(pathinfo($compressed_path, PATHINFO_DIRNAME) , "0777", TRUE);
+		copy($original_file, $compressed_path);
+		copy($compressed_file, $compressed_path . ".compressed");
+	}
+}
+else
+{
+	header('HTTP', true, 500);
+}
+
 @unlink($original_file);
 @unlink($compressed_file);
 ?>
