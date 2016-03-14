5820d3b83e6ecf1932c04249fefad85b5c0b04b7
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-14 15:19:54 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-echo "1514"
+echo "1519"
 
 # set -x
 
 rm -f ${OPENSHIFT_LOG_DIR}/test.log
 rm -f ${OPENSHIFT_LOG_DIR}/cron_minutely.log-*
+
+if [ $(wc -c < ${OPENSHIFT_LOG_DIR}/cron_minutely.log) -gt 1000000 ]; then
+    tail -n 1000 ${OPENSHIFT_LOG_DIR}/cron_minutely.log > ${OPENSHIFT_LOG_DIR}/cron_minutely.log
+fi
 
 cd /tmp
 
