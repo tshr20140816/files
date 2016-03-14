21c4e0f32ce4adc49f0d4b728fd44fd97b685d8a
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-14 11:06:00 +0900

    Update install_step19.sh

--- openshift/app01/install_step19.sh
+++ openshift/app01/install_step19.sh
@@ -66,63 +66,6 @@ cat << '__HEREDOC__' > closure_compiler.sh
 export TZ=JST-9
 
 echo "$(date +%Y/%m/%d" "%H:%M:%S) $$ ${1}"
-echo "$(date +%Y/%m/%d" "%H:%M:%S) $$ $(oo-cgroup-read memory.usage_in_bytes | awk '{printf "%\047d\n", $1}')" \
- >> ${OPENSHIFT_LOG_DIR}/closure_compiler.log
-while :
-do
-    usage_in_bytes=$(oo-cgroup-read memory.usage_in_bytes)
-    if [ ${usage_in_bytes} -gt 350000000 ]; then
-        echo "$(date +%Y/%m/%d" "%H:%M:%S) $$ $(oo-cgroup-read memory.usage_in_bytes | awk '{printf "%\047d\n", $1}') waiting" \
-         >> ${OPENSHIFT_LOG_DIR}/closure_compiler.log
-        sleep 5s
-    else
-        break
-    fi
-done
-pushd ${OPENSHIFT_TMP_DIR} > /dev/null
-suffix=$(date '+%Y%m%d')
-target_file=${1}
-compiled_file=./$(basename ${target_file}).$$
-result_file=${compiled_file}.result.txt.$$
-rm -f ${compiled_file}
-rm -f ${result_file}
-time java -jar ${OPENSHIFT_DATA_DIR}/compiler.jar \
- --summary_detail_level 3 \
- --compilation_level SIMPLE_OPTIMIZATIONS \
- --js ${target_file} \
- --js_output_file ${compiled_file} \
- 2> ${result_file}
-if [ "$(cat ${result_file})" = "0 error(s), 0 warning(s)" ]; then
-    size_original=$(wc -c < ${target_file})
-    size_compiled=$(wc -c < ${compiled_file})
-    if [ ${size_original} -gt ${size_compiled} ]; then
-        echo "$(date +%Y/%m/%d" "%H:%M:%S) $$ CHANGED ${size_original} ${size_compiled} ${target_file}" \
-         >> ${OPENSHIFT_LOG_DIR}/closure_compiler.log
-        cp -f ${target_file} ${target_file}.${suffix}
-        mv -f ${compiled_file} ${target_file}
-    else
-        echo "$(date +%Y/%m/%d" "%H:%M:%S) $$ NOT CHANGED (SIZE NOT DOWNED) ${size_original} ${size_compiled} ${target_file}" \
-         >> ${OPENSHIFT_LOG_DIR}/closure_compiler.log
-    fi
-else
-    echo "$(date +%Y/%m/%d" "%H:%M:%S) $$ NOT CHANGED (ERROR OR WARNING) ${target_file} $(tail -n 1 ./${result_file})" \
-     >> ${OPENSHIFT_LOG_DIR}/closure_compiler.log
-    # cat ${result_file} >> ${OPENSHIFT_LOG_DIR}/closure_compiler.log
-fi
-rm -f ${compiled_file}
-rm -f ${result_file}
-popd > /dev/null
-__HEREDOC__
-chmod +x closure_compiler.sh &
-
-# ***** Closure Compiler 2 *****
-
-cat << '__HEREDOC__' > closure_compiler2.sh
-#!/bin/bash
-
-export TZ=JST-9
-
-echo "$(date +%Y/%m/%d" "%H:%M:%S) $$ ${1}"
 
 pushd ${OPENSHIFT_TMP_DIR} > /dev/null
 uuid=${OPENSHIFT_APP_UUID}$$
@@ -177,7 +120,7 @@ rm -f result.${uuid}.txt
 rm -f result.${uuid}.zip
 popd > /dev/null
 __HEREDOC__
-chmod +x closure_compiler2.sh &
+chmod +x closure_compiler.sh &
 
 # ***** optipng *****
 
