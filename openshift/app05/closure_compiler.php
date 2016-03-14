f89b79e38786a56cda9c6c28c4e1bdcfdc401c44
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-14 09:21:53 +0900

    Update closure_compiler.php

--- openshift/app05/closure_compiler.php
+++ openshift/app05/closure_compiler.php
@@ -1,5 +1,4 @@
 <?php
-
 // wget http://dl.google.com/closure-compiler/compiler-latest.zip
 // curl https://xxx/xxx.php -F "file=@./jquery-1.7.1.min.js" -F "suffix=uuid" -F "path=app-root/"
 
@@ -17,7 +16,6 @@ if (preg_match('/^\w+$/', $suffix) == 0)
     header('HTTP', true, 500);
     exit;
 }
-
 $file_name = $_FILES['file']['name'];
 $original_file = $file_name . "." . $suffix;
 $compiled_file = "compiled.$suffix.js";
@@ -25,21 +23,16 @@ $result_file = "result.$suffix.txt";
 $zip_file = "result.$suffix.zip";
 $download_file = "result.$suffix.zip";
 $closure_compiler = getenv("OPENSHIFT_DATA_DIR") . "/compiler.jar";
-
 if (preg_match('/\.js$/', $file_name) == 0)
 {
     header('HTTP', true, 500);
     exit;
 }
-
 move_uploaded_file($_FILES['file']['tmp_name'], getenv("OPENSHIFT_TMP_DIR") . "/" . $original_file);
 chdir(getenv("OPENSHIFT_TMP_DIR"));
 $cmd = "java -jar $closure_compiler --summary_detail_level 3 --compilation_level SIMPLE_OPTIMIZATIONS --js $original_file --js_output_file $compiled_file 2>&1";
 exec($cmd, $arr, $res);
-$fp = fopen($result_file, "w");
-fwrite($fp, $arr[0]);
-fclose($fp);
-
+file_put_contents($result_file, $arr[0]);
 if (file_exists($compiled_file))
 {
     $cmd = "zip -9 $zip_file $compiled_file $result_file";
@@ -48,7 +41,6 @@ else
 {
     $cmd = "zip -9 $zip_file $result_file";
 }
-
 exec($cmd, $arr, $res);
 header("Content-Type: application/octet-stream");
 header("Content-Disposition: attachment; filename=$download_file");
