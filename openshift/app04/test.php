e50ddd62771979a5ee2001a4955cc8ad5f6d5add
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-15 13:24:44 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -1,6 +1,6 @@
 <?php
 $d1 = $_POST["d1"];
-$d2 = $_POST["d1"];
+$d2 = $_POST["d2"];
 
 file_put_contents(getenv("OPENSHIFT_TMP_DIR") . "d1.txt", $d1);
 file_put_contents(getenv("OPENSHIFT_TMP_DIR") . "d2.txt", $d2);
