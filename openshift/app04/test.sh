28c522ae815516cca465815399102c5c0101649e
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-15 14:21:25 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "1416"
+echo "1421"
 
 # set -x
 
@@ -23,12 +23,13 @@ export GEM_HOME=${OPENSHIFT_DATA_DIR}/.gem
 export PATH="${OPENSHIFT_DATA_DIR}/.gem/bin:$PATH"
 export HOME=${OPENSHIFT_DATA_DIR}
 gem --version
-gem environment
+# gem environment
 
 # yes | rhc app delete -a portal5
 # yes | rhc app create portal5 diy-0.1 mysql-5.5 cron-1.4 phpmyadmin-4 --server openshift.redhat.com
 
 server_link=$(rhc apps | grep ssh | grep portal5 | awk '{print $3}' | awk -F/ '{print $3}')
+echo ${server_link}
 ssh ${server_link} cat /proc/cpuinfo
 
 export HOME=${OPENSHIFT_HOME_DIR}
