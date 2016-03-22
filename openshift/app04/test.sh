8008e9270430cbab4ba39ad01ec7fb71b6ed0601
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-22 09:45:24 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -15,32 +15,6 @@ fi
 
 cd /tmp
 
-# pushd  ${OPENSHIFT_DATA_DIR}/apache/htdocs
-# rm -f ttrss_archive.zip
-# wget https://tt-rss.org/gitlab/fox/tt-rss/repository/archive.zip?ref=master -O ttrss_archive.zip
-# unzip ttrss_archive.zip
-# mv tt-rss.git ttrss
-# rm -f ttrss_archive.zip
-# popd
-
-# rm -f js_list.txt
-# find ${OPENSHIFT_DATA_DIR} -name "*.js" -type f -print | grep ttrss | tee -a js_list.txt
-
-wc -l js_list.txt
-rm -f js_list.txt
-
-flag=0
-
-for target_file in $(cat js_list.txt)
-do
-  path=$(echo ${target_file} | sed -e "s|${OPENSHIFT_HOMEDIR}||g")
-  if [ $flag -eq 1 ]; then
-    echo ${path}
-    curl $(cat sv.txt) -F "file=@${target_file}" -F "suffix=${OPENSHIFT_APP_UUID}" -F "path=${path}" -o /dev/null 2>/dev/null
-  fi
-  if [ "app-root/data/apache/htdocs/ttrss/lib/dojo/store/util/SimpleQueryEngine.js" = ${path} ]; then
-    flag=1
-  fi
-done
+ls -lang
 
 exit
