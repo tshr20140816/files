d05db6f86d9301ce6bd5d1b09321fcf165807ba6
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-23 17:24:56 +0900

    Update install_distcc_client.sh

--- openshift/app05/install_distcc_client.sh
+++ openshift/app05/install_distcc_client.sh
@@ -71,7 +71,7 @@ pushd ${OPENSHIFT_REPO_DIR} > /dev/null
 cat << '__HEREDOC__' > yuicompressor.php
 <?php
 date_default_timezone_set('Asia/Tokyo');
-$log_file = getenv("OPENSHIFT_LOG_DIR") . "yuicompressor_php_" . date("Ymd") . ".log";
+$log_file = getenv("OPENSHIFT_LOG_DIR") . basename(_SERVER["SCRIPT_NAME"]) . "." . date("Ymd") . ".log";
 if (!isset($_POST['suffix']))
 {
     file_put_contents($log_file, date("YmdHis") . " CHECK POINT 010 suffix\r\n", FILE_APPEND);
@@ -176,7 +176,7 @@ pushd ${OPENSHIFT_REPO_DIR} > /dev/null
 cat << '__HEREDOC__' > closure_compiler.php
 <?php
 date_default_timezone_set('Asia/Tokyo');
-$log_file = getenv("OPENSHIFT_LOG_DIR") . "closure_compiler_php_" . date("Ymd") . ".log";
+$log_file = getenv("OPENSHIFT_LOG_DIR") . basename(_SERVER["SCRIPT_NAME"]) . "." . date("Ymd") . ".log";
 if (!isset($_POST['suffix']))
 {
     file_put_contents($log_file, date("YmdHis") . " CHECK POINT 010 suffix\r\n", FILE_APPEND);
@@ -287,7 +287,7 @@ pushd ${OPENSHIFT_REPO_DIR} > /dev/null
 cat << '__HEREDOC__' > compressed_file_upload.php
 <?php
 date_default_timezone_set('Asia/Tokyo');
-$log_file = getenv("OPENSHIFT_LOG_DIR") . "compressed_file_upload_php_" . date("Ymd") . ".log";
+$log_file = getenv("OPENSHIFT_LOG_DIR") . basename(_SERVER["SCRIPT_NAME"]) . "." . date("Ymd") . ".log";
 if (!isset($_POST['path']))
 {
     file_put_contents($log_file, date("YmdHis") . " CHECK POINT 010 path\r\n", FILE_APPEND);
