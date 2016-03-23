1add268b0dcb2f0eb855c1b2a9c031b7431b421f
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-24 08:42:25 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -13,30 +13,13 @@ if [ $(wc -c < ${OPENSHIFT_LOG_DIR}/cron_minutely.log) -gt 100000 ]; then
     tail -n 1000 ${OPENSHIFT_LOG_DIR}/cron_minutely.log > ${OPENSHIFT_LOG_DIR}/cron_minutely.log
 fi
 
-export GNUPGHOME=${OPENSHIFT_DATA_DIR}/.gnupg
-rm -rf ${GNUPGHOME}
-
 cd /tmp
 
 ls -lang
 
-rm -f parallel-latest.tar.bz2.sig
-rm -f parallel-latest.tar.bz2
-rm -f super_pi-jp.tar.gz
+rm -f test.sh
+rm -f test2.sh
 
-touch test.sh
-cp -f test.sh test2.sh
-
-echo "cmp 1"
-cmp test.sh test2.sh
-echo $?
-
-echo "cmp 2"
-echo "AAA" >> test2.sh
-
-cmp test.sh test2.sh
-echo $?
-
-ls -lang
+tree ${OPENSHIFT_DATA_DIR}
 
 exit
