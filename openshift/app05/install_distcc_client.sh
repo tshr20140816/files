0c09f61834723fa348efb26ba3c442fd80cd0dda
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-25 10:25:02 +0900

    Update install_distcc_client.sh

--- openshift/app05/install_distcc_client.sh
+++ openshift/app05/install_distcc_client.sh
@@ -307,6 +307,93 @@ cat << '__HEREDOC__' > optipng.php
 <?php
 date_default_timezone_set('Asia/Tokyo');
 $log_file = getenv("OPENSHIFT_LOG_DIR") . basename($_SERVER["SCRIPT_NAME"]) . "." . date("Ymd") . ".log";
+if (!isset($_POST['suffix']))
+{
+    file_put_contents($log_file, date("YmdHis") . " CHECK POINT 010 suffix\r\n", FILE_APPEND);
+    header('HTTP', true, 500);
+    exit;
+}
+if (!isset($_POST['path']))
+{
+    file_put_contents($log_file, date("YmdHis") . " CHECK POINT 020 path\r\n", FILE_APPEND);
+    header('HTTP', true, 500);
+    exit;
+}
+$suffix = $_POST["suffix"];
+$path = $_POST["path"];
+if (preg_match('/^\w+$/', $suffix) == 0)
+{
+    file_put_contents($log_file, date("YmdHis") . " CHECK POINT 030 $suffix\r\n", FILE_APPEND);
+    header('HTTP', true, 500);
+    exit;
+}
+if (preg_match('/.*\.\..*/', $path) == 1)
+{
+    file_put_contents($log_file, date("YmdHis") . " CHECK POINT 040 $path\r\n", FILE_APPEND);
+    header('HTTP', true, 500);
+    exit;
+}
+if (preg_match('/^app-root\/data\/.+$/', $path) == 0)
+{
+    file_put_contents($log_file, date("YmdHis") . " CHECK POINT 050 $path\r\n", FILE_APPEND);
+    header('HTTP', true, 500);
+    exit;
+}
+$compressed_path = getenv("OPENSHIFT_DATA_DIR") . "compressed/";
+$compressed_path = preg_replace("/^app-root\/data\//", $compressed_path, $path);
+$file_name = $_FILES['file']['name'];
+if (preg_match('/\.(png|gif)$/', $file_name) == 0)
+{
+    file_put_contents($log_file, date("YmdHis") . " CHECK POINT 060 $file_name\r\n", FILE_APPEND);
+    header('HTTP', true, 500);
+    exit;
+}
+$image_type = substr($file_name, -3);
+$compressed_file = getenv("OPENSHIFT_TMP_DIR") . "compressed.$suffix.$image_type";
+$original_file = getenv("OPENSHIFT_TMP_DIR") . $file_name . "." . $suffix;
+
+$optipng = getenv("OPENSHIFT_DATA_DIR") . "/optipng/bin/optipng";
+file_put_contents($log_file, date("YmdHis") . " CHECK POINT 070 suffix $suffix\r\n", FILE_APPEND);
+file_put_contents($log_file, date("YmdHis") . " CHECK POINT 080 path $path\r\n", FILE_APPEND);
+file_put_contents($log_file, date("YmdHis") . " CHECK POINT 090 compressed_path $compressed_path\r\n", FILE_APPEND);
+file_put_contents($log_file, date("YmdHis") . " CHECK POINT 100 file_name $file_name\r\n", FILE_APPEND);
+file_put_contents($log_file, date("YmdHis") . " CHECK POINT 110 original_file $original_file\r\n", FILE_APPEND);
+file_put_contents($log_file, date("YmdHis") . " CHECK POINT 120 compressed_file $compressed_file\r\n", FILE_APPEND);
+file_put_contents($log_file, date("YmdHis") . " TARGET $path\r\n", FILE_APPEND);
+move_uploaded_file($_FILES['file']['tmp_name'], $original_file);
+chdir(getenv("OPENSHIFT_TMP_DIR"));
+if (file_exists($compressed_path . ".compressed") && file_exists($compressed_path)
+    && (file_get_contents($original_file) == file_get_contents($compressed_path)))
+{
+    file_put_contents($log_file, date("YmdHis") . " CACHE HIT $path\r\n", FILE_APPEND);
+    copy($compressed_path . ".compressed", $compressed_file);
+}
+else
+{
+    file_put_contents($log_file, date("YmdHis") . " CACHE MISS $path\r\n", FILE_APPEND);
+    $cmd = "$optipng -o7 -zm1-9 -out $compressed_file $original_file 2>&1";
+    exec($cmd, $arr, $res);
+    $tmp = var_dump($arr);
+    file_put_contents($log_file, date("YmdHis") . " RESULT $tmp\r\n", FILE_APPEND);
+}
+if (file_exists($compressed_file))
+{
+    header("Content-Type: image/$image_type");
+    echo file_get_contents($compressed_file);
+    if (!file_exists($compressed_path . ".compressed"))
+    {
+        @mkdir(pathinfo($compressed_path, PATHINFO_DIRNAME) , 0777, TRUE);
+        copy($original_file, $compressed_path);
+        copy($compressed_file, $compressed_path . ".compressed");
+    }
+}
+else
+{
+    file_put_contents($log_file, date("YmdHis") . " CHECK POINT 130 $compressed_file\r\n", FILE_APPEND);
+    header('HTTP', true, 500);
+}
+@unlink($original_file);
+@unlink($compressed_file);
 ?>
 __HEREDOC__
 popd > /dev/null
