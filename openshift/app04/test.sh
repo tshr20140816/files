7c2a725ec6bfbbc6009bb12de410b0d821894d2a
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-03-31 17:10:09 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -20,6 +20,7 @@ ls -lang
 # wget --help
 # curl --help
 
+rm -rf ./usr
 rm -f *.rpm
 
 echo "START $(date +%Y/%m/%d" "%H:%M:%S)"
@@ -31,25 +32,28 @@ echo "START $(date +%Y/%m/%d" "%H:%M:%S)"
 
 # tree ./
 
-echo ${LD_LIBRARY_PATH}
-# export LD_LIBRARY_PATH=/tmp/usr/lib64/ghc-7.4.2/regex-compat-0.95.1:${LD_LIBRARY_PATH}
-export LD_LIBRARY_PATH=/tmp/usr/lib64
-echo ${LD_LIBRARY_PATH}
+wget http://olea.org/paquetes-rpm/fedora-19/ShellCheck-0.2.0-2.fc19.x86_64.rpm
+rpm2cpio ShellCheck-0.2.0-2.fc19.x86_64.rpm | cpio -idmv
 
+wget http://archives.fedoraproject.org/pub/archive/fedora/linux/releases/19/Everything/x86_64/os/Packages/g/ghc-regex-compat-0.95.1-22.fc19.x86_64.rpm
+rpm2cpio ghc-regex-compat-0.95.1-22.fc19.x86_64.rpm | cpio -idmv
 cp /tmp/usr/lib64/ghc-7.4.2/regex-compat-0.95.1/libHSregex-compat-0.95.1-ghc7.4.2.so /tmp/usr/lib64/
-./usr/bin/shellcheck --version
-ldd ./usr/bin/shellcheck
-
-# wget http://archives.fedoraproject.org/pub/archive/fedora/linux/releases/19/Everything/x86_64/os/Packages/g/ghc-regex-compat-0.95.1-22.fc19.x86_64.rpm
-# rpm2cpio ghc-regex-compat-0.95.1-22.fc19.x86_64.rpm | cpio -idmv
 
 wget http://archives.fedoraproject.org/pub/archive/fedora/linux/releases/19/Everything/x86_64/os/Packages/g/ghc-regex-posix-0.95.2-22.fc19.x86_64.rpm
 rpm2cpio ghc-regex-posix-0.95.2-22.fc19.x86_64.rpm | cpio -idmv
+cp /tmp/usr/lib64/ghc-7.4.2/regex-posix-0.95.2/libHSregex-posix-0.95.2-ghc7.4.2.so /tmp/usr/lib64/
 
 wget http://archives.fedoraproject.org/pub/archive/fedora/linux/releases/19/Everything/x86_64/os/Packages/g/ghc-regex-base-0.93.2-22.fc19.x86_64.rpm
 rpm2cpio ghc-regex-base-0.93.2-22.fc19.x86_64.rpm | cpio -idmv
+cp /tmp/usr/lib64/ghc-7.4.2/regex-base-0.93.2/libHSregex-base-0.93.2-ghc7.4.2.so /tmp/usr/lib64/
 
 tree ./
+
+export LD_LIBRARY_PATH=/tmp/usr/lib64
+echo ${LD_LIBRARY_PATH}
+
+./usr/bin/shellcheck --version
+ldd ./usr/bin/shellcheck
 
 echo "FINISH $(date +%Y/%m/%d" "%H:%M:%S)"
 
