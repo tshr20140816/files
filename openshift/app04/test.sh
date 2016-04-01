934126033191c91fa341faebd5bb67127609bfc4
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-04-01 10:49:14 +0900

    Create test.sh

---
+++ openshift/app04/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "1041"
+
+# set -x
+
+rm -f ${OPENSHIFT_LOG_DIR}/test.log &
+rm -f ${OPENSHIFT_LOG_DIR}/cron_minutely.log-* &
+
+ls -lang ${OPENSHIFT_LOG_DIR}/cron_minutely.log
+
+if [ $(wc -c < ${OPENSHIFT_LOG_DIR}/cron_minutely.log) -gt 100000 ]; then
+    tail -n 1000 ${OPENSHIFT_LOG_DIR}/cron_minutely.log > ${OPENSHIFT_LOG_DIR}/cron_minutely.log
+fi
+
+cd /tmp
+
+ls -lang
