94aae7aa241c596ce5740c001793aa7845ee332a
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-04-08 01:26:05 +0900

    Update install_ghc.sh

--- openshift/app05/install_ghc.sh
+++ openshift/app05/install_ghc.sh
@@ -119,7 +119,7 @@ do
     tar xfz "${package}".tar.gz
     cd "${package}"
     # cabal install -j1 -v3 --disable-documentation
-    cabal install -j1 -v3 --disable-documentation 2>&1 | tee ${OPENSHIFT_LOG_DIR}/${package}.log
+    cabal install -j1 -v3 --disable-documentation --with-gcc=${OPENSHIFT_DATA_DIR}/local/bin/gcc 2>&1 | tee ${OPENSHIFT_LOG_DIR}/${package}.log
     cd ..
     rm -rf "${package}"
     rm -f "${package}".tar.gz
