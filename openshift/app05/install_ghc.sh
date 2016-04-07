e5931a437758bc063878be54941a93c6b111758b
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-04-08 00:55:49 +0900

    Update install_ghc.sh

--- openshift/app05/install_ghc.sh
+++ openshift/app05/install_ghc.sh
@@ -7,6 +7,26 @@ export TZ=JST-9
 quota -s
 oo-cgroup-read memory.failcnt
 
+cd ${OPENSHIFT_TMP_DIR}
+
+if [ ! -f ${OPENSHIFT_DATA_DIR}/ccache/bin/ccache ]; then
+    wget http://samba.org/ftp/ccache/ccache-3.2.4.tar.xz
+    tar xfz ccache-3.2.4.tar.xz
+    cd ccache-3.2.4
+    ./configure --prefix=${OPENSHIFT_DATA_DIR}/local
+    time make -j1
+    make install
+    cd ..
+    rm -rf ccache-3.2.4
+    rm -f ccache-3.2.4.tar.xz
+fi
+
+export CCACHE_DIR=${OPENSHIFT_DATA_DIR}/ccache
+export CCACHE_TEMPDIR=${OPENSHIFT_TMP_DIR}/ccache
+# export CCACHE_LOGFILE=/dev/null
+export CCACHE_LOGFILE=${OPENSHIFT_LOG_DIR}/ccache.$(date +%Y%m%d%H%M%S).log
+export CCACHE_MAXSIZE=200M
+
 cd $OPENSHIFT_DATA_DIR
 
 mkdir haskell
@@ -36,6 +56,41 @@ package_list+=("QuickCheck-2.8.2")
 package_list+=("mtl-2.2.1")
 package_list+=("parsec-3.1.9")
 package_list+=("regex-base-0.93.2")
+# package_list+=("regex-tdfa-1.2.1")
+# package_list+=("ShellCheck-0.4.3")
+
+for package in ${package_list[@]}
+do
+    echo "$(date +%Y/%m/%d" "%H:%M:%S) ${package}"
+    if [ $(ghc-pkg list | grep -c ${package}) -ne 0 ]; then
+        continue
+    fi
+    cd ${OPENSHIFT_TMP_DIR}
+    rm -f "${package}".tar.gz
+    wget https://hackage.haskell.org/package/"${package}"/"${package}".tar.gz
+    tar xfz "${package}".tar.gz
+    cd "${package}"
+    # cabal install -j1 -v3 --disable-documentation
+    cabal install -j2 --disable-documentation
+    cd ..
+    rm -rf "${package}"
+    rm -f "${package}".tar.gz
+    quota -s
+    oo-cgroup-read memory.failcnt
+    if [ $(ghc-pkg list | grep -c ${package}) -eq 0 ]; then
+        break
+    fi
+done
+
+export CC="ccache gcc"
+export CXX="ccache g++"
+export PATH="${OPENSHIFT_DATA_DIR}/local/bin:$PATH"
+
+ccache -s
+ccache -z
+ccache -s
+
+package_list=()
 package_list+=("regex-tdfa-1.2.1")
 package_list+=("ShellCheck-0.4.3")
 
@@ -51,7 +106,7 @@ do
     tar xfz "${package}".tar.gz
     cd "${package}"
     # cabal install -j1 -v3 --disable-documentation
-    cabal install -j2 --disable-documentation
+    cabal install -j1 -v3 --disable-documentation
     cd ..
     rm -rf "${package}"
     rm -f "${package}".tar.gz
