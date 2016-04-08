fd3467f0079ef661ae4d15da380b8a6b27a0945c
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-04-08 09:02:55 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "0850"
+echo "0900"
 
-# set -x
+set -x
 
 quota -s
 oo-cgroup-read memory.failcnt
@@ -26,22 +26,87 @@ ls -lang
 
 /usr/bin/gear --help
 
+cd $OPENSHIFT_DATA_DIR
+
+mkdir haskell
+cd haskell
+[ ! -f network.tar.gz ] && wget -q http://www.accursoft.com/cartridges/network.tar.gz
+[ ! -f $OPENSHIFT_DATA_DIR/haskell/usr/bin/cabal ] && tar xfz network.tar.gz
+
 export PATH=$PATH:$OPENSHIFT_DATA_DIR/haskell/usr/bin
 export HOME=$OPENSHIFT_DATA_DIR
 export OPENSHIFT_HASKELL_DIR=$OPENSHIFT_DATA_DIR/haskell
 
-# cabal configure --ghc-option=+RTS --ghc-option=-M128m --ghc-option=-RTS -v
-ghc --help
-ghc --info
+cabal install --help
+ghc-pkg list
+ghc-pkg recache
 
-cd ${OPENSHIFT_TMP_DIR}
+quota -s
+oo-cgroup-read memory.failcnt
 
-cd $OPENSHIFT_DATA_DIR
+package_list=()
+package_list+=("hashable-1.2.4.0")
+package_list+=("unordered-containers-0.2.7.0")
+package_list+=("tagged-0.8.3")
+package_list+=("semigroups-0.18.1")
+package_list+=("random-1.1")
+package_list+=("primitive-0.6.1.0")
+package_list+=("tf-random-0.5")
+package_list+=("QuickCheck-2.8.2")
+package_list+=("mtl-2.2.1")
+package_list+=("parsec-3.1.9")
+package_list+=("regex-base-0.93.2")
 
-rm -rf .cabal
-rm -rf .ghc
+for package in ${package_list[@]}
+do
+    echo "$(date +%Y/%m/%d" "%H:%M:%S) ${package}"
+    if [ $(ghc-pkg list | grep -c ${package}) -ne 0 ]; then
+        continue
+    fi
+    cd ${OPENSHIFT_TMP_DIR}
+    rm -f "${package}".tar.gz
+    rm -rf "${package}"
+    wget https://hackage.haskell.org/package/"${package}"/"${package}".tar.gz
+    tar xfz "${package}".tar.gz
+    cd "${package}"
+    cabal install -j2 --disable-documentation
+    cd ..
+    rm -rf "${package}"
+    rm -f "${package}".tar.gz
+    quota -s
+    oo-cgroup-read memory.failcnt
+    if [ $(ghc-pkg list | grep -c ${package}) -eq 0 ]; then
+        break
+    fi
+done
 
-ls -lang
+package_list=()
+package_list+=("regex-tdfa-1.2.1")
+package_list+=("ShellCheck-0.4.3")
+
+for package in ${package_list[@]}
+do
+    echo "$(date +%Y/%m/%d" "%H:%M:%S) ${package}"
+    if [ $(ghc-pkg list | grep -c ${package}) -ne 0 ]; then
+        continue
+    fi
+    cat ${OPENSHIFT_LOG_DIR}/${package}.log
+    cd ${OPENSHIFT_TMP_DIR}
+    rm -f "${package}".tar.gz
+    rm -rf "${package}"
+    wget https://hackage.haskell.org/package/"${package}"/"${package}".tar.gz
+    tar xfz "${package}".tar.gz
+    cd "${package}"
+    cabal configure --ghc-option=+RTS --ghc-option=-M128m --ghc-option=-RTS -v
+    cabal install -j1 -v3 --disable-documentation 2>&1 | tee ${OPENSHIFT_LOG_DIR}/${package}.log
+    cd ..
+    rm -rf "${package}"
+    rm -f "${package}".tar.gz
+    quota -s
+    oo-cgroup-read memory.failcnt
+    if [ $(ghc-pkg list | grep -c ${package}) -eq 0 ]; then
+        break
+    fi
+done
 
 echo "FINISH"
-
