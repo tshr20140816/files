7852b18c17f8fde0f3f12ded88256c011eee0af0
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-04-08 10:09:55 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -16,8 +16,6 @@ if [ $(wc -c < ${OPENSHIFT_LOG_DIR}/cron_minutely.log) -gt 100000 ]; then
     tail -n 1000 ${OPENSHIFT_LOG_DIR}/cron_minutely.log > ${OPENSHIFT_LOG_DIR}/cron_minutely.log
 fi
 
-1|/usr/bin/gear status
-2|/usr/bin/gear status
 /usr/bin/gear start --trace
 
 cd /tmp
