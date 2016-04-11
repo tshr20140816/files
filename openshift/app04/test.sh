342e3b9c4349419e0e1a5cc3f3d072d75bd3d355
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-04-11 16:03:07 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "1555"
+echo "1603"
 
 set -x
 
@@ -25,11 +25,19 @@ shopt
 
 cd /tmp
 
+rm -rf ShellCheck-0.4.3
+rm -f ShellCheck-0.4.3*
+rm -f regex.log
+rm -f monitor_resourse.sh
+
 ls -lang
 
 cd $OPENSHIFT_DATA_DIR
 
 rm -rf test
+
+tree -a $OPENSHIFT_DATA_DIR
+
 mkdir test
 cd test
 wget https://yum.gleez.com/6/x86_64/hhvm-3.5.0-4.el6.x86_64.rpm
