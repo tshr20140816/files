01b1f8ae857f3dee15796d4b1c5c9ea73f43a600
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-04-11 16:52:48 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "1646"
+echo "1652"
 
 set -x
 
@@ -43,6 +43,9 @@ rpm2cpio boost-regex-1.41.0-27.el6.x86_64.rpm | cpio -idmv
 
 wget http://mirror.centos.org/centos/6/os/x86_64/Packages/libvpx-1.3.0-5.el6_5.x86_64.rpm
 rpm2cpio libvpx-1.3.0-5.el6_5.x86_64.rpm | cpio -idmv
+
+wget http://mirror.centos.org/centos/6/os/x86_64/Packages/mysql-libs-5.1.73-5.el6_6.x86_64.rpm
+rpm2cpio mysql-libs-5.1.73-5.el6_6.x86_64.rpm | cpio -idmv
 
 cd usr/lib64
 ln -s libboost_regex.so.5 libboost_regex.so.1.54.0
