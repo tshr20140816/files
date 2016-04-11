46d2927e7e0c151e8821be801f8dcbe12962abf7
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-04-11 15:42:33 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "1525"
+echo "1542"
 
 set -x
 
@@ -27,20 +27,14 @@ cd /tmp
 
 ls -lang
 
-rm -rf ghc*
-
 cd $OPENSHIFT_DATA_DIR
-
-rmdir -rf haskell
-rm -rf .cabal
-rm -rf .ghc
 
 rm -rf test
 mkdir test
 cd test
 wget https://yum.gleez.com/6/x86_64/hhvm-3.5.0-4.el6.x86_64.rpm
 
-rpm2cpio glibc-devel-2.12-1.166.el6.i686.rpm | cpio -idmv
+rpm2cpio hhvm-3.5.0-4.el6.x86_64.rpm | cpio -idmv
 
 cd ../
 
