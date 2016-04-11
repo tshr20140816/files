27ff279bf8947bc2e4ba35608f735cf8059b86be
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-04-11 17:06:53 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -47,6 +47,9 @@ rpm2cpio libvpx-1.3.0-5.el6_5.x86_64.rpm | cpio -idmv
 # wget http://mirror.centos.org/centos/6/os/x86_64/Packages/mysql-libs-5.1.73-5.el6_6.x86_64.rpm
 # rpm2cpio mysql-libs-5.1.73-5.el6_6.x86_64.rpm | cpio -idmv
 
+wget http://dl.fedoraproject.org/pub/epel/6/x86_64/inotify-tools-3.14-1.el6.x86_64.rpm
+rpm2cpio inotify-tools-3.14-1.el6.x86_64.rpm | cpio -idmv
+
 cd usr/lib64
 ln -s libboost_regex.so.5 libboost_regex.so.1.54.0
 ln -s /usr/lib64/mysql/libmysqlclient.so.16.0.0 libmysqlclient.so.18
