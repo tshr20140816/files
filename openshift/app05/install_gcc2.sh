1c43f10f79358039e1574b54a9a8a738d2c465a4
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-04-11 16:23:25 +0900

    Update install_gcc2.sh

--- openshift/app05/install_gcc2.sh
+++ openshift/app05/install_gcc2.sh
@@ -7,11 +7,12 @@ cd /tmp
 # export CFLAGS="-O2 -march=native -pipe -fomit-frame-pointer -s"
 # export CXXFLAGS="${CFLAGS}"
 
-wget http://ftp.jaist.ac.jp/pub/GNU/gmp/gmp-4.3.2.tar.bz2
+[ ! -f gmp-4.3.2.tar.bz2 ] && wget http://ftp.jaist.ac.jp/pub/GNU/gmp/gmp-4.3.2.tar.bz2
+rm -rf gmp-4.3.2
 tar jxf gmp-4.3.2.tar.bz2
 cd gmp-4.3.2
 ./configure --help
-./configure --prefix=/tmp/gcc --disable-shared --enable-static \
+./configure --prefix=$OPENSHIFT_DATA_DIR/gcc --disable-shared --enable-static \
  --infodir=/tmp/gomi --mandir=/tmp/gomi --docdir=/tmp/gomi
 time make -j2
 make install
@@ -19,11 +20,12 @@ rm -rf /tmp/gmp-4.3.2
 
 cd /tmp
 
-wget http://ftp.jaist.ac.jp/pub/GNU/mpfr/mpfr-2.4.2.tar.xz
+[ ! -f mpfr-2.4.2.tar.xz ] && wget http://ftp.jaist.ac.jp/pub/GNU/mpfr/mpfr-2.4.2.tar.xz
+rm -rf mpfr-2.4.2
 tar Jxf mpfr-2.4.2.tar.xz
 cd mpfr-2.4.2
 ./configure --help
-./configure --prefix=/tmp/gcc --disable-shared --enable-static --with-gmp=/tmp/gcc \
+./configure --prefix=$OPENSHIFT_DATA_DIR/gcc --disable-shared --enable-static --with-gmp=$OPENSHIFT_DATA_DIR/gcc \
  --infodir=/tmp/gomi --mandir=/tmp/gomi --docdir=/tmp/gomi
 time make -j2
 make install
@@ -31,11 +33,12 @@ rm -rf /tmp/mpfr-2.4.2
 
 cd /tmp
 
-wget http://ftp.jaist.ac.jp/pub/GNU/mpc/mpc-1.0.3.tar.gz
+[ ! -f mpc-1.0.3.tar.gz ] && wget http://ftp.jaist.ac.jp/pub/GNU/mpc/mpc-1.0.3.tar.gz
+rm -rf mpc-1.0.3
 tar zxf mpc-1.0.3.tar.gz
 cd mpc-1.0.3
 ./configure --help
-./configure --prefix=/tmp/gcc --disable-shared --enable-static --with-gmp=/tmp/gcc --with-mpfr=/tmp/gcc \
+./configure --prefix=$OPENSHIFT_DATA_DIR/gcc --disable-shared --enable-static --with-gmp=$OPENSHIFT_DATA_DIR/gcc --with-mpfr=$OPENSHIFT_DATA_DIR/gcc \
  --infodir=/tmp/gomi --mandir=/tmp/gomi --docdir=/tmp/gomi
 time make -j2
 make install
@@ -43,15 +46,16 @@ rm -rf /tmp/mpc-1.0.3
 
 cd /tmp
 
-wget http://ftp.tsukuba.wide.ad.jp/software/gcc/releases/gcc-4.9.3/gcc-4.9.3.tar.bz2
+[ ! -f gcc-4.9.3.tar.bz2 ] && wget http://ftp.tsukuba.wide.ad.jp/software/gcc/releases/gcc-4.9.3/gcc-4.9.3.tar.bz2
 
+rm -rf gcc-4.9.3
 tar jxf gcc-4.9.3.tar.bz2
 
 cd gcc-4.9.3
 mkdir work
 cd work
 ../configure --help
-../configure --with-gmp=/tmp/gcc --with-mpfr=/tmp/gcc --prefix=/tmp/gcc \
+../configure --with-gmp=$OPENSHIFT_DATA_DIR/gcc --with-mpfr=$OPENSHIFT_DATA_DIR/gcc --prefix=$OPENSHIFT_DATA_DIR/gcc \
  --infodir=/tmp/gomi --mandir=/tmp/gomi --disable-multilib --enable-stage1-languages=c,c++ \
  --enable-stage1-checking=c,c++ target=x86_64-unknown-linux-gnu \
  --disable-shared --enable-static \
@@ -62,6 +66,6 @@ make install
 rm -rf /tmp/gcc-4.9.3
 
 cd /tmp
-tree /tmp/gcc
+tree $OPENSHIFT_DATA_DIR/gcc
 
 # http://ameblo.jp/sora8492/entry-11838796776.html
