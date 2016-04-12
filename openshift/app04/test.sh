1176eb54c4b8fb01ee60ddf0bb23c47c927fc1eb
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-04-13 08:43:52 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "1719"
+echo "0843"
 
 set -x
 
@@ -112,10 +112,9 @@ cd usr/lib64
 ln -s /usr/lib64/mysql/libmysqlclient.so.16.0.0 libmysqlclient.so.18
 ln -s libwebp.so.5.0.3 libwebp.so.4
 
-rm -rf $OPENSHIFT_DATA_DIR/test/share/doc/
-rm -rf $OPENSHIFT_DATA_DIR/test/share/man/
-rm -rf $OPENSHIFT_DATA_DIR/test/share/hhvm/LICENSE/
-
+rm -rf $OPENSHIFT_DATA_DIR/test/usr/share/doc/
+rm -rf $OPENSHIFT_DATA_DIR/test/usr/share/man/
+rm -rf $OPENSHIFT_DATA_DIR/test/usr/share/hhvm/LICENSE/
 cd $OPENSHIFT_DATA_DIR
 
 tree test
