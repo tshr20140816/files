e965048be409cdd92ad828d2883c1ce7b6363558
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-04-13 09:41:09 +0900

    Create test.sh

---
+++ openshift/app04/test.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "0941"
+
+set -x
+
+quota -s
+oo-cgroup-read memory.failcnt
+
+rm -f ${OPENSHIFT_LOG_DIR}/test.log &
+rm -f ${OPENSHIFT_LOG_DIR}/cron_minutely.log-* &
+
+ls -lang ${OPENSHIFT_LOG_DIR}/cron_minutely.log
+
+if [ $(wc -c < ${OPENSHIFT_LOG_DIR}/cron_minutely.log) -gt 100000 ]; then
+    tail -n 1000 ${OPENSHIFT_LOG_DIR}/cron_minutely.log > ${OPENSHIFT_LOG_DIR}/cron_minutely.log
+fi
+
+# tree -a ${OPENSHIFT_DATA_DIR}
+# exit
+
+/usr/bin/gear start --trace
+
+# shopt
+
+cd /tmp
+
+rm -f cc*
+rm -f jam*
+
+ls -lang
+
+cd $OPENSHIFT_DATA_DIR
+
+rm -rf usr
+rm -rf test
+rm -rf etc
+rm -f list.txt
+
+ls -lang
+
+quota -s
+
+echo "FINISH"
