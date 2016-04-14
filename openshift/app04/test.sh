a13f6b4dcd8fa1e6c3f340cefbcc785554dbf05e
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-04-14 17:26:38 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "1721"
+echo "1726"
 
 set -x
 
@@ -30,10 +30,7 @@ cd /tmp
 
 ls -lang
 
-rm -f $OPENSHIFT_DATA_DIR/gcc-4.9.3.tar.bz2
-rm -f $OPENSHIFT_DATA_DIR/file_list.txt
-rm -f $OPENSHIFT_DATA_DIR/1
-cat $OPENSHIFT_DATA_DIR/index.html
+rm -f  $OPENSHIFT_DATA_DIR/index.html
 
 ls -lang $OPENSHIFT_DATA_DIR
 
