88ff16c67d4e6faa8f7eed1c682b39de2c8262e6
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-04-15 14:40:45 +0900

    Update install_boost.sh

--- openshift/app04/install_boost.sh
+++ openshift/app04/install_boost.sh
@@ -57,8 +57,22 @@ exec /usr/bin/ssh -F ${OPENSHIFT_DATA_DIR}/.ssh/config "$@"
 __HEREDOC__
 chmod +x distcc-ssh
 
+export GEM_HOME=${OPENSHIFT_DATA_DIR}/.gem
+export PATH="${OPENSHIFT_DATA_DIR}/.gem/bin:$PATH"
+export HOME=${OPENSHIFT_DATA_DIR}
+# gem --version
+# gem environment
+# gem help install
 
+gem install rhc --no-rdoc --no-ri -- --with-cflags=\"-O2 -pipe -march=native -fomit-frame-pointer -s\"
+
+# yes | rhc setup --server openshift.redhat.com --create-token -l ${distcc_server_account} -p ${distcc_server_password}
+# rhc apps | grep -e SSH | grep -v -e ${OPENSHIFT_APP_UUID} | awk '{print $2}' | tee -a ${OPENSHIFT_DATA_DIR}/params/user_fqdn.txt
+
+# ssh -F ${OPENSHIFT_DATA_DIR}/.ssh/config ${user_fqdn} pwd
+# ssh -t -t -O check -F ${OPENSHIFT_DATA_DIR}/.ssh/config ${user_fqdn}
+# distcc_hosts_string="${user_fqdn}/4:/var/lib/openshift/${user_string}/app-root/data/distcc/bin/distccd_start "
 
 export PATH="${OPENSHIFT_DATA_DIR}/distcc/bin:$PATH"
 export DISTCC_DIR=${OPENSHIFT_DATA_DIR}.distcc
-export DISTCC_LOG=/dev/null
+export DISTCC_LOG=${OPENSHIFT_LOG_DIR}/distcc.log
