aad146737387eebb97de00563a3a239b392fbb51
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-04-21 15:09:30 +0900

    Update install_ghc.sh

--- openshift/app05/install_ghc.sh
+++ openshift/app05/install_ghc.sh
@@ -160,14 +160,14 @@ do
     wget -nc -q https://hackage.haskell.org/package/"${package}"/"${package}".tar.gz
     tar xfz "${package}".tar.gz
     pushd "${package}" > /dev/null
-    if [ "${package}" != "regex-tdfa-1.2.1" ]; then
-        cabal install -j2 --disable-documentation -O2 \
-         --enable-split-objs --disable-library-for-ghci --enable-executable-stripping --enable-library-stripping \
-         --disable-tests --disable-coverage --disable-benchmarks
-    elif [ "${package}" != "ShellCheck-0.4.3" ]; then
+    if [ "${package}" == "ShellCheck-0.4.3" ]; then
         cabal install -j1 -v3 --disable-optimization --disable-documentation \
          --disable-tests --disable-coverage --disable-benchmarks --disable-library-for-ghci \
          --ghc-options="+RTS -N1 -M448m -RTS"
+    elif [ "${package}" != "regex-tdfa-1.2.1" ]; then
+        cabal install -j2 --disable-documentation -O2 \
+         --enable-split-objs --disable-library-for-ghci --enable-executable-stripping --enable-library-stripping \
+         --disable-tests --disable-coverage --disable-benchmarks
     else
         PATH_ORG="${PATH}"
         export PATH="${OPENSHIFT_DATA_DIR}/local/bin:${PATH}"
