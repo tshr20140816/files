6323fec3f23cc32a65c2651b24487c9e6b54228a
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-04-22 11:15:49 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "1059"
+echo "1115"
 
 set -x
 
@@ -20,51 +20,27 @@ if [ $(wc -c < ${OPENSHIFT_LOG_DIR}/cron_minutely.log) -gt 100000 ]; then
     tail -n 1000 ${OPENSHIFT_LOG_DIR}/cron_minutely.log > ${OPENSHIFT_LOG_DIR}/cron_minutely.log
 fi
 
-# tree -a ${OPENSHIFT_DATA_DIR}
-# exit
-
 /usr/bin/gear start --trace
 
-ssh --help
+# -----
 
-# shopt
+rm -f jam*
+
+# -----
 
 cd /tmp
 
-rm -f zabbix-3.0.1.tar.gz
-rm -rf zabbix-3.0.1
+wget -nc -q http://heanet.dl.sourceforge.net/project/boost/boost/1.54.0/boost_1_54_0.tar.bz2
+rm -rf boost_1_54_0
+tar jxf boost_1_54_0.tar.bz2
 
-ls -lang
-
-exit
-
-cd $OPENSHIFT_DATA_DIR
-
-mkdir -p local/bin
-cat << '__HEREDOC__' > local/bin/wrap_gcc
-#!/bin/bash
-
-set -x
-time /usr/bin/gcc "$@"
-__HEREDOC__
-chmod +x ${OPENSHIFT_DATA_DIR}/local/bin/wrap_gcc
-export PATH="${OPENSHIFT_DATA_DIR}/local/bin:$PATH"
+# export PATH="${OPENSHIFT_DATA_DIR}/local/bin:$PATH"
 export CFLAGS="-O2 -march=native -fomit-frame-pointer -s -pipe"
 export CXXFLAGS="${CFLAGS}"
 export HOME=${OPENSHIFT_DATA_DIR}
 cat << '__HEREDOC__' > ${OPENSHIFT_DATA_DIR}/user-config.jam
-using gcc : : wrap_gcc : <cflags>"-O2 -march=native -fomit-frame-pointer -s -pipe" <cxxflags>"-O2 -march=native -fomit-frame-pointer -s -pipe" ;
+using gcc : : gcc : <cflags>"-O2 -march=native -fomit-frame-pointer -s -pipe" <cxxflags>"-O2 -march=native -fomit-frame-pointer -s -pipe" ;
 __HEREDOC__
-
-rm -rf $OPENSHIFT_DATA_DIR/boost
-[ ! -f boost_1_54_0.tar.bz2 ] && wget -q http://heanet.dl.sourceforge.net/project/boost/boost/1.54.0/boost_1_54_0.tar.bz2
-rm -rf boost_1_54_0
-tar jxf boost_1_54_0.tar.bz2
-# rm -f boost_1_54_0.tar.bz2
-
-wget https://github.com/tshr20140816/files/raw/master/openshift/app01/monitor_resourse.sh
-bash monitor_resourse.sh &
-pid=$!
 
 cd boost_1_54_0
 ./bootstrap.sh
@@ -92,27 +68,14 @@ time ./b2 install -j1 --prefix=$OPENSHIFT_DATA_DIR/boost \
  --without-timer \
  --without-wave
 
-kill ${pid}
+cd /tmp
+rm -rf boost_1_54_0
+rm -f boost_1_54_0.tar.bz2
 
-rm -rf ${OPENSHIFT_DATA_DIR}/boost_1_54_0
+tree -a $OPENSHIFT_DATA_DIR/usr/lib
+tree -a $OPENSHIFT_DATA_DIR/boost
+
 quota -s
 
 echo "FINISH"
 exit
-
-ls -lang $OPENSHIFT_DATA_DIR
-
-wget -q http://www.accursoft.com/cartridges/network.tar.gz
-wget -q http://www.accursoft.com/cartridges/yesod.tar.gz
-wget -q http://www.accursoft.com/cartridges/snap.tar.gz
-wget -q http://www.accursoft.com/cartridges/happstack.tar.gz
-wget -q http://www.accursoft.com/cartridges/mflow.tar.gz
-wget -q http://www.accursoft.com/cartridges/scotty.tar.gz
-
-ls -lang
-
-rm -f *.gz
-
-quota -s
-
-echo "FINISH"
