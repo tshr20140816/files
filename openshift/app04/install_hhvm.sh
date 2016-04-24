077aa6c23adcc3315fea42230bffb19b1df5ddbd
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-04-24 11:27:15 +0900

    Update install_hhvm.sh

--- openshift/app04/install_hhvm.sh
+++ openshift/app04/install_hhvm.sh
@@ -65,10 +65,41 @@ time ./b2 install -j1 --prefix=$OPENSHIFT_DATA_DIR/boost \
  --without-test \
  --without-timer \
  --without-wave
- popd > /dev/null
- rm -rf boost_1_54_0
- popd > /dev/null
- rm -rf $OPENSHIFT_DATA_DIR/boost
+popd > /dev/null
+rm -rf boost_1_54_0
+popd > /dev/null
+rm -rf $OPENSHIFT_DATA_DIR/boost
  
- tree -a $OPENSHIFT_DATA_DIR/usr/lib
+tree -a $OPENSHIFT_DATA_DIR/usr/lib
+ 
+pushd $OPENSHIFT_DATA_DIR > /dev/null
+wget -nc -q https://yum.gleez.com/6/x86_64/hhvm-3.5.0-4.el6.x86_64.rpm
+
+wget -nc -q http://mirror.centos.org/centos/6/os/x86_64/Packages/libvpx-1.3.0-5.el6_5.x86_64.rpm
+wget -nc -q http://mirror.centos.org/centos/6/os/x86_64/Packages/tbb-2.2-3.20090809.el6.x86_64.rpm
+wget -nc -q http://mirror.centos.org/centos/6/os/x86_64/Packages/oniguruma-5.9.1-3.1.el6.x86_64.rpm
+
+wget -nc -q http://dl.fedoraproject.org/pub/epel/6/x86_64/inotify-tools-3.14-1.el6.x86_64.rpm
+wget -nc -q http://dl.fedoraproject.org/pub/epel/6/x86_64/libdwarf-20140413-1.el6.x86_64.rpm
+wget -nc -q http://dl.fedoraproject.org/pub/epel/6/x86_64/libwebp-0.4.3-3.el6.x86_64.rpm
+wget -nc -q http://dl.fedoraproject.org/pub/epel/6/x86_64/lcms2-2.7-3.el6.x86_64.rpm
+
+find ./ -name "*.rpm" -print > list.txt
+
+while read -r LINE
+do
+    rpm2cpio ${LINE} | cpio -idmv
+    rm -f ${LINE}
+done < list.txt
+rm -f list.txt
+
+pushd usr/lib64 > /dev/null
+ln -s /usr/lib64/mysql/libmysqlclient.so.16.0.0 libmysqlclient.so.18
+ln -s libwebp.so.5.0.3 libwebp.so.4
+popd > /dev/null
+popd > /dev/null
+rm -rf $OPENSHIFT_DATA_DIR/usr/share/doc/
+rm -rf $OPENSHIFT_DATA_DIR/usr/share/man/
+rm -rf $OPENSHIFT_DATA_DIR/usr/share/hhvm/LICENSE/
+
  
