01b1a08ede4487c6b49c1355fe85e713660d9405
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-04-26 11:52:57 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "1142"
+echo "1152"
 
 set -x
 
@@ -34,6 +34,7 @@ quota -s
 cd /tmp
 rm -rf 20160425
 rm -rf gomi
+rm -rf ${OPENSHIFT_DATA_DIR}/usr
 
 # -----
 
@@ -46,9 +47,11 @@ cd 20160425
 wget -q ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.38.tar.bz2
 tar xf pcre-8.38.tar.bz2
 cd pcre-8.38
-ls -lang
+# ls -lang
 ./configure --help
-exit
+./configure --prefix=${OPENSHIFT_DATA_DIR}/usr --mandir=${OPENSHIFT_TMP_DIR}/gomi/man --docdir=${OPENSHIFT_TMP_DIR}/gomi/doc
+time make -j4
+make install
 
 wget -q http://ftp.yz.yamagata-u.ac.jp/pub/network/apache//httpd/httpd-2.4.20.tar.bz2
 tar xf httpd-2.4.20.tar.bz2
@@ -64,7 +67,7 @@ rm -f *.bz2
 cd httpd-2.4.20
 ./configure --help
 ./configure --prefix=${OPENSHIFT_DATA_DIR}/usr --mandir=${OPENSHIFT_TMP_DIR}/gomi/man --docdir=${OPENSHIFT_TMP_DIR}/gomi/doc \
- -enable-mods-shared='all proxy' --with-mpm=event
+ -enable-mods-shared='all proxy' --with-mpm=event --with-pcre-dir=${OPENSHIFT_DATA_DIR}/usr
 time make -j4
 
 cd ..
