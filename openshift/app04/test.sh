c1f0970871e7d9f1985220f38549e4ec4fa0f8f7
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-04-26 13:23:51 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "1305"
+echo "1323"
 
 set -x
 
@@ -42,40 +42,15 @@ export CFLAGS="-O2 -march=native -fomit-frame-pointer -s -pipe"
 export CXXFLAGS="${CFLAGS}"
 
 cd /tmp
-mkdir 20160425
-cd 20160425
-wget -q ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.38.tar.bz2
-tar xf pcre-8.38.tar.bz2
-cd pcre-8.38
-# ls -lang
+tmp_dir=$(mktemp -d tmp.XXXXX)
+cd ${tmp_dir}
+wget -q http://ftp.kddilabs.jp/infosystems/apache//httpd/httpd-2.2.31.tar.bz2
+tar xf httpd-2.2.31.tar.bz2
+cd httpd-2.2.31
 ./configure --help
-./configure --prefix=${OPENSHIFT_DATA_DIR}/usr --mandir=${OPENSHIFT_TMP_DIR}/gomi/man --docdir=${OPENSHIFT_TMP_DIR}/gomi/doc
-time make -j4
-make install
-cd ..
 
-wget -q http://ftp.yz.yamagata-u.ac.jp/pub/network/apache//httpd/httpd-2.4.20.tar.bz2
-tar xf httpd-2.4.20.tar.bz2
-wget -q http://ftp.tsukuba.wide.ad.jp/software/apache//apr/apr-1.5.2.tar.bz2
-tar xf apr-1.5.2.tar.bz2
-# cp -Rp ./apr-1.5.2 ./httpd-2.4.20/srclib/apr
-mv -f ./apr-1.5.2 ./httpd-2.4.20/srclib/apr
-wget -q http://ftp.tsukuba.wide.ad.jp/software/apache//apr/apr-util-1.5.4.tar.bz2
-tar xf apr-util-1.5.4.tar.bz2
-mv -f ./apr-util-1.5.4 ./httpd-2.4.20/srclib/apr-util
-rm -f *.bz2
-
-cd httpd-2.4.20
-./configure --help
-./configure --prefix=${OPENSHIFT_DATA_DIR}/usr --mandir=${OPENSHIFT_TMP_DIR}/gomi/man --docdir=${OPENSHIFT_TMP_DIR}/gomi/doc \
- -enable-mods-shared='all proxy' --with-mpm=event --with-pcre=${OPENSHIFT_DATA_DIR}/usr
-time make -j4
-
-cd ..
-wget -q http://us1.php.net/get/php-7.0.5.tar.xz/from/this/mirror -O php-7.0.5.tar.xz
-tar xf php-7.0.5.tar.xz
-cd php-7.0.5
-./configure --help
+cd /tmp
+rm -rf ${tmp_dir}
 
 quota -s
 echo "FINISH"
