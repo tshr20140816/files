622fde518cd8d5929c2cc702b19777425f16f445
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-04-26 11:10:57 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "1047"
+echo "1110"
 
 set -x
 
@@ -46,6 +46,15 @@ cd 20160425
 wget http://ftp.yz.yamagata-u.ac.jp/pub/network/apache//httpd/httpd-2.4.20.tar.bz2
 tar xf httpd-2.4.20.tar.bz2
 cd httpd-2.4.20
+./configure --help
+./configure --prefix=${OPENSHIFT_DATA_DIR}/usr --mandir=${OPENSHIFT_TMP_DIR}/gomi/man --docdir=${OPENSHIFT_TMP_DIR}/gomi/doc \
+ -enable-mods-shared='all proxy' --with-mpm=event
+time make -j4
+
+cd ..
+wget http://us1.php.net/get/php-7.0.5.tar.xz/from/this/mirror -O php-7.0.5.tar.xz
+tar xf php-7.0.5.tar.xz
+cd php-7.0.5
 ./configure --help
 
 quota -s
