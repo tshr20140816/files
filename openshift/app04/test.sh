ae744dd9e73013ab01daa0ae2771bf0f548a4f94
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-05-06 15:37:14 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "1531"
+echo "1537"
 
 set -x
 
@@ -62,7 +62,13 @@ mkdir 20160506
 cd 20160506
 wget -q https://files.phpmyadmin.net/phpMyAdmin/4.4.15.5/phpMyAdmin-4.4.15.5-english.tar.bz2
 tar xf phpMyAdmin-4.4.15.5-english.tar.bz2
-tree -a ./
+# tree -a ./
+
+cd phpMyAdmin-4.4.15.5-english
+ls -lang 
+
+cat config.sample.inc.php
+
 
 quota -s
 echo "FINISH"
