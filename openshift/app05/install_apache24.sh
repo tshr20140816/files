11d1dedf82b702edc3524a79668765bc709dfab2
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-05-06 16:05:08 +0900

    Update install_apache24.sh

--- openshift/app05/install_apache24.sh
+++ openshift/app05/install_apache24.sh
@@ -15,99 +15,106 @@ tmp_dir=/dummy
 
 # ccache
 
+ccache_version=3.2.4
 if [ ! -f ${OPENSHIFT_DATA_DIR}/usr/bin/ccache ]; then
     pushd ${OPENSHIFT_TMP_DIR}} > /dev/null
-    wget -q https://www.samba.org/ftp/ccache/ccache-3.2.4.tar.xz
-    tar Jxf ccache-3.2.4.tar.xz
-    rm -f ccache-3.2.4.tar.xz
-    pushd ccache-3.2.4 > /dev/null
+    wget -q https://www.samba.org/ftp/ccache/ccache-${ccache_version}.tar.xz
+    tar Jxf ccache-${ccache_version}.tar.xz
+    rm -f ccache-${ccache_version}.tar.xz
+    pushd ccache-${ccache_version} > /dev/null
     ./configure --help
     ./configure --prefix=${OPENSHIFT_DATA_DIR}/usr --mandir=${OPENSHIFT_TMP_DIR}/gomi --infodir=${OPENSHIFT_TMP_DIR}/gomi
     time make -j4
     make install
     popd > /dev/null
-    rm -rf ccache-3.2.4 gomi
+    rm -rf ccache-${ccache_version} gomi
     popd > /dev/null
     mkdir ${OPENSHIFT_DATA_DIR}/ccache
 fi
 
 # distcc
 
+distcc_version=3.1
 if [ ! -f ${OPENSHIFT_DATA_DIR}/usr/bin/distcc ]; then
     pushd ${OPENSHIFT_TMP_DIR} > /dev/null
-    wget -q https://distcc.googlecode.com/files/distcc-3.1.tar.bz2
-    tar jxf distcc-3.1.tar.bz2
-    rm -f distcc-3.1.tar.bz2
-    pushd distcc-3.1 > /dev/null
+    wget -q https://distcc.googlecode.com/files/distcc-${distcc_version}.tar.bz2
+    tar jxf distcc--${distcc_version}.tar.bz2
+    rm -f distcc--${distcc_version}.tar.bz2
+    pushd distcc--${distcc_version} > /dev/null
     ./configure --help
     ./configure --prefix=${OPENSHIFT_DATA_DIR}/usr --mandir=${OPENSHIFT_TMP_DIR}/gomi --with-docdir==${OPENSHIFT_TMP_DIR}/gomi
     time make -j4
     make install
     popd > /dev/null
-    rm -rf distcc-3.1 gomi
+    rm -rf distcc--${distcc_version} gomi
     popd > /dev/null
 fi
 
 # pcre
 
+pcre_version=8.38
 if [ ! -f ${OPENSHIFT_DATA_DIR}/usr/lib/libpcre.so ]; then
     pushd ${OPENSHIFT_TMP_DIR} > /dev/null
-    wget -q ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.38.tar.bz2
-    tar xf pcre-8.38.tar.bz2
-    rm -f pcre-8.38.tar.bz2
-    pushd pcre-8.38 > /dev/null
+    wget -q ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-${pcre_version}.tar.bz2
+    tar xf pcre-${pcre_version}.tar.bz2
+    rm -f pcre-${pcre_version}.tar.bz2
+    pushd pcre-${pcre_version} > /dev/null
     ./configure --help
     ./configure --prefix=${OPENSHIFT_DATA_DIR}/usr --mandir=${OPENSHIFT_TMP_DIR}/gomi --docdir=${OPENSHIFT_TMP_DIR}/gomi --enable-static=no
     time make -j4
     make install
     popd > /dev/null
-    rm -rf pcre-8.38
+    rm -rf pcre-${pcre_version}
     popd > /dev/null
 fi
 
 # apache
 
+apache_version=2.4.20
+apr_version=1.5.2
+aprutil_version=1.5.4
 if [ ! -f ${OPENSHIFT_DATA_DIR}/usr/bin/httpd ]; then
-cd ${OPENSHIFT_TMP_DIR}
-wget -q http://ftp.yz.yamagata-u.ac.jp/pub/network/apache//httpd/httpd-2.4.20.tar.bz2
-tar xf httpd-2.4.20.tar.bz2
-wget -q http://ftp.tsukuba.wide.ad.jp/software/apache//apr/apr-1.5.2.tar.bz2
-tar xf apr-1.5.2.tar.bz2
-mv -f ./apr-1.5.2 ./httpd-2.4.20/srclib/apr
-wget -q http://ftp.tsukuba.wide.ad.jp/software/apache//apr/apr-util-1.5.4.tar.bz2
-tar xf apr-util-1.5.4.tar.bz2
-mv -f ./apr-util-1.5.4 ./httpd-2.4.20/srclib/apr-util
-rm -f *.bz2
+    pushd ${OPENSHIFT_TMP_DIR} > /dev/null
+    wget -q http://ftp.yz.yamagata-u.ac.jp/pub/network/apache//httpd/httpd-${apache_version}.tar.bz2
+    tar xf httpd-${apache_version}.tar.bz2
+    wget -q http://ftp.tsukuba.wide.ad.jp/software/apache//apr/apr-${apr_version}.tar.bz2
+    tar xf apr-${apr_version}.tar.bz2
+    mv -f ./apr-${apr_version} ./httpd-${apache_version}/srclib/apr
+    wget -q http://ftp.tsukuba.wide.ad.jp/software/apache//apr/apr-util-${aprutil_version}.tar.bz2
+    tar xf apr-util-${aprutil_version}.tar.bz2
+    mv -f ./apr-util-${aprutil_version} ./httpd-${apache_version}/srclib/apr-util
+    rm -f *.bz2
 
-cd httpd-2.4.20
-./configure --help
-./configure --prefix=${OPENSHIFT_DATA_DIR}/usr --mandir=${OPENSHIFT_TMP_DIR}/gomi --docdir=${OPENSHIFT_TMP_DIR}/gomi \
- --enable-mods-shared='all proxy' --with-mpm=event --with-pcre=${OPENSHIFT_DATA_DIR}/usr \
- --disable-authn-anon \
- --disable-authn-dbd \
- --disable-authn-dbm \
- --disable-authz-dbm \
- --disable-authz-groupfile \
- --disable-authz-owner \
- --disable-dbd \
- --disable-info \
- --disable-log-forensic \
- --disable-proxy-ajp \
- --disable-proxy-balancer \
- --disable-proxy-ftp \
- --disable-proxy-scgi \
- --disable-speling \
- --disable-status \
- --disable-userdir \
- --disable-version \
- --disable-vhost-alias \
- --disable-dialup
+    pushd httpd-${apache_version} > /dev/null
+    ./configure --help
+    ./configure --prefix=${OPENSHIFT_DATA_DIR}/usr --mandir=${OPENSHIFT_TMP_DIR}/gomi --docdir=${OPENSHIFT_TMP_DIR}/gomi \
+     --enable-mods-shared='all proxy' --with-mpm=event --with-pcre=${OPENSHIFT_DATA_DIR}/usr \
+     --disable-authn-anon \
+     --disable-authn-dbd \
+     --disable-authn-dbm \
+     --disable-authz-dbm \
+     --disable-authz-groupfile \
+     --disable-authz-owner \
+     --disable-dbd \
+     --disable-info \
+     --disable-log-forensic \
+     --disable-proxy-ajp \
+     --disable-proxy-balancer \
+     --disable-proxy-ftp \
+     --disable-proxy-scgi \
+     --disable-speling \
+     --disable-status \
+     --disable-userdir \
+     --disable-version \
+     --disable-vhost-alias \
+     --disable-dialup
 
-time make -j4
-make install
-cd /tmp
-rm -rf httpd-2.4.20
-rm -rf ${OPENSHIFT_DATA_DIR}/usr/manual
+    time make -j4
+    make install
+    popd > /dev/null
+    rm -rf httpd-${apache_version}
+    popd > /dev/null
+    rm -rf ${OPENSHIFT_DATA_DIR}/usr/manual
 fi
 
 cat << '__HEREDOC__' > /dev/null
