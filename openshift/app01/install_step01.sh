353925faddbef9d94593256f21c08ff87fdee4dd
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-05-25 12:53:00 +0900

    Update install_step01.sh

--- openshift/app01/install_step01.sh
+++ openshift/app01/install_step01.sh
@@ -3,6 +3,7 @@
 set -x
 
 # History
+# 2016.05.25 fio_version 2.10 → 2.11
 # 2016.05.22 fio_version 2.9 → 2.10
 # 2016.05.13 wordpress_version 4.5-ja → 4.5.2-ja
 # 2016.05.10 fio_version 2.8 → 2.9
@@ -113,7 +114,7 @@ curl_version 7.47.1
 distcc_version 3.1
 delegate_version 9.9.13
 expect_version 5.45
-fio_version 2.10
+fio_version 2.11
 ipafont_version 00303
 jpegoptim_version 1.4.3
 libmemcached_version 1.0.18
