612663eeb10553670c17097de8645cb157553510
tshr20140816 <tshr20140816@yahoo.co.jp>
2016-12-08 16:35:23 +0900

    Update 20161208.sh

--- openshift/app07/20161208.sh
+++ openshift/app07/20161208.sh
@@ -17,6 +17,7 @@ pushd ${OPENSHIFT_TMP_DIR} > /dev/null
 wget -q https://www.samba.org/ftp/ccache/ccache-${ccache_version}.tar.xz
 tar xf ccache-${ccache_version}.tar.xz
 pushd ccache-${ccache_version} > /dev/null
+./configure --help > ${OPENSHIFT_LOG_DIR}/configure_ccache
 ./configure --prefix=${OPENSHIFT_DATA_DIR}/usr
 time make -j4
 make install
@@ -38,18 +39,49 @@ mkdir -p ${CCACHE_TEMPDIR}
 
 # ***** apache *****
 
+# *** pcre ***
+
+pcre_version=8.39
+
+pushd ${OPENSHIFT_TMP_DIR} > /dev/null
+wget -q ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-${pcre_version}.tar.bz2
+tar xf pcre-${pcre_version}.tar.bz2
+pushd pcre-${pcre_version} > /dev/null
+./configure --prefix=${OPENSHIFT_DATA_DIR}/usr --enable-static=no
+time make -j4
+make install
+popd > /dev/null
+rm -rf pcre-${pcre_version}
+rm -f pcre-${pcre_version}.tar.bz2
+popd > /dev/null
+
+# *** httpd ***
+
 apache_version=2.4.23
+apr_version=1.5.2
+aprutil_version=1.5.4
 
 pushd ${OPENSHIFT_TMP_DIR} > /dev/null
 wget -q http://ftp.yz.yamagata-u.ac.jp/pub/network/apache/httpd/httpd-${apache_version}.tar.bz2
 tar xf httpd-${apache_version}.tar.bz2
+
+wget -q http://ftp.tsukuba.wide.ad.jp/software/apache//apr/apr-${apr_version}.tar.bz2
+tar xf apr-${apr_version}.tar.bz2
+mv -f ./apr-${apr_version} ./httpd-${apache_version}/srclib/apr
+
+wget -q http://ftp.tsukuba.wide.ad.jp/software/apache//apr/apr-util-${aprutil_version}.tar.bz2
+tar xf apr-util-${aprutil_version}.tar.bz2
+mv -f ./apr-util-${aprutil_version} ./httpd-${apache_version}/srclib/apr-util
+
 pushd httpd-${apache_version} > /dev/null
-./configure --prefix=${OPENSHIFT_DATA_DIR}/usr
+./configure --help > ${OPENSHIFT_LOG_DIR}/configure_apache
+./configure --prefix=${OPENSHIFT_DATA_DIR}/usr \
+ --with-pcre=${OPENSHIFT_DATA_DIR}/usr
 time make -j4
 make install
 popd > /dev/null
 rm -rf httpd-${apache_version}
-rm -f httpd-${apache_version}.tar.bz2
+rm -f *.tar.bz2
 popd > /dev/null
 
 # ***** php *****
@@ -60,7 +92,12 @@ pushd ${OPENSHIFT_TMP_DIR} > /dev/null
 wget -q http://jp2.php.net/get/php-${php_version}.tar.xz/from/this/mirror -O php-${php_version}.tar.xz
 tar xf php-${php_version}.tar.xz
 pushd php-${php_version} > /dev/null
+./configure --help > ${OPENSHIFT_LOG_DIR}/configure_php
 ./configure --prefix=${OPENSHIFT_DATA_DIR}/usr
+
+# ./configure --prefix=${OPENSHIFT_DATA_DIR}/usr \
+#  --enable-fpm
+
 time make -j1
 make install
 popd > /dev/null
