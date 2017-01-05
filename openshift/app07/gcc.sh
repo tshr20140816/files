804d96ffabd1e8e330c65b0708565927c4b7e936
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-01-05 13:43:03 +0900

    Update gcc.sh

--- openshift/app07/gcc.sh
+++ openshift/app07/gcc.sh
@@ -7,10 +7,10 @@ do
 	dt=$(date +%Y/%m/%d" "%H:%M:%S)
 	usage_in_bytes=$(oo-cgroup-read memory.usage_in_bytes)
 	usage_in_bytes_format=$(echo "${usage_in_bytes}" | awk '{printf "%\047d\n", $0}')
+	echo "$dt $usage_in_bytes_format"
 	if [ "${usage_in_bytes}" -lt 300000000 ]; then
 		break
 	fi
-	echo "$dt $usage_in_bytes_format"
 	sleep 1s
 done
 
