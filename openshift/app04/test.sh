d87fa130b7c5f0dc8e3bc629e178d1581d0bd595
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-03-21 15:10:27 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "1458"
+echo "1510"
 
 set -x
 
@@ -30,18 +30,17 @@ quota -s
 
 cd /tmp
 rm -f ./ *
-rm -f binutils-2.20.51.0.2-5.43.el6.src.rpm
-wget http://vault.centos.org/6.7/os/Source/SPackages/binutils-2.20.51.0.2-5.43.el6.src.rpm
-rpm2cpio binutils-2.20.51.0.2-5.43.el6.src.rpm | cpio -idmv
-tar xf binutils-2.20.51.0.2.tar.bz2
+rm -rf binutils-2.20.51.0.2
+wget http://mirror.centos.org/centos/6/os/x86_64/Packages/binutils-devel-2.20.51.0.2-5.44.el6.x86_64.rpm
+rpm2cpio binutils-devel-2.20.51.0.2-5.44.el6.x86_64.rpm | cpio -idmv
 ls -lang
 
-cd /tmp
+# cd /tmp
 # rm -f master.zip
 # rm -rf distcc-master
 # wget https://github.com/distcc/distcc/archive/master.zip
-rm -f distcc-3.1.zip
-rm -rf distcc-distcc-3.1
+# rm -f distcc-3.1.zip
+# rm -rf distcc-distcc-3.1
 # wget https://github.com/distcc/distcc/archive/distcc-3.1.zip
 # unzip distcc-3.1.zip
 # ls -lang
