0b67fb374bdc0e0a5f5cddea4369c27dfe8bcc62
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-04-12 08:43:32 +0900

    Update test.sh

--- openshift/app04/test.sh
+++ openshift/app04/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "1158"
+echo "0841"
 
 set -x
 
@@ -28,41 +28,7 @@ ls -lang ${OPENSHIFT_REPO_DIR}
 
 quota -s
 
-# cd /tmp
-# rm -f ./ *
-# rm -rf binutils-2.20.51.0.2
-# wget http://mirror.centos.org/centos/6/os/x86_64/Packages/binutils-devel-2.20.51.0.2-5.44.el6.x86_64.rpm
-# rpm2cpio binutils-devel-2.20.51.0.2-5.44.el6.x86_64.rpm | cpio -idmv
-# ls -lang
-
-cd /tmp
-rm -f master.zip
-rm -rf distcc-master
-# wget https://github.com/distcc/distcc/archive/master.zip
-unzip master.zip
-cd distcc-master
-./autogen.sh
-./configure --help
-./configure --without-libiberty --infodir=/tmp/ --mandir=/tmp/ --with-docdir=/tmp/
-
-cat survey.txt
-
-time make -j4
-
-# rm -f distcc-3.1.zip
-# rm -rf distcc-distcc-3.1
-# wget https://github.com/distcc/distcc/archive/distcc-3.1.zip
-# unzip distcc-3.1.zip
-# ls -lang
-# cd distcc-distcc-3.1
-# ./autogen.sh
-# ./configure --prefix=${OPENSHIFT_DATA_DIR}/distcc
-# time make -j4
-
-cd /tmp
-rm -rf distcc-distcc-3.1
-rm -rf usr
-ls -lang
+curl http://ranking.rakuten.co.jp/daily/?nrw=1&stx=itunes&prl=&pru=&rvf=&arf=
 
 quota -s
 echo "FINISH"
