2e6d4992bb6e3e944718b0adcea677bcd2f334d5
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-05-15 14:47:01 +0900

    Update test.php

--- openshift/app04/test.php
+++ openshift/app04/test.php
@@ -1,7 +1,6 @@
 <?php
-$d1 = $_POST["d1"];
-$d2 = $_POST["d2"];
 
-file_put_contents(getenv("OPENSHIFT_TMP_DIR") . "d1.txt", $d1);
-file_put_contents(getenv("OPENSHIFT_TMP_DIR") . "d2.txt", $d2);
+$s = file_get_contents("http://www.shop.carp.co.jp/headline2017.html");
+
+
 ?>
