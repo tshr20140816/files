dfa873b9995307b3367d4dbc4795e2c7772bf22f
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-09-28 23:00:56 +0900

    Update install_step_02.sh

--- install/20170928/install_step_02.sh
+++ install/20170928/install_step_02.sh
@@ -8,7 +8,7 @@ sudo userdel -r pi
 
 # uninstall
 
-sudo apt-get -y --purge remove aptitude aptitude-common isc-dhcp-client isc-dhcp-common wireless-regdb wireless-tools wpasupplicant
+sudo apt-get -y --purge remove manpages aptitude aptitude-common isc-dhcp-client isc-dhcp-common wireless-regdb wireless-tools wpasupplicant
 sudo apt-get -y autoremove
 sudo apt-get -y autoclean
 
