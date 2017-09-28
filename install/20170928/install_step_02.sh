3b00add697fec3ba2f8d14dbc28dc7a9f873d1ca
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-09-28 14:39:12 +0900

    Update install_step_02.sh

--- install/20170928/install_step_02.sh
+++ install/20170928/install_step_02.sh
@@ -12,3 +12,34 @@ sudo apt-get -y --purge remove aptitude aptitude-common isc-dhcp-client isc-dhcp
 sudo apt-get -y autoremove
 sudo apt-get -y autoclean
 
+# 
+
+time sudo apt-get -y install postgresql apache2 php php-pgsql php-mbstring php-xml git
+
+cd /etc/apache2
+sudo mv apache2.conf apache2.conf.org
+sudo wget https://github.com/tshr20140816/files/raw/master/apache24/sample1.conf
+sudo mv sample1.conf apache2.conf
+
+mkdir -p /var/www/80
+mkdir -p /var/www/443
+mkdir -p /var/www/10080
+
+cd ~
+
+sudo apt-get -y install nodejs npm git
+sudo npm update -g npm
+sudo npm cache clean
+sudo npm install -g n
+sudo n stable
+node --version
+npm --version
+time sudo npm install -g heroku-cli
+
+wget http://delegate.hpcc.jp/anonftp/DeleGate/delegate9.9.13.tar.gz
+tar xfz delegate9.9.13.tar.gz
+cd delegate9.9.13
+export CFLAGS='-mcpu=arm1176jzf-s -mfpu=vfp -Wno-error=narrowing'
+export CXXFLAGS="${CFLAGS}"
+time make -j1 ADMIN="admin@localhost"
+
