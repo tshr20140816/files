036a19f09ea2cf5c8bbb5c3f9f230ebf2567aeac
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-19 08:55:06 +0900

    Create ie_server.sh

---
+++ memo/ie_server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x
+
+global_ip_file='/tmp/global_ip'
+global_ip_old='0.0.0.0'
+
+if [ -e ${global_ip_file} ]; then
+  global_ip_old=(cat ${global_ip_file})
+fi
+global_ip_now=$(wget https://ieserver.net/ipcheck.shtml)
+
+echo ${global_ip_old}
+echo "\n"
+echo ${global_ip_now}
