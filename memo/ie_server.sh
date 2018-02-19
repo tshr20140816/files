e6c3c21291d6c53036a456c2bad8d49d8ad690eb
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-19 09:35:33 +0900

    Update ie_server.sh

--- memo/ie_server.sh
+++ memo/ie_server.sh
@@ -8,8 +8,14 @@ global_ip_old='0.0.0.0'
 if [ -e ${global_ip_file} ]; then
   global_ip_old=(cat ${global_ip_file})
 fi
-global_ip_now=$(wget -qO https://ieserver.net/ipcheck.shtml)
+global_ip_now=$(wget -q -O https://ieserver.net/ipcheck.shtml)
 
 echo ${global_ip_old}
 echo "\n"
 echo ${global_ip_now}
+
+if [[ ${global_ip_now} =~ ^([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+)$ ]]; then
+  if [ ${global_ip_old} -ne ${global_ip_now} ]; then
+    echo 'CHANGE'
+  fi
+fi
