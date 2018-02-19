0f08d5fdd91179a98b5d865b34e004f3d14bf5f4
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-19 23:20:38 +0900

    Update ie_server.sh

--- memo/ie_server.sh
+++ memo/ie_server.sh
@@ -21,7 +21,7 @@ global_ip_now=$(wget -qO - https://ieserver.net/ipcheck.shtml)
 if [[ ${global_ip_now} =~ ^([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+)$ ]]; then
   if [ ${global_ip_old} != ${global_ip_now} ]; then
     echo ${global_ip_now} > ${global_ip_file}
-    # wget -q --post-data="updatehost=1&password=${password}&domain=${domain}&username=${username}' \
+    # wget -q --post-data="updatehost=1&password=${password}&domain=${domain}&username=${username}" \
     # -O - https://ieserver.net/cgi-bin/dip.cgi
   fi
 fi
