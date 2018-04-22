3959351098e410294820f9de653d94ff67b9e201
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-04-23 00:57:57 +0900

    Update 20180422.sh

--- memo/20180422.sh
+++ memo/20180422.sh
@@ -9,3 +9,7 @@ sudo systemctl disable plymouth
 sudo echo -e "\n" >> /etc/sysctl.conf
 sudo echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
 sudo sysctl -p
+
+sudo echo -e "\n" >> /etc/systemd/timesyncd.conf
+sudo echo "NTP=ntp.jst.mfeed.ad.jp" >> /etc/systemd/timesyncd.conf
+sudo systemctl restart systemd-timesyncd
