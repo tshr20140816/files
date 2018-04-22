f8df5fe727cd1df78eeb2cd2c9ee41c067a0a347
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-04-23 01:16:28 +0900

    Update 20180422.sh

--- memo/20180422.sh
+++ memo/20180422.sh
@@ -10,8 +10,16 @@ sudo systemctl disable plymouth
 
 sudo echo -e "\n" >> /etc/sysctl.conf
 sudo echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+sudo echo -e "\n" >> /etc/sysctl.conf
 sudo sysctl -p
 
 sudo echo -e "\n" >> /etc/systemd/timesyncd.conf
 sudo echo "NTP=ntp.jst.mfeed.ad.jp" >> /etc/systemd/timesyncd.conf
+sudo echo -e "\n" >> /etc/systemd/timesyncd.conf
 sudo systemctl restart systemd-timesyncd
+
+sudo echo -e "\n" >> /etc/fstab
+sudo echo "tmpfs /tmp tmpfs defaults 0 0"
+sudo echo -e "\n" >> /etc/fstab
+sudo echo "tmpfs /var/cache/apt tmpfs defaults 0 0"
+sudo echo -e "\n" >> /etc/fstab
