f6d6682d88fd79a4ad895d67d8b85bd07eec62d2
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-04-23 15:33:12 +0900

    Update 04.sh

--- homeserver/04.sh
+++ homeserver/04.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 
+sudo apt-get -y install apache2
+sudo apt-get -y install postgresql-9.6 php7.0 rsyslog-gnutls php7.0-pgsql php7.0-mbstring php7.0-xml git
+
+curl -O https://www.loggly.com/install/configure-linux.sh
