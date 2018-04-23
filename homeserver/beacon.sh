fde0dbb9d42832ea5bc2f56964a4ed9ea6e97ab1
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-04-23 09:33:51 +0900

    Update beacon.sh

--- homeserver/beacon.sh
+++ homeserver/beacon.sh
@@ -3,8 +3,9 @@
 loggly_token=dummy
 mkdir /tmp/beacon
 
+log_file=/tmp/beacon/$(date +"%H%M")
 temperature=$(cat /sys/class/thermal/thermal_zone0/temp)
 host_name=$(hostname)
 
 curl -ivh -H 'content-type:text/plain' -d "${host_name} ${temperature}" \
- https://logs-01.loggly.com/inputs/${loggly_token}/tag/beacon/ > /tmp/beacon/$(date +"%H%M") 2>&1
+ https://logs-01.loggly.com/inputs/${loggly_token}/tag/beacon/ > ${log_file} 2>&1
