53f59858af50531ae07387c3839dcac9a41ebb60
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-05-25 02:19:29 +0900

    Create logrotate_etc.sh

---
+++ homeserver/logrotate_etc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+day=$(date '+%-e')
+week_number=$((${day} / 7))
+cd /tmp
+
+files[0]="messages"
+files[1]="auth.log"
+files[2]="mail.info"
+files[3]="mail.warn"
+files[4]="mail.log"
+files[5]="daemon.log"
+files[6]="user.log"
+
+for file in "${files[@]}"; do
+  file_name=${file}.${week_number}
+  mv ${file}.1 ${file_name}
+  xz -z ${file_name}
+  openssl enc -e -aes-256-cbc -in ${file_name}.xz -out ${file_name}.xz.enc -k ${file_name}X*
+  mv -f ${file_name}.xz.enc /var/box/$(hostname)/
+  rm -f ${file_name}.xz
+done
