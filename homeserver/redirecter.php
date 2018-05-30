34e500935f6c145943b5c90231add406e04ebc63
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-05-30 14:44:45 +0900

    Update redirecter.php

--- homeserver/redirecter.php
+++ homeserver/redirecter.php
@@ -1,8 +1,19 @@
 <?php
 
+$stdin = fopen('php://stdin', 'r');
+ob_implicit_flush(true);
+
 while ($line = fgets($stdin)) {
 
   error_log($line);
+  
+  $response = '';
+  if (file_exists('/tmp/REDIRECT_ADDRESS')) {
+    $response = file_get_contents('/tmp/REDIRECT_ADDRESS');
+  } else {
+    $response = file_get_contents('https://xxx.herokuapp.com/fqdn.php');
+    file_put_contents('/tmp/REDIRECT_ADDRESS', $response);
+  }
   
   echo '302:' . $line . "\n";
   //echo $line;
