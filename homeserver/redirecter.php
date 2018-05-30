b3a5e00f31fc48863c070c8a71dc84e0e5207642
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-05-30 14:51:30 +0900

    Update redirecter.php

--- homeserver/redirecter.php
+++ homeserver/redirecter.php
@@ -3,17 +3,23 @@
 $stdin = fopen('php://stdin', 'r');
 ob_implicit_flush(true);
 
+$redirect_address_file = '/tmp/REDIRECT_ADDRESS';
+
 while ($line = fgets($stdin)) {
 
   error_log($line);
   
-  $response = '';
-  if (file_exists('/tmp/REDIRECT_ADDRESS')) {
-    $response = file_get_contents('/tmp/REDIRECT_ADDRESS');
-  } else {
-    $response = file_get_contents('https://xxx.herokuapp.com/fqdn.php');
-    file_put_contents('/tmp/REDIRECT_ADDRESS', $response);
+  $redirect_address = null;
+  if (file_exists($redirect_address_file)) {
+    if (time() - filemtime($redirect_address_file) < 60 * 60 * 6) {
+      $redirect_address = file_get_contents($redirect_address_file);
+    }
   }
+  if (is_null($response)) {
+    $redirect_address = file_get_contents('https://xxx.herokuapp.com/fqdn.php');
+    file_put_contents($redirect_address_file, $redirect_address);
+  }
+  error_log($redirect_address);
   
   echo '302:' . $line . "\n";
   //echo $line;
